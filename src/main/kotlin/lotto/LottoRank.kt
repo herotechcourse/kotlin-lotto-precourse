a63ebb2d87cdc29be8af55b3841418e180934a04
2taezeat <xorudfl772@naver.com>
2025-04-23 13:39:26 +0900

    chore: change LottoRank order

--- src/main/kotlin/lotto/LottoRank.kt
+++ src/main/kotlin/lotto/LottoRank.kt
@@ -7,12 +7,12 @@ enum class LottoRank(
     val prize: Int,
     val description: String
 ) {
-    FIRST(6, false, PRIZE_FIRST, LottoRankFormatter.formatDescription(6, false, PRIZE_FIRST)),
-    SECOND(5, true, PRIZE_SECOND, LottoRankFormatter.formatDescription(5, true, PRIZE_SECOND)),
-    THIRD(5, false, PRIZE_THIRD, LottoRankFormatter.formatDescription(5, false, PRIZE_THIRD)),
-    FOURTH(4, false, PRIZE_FOURTH, LottoRankFormatter.formatDescription(4, false, PRIZE_FOURTH)),
+    MISS(0, false, 0, "No prize"),
     FIFTH(3, false, PRIZE_FIFTH, LottoRankFormatter.formatDescription(3, false, PRIZE_FIFTH)),
-    MISS(0, false, 0, "No prize");
+    FOURTH(4, false, PRIZE_FOURTH, LottoRankFormatter.formatDescription(4, false, PRIZE_FOURTH)),
+    THIRD(5, false, PRIZE_THIRD, LottoRankFormatter.formatDescription(5, false, PRIZE_THIRD)),
+    SECOND(5, true, PRIZE_SECOND, LottoRankFormatter.formatDescription(5, true, PRIZE_SECOND)),
+    FIRST(6, false, PRIZE_FIRST, LottoRankFormatter.formatDescription(6, false, PRIZE_FIRST));
 
     companion object {
         fun from(matchCount: Int, isBonus: Boolean): LottoRank {
