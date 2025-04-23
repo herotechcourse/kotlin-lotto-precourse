8f36c43d59b895722a28dca0bdb18d6d10d779c4
yufenlai0825 <yufenlai0825@gmail.com>
2025-04-23 15:55:17 +0200

    Ticket amount input and validation

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,17 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: Implement the program
+
+    // purchase ticket
+    println("Please enter the purchase amount.")
+    val input = Console.readLine().toIntOrNull()
+    if (input == null || (input % 1000) !== 0 ){
+        throw IllegalArgumentException("Each ticket costs 1,000 KRW. Please enter a valid amount.")
+    }
+
+    val ticketNumbers = input / 1000
+    println("You have purchased $ticketNumbers tickets.")
+
 }
