6e9b985ec9c3c7a570e4444a86ef8b7e86b038aa
jongbinchoi <jongbin985@naver.com>
2025-04-24 02:19:35 +0900

    refactor: validate lotto numbers for duplication and extract constants

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -4,20 +4,47 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
+    // 로또 번호가 6개 초과일 경우 예외 발생
     @Test
     fun `throws an exception when lotto numbers exceed six`() {
-        assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
+        // given :  input value that causes an exception
+        val numbers = listOf(1, 2, 3, 4, 5, 6, 7)
+
+        //when & then
+        val exception = assertThrows<IllegalArgumentException> {
+            Lotto(numbers)
         }
+
+        // then : Check whether the thrown exception contains the expected error message
+        assert(exception.message?.contains("exactly 6 numbers") == true)
     }
 
-    // TODO: Implement production code to pass the test
+    // 중복된 숫자가 포함될 경우 예외 발생
     @Test
     fun `throws an exception when lotto numbers contain duplicates`() {
-        assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 5))
+        // given
+        val numbers = listOf(1, 2, 3, 4, 5, 5)
+
+        // when
+        val exception = assertThrows<IllegalArgumentException> {
+            Lotto(numbers)
         }
+
+        //then
+        assert(exception.message?.contains("must contain 6 unique numbers") == true)
     }
 
-    // TODO: Implement tests based on the added features
+    @Test
+    fun `throw exception when number is out of range`() {
+        // given
+        val numbers = listOf(0, 2, 3, 4, 5, 6)
+
+        // when
+        val exception = assertThrows<IllegalArgumentException> {
+            Lotto(numbers)
+        }
+
+        //
+        assert(exception.message?.contains("must be between 1 and 45") == true)
+    }
 }
