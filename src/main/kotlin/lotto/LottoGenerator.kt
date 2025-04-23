ce27d2586b59e01ad9a80c6e21e341833e97a62e
2taezeat <xorudfl772@naver.com>
2025-04-23 13:12:51 +0900

    feat: generate random lotto number

--- src/main/kotlin/lotto/LottoGenerator.kt
+++ src/main/kotlin/lotto/LottoGenerator.kt
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Randoms
 
 
 class LottoGenerator {
-
     fun generateLotto(): Lotto {
         val randomNumbers = Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_NUMBER_COUNT)
         return Lotto(randomNumbers)
