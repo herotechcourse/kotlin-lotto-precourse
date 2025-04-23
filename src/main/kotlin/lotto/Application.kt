e878063e75fbd44a2ca7743931dfb60b5725ec23
yufenlai0825 <yufenlai0825@gmail.com>
2025-04-23 16:43:56 +0200

    add prize ranks

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,15 +3,21 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
+// prize ranks
+enum class prizeTypes {
+    fifth, fourth, third, second, first
+}
+
 fun main() {
 
-    // purchase ticket
+    // ticket amounts
     println("Please enter the purchase amount.")
     val input = Console.readLine().toIntOrNull()
     if (input == null || (input % 1000) != 0 ){
         throw IllegalArgumentException("Each ticket costs 1,000 KRW. Please enter a valid amount.")
     }
 
+    // purchased tickets
     val tickets = input / 1000
     println("You have purchased $tickets tickets.")
     repeat(tickets){
