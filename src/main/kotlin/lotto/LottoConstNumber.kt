bf9d2f2c1f3c51e67c10477264fd6a27f88bb3ff
2taezeat <xorudfl772@naver.com>
2025-04-23 13:03:20 +0900

    feat: format LottoRank, desciption, KRW currency

--- src/main/kotlin/lotto/LottoConstNumber.kt
+++ src/main/kotlin/lotto/LottoConstNumber.kt
@@ -4,3 +4,10 @@ const val LOTTO_NUMBER_MIN = 1
 const val LOTTO_NUMBER_MAX = 45
 const val LOTTO_NUMBER_COUNT = 6
 const val LOTTO_UNIT_PRICE = 1000
+
+
+const val PRIZE_FIRST = 2_000_000_000
+const val PRIZE_SECOND = 30_000_000
+const val PRIZE_THIRD = 1_500_000
+const val PRIZE_FOURTH = 50_000
+const val PRIZE_FIFTH = 5_000
