c5d5aa971529f548f6bdae4a3b631e0b869a7f3f
LeeJaeHyeok97 <hazardous10@naver.com>
2025-04-23 16:03:36 +0900

    feat: implement purchase count output

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -3,6 +3,10 @@ package lotto
 class LottoGame {
     fun run() {
         OutputView().welcome()
-        InputView().purchaseAmount()
+        val purchaseAmount = InputView().purchaseAmount()
+        val ticketCount = calculateTicketCount(purchaseAmount)
+        OutputView().printPurchasedCount(ticketCount)
     }
+
+    private fun calculateTicketCount(purchaseAmount: String) = purchaseAmount.toInt() / 1000
 }
--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -4,4 +4,8 @@ class OutputView {
     fun welcome() {
         println("Please enter the purchase amount.")
     }
+
+    fun printPurchasedCount(ticketCount: Int) {
+        println("You have purchased ${ticketCount} tickets.")
+    }
 }
