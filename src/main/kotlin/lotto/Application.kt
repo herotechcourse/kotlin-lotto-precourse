ad02e8088cd7a821a826fae56020109510133d13
David Gonzalez <davidegonzale@Davids-Air.fritz.box>
2025-04-23 18:31:56 +0200

    feat(app): user input flow with amount, winning numbers, and bonus

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,13 @@
 package lotto
 
 fun main() {
-    // TODO: Implement the program
+    val inputView = InputView()
+    val outputView = OutputView()
+
+    val amount = inputView.inputAmount()
+    val tickets = LottoTickets.generate(amount)
+    outputView.printTickets(tickets)
+
+    val winningNumbers = inputView.inputWinningNumbers()
+    val bonusNumber = inputView.inputBonusNumber(winningNumbers)
 }
