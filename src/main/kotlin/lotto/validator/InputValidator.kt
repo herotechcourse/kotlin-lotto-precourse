724bf8e76638b88e120fe87dec65a40a3c1c9845
moonseonghui <msh9980@naver.com>
2025-04-24 00:40:43 +0900

    feat(InputValidator): implement isNumeric in InputValidator

--- src/main/kotlin/lotto/validator/InputValidator.kt
+++ src/main/kotlin/lotto/validator/InputValidator.kt
@@ -1,9 +1,15 @@
 package lotto.validator
 
 object InputValidator {
-    fun notEmpty(input: String?) {
-        if (input.isNullOrEmpty()) {
+    fun notEmpty(input: String) {
+        if (input.isEmpty()) {
             throw IllegalArgumentException("[ERROR] Input cannot be empty.")
+        }
+    }
+
+    fun isNumeric(input: String) {
+        if (!input.all { it.isDigit() }) {
+            throw IllegalArgumentException("[ERROR] Input must be a number.")
         }
     }
 }
