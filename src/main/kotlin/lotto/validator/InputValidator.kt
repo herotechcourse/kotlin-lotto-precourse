c4a70f1d925ae7ae9201e54e6264c9294afd4768
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-04-23 17:17:08 +0200

    feat: limit inputValidation to only validate input and not the Lotto logic

--- src/main/kotlin/lotto/validator/InputValidator.kt
+++ src/main/kotlin/lotto/validator/InputValidator.kt
@@ -5,6 +5,20 @@ object InputValidator {
     fun validateAmount(input: String): Int {
         val amount = input.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] Input must be a number.")
         require(amount > 0 && amount % 1000 == 0) { "[ERROR] Amount must be divisible by 1000" }
+        
         return amount
+    }
+
+    fun validateWinningNumbers(input: String): List<Int> {
+        val split = input.split(",").map { it.trim() }
+        return split.map {
+            it.toIntOrNull()
+                ?: throw IllegalArgumentException("[ERROR] All inputs must be valid numbers.")
+        }
+    }
+
+    fun validateBonusNumber(input: String): Int {
+        return input.toIntOrNull()
+            ?: throw IllegalArgumentException("[ERROR] Bonus number must be a number.")
     }
 }
