6d2d6dab02f8ea5b89649038a5d7f76521e0c762
moonseonghui <msh9980@naver.com>
2025-04-24 00:58:57 +0900

    feat(InputValidator): implement isMultipleOfThousand in InputValidator

--- src/main/kotlin/lotto/validator/InputValidator.kt
+++ src/main/kotlin/lotto/validator/InputValidator.kt
@@ -1,6 +1,8 @@
 package lotto.validator
 
 object InputValidator {
+    private const val THRESHOLD_UNIT = 1000
+
     fun notEmpty(input: String) {
         if (input.isEmpty()) {
             throw IllegalArgumentException("[ERROR] Input cannot be empty.")
@@ -16,6 +18,12 @@ object InputValidator {
     fun withinRange(input: Int, minRange: Int, maxRange: Int) {
         if (input < minRange || input > maxRange) {
             throw IllegalArgumentException("[ERROR] Input must be a number between $minRange and $maxRange.")
+        }
+    }
+
+    fun isMultipleOfThousand(input: Int) {
+        if (input % THRESHOLD_UNIT != 0) {
+            throw IllegalArgumentException("[ERROR] Input must be a multiple of $THRESHOLD_UNIT.")
         }
     }
 }
