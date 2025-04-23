aef6f29518c1244f9654f959dbb43b2b6fbd0e62
2taezeat <xorudfl772@naver.com>
2025-04-23 13:01:17 +0900

    feat: LottoRank Enum Define

---
+++ src/main/kotlin/lotto/LottoRank.kt
@@ -0,0 +1,29 @@
+package lotto
+
+
+enum class LottoRank(
+    val matchCount: Int,
+    val isBonus: Boolean = false,
+    val prize: Int,
+    val description: String
+) {
+    FIRST(6, false, PRIZE_FIRST, LottoRankFormatter.formatDescription(6, false, PRIZE_FIRST)),
+    SECOND(5, true, PRIZE_SECOND, LottoRankFormatter.formatDescription(5, true, PRIZE_SECOND)),
+    THIRD(5, false, PRIZE_THIRD, LottoRankFormatter.formatDescription(5, false, PRIZE_THIRD)),
+    FOURTH(4, false, PRIZE_FOURTH, LottoRankFormatter.formatDescription(4, false, PRIZE_FOURTH)),
+    FIFTH(3, false, PRIZE_FIFTH, LottoRankFormatter.formatDescription(3, false, PRIZE_FIFTH)),
+    MISS(0, false, 0, "No prize");
+
+    companion object {
+        fun from(matchCount: Int, isBonus: Boolean): LottoRank {
+            return when {
+                matchCount == 6 -> FIRST
+                matchCount == 5 && isBonus -> SECOND
+                matchCount == 5 -> THIRD
+                matchCount == 4 -> FOURTH
+                matchCount == 3 -> FIFTH
+                else -> MISS
+            }
+        }
+    }
+}
