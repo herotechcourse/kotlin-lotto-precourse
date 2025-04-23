cc1b9d801bbdc38ef7d5d86fd7e80a410e15ab41
yufenlai0825 <yufenlai0825@gmail.com>
2025-04-23 16:34:45 +0200

    add random numbers for each ticket and print

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,17 +1,22 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
 
     // purchase ticket
     println("Please enter the purchase amount.")
     val input = Console.readLine().toIntOrNull()
-    if (input == null || (input % 1000) !== 0 ){
+    if (input == null || (input % 1000) != 0 ){
         throw IllegalArgumentException("Each ticket costs 1,000 KRW. Please enter a valid amount.")
     }
 
-    val ticketNumbers = input / 1000
-    println("You have purchased $ticketNumbers tickets.")
+    val tickets = input / 1000
+    println("You have purchased $tickets tickets.")
+    repeat(tickets){
+        val ticketNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+        println(ticketNumbers)
+    }
 
 }
