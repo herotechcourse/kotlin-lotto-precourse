1c48801b5942aaa6b3521e8fc60ff28f62d68a4e
minsang <minsang007@gmail.com>
2025-04-23 17:57:14 +0900

    feat(input): add numeric validation to PurchaseAmountInputView using NumericValidator
    Validate purchase amount input using NumericValidator before parsing it to an integer.

--- src/main/kotlin/lotto/view/input/PurchaseAmountInputView.kt
+++ src/main/kotlin/lotto/view/input/PurchaseAmountInputView.kt
@@ -1,7 +1,15 @@
 package lotto.view.input
 
 import camp.nextstep.edu.missionutils.Console
+import lotto.view.validator.NumericValidator
 
 class PurchaseAmountInputView : InputView {
-    override fun readInput() = Console.readLine().toInt()
+    override fun readInput(): Int {
+        println("Input purchase amount:")
+
+        val purchaseAmount = Console.readLine()
+        NumericValidator.validate(purchaseAmount)
+
+        return purchaseAmount.toInt()
+    }
 }
