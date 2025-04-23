63369ef890df6a7bdfe691488e12dcce84c09eee
2taezeat <xorudfl772@naver.com>
2025-04-23 09:45:45 +0900

    feat: apply validator in InputView

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -5,20 +5,23 @@ import camp.nextstep.edu.missionutils.Console
 
 object InputView {
     fun readPurchaseAmount(): Int {
-        return Console.readLine().toInt()
+        val amount = Console.readLine().toInt()
+        Validator.validateAmount(amount)
+        return amount
     }
-
 
     fun raedWinnerNumbers(): List<Int> {
         val winnerNumbers = Console.readLine()
             .split(",")
             .map { it.trim().toInt() }
-
+        Validator.validateWinningNumbers(winnerNumbers)
         return winnerNumbers
     }
 
-
-    fun readBonusNumber(): Int {
-        return Console.readLine().toInt()
+    fun readBonusNumber(winningNumbers: List<Int>): Int {
+        val bonus = Console.readLine().toInt()
+        Validator.validateBonusNumber(bonus, winningNumbers)
+        return bonus
     }
+
 }
