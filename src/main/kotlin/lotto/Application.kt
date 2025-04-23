eede976e55cd2b20827063a21b544d4810562d77
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-04-23 15:09:00 +0200

    feat: add main logic to run and Test cases

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,17 @@
 package lotto
 
+import lotto.handler.InputHandler
+import lotto.handler.OutputHandler
+import lotto.service.LottoManager
+
 fun main() {
     // TODO: Implement the program
+    val purchasedAmount = InputHandler.getPurchaseAmount();
+
+    val ticketAmount = purchasedAmount / 1000
+
+    val tickets = LottoManager.generateMultipleTickets(ticketAmount)
+
+    val outputHandler = lotto.handler.OutputHandler()
+    outputHandler.displayPurchasedTicketsInfo(tickets)
 }
