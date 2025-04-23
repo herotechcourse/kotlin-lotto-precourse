e28ff0696ca4b2303d409112af3c8a4dde00ea3d
2taezeat <xorudfl772@naver.com>
2025-04-23 13:38:53 +0900

    feat: lotto init, provide sorted number, match numbers, check bonus number

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,28 @@
 package lotto
 
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.size == LOTTO_NUMBER_COUNT) { ERROR_INVALID_SIZE }
+        require(numbers.toSet().size == LOTTO_NUMBER_COUNT) { ERROR_DUPLICATE_NUMBER }
+        require(numbers.all { it in LOTTO_NUMBER_MIN..LOTTO_NUMBER_MAX }) { ERROR_OUT_OF_RANGE }
     }
 
-    // TODO: Implement additional functions
+    fun getSortedNumbers(): List<Int> {
+        return numbers.sorted()
+    }
+
+    fun countingWinningNumbers(winningNumbers: List<Int>): Int {
+        return numbers.count { it in winningNumbers }
+    }
+
+    fun isContainsBonusNumber(bonusNumber: Int): Boolean {
+        return bonusNumber in numbers
+    }
+
+    companion object {
+        private const val ERROR_INVALID_SIZE = "[ERROR] Lotto must contain exactly 6 numbers."
+        private const val ERROR_DUPLICATE_NUMBER = "[ERROR] Lotto numbers must be unique."
+        private const val ERROR_OUT_OF_RANGE = "[ERROR] Lotto numbers must be between 1 and 45."
+    }
 }
