9f0fff589974ee462955957bbfd5f4a2b7abd7f5
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-04-23 15:07:50 +0200

    feat: create LottoManager and add ticket generating methods

---
+++ src/main/kotlin/lotto/service/LottoManager.kt
@@ -0,0 +1,15 @@
+package lotto.service
+
+import lotto.Lotto
+import camp.nextstep.edu.missionutils.Randoms
+
+object LottoManager {
+    fun generateLottoTicket(): Lotto {
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+        return Lotto(numbers)
+    }
+
+    fun generateMultipleTickets(amount: Int): List<Lotto> {
+        return List(amount) { generateLottoTicket() }
+    }
+}
