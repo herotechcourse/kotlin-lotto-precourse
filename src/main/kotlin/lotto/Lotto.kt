a03f5de2eba7c26c431609a4ab738107cb2350ec
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-04-24 10:25:00 +0200

    feat(Lotto): add checks for numbers list
    check if the list items are unique and between the range of 1-45. Throw error otherwise

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,14 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(!numbers.groupingBy { it }.eachCount().any { it.value > 1 }) { "[ERROR] Lotto must not contain duplicate numbers." }
+        require(numbers.all { it >= 1 && it <= 45 }) { "[ERROR] Numbers must be in the range from 1 to 45" }
     }
 
-    // TODO: Implement additional functions
+    // sort
+
+    // compare
+
+    // return
+
 }
