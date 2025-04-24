c13dc7827cd5443d6f09a6cc6866b7dca40f4dd6
YongLeeCode <yonghwan311@gmail.com>
2025-04-24 22:18:47 +0900

    test(input): define test structure for validating purchase amount input
    test cases:
    - non-numeric input
    - not divisible by 1000
    - negative values

---
+++ src/test/kotlin/lotto/input/MoneyInputTest.kt
@@ -0,0 +1,34 @@
+package lotto.input
+
+import lotto.Lotto
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+/**
+ * @packageName    : lotto
+ * @fileName       : InputTest
+ * @author         : yong
+ * @date           : 4/23/25
+ * @description    :
+ */
+class MoneyInputTest {
+    @Test
+    fun `should pass when purchase amount is divisible by 1000 and positive`() {
+
+    }
+
+    @Test
+    fun `should throw exception when purchase amount is not divisible by 1000`() {
+
+    }
+
+    @Test
+    fun `should throw exception when purchase amount is negative`() {
+
+    }
+
+    @Test
+    fun `should throw exception when purchase amount is not a number`() {
+
+    }
+}
