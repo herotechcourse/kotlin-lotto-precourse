d5aab28d897b604e0ec4c61c30ea7437d577a75b
Santiago Tena Hernandez <stenahe@gmail.com>
2025-04-24 20:53:22 +0200

    feat(user interface): add base methods to display messages to user

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,4 +1,29 @@
 package lotto.view
 
+import lotto.Lotto
+import lotto.TICKET_COST
+import lotto.UserInput
+import lotto.ticket.TicketPurchase
+
 class OutputView {
+    companion object {
+        fun displayPurchaseMessage(ticketPurchase: TicketPurchase) {
+            val amountOfTicketsBought: Int = ticketPurchase.amountOfTicketsBought
+            println("You have purchased $amountOfTicketsBought tickets.")
+        }
+
+        fun displayBoughtTickets(ticketPurchase: TicketPurchase) {
+            println("Tickets")
+            println()
+        }
+
+        fun displayWinningStatistics(lotto: Lotto) {
+            println("Winning statistics")
+            println("---")
+        }
+
+        fun displayReturnRate(lotto: Lotto) {
+            println("Total return rate is X%.")
+        }
+    }
 }
