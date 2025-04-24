0ed310ae36be49c05ff35846579a94152e4d617e
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-04-24 21:13:14 +0200

    feat(OutputView): print statistics and profit rate
    add formatString function to build the string with the information provided by statistics
    add printStatistics to print the string according to expected output
    add printReturnRate to print final profit rate

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -3,7 +3,7 @@ package lotto
 import lotto.Game.Prize
 import java.util.DoubleSummaryStatistics
 
-class OutputView(tickets: List<Lotto>) {
+class OutputView() {
     fun printTickets(tickets: List<Lotto>) {
         println("You have purchased ${tickets.size} tickets")
         tickets.map {
@@ -12,4 +12,25 @@ class OutputView(tickets: List<Lotto>) {
         println()
     }
 
+    // format statistics string
+    fun formatString(prize: Prize, count: Int): String {
+        val matchDescription = if (prize.bonus) {
+            "${prize.matchCount} Matches + Bonus Ball"
+        } else {
+            "${prize.matchCount} Matches"
+        }
+        val formattedAmount = "%,d".format(prize.amount) // Format amount with commas
+        val ticketText = if (count == 1) "ticket" else "tickets"
+            return "$matchDescription ($formattedAmount KRW) - $count $ticketText"
+    }
+
+    fun printStatistics(statistics:  List<Pair<Prize, Int>>) {
+        println("Winning statistics:")
+        println("---")
+        statistics.reversed().forEach() { println(formatString(it.first, it.second)) }
+    }
+
+    fun printReturnRate(returnRate: Double) {
+        println("Total return rate is $returnRate%")
+    }
 }
