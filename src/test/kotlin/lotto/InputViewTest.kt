ddcd78a63364961f36e7af72cc89e24da96e3212
Petra Bencze <petra.bncz@gmail.com>
2025-04-24 16:40:27 +0200

    feat(InputViewTest): valid purchase amount test

--- src/test/kotlin/lotto/InputViewTest.kt
+++ src/test/kotlin/lotto/InputViewTest.kt
@@ -1,20 +1,27 @@
 package lotto
 
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import org.assertj.core.api.Assertions.assertThatNoException
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EmptySource
-import org.junit.jupiter.params.provider.NullSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class InputViewTest {
     @ParameterizedTest(name = "{index}. purchase amount of \"{0}\"")
     @ValueSource(strings = ["0", "999", "1200", "abc", " ", "2147483647", "16000"])
     @EmptySource // for empty input; use @NullSource for null input
-    fun `test parseAmount`(input: String) {
+    fun `test invalid purchase amount`(input: String) {
         assertThatIllegalArgumentException()
             .isThrownBy{ InputView().parseAmount(input) }
             .withMessageStartingWith("[ERROR]")
+    }
+
+    @ParameterizedTest(name = "{index}. purchase amount of \"{0}\"")
+    @ValueSource(strings = ["1000", "12000", "15000"])
+    fun `test valid purchase amount`(input: String) {
+        assertThatNoException().isThrownBy{ InputView().parseAmount(input) }
+        val value = InputView().parseAmount(input)
+        assertThat(value).isEqualTo(input.toInt())
     }
 }
