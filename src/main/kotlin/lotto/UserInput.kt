7567aa27d4053d6b547ad830770c53766a58e08a
Santiago Tena Hernandez <stenahe@gmail.com>
2025-04-24 15:19:54 +0200

    feat(user input): add class and input sanitation

---
+++ src/main/kotlin/lotto/UserInput.kt
@@ -0,0 +1,49 @@
+package lotto
+
+class UserInput{
+
+    init {
+        require(TICKET_COST > 0) {"[Error] TICKET_COST ($TICKET_COST) must be greater than 0"}
+        require(LOWEST_NUMBER < HIGHEST_NUMBER) {"[Error] LOWEST_NUMBER ($LOWEST_NUMBER) must be smaller than HIGHEST_NUMBER ($HIGHEST_NUMBER)"}
+    }
+
+    var purchaseAmount: Int = 0
+        private set
+    var winningNumbers: MutableList<Int> = mutableListOf()
+        private set
+    var bonusNumber: Int = 0
+        private set
+
+    fun setPurchaseAmount(input: String) {
+        val purchaseAmount = input.toIntOrNull()
+
+        require(purchaseAmount != null) { "[ERROR] Purchase amount must be a number" }
+        require(purchaseAmount >= 0) { "[ERROR] Purchase amount must be a positive number" }
+        require(purchaseAmount >= TICKET_COST) { "[ERROR] Purchase amount must be equal or greater than $TICKET_COST" }
+
+        this.purchaseAmount = purchaseAmount
+    }
+
+    fun setWinningNumbers(input: String) {
+        val splitNumbers = input.split(",").map { it.trim() }
+        val numbers = mutableListOf<Int>()
+
+        for (splitNumber in splitNumbers) {
+            val number = splitNumber.toIntOrNull()
+            require(number != null) { "[ERROR] All entries must be a number" }
+            require(number >= LOWEST_NUMBER && number <= HIGHEST_NUMBER) { "[ERROR] All numbers must be between $LOWEST_NUMBER and $HIGHEST_NUMBER ($number)" }
+            numbers.add(number)
+        }
+
+        this.winningNumbers = numbers
+    }
+
+    fun setBonusNumber(input: String) {
+        val bonusNumber = input.toIntOrNull()
+
+        require(bonusNumber != null) { "[ERROR] Bonus number must be a number" }
+        require(bonusNumber >= LOWEST_NUMBER && bonusNumber <= HIGHEST_NUMBER) { "[ERROR] All numbers must be between $LOWEST_NUMBER and $HIGHEST_NUMBER ($bonusNumber)" }
+
+        this.bonusNumber = bonusNumber
+    }
+}
