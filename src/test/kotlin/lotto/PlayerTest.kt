f5e3ebef7021e0a2ae314bf356048d26884e66db
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-04-24 12:23:33 +0200

    test(Player): add test class for Player
    add PlayerTest class
    create test to check if the correct number of tickets are generated

--- src/test/kotlin/lotto/PlayerTest.kt
+++ src/test/kotlin/lotto/PlayerTest.kt
@@ -1,4 +1,18 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
 class PlayerTest {
+
+    val player = Player(10000)
+
+    @Test
+    fun `Create ticket list with size equal to number of tickets`() {
+       val result = player.tickets
+        assertThat(result).hasSize(10)
+    }
 }
