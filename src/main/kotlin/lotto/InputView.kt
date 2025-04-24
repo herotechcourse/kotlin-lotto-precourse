e900931b6be1ebb14093e6d1a7dc02c6b2e8e167
Adijat Ojutomori <mac@Ojus-Macbook-Air.local>
2025-04-24 10:59:25 +0200

    feat(InputView): collection of purchase amount, winning number and bonus number

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -1,4 +1,23 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 class InputView {
+    fun readPurchaseAmount(): Int {
+        println("Please enter the purchase amount.")
+        val input = Console.readLine()
+        return input.toInt()
+    }
+
+    fun readWinningNumbers(): Int {
+        println("Please enter last week's winning numbers.")
+        val input = Console.readLine()
+        return input.toInt()
+    }
+
+    fun readBonusNumber(winningNumbers: Lotto): Int {
+        println("Please enter the bonus number.")
+        val input = Console.readLine()
+        return input.toInt()
+    }
 }
