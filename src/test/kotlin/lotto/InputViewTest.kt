e9e83a3fc279a5b2506cd8a1c69bfc657e62400d
Petra Bencze <petra.bncz@gmail.com>
2025-04-24 17:05:49 +0200

    feat(InputViewTest): add tests for bonus and winning number parsing

--- src/test/kotlin/lotto/InputViewTest.kt
+++ src/test/kotlin/lotto/InputViewTest.kt
@@ -8,8 +8,8 @@ import org.junit.jupiter.params.provider.EmptySource
 import org.junit.jupiter.params.provider.ValueSource
 
 class InputViewTest {
-    @ParameterizedTest(name = "{index}. purchase amount of \"{0}\"")
-    @ValueSource(strings = ["0", "999", "1200", "abc", " ", "2147483647", "16000"])
+    @ParameterizedTest(name = "{index}. invalid purchase amount of \"{0}\"")
+    @ValueSource(strings = ["0", "999", "1200", "abc", " ", "2147483647", "16000", "-1000"])
     @EmptySource // for empty input; use @NullSource for null input
     fun `test invalid purchase amount`(input: String) {
         assertThatIllegalArgumentException()
@@ -17,11 +17,48 @@ class InputViewTest {
             .withMessageStartingWith("[ERROR]")
     }
 
-    @ParameterizedTest(name = "{index}. purchase amount of \"{0}\"")
+    @ParameterizedTest(name = "{index}. valid purchase amount of \"{0}\"")
     @ValueSource(strings = ["1000", "12000", "15000"])
     fun `test valid purchase amount`(input: String) {
         assertThatNoException().isThrownBy{ InputView().parseAmount(input) }
         val value = InputView().parseAmount(input)
         assertThat(value).isEqualTo(input.toInt())
     }
+
+    @ParameterizedTest(name = "{index}. invalid bonus number of \"{0}\"")
+    @ValueSource(strings = ["0", "111", "a", " ", "-1"])
+    @EmptySource
+    fun `test invalid bonus number`(input: String) {
+        assertThatIllegalArgumentException()
+            .isThrownBy{ InputView().parseBonusNumber(input) }
+            .withMessageStartingWith("[ERROR]")
+    }
+
+    @ParameterizedTest(name = "{index}. valid bonus number of \"{0}\"")
+    @ValueSource(strings = ["1", "45", "12"])
+    fun `test valid bonus number`(input: String) {
+        assertThatNoException().isThrownBy{ InputView().parseBonusNumber(input) }
+        val value = InputView().parseBonusNumber(input)
+        assertThat(value).isEqualTo(input.toInt())
+    }
+
+    @ParameterizedTest(name = "{index}. invalid list of \"{0}\"")
+    @ValueSource(strings = ["a,b,c,1,2,3", "1,,2,3,4,5,6", "1,2,3,4,5,6,", " "])
+    @EmptySource
+    fun `test invalid winning numbers list`(input: String) {
+        assertThatIllegalArgumentException()
+            .isThrownBy{ InputView().parseWinningNumbers(input) }
+            .withMessageStartingWith("[ERROR]")
+    }
+
+    @ParameterizedTest(name = "{index}. valid list of \"{0}\"")
+    @ValueSource(strings = ["1,2,3,4,5,6", "121,456,3"])
+    fun `test valid winning numbers list`(input: String) {
+        assertThatNoException().isThrownBy{ InputView().parseWinningNumbers(input) }
+        val value = InputView().parseWinningNumbers(input)
+    }
+
+
+
+
 }
