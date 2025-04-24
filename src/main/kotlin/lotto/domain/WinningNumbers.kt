73526834ef39f3acd9317eed154653c913eef099
geonheey <ghyjas0704@gmail.com>
2025-04-25 08:24:00 +0900

    chore: remove unused files

--- src/main/kotlin/lotto/domain/WinningNumbers.kt
+++
@@ -1,9 +0,0 @@
-package lotto.domain
-
-class WinningNumbers(val numbers: List<Int>, val bonusNumber: Int) {
-
-    init {
-        require(numbers.size == 6) { "[ERROR] Winning numbers must contain exactly 6 numbers." }
-        require(numbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
-    }
-}
