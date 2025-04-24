e962e8cc35d302bf8bf917b3890a77d475e01666
geonheey <ghyjas0704@gmail.com>
2025-04-24 15:14:43 +0900

    refactor(domain): separate Lotto business logic into LottoTicket
    - Remove match count and bonus number check logic from Lotto to LottoTicket for responsibility separation
    - Create LottoTicket to handle match count and bonus number check

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,13 +6,6 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.toSet().size == 6) { "[ERROR] Lotto numbers must be unique." }
         require(numbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
     }
-    fun getNumbers(): List<Int> = numbers.sorted()
 
-    fun matchingCount(winningNumbers: List<Int>): Int {
-        return numbers.count { it in winningNumbers }
-    }
-
-    fun containsBonus(bonusNumber: Int): Boolean {
-        return numbers.contains(bonusNumber)
-    }
+    fun getNumbers(): List<Int> = numbers
 }
