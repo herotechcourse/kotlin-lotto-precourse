1e8ccc444fc3d704cd06827cfd6fa07b5eee9b9a
atash1986 <msc.ashourzadeh@gmail.com>
2025-04-24 21:12:11 +0430

    feat(OutputView): add print tickets function as a function in the OutputView class
    - It receives the tickets list
    - it prints the number of tickets
    - It prints the numbers on the ticket using the overrided toString method

---
+++ src/main/kotlin/views/OutputView.kt
@@ -0,0 +1,12 @@
+package views
+
+import lotto.Lotto
+
+class OutputView {
+    fun printTickets(tickets: List<Lotto>) {
+        println("You have purchased ${tickets.size} tickets.")
+        for(ticket in tickets) {
+            println(ticket)
+        }
+    }
+}
