9dc54eddc0e83c7438a9d17a8ad2926e3aef59e7
Alex <alexis.mar.schmidt@gmail.com>
2025-04-24 14:23:31 +0200

    feat(lotto-service): create ticket handling system
    - add list for ticket storage
    - create ticket purchase logic using LottoGenerator
    - implement accesor to tickets

--- src/main/kotlin/lotto/LottoService.kt
+++ src/main/kotlin/lotto/LottoService.kt
@@ -1,4 +1,21 @@
 package lotto
 
-class LottoService {
+class LottoService() {
+
+    private var winnerNumbers: List<Int>? = null
+    private var bonusNumber: Int? = null
+    private var lottoTickets: MutableList<Lotto> = mutableListOf()
+
+    fun purchaseTickets(amount: Int) {
+        val lottoGenerator = LottoGenerator()
+        val numberOfTickets = amount / LottoConstants.TICKET_PRICE
+        lottoTickets = MutableList(numberOfTickets){
+            lottoGenerator.createLottoTicket()
+        }
+    }
+
+    fun getTickets(): MutableList<Lotto> {
+        return lottoTickets
+    }
+
 }
