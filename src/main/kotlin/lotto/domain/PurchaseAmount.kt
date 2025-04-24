8530aae146e14cab267a99a868964a7650a0ad67
kkonii <119216472+kkonii@users.noreply.github.com>
2025-04-24 20:02:57 +0900

    refactor: move unit and range validation from InputValidator to PurchaseAmount VO

---
+++ src/main/kotlin/lotto/domain/PurchaseAmount.kt
@@ -0,0 +1,21 @@
+package lotto.domain
+
+class PurchaseAmount(private val amount: Int) {
+    init {
+        validateAmountRange(amount)
+        validatePurchasableUnit(amount)
+    }
+
+    private fun validateAmountRange(amount: Int){
+        require(amount <= MAXIMUM_AMOUNT) {"[ERROR] Amount must not exceed $MAXIMUM_AMOUNT"}
+    }
+
+    private fun validatePurchasableUnit(amount: Int) {
+        require(amount % PURCHASE_UNIT == 0) {"[ERROR] Purchase amount must entered as $PURCHASE_UNIT"}
+    }
+
+    companion object {
+        private const val MAXIMUM_AMOUNT = 100_000
+        private const val PURCHASE_UNIT = 1_000
+    }
+}
---
+++ src/test/kotlin/lotto/domain/PurchaseAmountTest.kt
@@ -0,0 +1,26 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class PurchaseAmountTest {
+    @Nested
+    inner class ValidationTest {
+        @ParameterizedTest
+        @DisplayName("[Exception] when purchase amount input exceed maximum limit, throw exception")
+        @ValueSource(ints = [150000, 500000, 100001])
+        fun `test purchase amount range`(input: Int) {
+            Assertions.assertThatIllegalArgumentException().isThrownBy { PurchaseAmount(input) }
+        }
+
+        @ParameterizedTest
+        @DisplayName("[Exception] throw when purchase amount is not a valid unit")
+        @ValueSource(ints = [10800, 5500, 3115])
+        fun `test purchase amount unit`(input: Int) {
+            Assertions.assertThatIllegalArgumentException().isThrownBy { PurchaseAmount(input) }
+        }
+    }
+}
