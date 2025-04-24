b91adcce476fddcce47cf2b2b340fef7d6dbe972
Petra Bencze <petra.bncz@gmail.com>
2025-04-24 16:44:51 +0200

    refactor(InputView): modify access

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -74,15 +74,15 @@ class InputView {
 
     // TODO make it private or extract it to be used by Lotto class
     companion object {
-        const val MAX_RETRY = 5
-        const val MIN_NUM = 1
-        const val MAX_NUM = 45
-        const val MAX_AMOUNT = 15000
-        const val AMOUNT_RANGE_MSG = "[ERROR] Amount cannot be 0 or higher than $MAX_AMOUNT"
-        const val MAX_RETRY_MSG = "[ERROR] Maximum retry attempts exceeded."
-        const val INVALID_INPUT_MSG = "[ERROR] Invalid input."
-        const val DIVISIBLE_AMOUNT_MSG = "[ERROR] Amount has to be divisible by 1000."
-        const val INVALID_RANGE_MSG = "[ERROR] Invalid number: not in range $MIN_NUM-$MAX_NUM."
+        private const val MAX_RETRY = 5
+        private const val MIN_NUM = 1
+        private const val MAX_NUM = 45
+        private const val MAX_AMOUNT = 15000
+        private const val AMOUNT_RANGE_MSG = "[ERROR] Amount cannot be 0 or higher than $MAX_AMOUNT"
+        private const val MAX_RETRY_MSG = "[ERROR] Maximum retry attempts exceeded."
+        private const val INVALID_INPUT_MSG = "[ERROR] Invalid input."
+        private const val DIVISIBLE_AMOUNT_MSG = "[ERROR] Amount has to be divisible by 1000."
+        private const val INVALID_RANGE_MSG = "[ERROR] Invalid number: not in range $MIN_NUM-$MAX_NUM."
     }
 
     class MaxRetryException(message: String, cause: Throwable? = null): RuntimeException(message, cause)
