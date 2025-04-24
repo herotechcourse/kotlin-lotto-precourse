373f09677983e965d84e67bbdb7cce9d53d72016
YuGyeong98 <kuk3097@naver.com>
2025-04-24 19:11:00 +0900

    feat(LottoTicketMachine): add lotto program process function

---
+++ src/main/kotlin/lotto/LottoTicketMachine.kt
@@ -0,0 +1,45 @@
+package lotto
+
+class LottoTicketMachine(
+    private val input: Input,
+    private val validator: Validator,
+    private val output: Output,
+) {
+
+    fun enterPurchaseAmount(): Int {
+        println("Please enter the purchase amount.")
+        return input.enterNumber { validator.validateStringToInt(it) }
+    }
+
+    fun issueTickets(purchaseAmount: Int): List<List<Int>> {
+        val issuedTicket = IssuedTicket(purchaseAmount, DefaultRandomGenerator())
+        val issuedTickets = IssuedTicketsGenerator.of(issuedTicket)
+        output.printIssuedTickets(issuedTicket.getCount(), issuedTickets)
+        return issuedTickets
+    }
+
+    fun enterWinningNumbers(): List<Int> {
+        println("Please enter last week's winning numbers.")
+        return input.enterNumber {
+            it.split(",")
+                .map {
+                    validator.validateStringToInt(it)
+                }
+        }
+    }
+
+    fun enterBonusNumber(): Int {
+        println("Please enter the bonus number.")
+        return input.enterNumber { validator.validateStringToInt(it) }
+    }
+
+    fun winningStatistics(
+        lotto: Lotto,
+        issuedTickets: List<List<Int>>,
+        bonusNumber: Int,
+        purchaseAmount: Int,
+    ) {
+        lotto.simulate(issuedTickets, bonusNumber)
+        output.printWinningStatistics(lotto.getProfitRate(purchaseAmount))
+    }
+}
