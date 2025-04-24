e8f05fc93506b915e2de36ec2b0e39914465d79d
vketteni <vketteni@gmail.com>
2025-04-24 13:19:13 +0000

    feat: prompt and take user input for purchase amount

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,29 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
+const val TICKET_PRICE = 1000;
+
 fun main() {
-    // TODO: Implement the program
+    // Ticket Purchase Flow
+    val purchaseAmount: Int = prompt_and_take_purchase_amount();
+}
+
+fun prompt_and_take_purchase_amount() : Int {
+    while (true) {
+        val purchasedAmount: Int
+        try {
+            purchasedAmount = Console.readLine().toInt()
+            validate_purchase_amount(purchasedAmount)
+            return purchasedAmount
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        } catch (e: NumberFormatException) {
+            println("[ERROR] Input must be a number")
+        }
+    }
+}
+
+fun validate_purchase_amount(purchaseAmount: Int) {
+    // TODO
 }
