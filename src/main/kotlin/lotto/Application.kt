5e5830d463f866a48cabc1ff0ccc6ce3c67dc5ad
atash1986 <msc.ashourzadeh@gmail.com>
2025-04-24 22:14:40 +0430

    feat(InputView): ask winning numbers
    - a function was developed in the inputView class to ask for the winning numbers
    - the related validations were developed
    - the mentioned function was called from the application class to read the winning numbers

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -6,9 +6,11 @@ fun main() {
    var inputView = InputView()
    val outputView = OutputView()
    val lottoMachine = LottoMachine()
- 
+
    var purchaseAmount=inputView.getPurchaseAmount()
    val ticketsCount=lottoMachine.calcTicketNumber(purchaseAmount)
    val tickets=lottoMachine.issueTickets(ticketsCount)
    outputView.printTickets(tickets)
+   val winningNumbers=inputView.getWinningNumbers()
+   println(winningNumbers)
 }
--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -12,5 +12,20 @@ class InputView {
         }
         throw IllegalArgumentException()
     }
+
+    fun getWinningNumbers(): List<Int>
+    {
+        println("Please enter last week's winning numbers:")
+        val input=Console.readLine() ?: ""
+        val winningNumbers=input.split(',').map({ it.toInt() })
+        if(winningNumbers.size < 6)
+            throw IllegalArgumentException("There must be exactly 6 numbers")
+        if(winningNumbers.distinct().size < 6)
+            throw IllegalArgumentException("There must not be repeated numbers")
+        if(!winningNumbers.all { it in 1..45 })
+            throw IllegalArgumentException("Each number must be between 1 and 45")
+        return winningNumbers
+    }
+    
 }
 
