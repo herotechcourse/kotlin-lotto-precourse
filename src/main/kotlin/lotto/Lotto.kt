29adb7e855256dd87de0042bb04d9a41b4c8ce12
Urbano Jr <urbanobazz@gmail.com>
2025-04-24 14:37:01 +0200

    feature($Lotto): add extra functionality
    Add to Lotto:
    - numbers validation.
    - getter for numbers.
    - method to count matches to winning numbers.
    - method to check if bonus number is in numbers.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,19 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.distinct().size == 6) { "[ERROR] Lotto numbers must be unique." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
     }
 
-    // TODO: Implement additional functions
+    fun getNumbers(): List<Int> {
+        return numbers.sorted()
+    }
+
+    fun countMatches(winningNumbers: List<Int>): Int {
+        return numbers.count { it in winningNumbers }
+    }
+
+    fun contains(number: Int): Boolean {
+        return numbers.contains(number)
+    }
 }
