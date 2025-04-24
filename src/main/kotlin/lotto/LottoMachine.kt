fbc2a900467bb38de236c39c9ccc97b867984a54
atash1986 <msc.ashourzadeh@gmail.com>
2025-04-24 21:10:04 +0430

    feat(LottoMachine): an issueTickets function was developed to generate the tickets
    - It asks the unique numbers from the static function in the lotto class
    - it repeats this process for each ticket according to the tickets count

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -2,8 +2,19 @@ package lotto
  
 class LottoMachine()
 {
-    fun calcTicketNumber(purchaseAmount:Int)
+    fun calcTicketNumber(purchaseAmount:Int):Int
     {
         return purchaseAmount/1000
+    }
+
+    fun issueTickets(ticketsCount:Int):MutableList<Lotto>
+    {
+        var tickets = mutableListOf<Lotto>()
+        for(t in 1..ticketsCount) {
+            val uniqueNumbers = Lotto.getUniqueNumbers() 
+            var ticket = Lotto(uniqueNumbers)
+            tickets.add(ticket)
+        }
+        return tickets
     }
 }
