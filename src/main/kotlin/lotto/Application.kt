aff5c8ef7120513aa1aa32bd8fc885c260e4fd24
Santiago Tena Hernandez <stenahe@gmail.com>
2025-04-24 20:52:04 +0200

    feat(overall logic): add input loops for errors

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,19 +1,49 @@
 package lotto
 
+import lotto.ticket.TicketPurchase
 import lotto.view.InputView
+import lotto.view.OutputView
 
 fun main() {
-    val userInput = obtainUserInput()
-    println(userInput.purchaseAmount)
-    println(userInput.winningNumbers)
-    println(userInput.bonusNumber)
-}
-
-fun obtainUserInput(): UserInput {
     val userInput = UserInput()
-    InputView.promptPurchaseAmount(userInput)
-    InputView.promptWinningNumbers(userInput)
-    InputView.promptBonusNumber(userInput)
-    return userInput
+    val ticketPurchase = TicketPurchase()
+
+    var isInputValid = false
+    while (!isInputValid) {
+        try {
+            InputView.promptPurchaseAmount(userInput)
+            isInputValid = true
+        } catch (e: Exception) {
+            println(e.message)
+        }
+    }
+
+    ticketPurchase.buyTickets(userInput)
+    OutputView.displayPurchaseMessage(ticketPurchase)
+    OutputView.displayBoughtTickets(ticketPurchase)
+
+    isInputValid = false
+    while (!isInputValid) {
+        try {
+            InputView.promptWinningNumbers(userInput)
+            isInputValid = true
+        } catch (e: Exception) {
+            println(e.message)
+        }
+    }
+
+    isInputValid = false
+    while (!isInputValid) {
+        try {
+            InputView.promptBonusNumber(userInput)
+            isInputValid = true
+        } catch (e: Exception) {
+            println(e.message)
+        }
+    }
+
+    val lotto = Lotto(userInput.winningNumbers)
+    OutputView.displayWinningStatistics(lotto)
+    OutputView.displayReturnRate(lotto)
 }
 
