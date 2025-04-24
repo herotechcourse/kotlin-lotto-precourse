092df0b7206f6ed71c3da3c40057785c99b76408
Santiago Tena Hernandez <stenahe@gmail.com>
2025-04-24 20:50:37 +0200

    fix(user interface): add missing new lines

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -9,18 +9,21 @@ class InputView {
             println("Please enter the purchase amount.")
             val pruchaseAmount = Console.readLine()
             userInput.setPurchaseAmount(pruchaseAmount)
+            println()
         }
 
         fun promptWinningNumbers(userInput: UserInput) {
             println("Please enter last week's winning numbers.")
             val winningNumbers = Console.readLine()
             userInput.setWinningNumbers(winningNumbers)
+            println()
         }
 
         fun promptBonusNumber(userInput: UserInput) {
             println("Please enter the bonus number.")
             val bonusNumber = Console.readLine()
             userInput.setBonusNumber(bonusNumber)
+            println()
         }
     }
 }
