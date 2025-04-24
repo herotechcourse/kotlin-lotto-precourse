6023c6ebf2ff50338d9f55ce44261173b1dea204
vketteni <vketteni@gmail.com>
2025-04-24 15:07:17 +0000

    feat: validate purchase amount

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -6,24 +6,38 @@ const val TICKET_PRICE = 1000;
 
 fun main() {
     // Ticket Purchase Flow
-    val purchaseAmount: Int = prompt_and_take_purchase_amount();
+    val amount: Int = promptAndTakeAmount();
 }
 
-fun prompt_and_take_purchase_amount() : Int {
+fun promptAndTakeAmount() : Int {
     while (true) {
-        val purchasedAmount: Int
-        try {
-            purchasedAmount = Console.readLine().toInt()
-            validate_purchase_amount(purchasedAmount)
-            return purchasedAmount
-        } catch (e: IllegalArgumentException) {
-            println(e.message)
-        } catch (e: NumberFormatException) {
-            println("[ERROR] Input must be a number")
-        }
+        val input = promptInput("Please enter the purchase amount.")
+        val amount = parseInputToInt(input) ?: continue
+        if (validateAmountOrShowError(amount)) return amount
     }
 }
 
-fun validate_purchase_amount(purchaseAmount: Int) {
-    // TODO
+fun promptInput(msg: String) : String {
+    println(msg)
+    return Console.readLine()
+}
+
+fun parseInputToInt(input: String?) : Int? = try {
+    input?.toInt()
+} catch (e: NumberFormatException) {
+    println("[ERROR] Input must be a number")
+    null
+}
+
+fun validateAmountOrShowError(amount: Int) : Boolean = try {
+    validateAmount(amount)
+    true
+} catch (e: IllegalArgumentException) {
+    println(e.message)
+    false
+}
+
+fun validateAmount(amount: Int) {
+    if (amount <= 0) throw IllegalArgumentException("[ERROR] Amount must be positive.")
+    if (amount % TICKET_PRICE != 0) throw IllegalArgumentException("[ERROR] Amount must be divisible by 1,000.")
 }
