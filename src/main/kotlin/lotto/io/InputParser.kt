58bb424721684871fc147e860d4a3f25f196967f
wjunseok <wminsoo1@naver.com>
2025-04-24 17:13:43 +0900

    feat(inputparser): implement parseWinningNumbers to validate and parse winning numbers
    - Checks that the input is not blank and contains exactly 6 comma-separated values.
    - Ensures all numbers are unique and valid integers.
    - Parses the input into a list of winning numbers.

--- src/main/kotlin/lotto/io/InputParser.kt
+++ src/main/kotlin/lotto/io/InputParser.kt
@@ -17,5 +17,24 @@ object InputParser {
 
         return amount / 1000
     }
-    
+
+    fun parseWinningNumbers(input: String): List<Int> {
+        require(input.isNotBlank()) { "[ERROR] Invalid input." }
+
+        val stringNumbers = input.split(",")
+        val winningNumbers = mutableListOf<Int>()
+
+        require(stringNumbers.size == COUNT) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(stringNumbers.size == stringNumbers.distinct().size) { "[ERROR] Lotto numbers must be unique." }
+
+        for (str in stringNumbers) {
+            val trimmed = str.trim()
+            val winningNumber = trimmed.toIntOrNull()
+            requireNotNull(winningNumber) { "[ERROR] Invalid WinningNumber." }
+            winningNumbers.add(winningNumber)
+        }
+
+        return winningNumbers
+    }
+
 }
