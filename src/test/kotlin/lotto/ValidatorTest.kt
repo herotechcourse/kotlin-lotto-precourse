aa7917670dd123ebb4ea37b5366b20dafaffcbc9
YuGyeong98 <kuk3097@naver.com>
2025-04-25 00:23:19 +0900

    test(ValidatorTest): add validate bonus number test function

--- src/test/kotlin/lotto/ValidatorTest.kt
+++ src/test/kotlin/lotto/ValidatorTest.kt
@@ -6,9 +6,10 @@ import org.junit.jupiter.api.assertThrows
 
 class ValidatorTest {
 
+    private val validator = Validator()
+
     @Test
     fun `throws an exception when non numerical characters entered`() {
-        val validator = Validator()
         val stringValue = "uuu"
 
         assertThrows<IllegalArgumentException> {
@@ -18,11 +19,30 @@ class ValidatorTest {
 
     @Test
     fun `no exception when numerical characters entered`() {
-        val validator = Validator()
         val stringValue = "9"
 
         assertDoesNotThrow {
             validator.validateStringToInt(stringValue)
+        }
+    }
+
+    @Test
+    fun `throws an exception when bonus number duplicate with winning numbers`() {
+        val bonusNumber = "4"
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+
+        assertThrows<IllegalArgumentException> {
+            validator.validateBonusNumber(bonusNumber, winningNumbers)
+        }
+    }
+
+    @Test
+    fun `no exception when bonus number not duplicate with winning numbers`() {
+        val bonusNumber = "10"
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+
+        assertDoesNotThrow {
+            validator.validateBonusNumber(bonusNumber, winningNumbers)
         }
     }
 }
