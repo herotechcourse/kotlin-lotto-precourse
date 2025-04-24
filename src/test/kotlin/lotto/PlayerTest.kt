f6cb8ceb05468d2c0c9c8d399d64afe86842184e
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-04-24 15:56:13 +0200

    refac(PlayerTest): delete unused import

--- src/test/kotlin/lotto/PlayerTest.kt
+++ src/test/kotlin/lotto/PlayerTest.kt
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class PlayerTest {
