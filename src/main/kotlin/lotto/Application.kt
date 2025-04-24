38bb80615ea53f7dc0b94772e917a0d6721828f0
hyeonjin-dot <guswls_2610@naver.com>
2025-04-24 21:02:05 +0900

    docs(readme): add initial feature list for lotto

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,16 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: Implement the program
+    println("Please enter the purchase amount.")
+    val money = Console.readLine().toIntOrNull()
+
+    // 가격에 맞게 티켓 구매
+
+    println("Please enter last week's winning numbers.")
+    val numbs = Console.readLine()
+
+    println("Please enter the bonus number.")
+    val bonus = Console.readLine().toIntOrNull()
 }
