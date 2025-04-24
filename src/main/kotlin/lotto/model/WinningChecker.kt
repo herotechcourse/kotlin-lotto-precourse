1bd2ed3eed799c4c02beb448890536c385a5b031
songpink <jejeje04@naver.com>
2025-04-25 00:00:38 +0900

    refactor: separate the method

--- src/main/kotlin/lotto/model/WinningChecker.kt
+++ src/main/kotlin/lotto/model/WinningChecker.kt
@@ -8,13 +8,12 @@ class WinningChecker {
         winningLotto: Lotto,
         bonusNumber: Int,
     ): Map<Rank, Int> {
-        val countOfRanks = userLottos.mapNotNull { rankOf(it, winningLotto, bonusNumber) }
-            .groupingBy { it }.eachCount().toMutableMap()
-        for (rank in listOf(Rank.FIFTH, Rank.FOURTH, Rank.THIRD, Rank.SECOND, Rank.FIRST)) {
-            if (rank !in countOfRanks.keys) {
-                countOfRanks[rank] = 0
-            }
-        }
+        val countOfRanks = userLottos
+            .mapNotNull { rankOf(it, winningLotto, bonusNumber) }
+            .groupingBy { it }
+            .eachCount()
+            .toMutableMap()
+        fillMissingRanks(countOfRanks)
         return countOfRanks
     }
 
@@ -26,6 +25,14 @@ class WinningChecker {
             4 -> Rank.FOURTH
             3 -> Rank.FIFTH
             else -> null
+        }
+    }
+
+    private fun fillMissingRanks(countOfRanks: MutableMap<Rank, Int>) {
+        for (rank in listOf(Rank.FIFTH, Rank.FOURTH, Rank.THIRD, Rank.SECOND, Rank.FIRST)) {
+            if (rank !in countOfRanks.keys) {
+                countOfRanks[rank] = 0
+            }
         }
     }
 }
