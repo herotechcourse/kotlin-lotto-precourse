016fd3ab87469f113837a23071157bd3bbba851a
kkonii <119216472+kkonii@users.noreply.github.com>
2025-04-25 00:38:02 +0900

    refactor(test): rename test method to clarify intent

--- src/test/kotlin/lotto/domain/PurchaseAmountTest.kt
+++ src/test/kotlin/lotto/domain/PurchaseAmountTest.kt
@@ -11,23 +11,21 @@ class PurchaseAmountTest {
     @Nested
     inner class ValidationTest {
         @ParameterizedTest
-        @DisplayName("[Exception] when purchase amount input exceed maximum limit, throw exception")
         @ValueSource(ints = [150000, 500000, 100001])
-        fun `test purchase amount range`(input: Int) {
+        fun `throw when purchase amount input exceed maximum limit`(input: Int) {
             Assertions.assertThatIllegalArgumentException().isThrownBy { PurchaseAmount(input) }
         }
 
         @ParameterizedTest
-        @DisplayName("[Exception] throw when purchase amount is not a valid unit")
         @ValueSource(ints = [10800, 5500, 3115])
-        fun `test purchase amount unit`(input: Int) {
+        fun `throw when purchase amount is not a valid unit`(input: Int) {
             Assertions.assertThatIllegalArgumentException().isThrownBy { PurchaseAmount(input) }
         }
     }
 
     @Test
     @DisplayName("[Success] when 3,000 for purchase amount, lottery ticket number is 3")
-    fun `test calculation ticket number`() {
+    fun `calculate number of the lottery ticket safely`() {
         val purchaseAmount = PurchaseAmount(3_000)
         org.junit.jupiter.api.Assertions.assertEquals(purchaseAmount.toTicketNumber(), 3)
     }
