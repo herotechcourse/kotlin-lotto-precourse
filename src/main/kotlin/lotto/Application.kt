837a5a5396ee1b82ccbdf7ae9b7235ad34e2c352
Daria Eliseeva <elisey422das@gmail.com>
2025-04-24 14:06:34 +0200

    feat: collect and validate user's input for purchase amount

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,23 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-   println("hello Artem!")
+    println("Please enter the purchase amount.")
+    val sumOfMoney = Console.readLine()?.toIntOrNull()
+        ?: throw IllegalArgumentException("The purchase amount should be an integer.")
+    val numberOfTickets = getNumberOfTickets(sumOfMoney)
+    validateSumOfMoney(sumOfMoney)
+    println("You have purchased $numberOfTickets tickets.")
+}
+
+fun getNumberOfTickets(sum: Int): Int {
+    return sum / 1000
+}
+
+fun validateSumOfMoney(sum: Int) {
+    if (sum < 1000) {
+        throw IllegalArgumentException("The amount must be divisible by 1,000")
+    }
+
 }
