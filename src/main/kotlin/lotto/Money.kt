336f5e918205211ac809799958533f6ad434cead
speculatingwook <bwook9908@gmail.com>
2025-04-24 16:34:26 +0900

    feat(money): calculate how many lottery tickets can be purchased with the given amount
    - implement Money class to calculate lottery tickets count
    - add unit tests for valid and invalid input cases

---
+++ src/main/kotlin/lotto/Money.kt
@@ -0,0 +1,22 @@
+package lotto
+
+import lotto.InputHandler.Companion.INPUT_NOT_DIVIDABLE
+import lotto.InputHandler.Companion.LOTTO_PRICE
+import lotto.InputHandler.Companion.NUMBER_NOT_POSITIVE
+
+class Money(private val amount: Int) {
+    init {
+        require(checkDivisibility(amount)) {
+            INPUT_NOT_DIVIDABLE
+        }
+        require(checkPositive(amount)) {
+            NUMBER_NOT_POSITIVE
+        }
+    }
+
+    val ticketCount: Int = amount / LOTTO_PRICE
+
+    private fun checkDivisibility(purchaseAmount: Int): Boolean = purchaseAmount % LOTTO_PRICE == 0
+    private fun checkPositive(purchaseAmount: Int): Boolean = purchaseAmount > 0
+
+}
