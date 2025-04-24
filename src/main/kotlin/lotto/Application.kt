c29aad5912cc4e0b42d550cd7d4f1a25d961c8e3
Arber Totraku <arbertotraku@gmail.com>
2025-04-24 19:06:31 +0200

    feat(app): implement main entry to run lotto game

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,18 @@
 package lotto
 
 fun main() {
-    // TODO: Implement the program
+    val amount = InputView.readPurchaseAmount()
+    val count = amount / 1000
+    val tickets = LottoMachine.generateTickets(count)
+
+    OutputView.printTickets(tickets)
+
+    val winningNumbers = InputView.readWinningNumbers()
+    val bonus = InputView.readBonusNumber(winningNumbers)
+
+    val winningLotto = WinningLotto(winningNumbers, bonus)
+    val result = LottoResult.calculate(tickets, winningLotto)
+
+    OutputView.printResult(result)
+    OutputView.printReturnRate(result, amount)
 }
