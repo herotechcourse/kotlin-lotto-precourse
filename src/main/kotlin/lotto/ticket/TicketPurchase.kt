c57636842b592bd32ff315236def90f2259b3d0c
Santiago Tena Hernandez <stenahe@gmail.com>
2025-04-24 20:53:56 +0200

    refactor(ticket purchase): rename

--- src/main/kotlin/lotto/ticket/TicketPurchase.kt
+++ src/main/kotlin/lotto/ticket/TicketPurchase.kt
@@ -1,14 +1,15 @@
 package lotto.ticket
 
 import lotto.TICKET_COST
+import lotto.UserInput
 
 class TicketPurchase {
-    var purchasedTickets: Int = 0
+    var amountOfTicketsBought: Int = 0
     private set
     var tickets: MutableSet<Ticket> = mutableSetOf()
     private set
 
-    fun setPurchasedTickets(purchaseAmount: Int) {
-        this.purchasedTickets = purchaseAmount.floorDiv(TICKET_COST)
+    fun buyTickets(userInput: UserInput) {
+        this.amountOfTicketsBought = userInput.purchaseAmount.floorDiv(TICKET_COST)
     }
 }
