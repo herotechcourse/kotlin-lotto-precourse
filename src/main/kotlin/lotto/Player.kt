958c36061e82da4990275c71c543860036bb6934
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-04-24 12:47:00 +0200

    feat(Player): add check for amount
    check if amount is enough to by at least one ticket and throw IllegalArgumentException otherwise
    check if amount is not too high - this condition should be reviewed in the future, but for now is set to a resonable value (100000) so program doesn't crash or takes too long too execute

--- src/main/kotlin/lotto/Player.kt
+++ src/main/kotlin/lotto/Player.kt
@@ -4,6 +4,10 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class Player(private val amount: Int) {
 
+    init {
+        require(amount >= 1000) { "[ERROR] That's enough money to by a ticket :( \n Each ticket costs 1000 KRW." }
+        require(amount <= 100000) {"[ERROR] Wow, that's too much to spend in Lotto tickets. \n Enter an amount less than 100000 KRW"}
+    }
     val numberOfTickets: Int = amount / 1000
 
     val tickets: List<Lotto> by lazy {
