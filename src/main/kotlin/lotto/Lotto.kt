7dd035d982147a3221e9a6906212c979720ff5aa
knopgm <knop.gm@gmail.com>
2025-04-24 14:20:49 +0200

    feat(Lotto): add more requirements to check if the list of numbers are matching the rules

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,10 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.distinct().size == numbers.size) {
+            "[ERROR] Lotto must not contain duplicate numbers."
+        }
+        require(numbers.all { it in 1..45 }) { "[ERROR] All numbers must be between 1 and 49." }
     }
 
     // TODO: Implement additional functions
