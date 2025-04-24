3a7ce0d174977082cadcfcd56df48c390c252021
YongLeeCode <yonghwan311@gmail.com>
2025-04-24 23:13:23 +0900

    test(input): update tests for purchase amount validation

--- src/test/kotlin/lotto/input/MoneyInputTest.kt
+++ src/test/kotlin/lotto/input/MoneyInputTest.kt
@@ -1,8 +1,9 @@
 package lotto.input
 
-import lotto.Lotto
+import lotto.InputView
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 
 /**
  * @packageName    : lotto
@@ -13,22 +14,42 @@ import org.junit.jupiter.api.assertThrows
  */
 class MoneyInputTest {
     @Test
-    fun `should pass when purchase amount is divisible by 1000 and positive`() {
+    fun `should return correct number of tickets when purchase amount is divisible by 1000 and positive`() {
+        // given
+        val amount: Int = GOOD_AMOUNT_FOR_PURCHASING_TICKET
 
+        // when
+        val result = InputView.purchaseAmountInput(amount)
+
+        // then
+        assertEquals(5, result)
     }
 
     @Test
     fun `should throw exception when purchase amount is not divisible by 1000`() {
+        // given
+        val amount: Int = INVALID_AMOUNT_FOR_PURCHASING_TICKET
 
+        // when & then
+        assertThrows(IllegalArgumentException::class.java) {
+            InputView.purchaseAmountInput(amount)
+        }
     }
 
     @Test
     fun `should throw exception when purchase amount is negative`() {
+        // given
+        val amount: Int = NEGATIVE_AMOUNT
 
+        // when & then
+        assertThrows(IllegalArgumentException::class.java) {
+            InputView.purchaseAmountInput(amount)
+        }
     }
 
-    @Test
-    fun `should throw exception when purchase amount is not a number`() {
-
+    companion object {
+        private const val GOOD_AMOUNT_FOR_PURCHASING_TICKET = 5000
+        private const val INVALID_AMOUNT_FOR_PURCHASING_TICKET = 4500
+        private const val NEGATIVE_AMOUNT = -1000
     }
 }
