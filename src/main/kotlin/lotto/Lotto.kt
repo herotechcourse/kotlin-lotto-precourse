da8cfc1c826371caafd9e51e468ff0deb49cded4
songpink <jejeje04@naver.com>
2025-04-24 21:46:12 +0900

    refactor: separate constants

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,10 +2,22 @@ package lotto
 
 class Lotto(private val _numbers: List<Int>) {
     init {
-        require(_numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
-        require(_numbers.toSet().size == _numbers.size) { "[ERROR] Lotto must contain unique numbers." }
-        require(_numbers.all { it in 1..45 }) { "[ERROR] All numbers must be in the range of 1 to 45." }
+        require(_numbers.size == LOTTO_NUMBER_SIZE) {
+            "[ERROR] Lotto must contain exactly ${LOTTO_NUMBER_SIZE} numbers."
+        }
+        require(_numbers.toSet().size == _numbers.size) {
+            "[ERROR] Lotto must contain unique numbers."
+        }
+        require(_numbers.all { it in 1..45 }) {
+            "[ERROR] All numbers must be in the range of ${MINIMUM_LOTTO_NUMBER} to ${MAXIMUM_LOTTO_NUMBER}."
+        }
     }
 
     val numbers get() = _numbers.toList()
+
+    companion object {
+        const val LOTTO_NUMBER_SIZE = 6
+        const val MINIMUM_LOTTO_NUMBER = 1
+        const val MAXIMUM_LOTTO_NUMBER = 45
+    }
 }
