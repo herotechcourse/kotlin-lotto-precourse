55cc8bff2269c142517d7c2c19b0cbc3e82a8ee3
Hyunjun <hyunjun1994@hanyang.ac.kr>
2025-04-24 22:58:43 +0900

    feat(lotto): update Lotto class

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,29 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.toSet().size == 6) { "[ERROR] Lotto numbers must not contain duplicates." }
+        require(numbers.all { it in 1 until 45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
     }
 
-    // TODO: Implement additional functions
+    fun toRank(winningNumbers: List<Int>, bonusNumber: Int): Rank? {
+        val matchCount = matchCountFrom(winningNumbers)
+        val isBonusMatched = numbers.contains(bonusNumber)
+
+        return Rank.from(matchCount, isBonusMatched)
+    }
+
+    private fun matchCountFrom(winningNumbers: List<Int>): Int {
+        return numbers.union(winningNumbers).count()
+    }
+
+    companion object {
+        fun create(): Lotto {
+            val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+            return try { Lotto(numbers) } catch (e: IllegalArgumentException) { create() }
+        }
+    }
 }
