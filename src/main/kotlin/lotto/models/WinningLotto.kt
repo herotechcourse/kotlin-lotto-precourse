1465b7d3ae35aaadd0059d7a85ab93644fa9b926
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-04-24 11:11:14 +0200

    refactor: rename winningNumbers to ticket

--- src/main/kotlin/lotto/models/WinningLotto.kt
+++ src/main/kotlin/lotto/models/WinningLotto.kt
@@ -3,12 +3,12 @@ package lotto.models
 import lotto.models.Lotto
 
 class WinningLotto(
-    val winningNumbers: Lotto,
+    val ticket: Lotto,
     val bonusNumber: Int
 ) {
     init {
         require(bonusNumber in 1..45) { "[ERROR] Bonus number must be between 1 and 45." }
-        require(bonusNumber !in winningNumbers.getNumbers()) {
+        require(bonusNumber !in ticket.getNumbers()) {
             "[ERROR] Bonus number must not be one of the winning numbers."
         }
     }
