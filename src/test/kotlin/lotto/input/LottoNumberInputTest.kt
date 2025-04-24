8cc5a6ee0e4997d0febd438a8b4d1a7aaff664ba
YongLeeCode <yonghwan311@gmail.com>
2025-04-24 22:17:28 +0900

    test(input): add validation tests for lotto number input
    test cases:
    - exactly 6 numbers required
    - numbers must be between 1 and 45
    - all numbers must be unique
    - bonus number must not be duplicated and within range

---
+++ src/test/kotlin/lotto/input/LottoNumberInputTest.kt
@@ -0,0 +1,38 @@
+package lotto.input
+
+import lotto.Lotto
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class LottoNumberInputTest {
+    @Test
+    fun `should create Lotto when given 6 unique numbers between 1 and 45`() {
+
+    }
+
+    @Test
+    fun `should throw exception when less than 6 numbers are given`() {
+
+    }
+
+    @Test
+    fun `should throw exception when duplicate numbers are given`() {
+
+    }
+
+    @Test
+    fun `should throw exception when numbers are out of range`() {
+
+    }
+
+    @Test
+    fun `should throw exception when bonus number is out of range`() {
+
+    }
+
+    @Test
+    fun `should throw exception when bonus number is duplicated with main numbers`() {
+
+    }
+}
