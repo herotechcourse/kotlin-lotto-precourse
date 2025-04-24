4d6c66b6471d4a6e4f3639fe78e8d6d6173a6b84
geonheey <ghyjas0704@gmail.com>
2025-04-24 20:53:43 +0900

    refactor(util): Implement InputValidator for input validation logic

--- src/main/kotlin/lotto/util/Validator.kt
+++
@@ -1,8 +0,0 @@
-package lotto.util
-
-object Validator {
-
-    fun validateNumberRange(numbers: List<Int>) {
-        require(numbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
-    }
-}
