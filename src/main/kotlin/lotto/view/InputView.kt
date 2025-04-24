856215ef26308b516c3b4f9a53d1cb36697f0aa6
Santiago Tena Hernandez <stenahe@gmail.com>
2025-04-24 16:56:28 +0200

    revert(view): move files

---
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -0,0 +1,26 @@
+package lotto.view
+
+import camp.nextstep.edu.missionutils.Console
+import lotto.UserInput
+
+class InputView {
+    companion object {
+        fun promptPurchaseAmount(userInput: UserInput) {
+            println("Please enter the purchase amount.")
+            val pruchaseAmount = Console.readLine()
+            userInput.setPurchaseAmount(pruchaseAmount)
+        }
+
+        fun promptWinningNumbers(userInput: UserInput) {
+            println("Please enter last week's winning numbers.")
+            val winningNumbers = Console.readLine()
+            userInput.setWinningNumbers(winningNumbers)
+        }
+
+        fun promptBonusNumber(userInput: UserInput) {
+            println("Please enter the bonus number.")
+            val bonusNumber = Console.readLine()
+            userInput.setBonusNumber(bonusNumber)
+        }
+    }
+}
