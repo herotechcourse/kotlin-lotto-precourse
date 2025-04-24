b96724a29f5943ccb205fe391a23ce6681ef198e
gokcesen <sengkce@gmail.com>
2025-04-24 13:43:38 +0200

    feat(lotto): add pre-validation and a getter function to Lotto class

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,12 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.size == numbers.toSet().size) {"[ERROR] All numbers must be unique"}
+        require(numbers.all { it in 1..45 }) {"[ERROR] All numbers must be between 1 and 45"}
     }
 
     // TODO: Implement additional functions
+    fun getTicketNumbers(): List<Int> {
+        return numbers
+    }
 }
