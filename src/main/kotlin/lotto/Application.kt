bb54eb2a1169da2f9c02bdb53d23d6963be7eaf2
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-04-24 11:11:51 +0200

    feat: add winner comparing logic to main

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,20 +3,24 @@ package lotto
 import lotto.view.InputView
 import lotto.view.OutputView
 import lotto.service.LottoManager
+import lotto.models.WinningLotto
+import lotto.models.Lotto
 
 fun main() {
-    // TODO: Implement the program
+    val outputView = OutputView()
+
     val purchasedAmount = InputView.getPurchaseAmount();
 
     val ticketAmount = purchasedAmount / 1000
 
     val tickets = LottoManager.generateMultipleTickets(ticketAmount)
 
-    val outputView = lotto.view.OutputView()
     outputView.displayPurchasedTicketsInfo(tickets)
 
     val winningNumbers = InputView.getWinningNumbers()
     val bonusNumber = InputView.getBonusNumber()
 
-    
+    val winningTicket = WinningLotto(Lotto(winningNumbers), bonusNumber)
+
+    val rankCount = LottoManager.compareTickets(tickets, winningTicket)
 }
