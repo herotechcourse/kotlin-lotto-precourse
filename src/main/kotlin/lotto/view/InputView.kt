ad20a4ab297d388972f3b7364a49d8fd2e9423c7
moonseonghui <msh9980@naver.com>
2025-04-24 23:25:09 +0900

    fix: handle unexpected EOF error in InputView promptAndReadLine

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -5,6 +5,10 @@ import camp.nextstep.edu.missionutils.Console
 object InputView {
     fun promptAndReadLine(promptMessage: String): String {
         println(promptMessage)
-        return Console.readLine()
+        return try {
+            Console.readLine()
+        } catch (e: NoSuchElementException) {
+            throw IllegalStateException("[ERROR] Unexpected end of input (EOF).")
+        }
     }
 }
