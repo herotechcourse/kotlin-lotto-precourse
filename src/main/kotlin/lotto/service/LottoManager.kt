2e2e6dd0ad7d873f5b4bb1e99b593d6b5ba2e8fc
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-04-24 11:10:53 +0200

    feat: add comparing methods

--- src/main/kotlin/lotto/service/LottoManager.kt
+++ src/main/kotlin/lotto/service/LottoManager.kt
@@ -1,6 +1,8 @@
 package lotto.service
 
 import lotto.models.Lotto
+import lotto.models.RankEnum
+import lotto.models.WinningLotto
 import camp.nextstep.edu.missionutils.Randoms
 
 object LottoManager {
@@ -11,5 +13,17 @@ object LottoManager {
 
     fun generateMultipleTickets(amount: Int): List<Lotto> {
         return List(amount) { generateLottoTicket() }
+    }
+
+    fun compareTicket(userTicket: Lotto, winningTicket: WinningLotto): RankEnum {
+        val matchCount = userTicket.getNumbers().count { it in winningTicket.ticket.getNumbers() }
+        val matchBonus = userTicket.getNumbers().contains(winningTicket.bonusNumber)
+        
+        return RankEnum.from(matchCount, matchBonus)
+    }   
+
+    fun compareTickets(userTickets: List<Lotto>, winningTicket: WinningLotto): Map<RankEnum, Int> {
+        return userTickets.groupingBy { compareTicket(it, winningTicket) }
+            .eachCount()
     }
 }
