bb3adebee6112e863737b5191ac11ab7748d7af3
YuGyeong98 <kuk3097@naver.com>
2025-04-24 16:07:53 +0900

    feat(IssuedTicket): add get count, get random unique numbers function

--- src/main/kotlin/lotto/IssuedTicket.kt
+++ src/main/kotlin/lotto/IssuedTicket.kt
@@ -8,4 +8,12 @@ class IssuedTicket(
     init {
         validatePurchaseAmount(purchaseAmount)
     }
+
+    fun getCount() = purchaseAmount / TICKET_COST
+
+    fun getRandomUniqueNumbers() = randomGenerator.nextSortedUniqueNumbers()
+
+    companion object {
+        private const val TICKET_COST = 1000
+    }
 }
