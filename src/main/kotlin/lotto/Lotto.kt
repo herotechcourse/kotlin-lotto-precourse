dab2f410fb91ef1f5769c8a089eb1add255a442d
Alex <alexis.mar.schmidt@gmail.com>
2025-04-24 16:41:11 +0200

    feat(lotto): add validation to init block
    - check that numbers are not repeated
    - check that number are not out of range 1-45

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,6 +6,12 @@ Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6) {
             String.format(Messages.ERROR, Messages.ERROR_NOT_SIX_NUMBERS)
         }
+        require(numbers.toSet().size == numbers.size) {
+            String.format(Messages.ERROR, Messages.ERROR_NOT_UNIQUE)
+        }
+        require(numbers.all { it in 1..45 }) {
+            String.format(Messages.ERROR, Messages.ERROR_OUT_OF_RANGE)
+        }
     }
 
     fun getNumbers(): List<Int> {
