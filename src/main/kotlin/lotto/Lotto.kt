c1adb5eaadd0677d9e8edc5f21330068aa8cf650
Arber Totraku <arbertotraku@gmail.com>
2025-04-24 19:01:18 +0200

    feat(lotto): create Lotto model with number validation

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,12 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.distinct().size == 6) { "[ERROR] Lotto numbers must be unique." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
     }
 
-    // TODO: Implement additional functions
+    fun matchCount(winningNumbers: List<Int>) = numbers.count { it in winningNumbers }
+    fun containsBonus(bonus: Int) = numbers.contains(bonus)
+    fun getNumbers(): List<Int> = numbers.sorted()
 }
+
