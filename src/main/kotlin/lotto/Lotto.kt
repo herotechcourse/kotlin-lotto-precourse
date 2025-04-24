2140724dcfc96e37ed15a19653c0ed6eeee2c344
atash1986 <msc.ashourzadeh@gmail.com>
2025-04-24 21:07:54 +0430

    feat(lotto): get a list of unique numbers for a lotto card
    - there's a static function in the lotto class that creates 6 unique random numbers between 1 and 45
    - it sorts the final results and output it
    - a toString method was developed for the lotto class
    - the lotto validation lines were updated

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,26 @@
 package lotto
 
+import kotlin.random.Random
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.distinct().size == 6) { "[ERROR] Lotto numbers must be unique." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
     }
 
-    // TODO: Implement additional functions
+    override fun toString():String {
+        return "$numbers"
+    }
+
+    companion object {
+        fun getUniqueNumbers(): List<Int> {
+            val numbers = mutableSetOf<Int>()
+            while (numbers.size < 6) {
+                val randomNumber = Random.nextInt(1, 46)
+                numbers.add(randomNumber)
+            }
+            return numbers.toList().sorted()
+        }
+    }
 }
