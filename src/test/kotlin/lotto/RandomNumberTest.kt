773983e21c888cb3754d25070939c2b294313a91
YongLeeCode <yonghwan311@gmail.com>
2025-04-24 22:23:40 +0900

    test(random-number): define tests for random number range, count, and uniqueness
    test cases:
    - numbers are between 1 and 45
    - 6 numbers are generated
    - no duplicates are present

---
+++ src/test/kotlin/lotto/RandomNumberTest.kt
@@ -0,0 +1,20 @@
+package lotto
+
+import org.junit.jupiter.api.Test
+
+class RandomNumberTest {
+    @Test
+    fun `should contain numbers only within the range 1 to 45`() {
+
+    }
+
+    @Test
+    fun `should contain exactly 6 numbers`() {
+
+    }
+
+    @Test
+    fun `should not contain duplicate numbers`() {
+
+    }
+}
