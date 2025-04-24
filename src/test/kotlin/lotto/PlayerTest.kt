d06f37b674de5edac255dfa4738e4a8725ee4140
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-04-24 15:51:13 +0200

    test(Player): add tests for amount check
    create tests to guarantee the Player class trhows IllegalArgumentException when the input is less than 1000 or more than 100000

--- src/test/kotlin/lotto/PlayerTest.kt
+++ src/test/kotlin/lotto/PlayerTest.kt
@@ -5,14 +5,32 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class PlayerTest {
 
-    val player = Player(10000)
+
+    @ParameterizedTest
+    @ValueSource(ints = [100,999,-3,0])
+    fun `throws an exception when amount is less than 1000`(input: Int) {
+        assertThrows<IllegalArgumentException> {
+            Player(input)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [100001,9999999, Int.MAX_VALUE])
+    fun `throws an exception when amount is more than 100000`(input: Int) {
+        assertThrows<IllegalArgumentException> {
+            Player(input)
+        }
+    }
 
     @Test
     fun `Create ticket list with size equal to number of tickets`() {
-       val result = player.tickets
+        val player = Player(10000)
+        val result = player.tickets
         assertThat(result).hasSize(10)
     }
+
 }
