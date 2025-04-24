b326ade35333611f3afeb9ba17db32da91ac3096
YuGyeong98 <kuk3097@naver.com>
2025-04-24 19:11:30 +0900

    feat(Application): implement lotto program

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,13 @@
 package lotto
 
 fun main() {
-    // TODO: Implement the program
+    val lottoTicketMachine = LottoTicketMachine(Input(), Validator(), Output())
+
+    val purchaseAmount = lottoTicketMachine.enterPurchaseAmount()
+    val issuedTickets = lottoTicketMachine.issueTickets(purchaseAmount)
+    val winningNumbers = lottoTicketMachine.enterWinningNumbers()
+    val bonusNumber = lottoTicketMachine.enterBonusNumber()
+
+    val lotto = Lotto(winningNumbers)
+    lottoTicketMachine.winningStatistics(lotto, issuedTickets, bonusNumber, purchaseAmount)
 }
