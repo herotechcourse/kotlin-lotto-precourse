41530d58c76b0d4d90ecbc57c3b440084b8c10f6
Petra Bencze <petra.bncz@gmail.com>
2025-04-25 13:09:09 +0200

    refactor(Ticket): remove bonus number, add sorted List

--- src/main/kotlin/lotto/Ticket.kt
+++ src/main/kotlin/lotto/Ticket.kt
@@ -2,8 +2,9 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
 
-class Ticket(val bonusNumber: Int) {
-    val numbers: List<Int>? = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+class Ticket() {
+    val numbers: List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+    val sortedNumbers: MutableList<Int> = numbers.sorted().toMutableList()
     // Property declarations
     // Initializer blocks
     // Secondary constructors
