4192b0248819abacf717fb65be14922300d6706b
Petra Bencze <petra.bncz@gmail.com>
2025-04-25 21:38:00 +0200

    refactor(Purchase, Lotto): implement additional checks
    check for bonus number range (Lotto) and for amount (Purchase)

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -7,12 +7,13 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
         require(numbers.distinct().size == numbers.size) { "[ERROR] Lotto must contain unique numbers." }
         numbers.forEach {
-            number -> require(number in 1..45) { "[ERROR] Lotto must contain numbers in range 1..45."}
+            number -> require(number in 1..45) { "[ERROR] Lotto must contain numbers in range 1..45." }
         }
     }
 
     fun addBonusNumber(number: Int) {
         require(number !in numbers) { "[ERROR] Bonus number has to be different than winning numbers." }
+        require(number in 1..45) { "[ERROR] Bonus number must be in range 1..45." }
         bonusNumber = number
     }
 
--- src/main/kotlin/lotto/Purchase.kt
+++ src/main/kotlin/lotto/Purchase.kt
@@ -3,6 +3,13 @@ package lotto
 class Purchase(val amount: Int) {
     val tickets: List<Ticket> = List(amount / TICKET_PRICE) { Ticket() }
 
+    init {
+        require(amount % TICKET_PRICE == 0) {
+            "[ERROR] Only purchase amount divisible" +
+                    " by $TICKET_PRICE is accepted."
+        }
+    }
+
     companion object {
         private const val TICKET_PRICE = 1000
     }
