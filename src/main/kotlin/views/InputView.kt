36a6eb8f81ffadfaeb0501f77602bcce6e25b5ab
atash1986 <msc.ashourzadeh@gmail.com>
2025-04-26 00:37:25 +0330

    fix(InputView): wrap all of input view functions with a try-catch to print the errors

--- src/main/kotlin/views/InputView.kt
+++ src/main/kotlin/views/InputView.kt
@@ -3,47 +3,65 @@ package views
 import camp.nextstep.edu.missionutils.Console
 
 class InputView {
-    val errPrefix = "[ERROR]"
     fun readLineFromConsole(): String? {
         return Console.readLine()
     }
 
+    fun printError(e: Exception) {
+        println("[ERROR] ${e.message}.")
+    }
+
     fun getPurchaseAmount():Int {
-        println()
-        println("Please Enter Purchase amount for lottery tickets:")
-        val input=readLineFromConsole()
-        val purchaseAmount = input?.toIntOrNull() ?: throw IllegalArgumentException("$errPrefix Invalid Purchase Amount Number")
-        if (purchaseAmount >= 0 && purchaseAmount % 1000 == 0) {
-            return purchaseAmount
+        try {
+            println()
+            println("Please Enter Purchase amount for lottery tickets:")
+            val input=readLineFromConsole()
+            val purchaseAmount = input?.toIntOrNull() ?: throw IllegalArgumentException("Invalid Purchase Amount Number")
+            if (purchaseAmount >= 0 && purchaseAmount % 1000 == 0) {
+                return purchaseAmount
+            }
+            throw IllegalArgumentException("Purchase Amount must be positive and dividable per 1000")
+        } catch (e: Exception) {
+            printError(e)
+            return 0
         }
-        throw IllegalArgumentException()
     }
 
     fun getWinningNumbers(): List<Int>
     {
-        println()
-        println("Please enter last week's winning numbers:")
-        val input=readLineFromConsole() ?: ""
-        val winningNumbers=input.split(',').map({ it.trim().toInt() })
-        if(winningNumbers.size < 6)
-            throw IllegalArgumentException("$errPrefix There must be exactly 6 numbers")
-        if(winningNumbers.distinct().size < 6)
-            throw IllegalArgumentException("$errPrefix There must not be repeated numbers")
-        if(!winningNumbers.all { it in 1..45 })
-            throw IllegalArgumentException("$errPrefix Each number must be between 1 and 45")
-        return winningNumbers
-    }
+        try {
+            println()
+            println("Please enter last week's winning numbers:")
+            val input=readLineFromConsole() ?: ""
+            val winningNumbers=input.split(',').map({ it.trim().toInt() })
+            if(winningNumbers.size < 6)
+                throw IllegalArgumentException("There must be exactly 6 numbers")
+            if(winningNumbers.distinct().size < 6)
+                throw IllegalArgumentException("There must not be repeated numbers")
+            if(!winningNumbers.all { it in 1..45 })
+                throw IllegalArgumentException("Each number must be between 1 and 45")
+            return winningNumbers
+        } catch (e: Exception) {
+            printError(e)
+            return emptyList()
+        }
+    } 
     
     fun getBonusNumber(): Int
     {
-        println()
-        println("Please enter the bonus number.")
-        val input=readLineFromConsole()
-        val bonusNumber = input?.toIntOrNull() ?: throw IllegalArgumentException("$errPrefix Invalid Bonus Number")
-        if (bonusNumber < 1 || bonusNumber > 45) {
-            throw IllegalArgumentException("$errPrefix Bonus number must be between 1 and 45")
-        }
-        return bonusNumber
+        try {
+            println()
+            println("Please enter the bonus number.")
+            val input=readLineFromConsole()
+            val bonusNumber = input?.toIntOrNull() ?: throw IllegalArgumentException("Invalid Bonus Number")
+            if (bonusNumber < 1 || bonusNumber > 45) {
+                throw IllegalArgumentException("Bonus number must be between 1 and 45")
+            }
+            return bonusNumber
+        } catch (e: Exception) {
+            printError(e)
+            return 0
+        }   
     }
 }
 
