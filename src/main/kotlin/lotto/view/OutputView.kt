e0d550fdd34dee3ac244e62322a122e0f5edbb17
권장순 <jsoonworld@gmail.com>
2025-04-25 15:37:21 +0900

    feat(OutputView) : add feature for input bonus number

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -21,9 +21,14 @@ class OutputView {
         println(WINNING_NUMBERS_INPUT_PROMPT)
     }
 
+    fun printBonusNumberInputPrompt() {
+        println(BONUS_NUMBER_INPUT_PROMPT)
+    }
+
     companion object {
         const val PURCHASE_LOTTO_AMOUNT_PROMPT_MESSAGE = "Please enter the purchase amount."
         const val ISSUED_LOTTO_COUNT_MESSAGE = "You have purchased %d tickets."
         const val WINNING_NUMBERS_INPUT_PROMPT = "Please enter last week's winning numbers."
+        const val BONUS_NUMBER_INPUT_PROMPT = "Please enter the bonus number."
     }
 }
