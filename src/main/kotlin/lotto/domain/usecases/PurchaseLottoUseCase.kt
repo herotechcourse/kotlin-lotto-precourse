697d15a73d445f2aa2b9c039ac0cb6f487e81383
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-04-25 19:04:44 +0200

    fix(doamin): remove unnecessary validation from Purchase usecase
    Already validated the amount when after user input directly

--- src/main/kotlin/lotto/domain/usecases/PurchaseLottoUseCase.kt
+++ src/main/kotlin/lotto/domain/usecases/PurchaseLottoUseCase.kt
@@ -7,12 +7,8 @@ class PurchaseLottoUseCase(
     private val lottoRepository: LottoRepository
 ) {
     fun run(purchaseAmount: Int): List<LottoTicket> {
-        validateAmount(purchaseAmount)
         return lottoRepository.generateLottoTickets(purchaseAmount)
     }
 
-    private fun validateAmount(amount: Int) {
-        require(amount >= 1000) { "[ERROR] Purchase amount must be at least 1,000 KRW." }
-        require(amount % 1000 == 0) { "[ERROR] Amount must be divisible by 1,000." }
-    }
+
 }
