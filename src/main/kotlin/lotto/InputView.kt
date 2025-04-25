36f980d5cc5130f43021e2f2e584a9b61e037463
Ugochi <amakomitchell5@gmail.com>
2025-04-25 21:25:43 +0200

    feat: add function to getWinnningNumbers

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -10,4 +10,14 @@ object InputView {
         return purchaseAmount?.takeIf { it > 0 && it % 1000 == 0 }
             ?: throw IllegalArgumentException("Amount must be a positive number that is divisible by 1000")
     }
+
+    fun getWinningNumbers(): Set<Int> {
+        println("Please enter last week's winning numbers (comma separated):")
+        val winningNumbersInput = Console.readLine()
+        val winningNumbers = winningNumbersInput.split(" ").map{it.toInt()}.toSet()
+        require(winningNumbers.size == 6 && winningNumbers.all { it in 1..45}) {
+            "Winning numbers must be 6 unique numbers between 1 and 45."
+        }
+        return winningNumbers
+    }
 }
