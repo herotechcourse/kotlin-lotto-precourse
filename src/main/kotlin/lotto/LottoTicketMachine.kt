e368e5e9f9671ac91760b5a7c928b471329c6cc2
soeunnPark <carol0724@naver.com>
2025-04-25 19:40:29 +0900

    refactor(lottoTicketMachine): correct parameter and return type
    - LottoTicketMachine makes random numbers(6 unique numbers in range 1~45) and return list of Lotto

--- src/main/kotlin/lotto/LottoTicketMachine.kt
+++ src/main/kotlin/lotto/LottoTicketMachine.kt
@@ -1,7 +1,13 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class LottoTicketMachine {
-    fun buyLotto(quantity: PurchaseAmountDto, ticket: List<LottoTicketDto>) : List<Lotto> {
-        return ticket.map { Lotto(it.numbers) }
+    fun buyLotto(quantity: Int) : List<Lotto> {
+        val lottoTicket : MutableList<Lotto> = mutableListOf()
+        for(i in 1..quantity) {
+            lottoTicket.add(Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)))
+        }
+        return lottoTicket
     }
 }
