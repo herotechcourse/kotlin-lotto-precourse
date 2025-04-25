1e40a1aeb6b2c77c67216a2bb6c959ba1c5d694b
jslusark <slusark.jessica@gmail.com>
2025-04-25 13:46:22 +0200

    feat(config): define universal game constants into its own file

---
+++ src/main/kotlin/lotto/Constants.kt
@@ -0,0 +1,12 @@
+package lotto
+
+/*Storing constants into their own file as they are universal configurations of the game*/
+const val TICKET_PRICE = 1000
+const val PRICE_LIMIT = 1_000_000
+
+const val MIN_RANGE = 1
+const val MAX_RANGE = 45
+
+const val TICKET_LEN = 6
+const val BONUS_LEN = 1
+const val MATCHES = 6
