833908bfc1b9f1fce0c65e9b149d9d44834d7dfc
GDMontsho <montshogoitseone@gmail.com>
2025-04-25 08:40:00 +0200

    feat(core): add LottoGame controller to handle game flow

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -1,5 +1,12 @@
 package lotto
 
 class LottoGame {
+    fun run(){
+        val amount = InputView.readPurchaseAmount()
+        val ticketCount = amount / 1000
+        OutputView.printTicketCount(ticketCount)
 
+        val tickets = LottoMachine.generateTickets(ticketCount)
+        OutputView.printTickets(tickets)
+    }
 }
