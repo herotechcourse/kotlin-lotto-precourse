c193416aeb699b83320204caee1c8394f34bee47
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-04-25 14:27:58 +0200

    feat: add logic to check if input is empty

--- src/main/kotlin/lotto/validator/InputValidator.kt
+++ src/main/kotlin/lotto/validator/InputValidator.kt
@@ -1,8 +1,15 @@
 package lotto.validator
 
 object InputValidator {
+    private fun validateNonEmpty(input: String, errorMessage: String) {
+        if (input.trim().isEmpty()) {
+            throw IllegalArgumentException(errorMessage)
+        }
+    }
 
     fun validateAmount(input: String): Int {
+        validateNonEmpty(input, "[ERROR] Input cannot be empty.")
+
         val amount = input.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] Input must be a number.")
         require(amount > 0 && amount % 1000 == 0) { "[ERROR] Amount must be divisible by 1000" }
         
@@ -10,6 +17,8 @@ object InputValidator {
     }
 
     fun validateWinningNumbers(input: String): List<Int> {
+        validateNonEmpty(input, "[ERROR] Winning Number cannot be empty.")
+
         val split = input.split(",").map { it.trim() }
         return split.map {
             it.toIntOrNull()
@@ -18,6 +27,8 @@ object InputValidator {
     }
 
     fun validateBonusNumber(input: String): Int {
+        validateNonEmpty(input, "[ERROR] Bonus Number cannot be empty.")
+
         return input.toIntOrNull()
             ?: throw IllegalArgumentException("[ERROR] Bonus number must be a number.")
     }
