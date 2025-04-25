3b20a7015f9281f0f699df419fa7bc7100b5a59a
Hanjjong <tim4974@naver.com>
2025-04-25 16:24:20 +0900

    feat: feat Lotto instance validation func

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,26 @@
 package lotto
 
+import lotto.util.Validator
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        validate(numbers);
     }
 
-    // TODO: Implement additional functions
+
+    private fun validate(numbers: List<Int>) {
+        Validator.isEqualListSize(numbers, LOTTO_NUMBER_COUNT)
+        Validator.isNotDuplicate(numbers)
+        numbers.forEach { number ->
+            Validator.isNumberWithinRange(number, LOTTO_NUMBER_MINIMUM, LOTTO_NUMBER_MAXIMUM);
+        }
+    }
+    companion object {
+        const val LOTTO_NUMBER_MINIMUM = 1
+        const val LOTTO_NUMBER_MAXIMUM = 45
+        const val LOTTO_NUMBER_COUNT = 6
+    }
+
+    fun getNumbers(): List<Int> = numbers
 }
