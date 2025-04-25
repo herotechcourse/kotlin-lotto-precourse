6158f8d4bb601c12d56a3fe059f2bf1618b685ac
Petra Bencze <petra.bncz@gmail.com>
2025-04-25 17:41:38 +0200

    chore(Application): add existing classes and functions to main for testing

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,10 +2,17 @@ package lotto
 
 fun main() {
     try {
-        InputView
-        InputView().readPurchaseAmount()
-        InputView().readWinningNumbers()
-        InputView().readBonusNumber()
+        val inputView = InputView()
+        val outputView = OutputView()
+
+        val amount = inputView.readPurchaseAmount()
+        val purchase = Purchase(amount)
+        outputView.displayTickets(purchase.tickets)
+
+        val winningNumbers = inputView.readWinningNumbers()
+        val lotto = Lotto(winningNumbers)
+        val bonusNumber = inputView.readBonusNumber()
+        lotto.addBonusNumber(bonusNumber)
     } catch (e: InputView.MaxRetryException) {
         println(e.message + " Exiting...")
         return
