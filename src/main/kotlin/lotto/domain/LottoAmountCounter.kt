c18dabfaba6f2954ef4a73b98511296265f0b1cd
권장순 <jsoonworld@gmail.com>
2025-04-25 22:24:43 +0900

    refactor(LottoAmountCounter): remove redundant code and maintain constant usage for zero check

--- src/main/kotlin/lotto/domain/LottoAmountCounter.kt
+++ src/main/kotlin/lotto/domain/LottoAmountCounter.kt
@@ -2,12 +2,13 @@ package lotto.domain
 
 class LottoAmountCounter() {
     fun count(amount: Int): Int {
-        require(amount % LOTTO_UNIT_PRICE == 0 ) { INVALID_LOTTO_AMOUNT}
-        return amount / 1000
+        require(amount % LOTTO_UNIT_PRICE == ZERO ) { INVALID_LOTTO_AMOUNT}
+        return amount / LOTTO_UNIT_PRICE
     }
 
     companion object {
         const val LOTTO_UNIT_PRICE = 1000
         const val INVALID_LOTTO_AMOUNT = "[ERROR] lotto amount must be divisible by 1,000"
+        const val ZERO = 0
     }
 }
