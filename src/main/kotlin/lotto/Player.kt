bab9f2f376118593b79b5fe117c5189543d1bf67
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-04-25 18:33:15 +0200

    refac(Player): move compation object to the end of the class

--- src/main/kotlin/lotto/Player.kt
+++ src/main/kotlin/lotto/Player.kt
@@ -3,11 +3,6 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 
 class Player(private val amount: Int) {
-    companion object {
-        const val TICKET_PRICE = 1_000
-        const val MAX_AMOUNT = 1_000_000
-    }
-
     init {
         require(amount >= TICKET_PRICE) { "[ERROR] That's enough money to by a ticket :( Each ticket costs 1000 KRW." }
         require(amount <= MAX_AMOUNT) { "[ERROR] Wow, that's too much to spend in Lotto tickets. Enter an amount less than 100000 KRW" }
@@ -29,5 +24,10 @@ class Player(private val amount: Int) {
 
     fun getAmount(): Int {
         return amount
+    }
+
+    companion object {
+        const val TICKET_PRICE = 1_000
+        const val MAX_AMOUNT = 1_000_000
     }
 }
