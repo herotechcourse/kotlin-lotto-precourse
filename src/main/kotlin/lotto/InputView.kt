6028fc39246b764e58f5c80a4e645036751f6f3e
Alex <alexis.mar.schmidt@gmail.com>
2025-04-25 11:04:01 +0200

    refactor(inputView): convert common input reader for reuse
    - replace input reading logic with a generic readInput() function
    - takes a prompt message and a validator function to improve
    code reuse

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -4,19 +4,27 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputView {
 
-    fun readPurchaseAmount():Int {
-
+    private fun <T> readInput(promptMessage: String,
+                      validator: (String) -> T): T {
         while (true) {
             try {
-                Messages.display(Messages.PURCHASE_AMOUNT)
+                Messages.display(promptMessage)
                 val input = Console.readLine()
-                return Validator.validatePurchaseAmount(input)
-            }
-            catch (e: IllegalArgumentException) {
-                Messages.display(Messages.ERROR,
+                return validator(input)
+            } catch (e: IllegalArgumentException) {
+                Messages.display(
+                    Messages.ERROR,
                     e.message ?: Messages.ERROR_INVALID_INPUT)
             }
         }
+    }
+
+    fun readPurchaseAmount(): Int {
+        return readInput(Messages.PURCHASE_AMOUNT, Validator::validatePurchaseAmount)
+    }
+
+    fun readWinnerNumbers(): List<Int> {
+        return readInput(Messages.WINNING_NUMBERS, Validator::validateWinningNumbers)
     }
 
 }
