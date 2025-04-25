55f03a7138c0fff2692d54db0d624bdb1a26210e
yeonupark <idepix@naver.com>
2025-04-25 19:41:02 +0900

    test(view): add test for printing ticket count in OutputView

--- src/test/kotlin/lotto/OutputViewTest.kt
+++ src/test/kotlin/lotto/OutputViewTest.kt
@@ -1,4 +1,23 @@
-package lotto
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import lotto.LottoMachine
+import lotto.view.OutputView
+import lotto.messages.PromptMessages
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
-class OutputViewTest {
+class OutputViewTest : NsTest() {
+
+    @Test
+    fun `prints formatted ticket count message`() {
+        assertSimpleTest {
+            OutputView().printTicketCount(LottoMachine(5000))
+            val output = output()
+            assertThat(output).contains(PromptMessages.TICKET_COUNT.with(5))
+        }
+    }
+
+    override fun runMain() {
+
+    }
 }
