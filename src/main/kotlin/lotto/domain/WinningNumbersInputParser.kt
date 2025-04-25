8d30d60aa929227bd92c86a2d51c569d710b5890
권장순 <jsoonworld@gmail.com>
2025-04-25 15:22:42 +0900

    feat(WinningNumbersInputParser) : add feature to parse winning numbers

---
+++ src/main/kotlin/lotto/domain/WinningNumbersInputParser.kt
@@ -0,0 +1,20 @@
+package lotto.domain
+
+class WinningNumbersInputParser {
+    fun parse(input: String): List<Int> {
+        val tokens = input.split(",").map { it.trim() }
+        require(tokens.size == 6) {INVALID_WINNING_NUMBERS_SIZE }
+
+        val numbers = tokens.map {
+            require(it.toIntOrNull() != null) {INVALID_NUMBER_FORMAT}
+            it.toInt()
+        }
+
+        return numbers
+    }
+
+    companion object {
+        const val INVALID_WINNING_NUMBERS_SIZE = "[ERROR] Winning numbers must contain exactly 6 values."
+        const val INVALID_NUMBER_FORMAT = "[ERROR] All winning numbers must be valid integers."
+    }
+}
