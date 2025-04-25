f13f6876059718d3196a925b55c64cda8752e402
Ugochi <amakomitchell5@gmail.com>
2025-04-26 00:33:05 +0200

    feat: Use InputView, LottoEngine, and OutputView for clean main flow

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,20 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: Implement the program
+    val amountPurchase = InputView.getPurchaseAmount()
+    val tickets = LottoEngine.buyTickets(amountPurchase)
+
+    OutputView.displayLottoTickets(tickets)
+
+    val winningNumbers = InputView.getWinningNumbers()
+    val bonusNumber = InputView.getBonusNumber(winningNumbers)
+
+    val results = LottoEngine.evaluateTicketResults(tickets, winningNumbers, bonusNumber)
+    val totalWinnings = LottoEngine.totalResultWinnings(results)
+
+    OutputView.displayLottoResults(results, amountPurchase, totalWinnings)
+
 }
