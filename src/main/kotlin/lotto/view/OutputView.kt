05c1f271eca5e424980e081f0b33c3262ab22a0b
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-04-25 16:44:05 +0200

    fix: typo in hyphen

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -16,11 +16,11 @@ class OutputView {
     fun displayResult(rankCount: Map<MatchPrize, Int>, returnRate: Double) {
         println("\nWinning Statistics")
         println("---")
-        println("3 Matches (5,000 KRW)d – ${rankCount[MatchPrize.THREE] ?: 0} tickets")
-        println("4 Matches (50,000 KRW)d – ${rankCount[MatchPrize.FOUR] ?: 0} tickets")
-        println("5 Matches (1,500,000 KRW)d – ${rankCount[MatchPrize.FIVE] ?: 0} tickets")
-        println("5 Matches + Bonus (30,000,000 KRW)d – ${rankCount[MatchPrize.FIVEWITHBONUS] ?: 0} tickets")
-        println("6 Matches (2,000,000,000 KRW)d – ${rankCount[MatchPrize.SIX] ?: 0} tickets")
+        println("3 Matches (5,000 KRW) – ${rankCount[MatchPrize.THREE] ?: 0} tickets")
+        println("4 Matches (50,000 KRW) – ${rankCount[MatchPrize.FOUR] ?: 0} tickets")
+        println("5 Matches (1,500,000 KRW) – ${rankCount[MatchPrize.FIVE] ?: 0} tickets")
+        println("5 Matches + Bonus (30,000,000 KRW) – ${rankCount[MatchPrize.FIVEWITHBONUS] ?: 0} tickets")
+        println("6 Matches (2,000,000,000 KRW) – ${rankCount[MatchPrize.SIX] ?: 0} tickets")
 
         println("Total return rate is %.1f%%.".format(returnRate))
     }
