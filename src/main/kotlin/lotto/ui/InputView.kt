7d4cd247cb85332dd5dc6c481f883d695375d2d6
Junyou Park <pjy008008@gmail.com>
2025-04-26 00:21:09 +0900

    refactor(ui): move all functions in InputView and OutputView to companion objects

--- src/main/kotlin/lotto/ui/InputView.kt
+++ src/main/kotlin/lotto/ui/InputView.kt
@@ -5,68 +5,69 @@ import lotto.Lotto
 import lotto.util.Constants
 
 class InputView {
-
-    private fun <T> readValidatedInput(prompt: String, parseAndValidate: () -> T): T {
-        while (true) {
-            println(prompt)
-            try {
-                return parseAndValidate()
-            } catch (e: IllegalArgumentException) {
-                println(e.message)
+    companion object{
+        private fun <T> readValidatedInput(prompt: String, parseAndValidate: () -> T): T {
+            while (true) {
+                println(prompt)
+                try {
+                    return parseAndValidate()
+                } catch (e: IllegalArgumentException) {
+                    println(e.message)
+                }
             }
         }
-    }
 
-    fun getPurchaseAmount(): Int {
-        return readValidatedInput("Please enter the purchase amount.") {
-            val amount = Console.readLine().toIntOrNull()
-                ?: throw IllegalArgumentException("${Constants.ERROR_PREFIX} Input must be a valid number.")
-            validatePurchaseAmount(amount)
-            amount
-        }
-    }
-
-    fun getWinningNumbers(): Lotto {
-        return readValidatedInput("\nPlease enter the last week's winning numbers.") {
-            val winningNumbers = parseWinningNumbers(Console.readLine())
-            Lotto(winningNumbers)
-        }
-    }
-
-    fun getBonusNumber(winningNumbers: Lotto): Int {
-        return readValidatedInput("\nPlease enter the bonus number."){
-            val number = Console.readLine().toIntOrNull()
-                ?: throw IllegalArgumentException("${Constants.ERROR_PREFIX} Bonus number must be number.")
-            validateBonusNumber(number, winningNumbers.getNumbers())
-            number
-        }
-    }
-
-    private fun validatePurchaseAmount(amount: Int) {
-        if (amount < Constants.MIN_PURCHASE_AMOUNT) {
-            throw IllegalArgumentException("${Constants.ERROR_PREFIX} Purchase amount must be at least ${Constants.MIN_PURCHASE_AMOUNT} KRW.")
-        }
-        if (amount % Constants.PURCHASE_AMOUNT_UNIT != 0) {
-            throw IllegalArgumentException("${Constants.ERROR_PREFIX} Purchase amount must be in units of ${Constants.PURCHASE_AMOUNT_UNIT} KRW.")
-        }
-    }
-
-    private fun parseWinningNumbers(input: String): List<Int> {
-        val numbers = input
-            .split(",")
-            .map {
-                it.trim().toIntOrNull()
-                    ?: throw IllegalArgumentException("${Constants.ERROR_PREFIX} All Lotto numbers must be numbers.")
+        fun getPurchaseAmount(): Int {
+            return readValidatedInput("Please enter the purchase amount.") {
+                val amount = Console.readLine().toIntOrNull()
+                    ?: throw IllegalArgumentException("${Constants.ERROR_PREFIX} Input must be a valid number.")
+                validatePurchaseAmount(amount)
+                amount
             }
-        return numbers
-    }
-
-    private fun validateBonusNumber(number: Int, winningNumbers: List<Int>) {
-        if (number !in 1..45) {
-            throw IllegalArgumentException("${Constants.ERROR_PREFIX} Bonus number must be between 1 and 45.")
         }
-        if (number in winningNumbers) {
-            throw IllegalArgumentException("${Constants.ERROR_PREFIX} Bonus number must not be a duplicate of the winning numbers.")
+
+        fun getWinningNumbers(): Lotto {
+            return readValidatedInput("\nPlease enter the last week's winning numbers.") {
+                val winningNumbers = parseWinningNumbers(Console.readLine())
+                Lotto(winningNumbers)
+            }
+        }
+
+        fun getBonusNumber(winningNumbers: Lotto): Int {
+            return readValidatedInput("\nPlease enter the bonus number."){
+                val number = Console.readLine().toIntOrNull()
+                    ?: throw IllegalArgumentException("${Constants.ERROR_PREFIX} Bonus number must be number.")
+                validateBonusNumber(number, winningNumbers.getNumbers())
+                number
+            }
+        }
+
+        private fun validatePurchaseAmount(amount: Int) {
+            if (amount < Constants.MIN_PURCHASE_AMOUNT) {
+                throw IllegalArgumentException("${Constants.ERROR_PREFIX} Purchase amount must be at least ${Constants.MIN_PURCHASE_AMOUNT} KRW.")
+            }
+            if (amount % Constants.PURCHASE_AMOUNT_UNIT != 0) {
+                throw IllegalArgumentException("${Constants.ERROR_PREFIX} Purchase amount must be in units of ${Constants.PURCHASE_AMOUNT_UNIT} KRW.")
+            }
+        }
+
+        private fun parseWinningNumbers(input: String): List<Int> {
+            val numbers = input
+                .split(",")
+                .map {
+                    it.trim().toIntOrNull()
+                        ?: throw IllegalArgumentException("${Constants.ERROR_PREFIX} All Lotto numbers must be numbers.")
+                }
+            return numbers
+        }
+
+        private fun validateBonusNumber(number: Int, winningNumbers: List<Int>) {
+            if (number !in 1..45) {
+                throw IllegalArgumentException("${Constants.ERROR_PREFIX} Bonus number must be between 1 and 45.")
+            }
+            if (number in winningNumbers) {
+                throw IllegalArgumentException("${Constants.ERROR_PREFIX} Bonus number must not be a duplicate of the winning numbers.")
+            }
         }
     }
 }
