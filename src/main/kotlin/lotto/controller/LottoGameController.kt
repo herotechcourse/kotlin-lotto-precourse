9e7ad01842d0ae67fca5300876de834a83393f20
Seungyeon Kim <sungyeon1227@gmail.com>
2025-04-26 04:04:59 +0900

    feat(controller): implement LottoGameController to coordinate game flow

--- src/main/kotlin/lotto/controller/LottoGameController.kt
+++ src/main/kotlin/lotto/controller/LottoGameController.kt
@@ -1,4 +1,24 @@
 package lotto.controller
 
+import lotto.domain.LottoResult
+import lotto.domain.LottoesGenerator
+import lotto.domain.WinningLotto
+import lotto.view.InputView
+import lotto.view.OutputView
+
 class LottoGameController {
+
+    fun run() {
+        val purchaseAmount = InputView.readPurchaseAmount()
+        val lottoes = LottoesGenerator.generate(purchaseAmount)
+        OutputView.printLottoes(lottoes)
+
+        val winningNumbers = InputView.readWinningNumbers()
+        val bonusNumber = InputView.readBonusNumber(winningNumbers)
+
+        val winningLotto = WinningLotto(winningNumbers, bonusNumber)
+        val result = LottoResult.of(lottoes, winningLotto)
+
+        OutputView.printResult(result, purchaseAmount)
+    }
 }
