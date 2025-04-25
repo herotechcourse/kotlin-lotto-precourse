d05b76476001bfae909907c708baf7bd7172a665
Santiago Tena Hernandez <stenahe@gmail.com>
2025-04-25 15:48:29 +0200

    fix(tickets): remove file

--- src/main/kotlin/lotto/ticket/Ticket.kt
+++
@@ -1,9 +0,0 @@
-package lotto.ticket
-
-import camp.nextstep.edu.missionutils.Randoms
-import lotto.HIGHEST_NUMBER
-import lotto.LOWEST_NUMBER
-
-class Ticket {
-    var numbers = Randoms.pickUniqueNumbersInRange(LOWEST_NUMBER, HIGHEST_NUMBER, 6)
-}
