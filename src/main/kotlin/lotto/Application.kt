1a1bccd4ac21282ce3c8c3771ff604dbbc22f9b0
Sunho12 <rhkrtjsgh1210@gmail.com>
2025-04-25 22:30:08 +0900

    chore(application): set up application entry point

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,21 @@
 package lotto
 
+import lotto.controller.LottoController
+import lotto.domain.LottoMachine
+import lotto.util.InputValidator
+import lotto.util.LottoNumberGenerator
+import lotto.view.InputView
+import lotto.view.OutputView
+
 fun main() {
-    // TODO: Implement the program
+    val inputView = InputView()
+    val outputView = OutputView()
+
+    val inputValidator = InputValidator()
+    val numberGenerator = LottoNumberGenerator()
+
+    val lottoMachine = LottoMachine(numberGenerator)
+    val controller = LottoController(inputView, outputView, lottoMachine, inputValidator)
+
+    controller.run()
 }
