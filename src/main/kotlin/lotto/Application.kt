e979ead90439f146d760c70f35d27d0d82ab7844
Fabio Niglio <fabio_niglio@hotmail.com>
2025-04-25 13:34:49 +0200

    feat: implement main program logic for lottery ticket purchase and display

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,20 @@
 package lotto
 
+import lotto.service.LottoMachine
+import lotto.view.InputView
+import lotto.view.OutputView
+
 fun main() {
     // TODO: Implement the program
+    try {
+        val amount = InputView.readPurchaeAmount()
+        val ticketCount = amount / 1000
+
+        val machine = LottoMachine()
+        val tickets = machine.generateTickets(ticketCount)
+
+        OutputView.printTickets(tickets)
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
 }
