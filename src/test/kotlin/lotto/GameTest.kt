c604e3db6d00a81eea6211d2c36592a6fb74f7d1
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-04-25 10:07:52 +0200

    test(Game): create test for getFinalPrizeAmount
    add test to check if the final prize is correctly calculated
    move some commom declarations to top level so all tests can use it

--- src/test/kotlin/lotto/GameTest.kt
+++ src/test/kotlin/lotto/GameTest.kt
@@ -5,16 +5,17 @@ import org.assertj.core.api.Assertions.assertThat
 
 class GameTest {
 
+    val winningTicket = Lotto(listOf(1, 2, 3, 4, 5, 6))
+    val bonusNumber = 7
+    val game = Game(Player(3000), winningTicket, bonusNumber)
+
     @Test
     fun `getMatches correctly pairs matches and bonus`() {
-        val winningTicket = Lotto(listOf(1, 2, 3, 4, 5, 6))
-        val bonusNumber = 7
         val tickets = listOf(
             Lotto(listOf(1, 2, 3, 4, 5, 6)),
             Lotto(listOf(1, 2, 3, 4, 5, 7)),
             Lotto(listOf(1, 3, 5, 7, 9, 11))
         )
-        val game = Game(Player(3000,), winningTicket, bonusNumber)
         val result = game.getMatches(tickets)
 
         val expectedMatches = listOf(
@@ -27,14 +28,12 @@ class GameTest {
 
     @Test
     fun `getStatistics does not count prizes incorrectly`() {
-        val winningTicket = Lotto(listOf(1, 2, 3, 4, 5, 6))
-        val bonusNumber = 7
         val finalMatches: List<Pair<Int, Boolean>> = listOf(
             Pair(3, false),
             Pair(0, true),
             Pair(5, true),
         )
-        val game = Game(Player(3000), winningTicket, bonusNumber)
+
         val result = game.getStatistics(finalMatches)
 
         val expected= listOf(
@@ -46,4 +45,20 @@ class GameTest {
         )
         assertThat(result).isEqualTo(expected)
     }
+
+    @Test
+    fun `getFinalPrizeAmount correctly`() {
+        val statistics= listOf(
+            Pair(Game.Prize.FIRST, 0),
+            Pair(Game.Prize.SECOND, 1),
+            Pair(Game.Prize.THIRD, 0),
+            Pair(Game.Prize.FOURTH, 0),
+            Pair(Game.Prize.FIFTH, 1)
+        )
+
+        val result = game.getFinalPrizeAmount(statistics)
+
+        assertThat(result).isEqualTo(30_005_000)
+    }
+    // TODO tests for getProfitRate
 }
