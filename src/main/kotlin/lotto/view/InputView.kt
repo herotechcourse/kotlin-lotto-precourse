6d5c1411ee06f0b4fd6516712e90c5d393392023
Fabio Niglio <fabio_niglio@hotmail.com>
2025-04-25 13:46:14 +0200

    feat: add methods to InputView for reading winning numbers and bonus number

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -18,4 +18,25 @@ object InputView {
         return amount
     }
 
+    fun readWinningNumbers(): List<Int> {
+        println("Please enter last week's winning numbers.")
+        val input = Console.readLine()
+
+        val numbers = input.split(",")
+            .map { it.trim() }
+            .map {
+                it.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] Please enter valid integers.")
+            }
+
+        return numbers
+    }
+
+    fun readBonusNumber(): Int {
+        println("Please enter the bonus number.")
+        val input = Console.readLine()
+
+        return input.toIntOrNull()?.takeIf { it in 1..45 }
+            ?: throw IllegalArgumentException("[ERROR] Bonus number must be an integer between 1 and 45.")
+    }
+
 }
