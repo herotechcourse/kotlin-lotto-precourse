9549030380d10291ef5df49d4b89184ee2028585
YuGyeong98 <kuk3097@naver.com>
2025-04-25 19:09:23 +0900

    test(LottoStatisticsTest): add five matches and with bonus update ticket count test function

--- src/test/kotlin/lotto/LottoStatisticsTest.kt
+++ src/test/kotlin/lotto/LottoStatisticsTest.kt
@@ -6,6 +6,28 @@ import org.junit.jupiter.api.Test
 class LottoStatisticsTest {
 
     @Test
+    fun `increase five matches prize's ticket count when match count is 5 and has bonus is false`() {
+        val lottoStatistics = LottoStatistics.of()
+        val matchCount = 5
+        val hasBonus = false
+
+        lottoStatistics.updateTicketCount(matchCount, hasBonus)
+
+        assertEquals(1, lottoStatistics.statistics[2].ticketCount)
+    }
+
+    @Test
+    fun `increase five matches with bonus prize's ticket count when match count is 5 and has bonus is true`() {
+        val lottoStatistics = LottoStatistics.of()
+        val matchCount = 5
+        val hasBonus = true
+
+        lottoStatistics.updateTicketCount(matchCount, hasBonus)
+
+        assertEquals(1, lottoStatistics.statistics[3].ticketCount)
+    }
+
+    @Test
     fun `profit rate is total prize amount divided by purchase amount percentage`() {
         val lotto = Lotto(listOf(1, 3, 4, 5, 22, 11))
         val issuedTickets = FixedIssuedTicketsGenerator.of()
