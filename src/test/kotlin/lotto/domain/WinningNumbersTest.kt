454fad489e9c886bf06630d5a7365e00b52591fd
ahyeon-github <ahyeon31@sookmyung.ac.kr>
2025-04-26 01:52:56 +0900

    test: fix factory method call from

--- src/test/kotlin/lotto/domain/WinningNumbersTest.kt
+++ src/test/kotlin/lotto/domain/WinningNumbersTest.kt
@@ -10,7 +10,7 @@ class WinningNumbersTest {
     fun `throws exception when the winning numbers are less than 6`() {
         val input = listOf(1, 2, 3, 4, 5)
         assertThrows<IllegalArgumentException> {
-            WinningNumbers.of(input)
+            WinningNumbers.from(input)
         }
     }
 
@@ -18,21 +18,21 @@ class WinningNumbersTest {
     fun `throws exception when the winning numbers contain duplicates`() {
         val input = listOf(1, 2, 3, 4, 5, 5)
         assertThrows<IllegalArgumentException> {
-            WinningNumbers.of(input)
+            WinningNumbers.from(input)
         }
     }
 
     @Test
     fun `creates valid WinningNumbers with 6 unique numbers`() {
         val input = listOf(1, 2, 3, 4, 5, 6)
-        val winningNumbers = WinningNumbers.of(input)
+        val winningNumbers = WinningNumbers.from(input)
         assertThat(winningNumbers.getNumbers().map { it.number })
             .containsExactlyInAnyOrderElementsOf(input)
     }
 
     @Test
     fun `counts how many numbers match between winning numbers and a lotto ticket`() {
-        val winning = WinningNumbers.of(listOf(1, 2, 3, 4, 5, 6))
+        val winning = WinningNumbers.from(listOf(1, 2, 3, 4, 5, 6))
         val ticket = Lotto.of(listOf(1, 2, 7, 8, 9, 10))
 
         val matchCount = winning.matchCount(ticket)
@@ -42,7 +42,7 @@ class WinningNumbersTest {
 
     @Test
     fun `checks if bonus number exists in winning numbers`() {
-        val winning = WinningNumbers.of(listOf(10, 11, 12, 13, 14, 15))
+        val winning = WinningNumbers.from(listOf(10, 11, 12, 13, 14, 15))
         val bonus = LottoNumber(12)
         val notBonus = LottoNumber(30)
 
