26413a3f682d57600ee71401505757b98ac87dc8
Petra Bencze <petra.bncz@gmail.com>
2025-04-25 16:54:03 +0200

    feat(OutputView): new OutputView clas with new display tickets function

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -1,4 +1,13 @@
 package lotto
 
 class OutputView {
+    fun displayTickets(tickets: List<Ticket>) {
+        println()
+        println("You have purchased ${tickets.size} tickets.")
+        for (ticket in tickets) {
+            print("[")
+            print(ticket.sortedNumbers.joinToString(", "))
+            println("]")
+        }
+    }
 }
