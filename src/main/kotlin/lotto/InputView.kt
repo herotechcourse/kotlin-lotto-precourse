3eb7808dd53a30b11388309b87f21488b8117cf8
GDMontsho <montshogoitseone@gmail.com>
2025-04-25 12:49:00 +0200

    feat(view): implemented additional input for winning numbers and bonus including validation

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -2,6 +2,7 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Console
 import java.lang.IllegalArgumentException
+import kotlin.IllegalArgumentException
 
 object InputView {
     fun readPurchaseAmount(): Int {
@@ -16,4 +17,37 @@ object InputView {
         }
         return  amount
     }
+}
+
+fun readWinningNumbers(): List<Int> {
+    println("Please enter the winning numbers separated by commas:")
+    val input = Console.readLine()
+    val numbers = input.split(",")
+        .mapNotNull {it.trim().toIntOrNull()}
+
+    if (numbers.size != 6 || numbers.toSet().size != 6) {
+        throw IllegalArgumentException("[ERROR] Please enter 6 unique winning numbers.")
+    }
+    if (numbers.any { it !in 1..45 }) {
+        throw IllegalArgumentException("[ERROR] Winning numbers must be between 1 and 45.")
+    }
+
+    return numbers
+}
+
+
+fun readBonusNumber (winningNumbers: List<Int>): Int {
+    println("Please enter the bonus number.")
+    val input = Console.readLine()
+    val bonus = input.toIntOrNull()
+        ?: throw IllegalArgumentException("[ERROR] The bonus number must be a valid number.")
+
+    if (bonus in winningNumbers) {
+        throw IllegalArgumentException("[ERROR] Bonus number cannot duplicate winning numbers.")
+    }
+
+    if (bonus !in 1..45) {
+        throw IllegalArgumentException("[ERROR] Bonus must be a number between 1 and 45.")
+    }
+    return bonus
 }
