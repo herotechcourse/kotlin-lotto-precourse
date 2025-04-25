c6ad5e4c66552a4075594d5c77d20f503e15057c
GDMontsho <montshogoitseone@gmail.com>
2025-04-25 09:09:16 +0200

    feat(view): implement the input and output views with validation and display

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -1,4 +1,19 @@
 package lotto
 
-class InputView {
+import camp.nextstep.edu.missionutils.Console
+import java.lang.IllegalArgumentException
+
+object InputView {
+    fun readPurchaseAmount(): Int {
+        println("Please enter the purchase amount.")
+        val input = Console.readLine()
+
+        val amount = input.toIntOrNull()
+            ?: throw IllegalArgumentException("[ERROR] Input must be a valid number")
+
+        if (amount % 10000 != 0 ) {
+            throw IllegalArgumentException("[ERROR] Amount must be divisible by 1, 000. ")
+        }
+        return  amount
+    }
 }
--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -1,4 +1,11 @@
 package lotto
 
-class OutputView {
+object OutputView {
+    fun printTicketCount(count: Int) {
+        println("You have purchased $count tickets")
+    }
+
+    fun printTickets (tickets: List <Lotto> ) {
+        tickets.forEach {println(it.getNumbers())}
+    }
 }
