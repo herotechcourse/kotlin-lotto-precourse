614fa7b6deb6364316168a675357d9d8a0c062ca
Petra Bencze <petra.bncz@gmail.com>
2025-04-25 16:42:43 +0200

    feat(Lotto): add bonus number function

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,12 +1,19 @@
 package lotto
 
 class Lotto(private val numbers: List<Int>) {
+    private var bonusNumber: Int = 0
+
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
-        require(numbers.distinct().size == numbers.size) { "[ERROR] Lotto must contain unique numbers."}
+        require(numbers.distinct().size == numbers.size) { "[ERROR] Lotto must contain unique numbers." }
         numbers.forEach {
             number -> require(number in 1..45) { "[ERROR] Lotto must contain numbers in range 1..45."}
         }
+    }
+
+    fun addBonusNumber(number: Int) {
+        require(number !in numbers) { "[ERROR] Bonus number has to be different than winning numbers." }
+        bonusNumber = number
     }
 
     // TODO: Implement additional functions
