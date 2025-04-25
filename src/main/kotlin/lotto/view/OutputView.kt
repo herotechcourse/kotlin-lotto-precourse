c7e5f47003e43d62942fb2912338e57128d0d393
Sunho12 <rhkrtjsgh1210@gmail.com>
2025-04-25 22:20:18 +0900

    feat(view): implement output printing

---
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -0,0 +1,30 @@
+package lotto.view
+
+import lotto.Lotto
+import lotto.domain.LottoResults
+import lotto.domain.PrizeRank
+
+class OutputView {
+    fun displayPurchasedTickets(ticketCnt: Int, tickets: List<Lotto>) {
+        println("\nYou have purchased ${ticketCnt} tickets.")
+
+        tickets.forEach { lotto ->
+            println(lotto.getNumbers().sorted())
+        }
+    }
+
+    fun displayWinningStatistics(results: LottoResults) {
+        println("\nWiWinning Statistics")
+        println("---")
+        println("${PrizeRank.FIFTH.matchCnt} Matches (${String.format("%,d", PrizeRank.FIFTH.prize)} KRW) - ${results.getCount(PrizeRank.FIFTH)} tickets")
+        println("${PrizeRank.FOURTH.matchCnt} Matches (${String.format("%,d", PrizeRank.FOURTH.prize)} KRW) - ${results.getCount(PrizeRank.FOURTH)} tickets")
+        println("${PrizeRank.THIRD.matchCnt} Matches (${String.format("%,d", PrizeRank.THIRD.prize)} KRW) - ${results.getCount(PrizeRank.THIRD)} KRW) - ${results.getCount(PrizeRank.THIRD)} tickets")
+        println("${PrizeRank.SECOND.matchCnt} Matches + Bonus Ball (${String.format("%,d", PrizeRank.SECOND.prize)} KRW) - ${results.getCount(PrizeRank.SECOND)} tickets")
+        println("${PrizeRank.FIRST.matchCnt} Matches (${String.format("%,d", PrizeRank.FIRST.prize)} KRW) - ${results.getCount(PrizeRank.FIRST)} tickets")
+    }
+
+    fun displayProfitRate(results: LottoResults, purchaseAmout: Int) {
+        val profitRate = results.getprofitRate(purchaseAmout)
+        println("Total return rate is ${String.format("%.1f", profitRate)}%.")
+    }
+}
