171991dcabc83f0f71aa742568cd408b06bb477f
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-04-25 15:04:13 +0200

    test: add another working test case

--- src/test/kotlin/lotto/ApplicationTest.kt
+++ src/test/kotlin/lotto/ApplicationTest.kt
@@ -42,6 +42,42 @@ class ApplicationTest : NsTest() {
         )
     }
 
+    @Test
+    fun `TEST 3 matches and 5 matches with bonus`() {
+        assertRandomUniqueNumbersInRangeTest(
+            {
+                run("9000", "1,2,3,4,5,6", "7")
+                assertThat(output()).contains(
+                    "You have purchased 9 tickets.",
+                    "[8, 21, 23, 41, 42, 43]",
+                    "[3, 5, 11, 16, 32, 38]",
+                    "[7, 11, 16, 35, 36, 44]",
+                    "[1, 8, 11, 31, 41, 42]",
+                    "[13, 14, 16, 38, 42, 45]",
+                    "[7, 11, 30, 40, 42, 43]",
+                    "[2, 13, 22, 32, 38, 45]",
+                    "[1, 3, 5, 14, 22, 45]",
+                    "[1, 2, 3, 4, 5, 7]",
+                    "3 Matches (5,000 KRW) - 1 tickets",
+                    "4 Matches (50,000 KRW) - 0 tickets",
+                    "5 Matches (1,500,000 KRW) - 0 tickets",
+                    "5 Matches + Bonus (30,000,000 KRW) - 1 tickets",
+                    "6 Matches (2,000,000,000 KRW) - 0 tickets",
+                    "Total return rate is 333388.9%.",
+                )
+            },
+            listOf(8, 21, 23, 41, 42, 43),
+            listOf(3, 5, 11, 16, 32, 38),
+            listOf(7, 11, 16, 35, 36, 44),
+            listOf(1, 8, 11, 31, 41, 42),
+            listOf(13, 14, 16, 38, 42, 45),
+            listOf(7, 11, 30, 40, 42, 43),
+            listOf(2, 13, 22, 32, 38, 45),
+            listOf(1, 3, 5, 14, 22, 45),
+            listOf(1, 2, 3, 4, 5, 7),
+        )
+    }
+
 
     @Test
     fun `EXCEPTION amount not divisible by 1000`() {
