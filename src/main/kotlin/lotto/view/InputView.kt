eec4b9eb6af315919d8923fdd75bfe39c4068b80
Jiihyun <cwlgusc@gmail.com>
2025-04-25 18:12:46 +0900

    refactor: change InputView class to object

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,7 +3,15 @@ package lotto.view
 import camp.nextstep.edu.missionutils.Console
 import lotto.util.StringParser
 
-class InputView {
+object InputView {
+
+    private const val READ_PURCHASE_AMOUNT_MESSAGE: String = "Please enter the purchase amount."
+    private const val READ_WINNING_NUMBERS_MESSAGE: String = "Please enter last week's winning numbers."
+    private const val READ_BONUS_NUMBER_MESSAGE: String = "Please enter the bonus number."
+
+    private const val WINNING_NUMBER_DELIMITER = ","
+
+    private const val INVALID_INPUT_ERROR = "[ERROR] Input value does not exist."
 
     fun readPurchaseAmount(): Int {
         println(READ_PURCHASE_AMOUNT_MESSAGE)
@@ -28,15 +36,4 @@ class InputView {
         require(input.isNotBlank()) { INVALID_INPUT_ERROR }
         return input
     }
-
-    companion object {
-        private const val READ_PURCHASE_AMOUNT_MESSAGE: String = "Please enter the purchase amount."
-        private const val READ_WINNING_NUMBERS_MESSAGE: String = "Please enter last week's winning numbers."
-        private const val READ_BONUS_NUMBER_MESSAGE: String = "Please enter the bonus number."
-
-        private const val WINNING_NUMBER_DELIMITER = ","
-
-        private const val INVALID_INPUT_ERROR = "[ERROR] Input value does not exist."
-    }
-
 }
