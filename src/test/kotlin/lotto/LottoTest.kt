1c5c727b889434b13314fa9c38de111ecd762b9f
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-04-25 12:51:52 +0200

    test: add test cases for Lotto

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -3,19 +3,41 @@ package lotto
 import lotto.models.Lotto
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.assertj.core.api.Assertions.assertThat
+
 
 class LottoTest {
     @Test
-    fun `throws an exception when lotto numbers exceed six`() {
+    fun `TEST creates Lotto with valid numbers`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        assertThat(lotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6)
+    }
+
+    @Test
+    fun `EXCEPTION lotto numbers exceed six`() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
         }
     }
 
     @Test
-    fun `throws an exception when lotto numbers contain duplicates`() {
+    fun `EXCEPTION lotto numbers less than six`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5))
+        }
+    }
+
+    @Test
+    fun `EXCEPTION lotto numbers contain duplicates`() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 5))
+        }
+    }
+
+    @Test
+    fun `EXCEPTION numbers are out of 1 to 45 range`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 46))
         }
     }
 
