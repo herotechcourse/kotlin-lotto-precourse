21d88052a8466ae408adc2886e187ac9018d2a62
kkonii <119216472+kkonii@users.noreply.github.com>
2025-04-25 19:46:24 +0900

    refactor(purchaseAmount): rename method to clarify intent

--- src/main/kotlin/lotto/domain/PurchaseAmount.kt
+++ src/main/kotlin/lotto/domain/PurchaseAmount.kt
@@ -14,7 +14,7 @@ class PurchaseAmount(private val amount: Int) {
         require(amount % PURCHASE_UNIT == 0) { "[ERROR] Purchase amount must entered as $PURCHASE_UNIT" }
     }
 
-    fun toTicketNumber(): Int = amount / PURCHASE_UNIT
+    fun toTicketAmount(): Int = amount / PURCHASE_UNIT
 
     companion object {
         private const val MAXIMUM_AMOUNT = 100_000
