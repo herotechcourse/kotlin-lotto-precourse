23f5e4ef1ad17f6183ca7b34f2152bf68280f363
Pavel Pukha <ppukha@gmail.com>
2025-04-26 00:22:30 +0200

    style(output): improve console readability with section headers

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -5,13 +5,18 @@ import lotto.service.Statistics
 object OutputView {
 
         fun printTickets(tickets: List<List<Int>>) {
+            println("=============== Ticket Purchase Summary ===============")
             println("You have purchased ${tickets.size} tickets.")
             tickets.forEach { println(it) }
+            println("=======================================================")
+            println()
         }
 
         fun printStatistics(statistics: Statistics) {
+            println("*************** Lotto Winning Statistics ***************")
             statistics.toString()
                 .lines()
                 .forEach { println(it) }
+            println("*********************************************************")
         }
     }
