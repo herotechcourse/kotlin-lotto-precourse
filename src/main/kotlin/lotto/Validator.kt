983d2997faa8c7e34c67df0a0c8102c5023b4b81
YuGyeong98 <kuk3097@naver.com>
2025-04-25 19:51:46 +0900

    refactor(StringUtil): string to int list function move to validator class

--- src/main/kotlin/lotto/Validator.kt
+++ src/main/kotlin/lotto/Validator.kt
@@ -1,9 +1,14 @@
 package lotto
 
-class Validator {
+object Validator {
+
+    private const val COMMA = ","
 
     fun validateStringToInt(stringValue: String): Int {
         return stringValue.toIntOrNull()
             ?: throw IllegalArgumentException("Non-numeric '$stringValue' is not allowed.")
     }
+
+    fun validateStringToIntList(stringValue: String) =
+        stringValue.split(COMMA).map { validateStringToInt(it) }
 }
