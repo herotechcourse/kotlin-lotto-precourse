eaa860acf746d7ed3552949ca398c09ee88e86b3
Pavel Pukha <ppukha@gmail.com>
2025-04-25 04:51:29 +0200

    chore(app): simplify main to single controller run call

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,29 +1,13 @@
 package lotto
 
-import lotto.domain.Rank
-import lotto.view.InputView
 import lotto.service.LottoMachine
-import lotto.service.Statistics
+import lotto.view.InputView
 import lotto.view.OutputView
 
 fun main() {
-
-    val purchaseAmount = InputView.readPurchaseAmount()
-    val machine = LottoMachine()
-    val tickets = machine.purchaseTickets(purchaseAmount)
-
-    OutputView.printTickets(tickets.map { it.getNumbers() })
-
-    val winningNumbers = InputView.readWinningNumbers()
-    val bonusNumber = InputView.readBonusNumber(winningNumbers)
-
-    val ranks = tickets.map { ticket ->
-        val numbers = ticket.getNumbers()
-        val matchCount = numbers.count { it in winningNumbers }
-        val hasBonus = numbers.contains(bonusNumber)
-        Rank.from(matchCount, hasBonus)
-    }
-
-    val statistics = Statistics.from(ranks, purchaseAmount)
-    OutputView.printStatistics(statistics)
+    LottoController(
+        machine = LottoMachine(),
+        input   = InputView,
+        output  = OutputView
+    ).run()
 }
