28c8b23459d29ddc691a1876f5c35013b135ebdd
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-04-25 17:30:16 +0200

    feat: add exception handlers in validation for input
    This is to separately validate at the input and domain at Lotto class

--- src/main/kotlin/lotto/validator/InputValidator.kt
+++ src/main/kotlin/lotto/validator/InputValidator.kt
@@ -15,16 +15,22 @@ object InputValidator {
         
         return amount
     }
-
+    
     fun validateWinningNumbers(input: String): List<Int> {
         validateNonEmpty(input, "[ERROR] Winning Number cannot be empty.")
-
+    
         val split = input.split(",").map { it.trim() }
-        return split.map {
-            it.toIntOrNull()
-                ?: throw IllegalArgumentException("[ERROR] All inputs must be valid numbers.")
+        val numbers = split.map {
+            it.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] All inputs must be valid numbers.")
         }
+    
+        require(numbers.size == 6) { "[ERROR] There must be exactly 6 winning numbers." }
+        require(numbers.distinct().size == 6) { "[ERROR] Winning numbers must not contain duplicates." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] Numbers must be between 1 and 45." }
+    
+        return numbers
     }
+
 
     fun validateBonusNumber(input: String): Int {
         validateNonEmpty(input, "[ERROR] Bonus Number cannot be empty.")
