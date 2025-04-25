99bad665b4bc1ef2c81b109d8d0902a6f2acc2d4
Ugochi <amakomitchell5@gmail.com>
2025-04-26 00:16:07 +0200

    feat: create function to evaluate and calculate ticket results

--- src/main/kotlin/lotto/LottoEngine.kt
+++ src/main/kotlin/lotto/LottoEngine.kt
@@ -12,4 +12,16 @@ object LottoEngine {
             Lotto(numbers)
         }
     }
+
+    fun evaluateTicketResults(tickets: List<Lotto>, winning: Set<Int>, bonus: Int): Map<Rank, Int> {
+        return tickets.groupingBy {
+            val match = it.matchCount(winning)
+            val hasBonus = it.contains(bonus)
+            Rank.of(match, hasBonus)
+        }.eachCount().toSortedMap(compareByDescending { it.ordinal })
+    }
+
+    fun totalResultWinnings(results: Map<Rank, Int>): Int {
+        return results.entries.sumOf { (rank, count) -> rank.prize * count }
+    }
 }
