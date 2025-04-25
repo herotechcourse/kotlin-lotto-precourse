096fd35d1a4f5ed4b692756e09fadbc5137fb8be
atash1986 <msc.ashourzadeh@gmail.com>
2025-04-25 23:39:03 +0330

    fix(InputView): add error prefix before error messages
    - also move the readline code to a specific function

--- src/main/kotlin/views/InputView.kt
+++ src/main/kotlin/views/InputView.kt
@@ -3,11 +3,16 @@ package views
 import camp.nextstep.edu.missionutils.Console
 
 class InputView {
+    val errPrefix = "[ERROR]"
+    fun readLineFromConsole(): String? {
+        return Console.readLine()
+    }
+
     fun getPurchaseAmount():Int {
         println()
         println("Please Enter Purchase amount for lottery tickets:")
-        val input=Console.readLine()
-        val purchaseAmount = input?.toIntOrNull() ?: throw IllegalArgumentException("Invalid Purchase Amount Number")
+        val input=readLineFromConsole()
+        val purchaseAmount = input?.toIntOrNull() ?: throw IllegalArgumentException("$errPrefix Invalid Purchase Amount Number")
         if (purchaseAmount >= 0 && purchaseAmount % 1000 == 0) {
             return purchaseAmount
         }
@@ -18,14 +23,14 @@ class InputView {
     {
         println()
         println("Please enter last week's winning numbers:")
-        val input=Console.readLine() ?: ""
+        val input=readLineFromConsole() ?: ""
         val winningNumbers=input.split(',').map({ it.trim().toInt() })
         if(winningNumbers.size < 6)
-            throw IllegalArgumentException("There must be exactly 6 numbers")
+            throw IllegalArgumentException("$errPrefix There must be exactly 6 numbers")
         if(winningNumbers.distinct().size < 6)
-            throw IllegalArgumentException("There must not be repeated numbers")
+            throw IllegalArgumentException("$errPrefix There must not be repeated numbers")
         if(!winningNumbers.all { it in 1..45 })
-            throw IllegalArgumentException("Each number must be between 1 and 45")
+            throw IllegalArgumentException("$errPrefix Each number must be between 1 and 45")
         return winningNumbers
     }
     
@@ -33,10 +38,10 @@ class InputView {
     {
         println()
         println("Please enter the bonus number.")
-        val input=Console.readLine()
-        val bonusNumber = input?.toIntOrNull() ?: throw IllegalArgumentException("Invalid Bonus Number")
+        val input=readLineFromConsole()
+        val bonusNumber = input?.toIntOrNull() ?: throw IllegalArgumentException("$errPrefix Invalid Bonus Number")
         if (bonusNumber < 1 || bonusNumber > 45) {
-            throw IllegalArgumentException("Bonus number must be between 1 and 45")
+            throw IllegalArgumentException("$errPrefix Bonus number must be between 1 and 45")
         }
         return bonusNumber
     }
