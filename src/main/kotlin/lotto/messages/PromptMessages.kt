e847b694779b3548d6a8216c35df473b100bb938
yeonupark <idepix@naver.com>
2025-04-25 19:40:11 +0900

    feat(view): print ticket count from LottoMachine in OutputView

--- src/main/kotlin/lotto/messages/PromptMessages.kt
+++ src/main/kotlin/lotto/messages/PromptMessages.kt
@@ -3,5 +3,11 @@ package lotto.messages
 enum class PromptMessages(val message: String) {
     PURCHASE_AMOUNT("Please enter the purchase amount."),
     WINNING_NUMBERS("Please enter last week's winning numbers."),
-    BONUS_NUMBER("Please enter the bonus number.")
+    BONUS_NUMBER("Please enter the bonus number."),
+    TICKET_COUNT("You have purchased %d tickets.");
+
+    fun with(vararg args: Any): String {
+        return message.format(*args)
+    }
+
 }
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,4 +1,12 @@
 package lotto.view
 
+import lotto.LottoMachine
+import lotto.messages.PromptMessages
+
 class OutputView {
+
+    fun printTicketCount(lottoMachine: LottoMachine) {
+        val ticketCount = lottoMachine.countTickets()
+        println(PromptMessages.TICKET_COUNT.with(ticketCount))
+    }
 }
