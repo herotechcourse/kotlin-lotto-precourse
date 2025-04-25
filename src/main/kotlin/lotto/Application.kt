da81db4afda8b78692b6ae392f6b398b6bc201a6
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-04-25 12:01:49 +0200

    refac(Application): create helper functions to organize Application calls
    add private helper functions to encapsulate related operations and make the main functions cleaner and more redable

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,11 +1,33 @@
 package lotto
 
-import camp.nextstep.edu.missionutils.Console
-
 fun main() {
     val inputView = InputView()
+    val outputView = OutputView()
 
-    inputView.getAmount()
-    inputView.getWinningNumbers()
-    inputView.getBonusNumber()
+    val player = initializePlayer(inputView, outputView)
+    val lotto = initializeLotto(inputView)
+    val game = playGame(inputView, player, lotto)
+
+    displayResults(outputView, game)
+}
+
+private fun initializePlayer(inputView: InputView, outputView: OutputView): Player {
+    val amount = inputView.getAmount()
+    val player = Player(amount)
+    outputView.printTickets(player.tickets)
+    return player
+}
+
+private fun initializeLotto(inputView: InputView): Lotto {
+    return Lotto(inputView.getWinningNumbers())
+}
+
+private fun playGame(inputView: InputView, player: Player, lotto: Lotto): Game {
+    val bonusNumber = inputView.getBonusNumber()
+    return Game(player, lotto, bonusNumber)
+}
+
+private fun displayResults(outputView: OutputView, game: Game) {
+    outputView.printStatistics(game.statistics)
+    outputView.printReturnRate(game.profitRate)
 }
