69a720f8d10872ad3d19fecee70d7ec1135abf86
권장순 <jsoonworld@gmail.com>
2025-04-25 14:00:49 +0900

    feat(OutputView) : add feature to printIssuedLottoCountMessage

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -5,7 +5,12 @@ class OutputView {
         println(PURCHASE_LOTTO_AMOUNT_PROMPT_MESSAGE)
     }
 
+    fun printIssuedLottoCountMessage(count: Int) {
+        println(String.format(ISSUED_LOTTO_COUNT_MESSAGE, count))
+    }
+
     companion object {
         const val PURCHASE_LOTTO_AMOUNT_PROMPT_MESSAGE = "Please enter the purchase amount."
+        const val ISSUED_LOTTO_COUNT_MESSAGE = "You have purchased %d tickets."
     }
 }
