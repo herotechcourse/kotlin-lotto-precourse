5f34622a9de65b99960ddd83d93e350108763610
moonseonghui <msh9980@naver.com>
2025-04-25 17:10:40 +0900

    refactor(InputView): remove EOF exception handling from InputView

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -5,10 +5,6 @@ import camp.nextstep.edu.missionutils.Console
 object InputView {
     fun promptAndReadLine(promptMessage: String): String {
         println(promptMessage)
-        return try {
-            Console.readLine()
-        } catch (e: NoSuchElementException) {
-            throw IllegalStateException("[ERROR] Unexpected end of input (EOF).")
-        }
+        return Console.readLine()
     }
 }
