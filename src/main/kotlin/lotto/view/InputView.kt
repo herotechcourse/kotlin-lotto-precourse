7ceab06e7b72205a36e69b1af1a59c4b8409c8be
김동영 <dongyi@gimdong-yeong-ui-MacBookAir.local>
2025-04-26 14:58:46 +0900

    feat(view): add input view for purchase amount, winning numbers, and bonus number

---
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -0,0 +1,16 @@
+package lotto.view
+
+object InputView {
+
+    fun purchaseAmount() {
+        println("Please enter the purchase amount.")
+    }
+
+    fun winningNumbers() {
+        println("Please enter last week's winning numbers.")
+    }
+
+    fun bonusNumber() {
+        println("Please enter the bonus number.")
+    }
+}
