ac70afe8b7bf7696252f53f3242f8233792a29d5
wjunseok <wminsoo1@naver.com>
2025-04-26 17:50:23 +0900

    refactor(lottoresultanalyzer): change analyze parameter to accept LottoMatchable instead of Lotto
    - Updated LottoResultAnalyzer to depend on the LottoMatchable interface instead of the concrete Lotto class.

--- src/main/kotlin/lotto/LottoResultAnalyzer.kt
+++ src/main/kotlin/lotto/LottoResultAnalyzer.kt
@@ -2,7 +2,7 @@ package lotto
 
 class LottoResultAnalyzer {
 
-    fun analyze(lottos: List<Lotto>, lottoEvaluator: LottoEvaluator): List<LottoResultType> {
+    fun analyze(lottos: List<LottoMatchable>, lottoEvaluator: LottoEvaluator): List<LottoResultType> {
         val lottoResultTypes = mutableListOf<LottoResultType>()
 
         for (lotto in lottos) {
