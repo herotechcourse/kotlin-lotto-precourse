45dd1d05caaa95315010ca465e5589d8053ff3d2
leejuan1 <sportleejuan@naver.com>
2025-04-26 23:40:18 +0900

    feat:add features

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,40 @@
 package lotto
 
+
+
 fun main() {
 
+    val price = readLine()!!.toInt()
+    val numbers = readln().split(",").map {it.toInt() }
 
-}
+//    for (i in numbers) {
+//        if (numbers[i] > 45)
+//            IllegalArgumentException();
+//        for(j in numbers){
+//            if(numbers[i]==numbers[j]){
+//                IllegalArgumentException();//이러면 안됨.
+//            }
+//        }
+//    }
+
+
+    if(price%1000!=0){
+        IllegalArgumentException();
+    }
+     val lottocount=price/1000
+
+
+    val juan=Lotto(listOf(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5]))
+    val CreatedRandomLottos=juan.CreateLottoList(lottocount)
+    val sortedNumbers=juan.getNumbers()
+
+    for(i in CreatedRandomLottos){
+
+    }
+
+
+
+    print(sortedNumbers)
+
+    }
+
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,5 +1,8 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+import java.util.Random
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
@@ -9,6 +12,15 @@ class Lotto(private val numbers: List<Int>) {
         return numbers.sorted()
     }
 
+    fun CreateLottoList(count: Int):List<Lotto>{
+        //val randomLotto: List<Int> = listOf()
+        val randomLotto=mutableListOf<Lotto>()
+        for(i in 1..count){
+            val RandomlottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+             randomLotto.add(Lotto(RandomlottoNumbers))
+        }
+        return randomLotto
+    }
     fun CorrectNumbers(){
 
     }
@@ -19,4 +31,6 @@ class Lotto(private val numbers: List<Int>) {
     fun Calculator(){
 
     }
+
+
 }
