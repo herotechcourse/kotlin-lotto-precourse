d86864be1a3a83369136dc39c527d04ea191cb8e
seyeok94 <seyeok94@mi.fu-berlin.de>
2025-04-27 00:20:55 +0200

    implement user prompt for last week's winning number and bonus number

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -7,12 +7,25 @@ fun main() {
     println("Please enter the purchase amount.")
     val amountInput = Console.readLine().toInt()
     val tickets = parseAmount(amountInput)
-    println("you have purchased $tickets tickets.")
+    println("\nYou have purchased $tickets tickets.")
     // parse tickets to generate numbers
     val lottoNumbers = generateLotto(tickets)
     for (numbers in lottoNumbers) {
         println(numbers)
     }
+
+    // prompt user for last week's winning numbers
+    println("\nPlease enter last week's winning numbers.")
+    val winningNumbers = mutableListOf<Int>()
+    val winningInput = Console.readLine()
+    val numbers = winningInput.split(",")
+    for (number in numbers) {
+        winningNumbers.add(number.toInt())
+    }
+
+    // prompt user for the bonus number
+    println("\nPlease enter the bonus number.")
+    val bonusNumber = Console.readLine().toInt()
 }
 
 fun parseAmount(input: Int): Int {
