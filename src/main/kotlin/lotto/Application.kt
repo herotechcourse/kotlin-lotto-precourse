bb6dcb1d292a8225449b52f9446e214fc8ac478c
Ugochi <amakomitchell5@gmail.com>
2025-04-26 23:07:21 +0200

    fix: reprompt user when invalid data input

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,17 +4,29 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     // TODO: Implement the program
-    val amountPurchase = InputView.getPurchaseAmount()
+    val amountPurchase = promptUntilValid{InputView.getPurchaseAmount()}
     val tickets = LottoEngine.buyTickets(amountPurchase)
 
     OutputView.displayLottoTickets(tickets)
 
-    val winningNumbers = InputView.getWinningNumbers()
-    val bonusNumber = InputView.getBonusNumber(winningNumbers)
+    val winningNumbers = promptUntilValid{InputView.getWinningNumbers()}
+    val bonusNumber = promptUntilValid{InputView.getBonusNumber(winningNumbers)}
 
     val results = LottoEngine.evaluateTicketResults(tickets, winningNumbers, bonusNumber)
     val totalWinnings = LottoEngine.totalResultWinnings(results)
 
     OutputView.displayLottoResults(results, amountPurchase, totalWinnings)
 
+}
+
+private inline fun <T> promptUntilValid(block: () -> T): T {
+    while (true) {
+        try {
+            return block()
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        } catch (e: IllegalStateException) {
+            println(e.message)
+        }
+    }
 }
