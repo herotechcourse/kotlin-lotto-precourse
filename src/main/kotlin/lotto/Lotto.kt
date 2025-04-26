01b181ce6bfd657d903c378e0176c983ea3196e8
Santiago Tena Hernandez <stenahe@gmail.com>
2025-04-27 00:48:16 +0200

    fix(error management): add uniqueness requirement

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,7 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.size == numbers.toSet().size) { "[ERROR] All numbers must be unique" }
     }
 
     fun getNumbers(): List<Int> {
