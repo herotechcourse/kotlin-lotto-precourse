7dc8239ef35abc6d57ab92f18bb0caf93660e36f
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-04-26 20:48:37 +0200

    feat(lotto): handling duplicate numbers in Lotto

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,7 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
   init {
     require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+    require(numbers.toSet().size == numbers.size){"[ERROR] Lotto must not contain duplicates."}
   }
 
   fun getNumbers(): List<Int> = numbers.sorted()
