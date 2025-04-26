7f40ece9cd328030da2c9db7cfd233f19213aeaa
Alex <alexis.mar.schmidt@gmail.com>
2025-04-26 18:16:38 +0200

    test(bonusNumber): create unit tests for bonusNumber validator
    - add tests to ensure that program throws IllegalArgumentException
    if number is out of range 1-45, is already in the winner numbers
    or it is an invalid value

---
+++ src/test/kotlin/lotto/BonusNumberTest.kt
@@ -0,0 +1,43 @@
+package lotto
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+
+class BonusNumberTest {
+    private val winnerNumbers = listOf(1, 2, 3, 4, 5, 6)
+
+    @Test
+    fun `should return number when input is valid and not repeated in winner numbers`() {
+        val result = Validator.validateBonusNumber("7", winnerNumbers)
+        assertThat(result).isEqualTo(7)
+    }
+
+    @Test
+    fun `should throw IllegalArgumentException when number is out of range`() {
+        assertThatThrownBy { Validator.validateBonusNumber("48", winnerNumbers) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(Messages.ERROR_OUT_OF_RANGE)
+    }
+
+    @Test
+    fun `should throw IllegalArgumentException when is not a number`() {
+        assertThatThrownBy { Validator.validateBonusNumber("lala", winnerNumbers) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(Messages.ERROR_INVALID_NUMBER)
+    }
+
+    @Test
+    fun `should throw IllegalArgumentException when number is already in winner numbers`() {
+        assertThatThrownBy { Validator.validateBonusNumber("2", winnerNumbers) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(Messages.ERROR_BONUS_IN_WINNER)
+    }
+
+    @Test
+    fun `should throw IllegalArgumentException when there is more than one number`() {
+        assertThatThrownBy { Validator.validateBonusNumber("8,9", winnerNumbers) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(Messages.ERROR_INVALID_NUMBER)
+    }
+}
