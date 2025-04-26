077852681c20bace7db61d9bb5f6a699e35385fc
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-04-26 16:22:28 +0200

    feat: Add InputView singleton to take input

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,31 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: Implement the program
+    println(InputView.inputPurchaseAmount())
+    println(InputView.inputLottoNumber())
+    println(InputView.inputBonusNumber())
+}
+
+object InputView {
+    fun inputPurchaseAmount():Int {
+        println("Please enter the purchase amount.")
+        val purchaseAmount = Console.readLine().toInt()
+        return purchaseAmount
+    }
+
+    fun inputLottoNumber():List<Int> {
+        println("Please enter last week's winning numbers.")
+        val winningNumbers = Console.readLine().removeWhiteSpaces().split(",").map { it.toInt() }
+        return winningNumbers
+    }
+
+    private fun String.removeWhiteSpaces() = replace("\\s".toRegex(), "")
+
+    fun inputBonusNumber():Int {
+        println("Please enter the bonus number.")
+        val bonusNumber = Console.readLine().toInt()
+        return bonusNumber
+    }
 }
