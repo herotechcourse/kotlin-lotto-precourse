c5c1ab79fac8fea7c922c26c89aa49f6183dd394
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2025-04-26 14:37:23 +0200

    feat(service): add generateTickets() method to LottoService
    - Implemented generateTickets() to create a list of Lotto tickets based on the purchase amount.
    - Each ticket consists of 6 unique random numbers between 1 and 45.
    - Integrated Randoms.pickUniqueNumbersInRange() for random number generation.

--- src/main/kotlin/lotto/service/LottoService.kt
+++ src/main/kotlin/lotto/service/LottoService.kt
@@ -1,4 +1,6 @@
 package lotto.service
+import lotto.Lotto
+import camp.nextstep.edu.missionutils.Randoms
 
 import lotto.exception.LottoInputException
 
@@ -6,5 +8,10 @@ object LottoService {
     fun validatePurchaseAmount(amount: Int) {
         if (amount <= 0 || amount % 1000 != 0)
             throw LottoInputException.InvalidAmount(amount)
+    }
+
+    fun generateTickets(amount: Int): List<Lotto> = List<Lotto>(amount) {
+            val ticketNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+            Lotto(ticketNumbers)
     }
 }
