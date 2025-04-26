ad4e9251735cc607b313bc307227cd47e26d9895
yeonupark <idepix@naver.com>
2025-04-26 11:07:26 +0900

    feat(domain): generate lottery numbers for each ticket

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -1,9 +1,19 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class LottoMachine(private val money: Int) {
 
-    fun countTickets(): Int {
-        return (money / 1000)
+    val tickets: List<Lotto> = generateTickets()
+
+    private fun generateTickets(): List<Lotto> {
+        return List(money / 1000) {
+            generateSingleTicket()
+        }
     }
 
+    private fun generateSingleTicket(): Lotto {
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+        return Lotto(numbers)
+    }
 }
