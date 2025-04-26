46960bd6246feff65684638c117046d03d632fb6
glotar <Glotar@mail.ru>
2025-04-26 11:47:49 +0200

    feature(application): output
    output in format required by assignment

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -1,24 +1,25 @@
 package lotto
 
-enum class OutputView {
-    REQUEST_PURCHASE_AMOUNT {
-        override fun printMessage(data: Any?) = println("Please enter the purchase amount.")
-    },
-    REQUEST_WINNING_NUMBERS {
-        override fun printMessage(data: Any?) = println("Please enter last week's winning numbers.")
-    },
-    REQUEST_BONUS_NUMBER {
-        override fun printMessage(data: Any?) = println("Please enter the bonus number.")
-    },
-    ERROR {
-        override fun printMessage(data: Any?) {
-            val errorMessage = data as? String ?: "Unknown"
-            println("[ERROR] $errorMessage")
+object OutputView {
+    fun requestPurchaseAmount() = println("Please enter the purchase amount.")
+    fun requestWinningNumbers() = println("Please enter last week's winning numbers.")
+    fun requestBonusNumber() = println("Please enter the bonus number.")
+    fun printTickets(tickets: List<List<Int>>) {
+        println("You have purchased ${tickets.size} tickets.")
+        for (ticket in tickets) {
+            println("[${ticket.sorted().joinToString(", ")}]")
         }
-    },
-    PRINT_RESULT {
-        override fun printMessage(data: Any?) = println("result will be printed here.")
-    };
+        println()
+    }
 
-    abstract fun printMessage(data: Any?)
+    fun printResults(results: Map<Rank, Int>) {
+        println("Winning Statistics\n---")
+        for (entry in results) {
+            println(
+                "${entry.key.matches} (${String.format("%,d", entry.key.winning)} KRW) " + "– ${entry.value} tickets"
+            )
+        }
+    }
+
+    fun printStatistics(result: Double) = println("Total return rate is ${String.format("%.1f", result)}%.")
 }
