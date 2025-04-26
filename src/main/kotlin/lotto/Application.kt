8effc401bd45ec7896d74ddcc22ca65b513de773
Karem Shabana <km425360@gmail.com>
2025-04-26 17:16:33 +0300

    feat: implement main program logic for Lotto application

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,21 @@
 package lotto
 
+import lotto.domain.LottoMachine
+import lotto.view.InputView
+import lotto.view.OutputView
+
 fun main() {
-    // TODO: Implement the program
+    try {
+        val amount = InputView.getPurchaseAmount()
+        val tickets = LottoMachine.generateTickets(amount)
+        OutputView.printTickets(tickets)
+
+        val winningNumbers = InputView.getWinningNumbers()
+        val bonusNumber = InputView.getBonusNumber(winningNumbers)
+
+        val result = LottoMachine.calculateResults(tickets, winningNumbers, bonusNumber)
+        OutputView.printStatistics(result, amount)
+    } catch (e: IllegalArgumentException) {
+        println("[ERROR] ${e.message}")
+    }
 }
