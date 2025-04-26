a5ee42474880e1201d952222834159958cff9f33
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2025-04-26 14:49:10 +0200

    feat(lotto): initialize Lotto numbers and implement access methods
    - Verify the uniqueness of the numbers in the initializer.
    - Added getNumbers() method to safely expose a copy of the ticket numbers.
    - Overridden toString() to format Lotto numbers in a readable list.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,12 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.size == numbers.toSet().size) { "[ERROR] Lotto ticket must not have duplicate numbers." }
     }
 
-    // TODO: Implement additional functions
+    fun getNumbers(): List<Int> = numbers.toList()
+
+    override fun toString(): String {
+        return numbers.joinToString(prefix = "[", postfix = "]", separator = ", ")
+    }
 }
