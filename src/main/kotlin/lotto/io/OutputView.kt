5a74eab8587004c5416a5a8bd8716dfd9f64d1eb
wjunseok <wminsoo1@naver.com>
2025-04-26 15:27:29 +0900

    feat(outputview): implement printResult to display winning statistics and total return rate
    - Prints the number of winning tickets for each prize tier.
    - Calculates and displays the total return rate based on the purchase amount.
    - Adds a private method countMatches to count the number of matches for each prize.

--- src/main/kotlin/lotto/io/OutputView.kt
+++ src/main/kotlin/lotto/io/OutputView.kt
@@ -14,4 +14,21 @@ object OutputView {
         println()
     }
 
+    fun printResult(lottoResultTypes: List<LottoResultType>, amount: Int) {
+        println("Winning Statistics")
+        println("---")
+        println("3 Matches (5,000 KRW) – ${countMatches(lottoResultTypes, FIFTH_PRIZE)} tickets")
+        println("4 Matches (50,000 KRW) – ${countMatches(lottoResultTypes, FOURTH_PRIZE)} tickets")
+        println("5 Matches (1,500,000 KRW) – ${countMatches(lottoResultTypes, THIRD_PRIZE)} tickets")
+        println("5 Matches + Bonus Ball (30,000,000 KRW) – ${countMatches(lottoResultTypes, SECOND_PRIZE)} tickets")
+        println("6 Matches (2,000,000,000 KRW) – ${countMatches(lottoResultTypes, FIRST_PRIZE)} tickets")
+
+        val rate = LottoResultType.calculate(lottoResultTypes).toDouble() / (amount * 1000) * 100
+        println("Total return rate is ${String.format("%.1f", rate)}%.")
+    }
+
+    private fun countMatches(lottoResultTypes: List<LottoResultType>, lottoResultType: LottoResultType): Int {
+        return lottoResultTypes.count() { matchResult -> matchResult == lottoResultType }
+    }
+
 }
