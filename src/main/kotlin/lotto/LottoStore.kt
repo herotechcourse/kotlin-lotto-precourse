fa0effb8102bd1f4dcf019dd169a6168a196391e
Jiihyun <cwlgusc@gmail.com>
2025-04-27 00:50:51 +0900

    refactor: delete LottoTicketCalculator class - move purchase amount validation logic to LottoStore - move calculating ticket quantity logic to LottoStore

--- src/main/kotlin/lotto/LottoStore.kt
+++ src/main/kotlin/lotto/LottoStore.kt
@@ -1,8 +1,30 @@
 package lotto
 
-class LottoStore(val lottoGenerator: LottoGenerator) {
+class LottoStore(
+    val lottoGenerator: LottoGenerator,
+    val value: Int
+) {
+    init {
+        require(value in TICKET_PRICE..MAX_PURCHASE_AMOUNT) { PURCHASE_AMOUNT_ERROR }
+        require(value % TICKET_PRICE == 0) { PURCHASE_AMOUNT_NOT_DIVISIBLE_ERROR }
+    }
 
-    fun generateLottoTickets(quantity: Int): List<Lotto> {
+    fun generateLottoTickets(): List<Lotto> {
+        val quantity = calculateQuantity()
         return List(quantity) { lottoGenerator.generate() }
+    }
+
+    private fun calculateQuantity(): Int {
+        return value / TICKET_PRICE
+    }
+
+    companion object {
+        private const val TICKET_PRICE: Int = 1_000
+        private const val MAX_PURCHASE_AMOUNT: Int = 100_000
+
+        private const val PURCHASE_AMOUNT_ERROR: String =
+            "[ERROR] Purchase amount can not exceed $MAX_PURCHASE_AMOUNT KRW."
+        private const val PURCHASE_AMOUNT_NOT_DIVISIBLE_ERROR: String =
+            "[ERROR] Purchase amount should be divisible by $TICKET_PRICE."
     }
 }
