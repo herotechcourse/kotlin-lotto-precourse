a326bfd8791a9daf7fb335aa3a9c74aed7253f88
곽병혁 <aiden-kwak@MacBook-Pro-2.local>
2025-04-26 12:25:39 +0900

    feat(config): implement game rules and validators
    - Define PrizeRank enum
    - Add Rule constants
    - Create Validator utility

---
+++ src/main/kotlin/lotto/util/Rule.kt
@@ -0,0 +1,10 @@
+package lotto.util
+
+import lotto.util.Rule
+
+object Rule {
+    const val LOTTO_PRICE = 1000
+    const val MIN_NUMBER = 1
+    const val MAX_NUMBER = 45
+    const val NUMBERS_COUNT = 6
+}
