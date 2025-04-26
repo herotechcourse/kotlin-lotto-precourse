99cf186f9fd8b0f6247a4f9143e29bdf163ce1c4
phan <hanfeelhoo@naver.com>
2025-04-26 19:55:09 +0900

    feat(LottoService): re-prompt when throw error, result calculation add zero count

--- src/main/kotlin/lotto/service/LottoService.kt
+++ src/main/kotlin/lotto/service/LottoService.kt
@@ -12,17 +12,30 @@ class LottoService (
     private val outputView: OutputView) {
 
     private fun purchaseLottos(): LottoPurchase {
-        val amount = inputView.purchaseAmount()
-            ?: throw IllegalArgumentException("[Error] Amount must be integer.")
-        val purchase = LottoPurchase(amount)
-        outputView.showPurchasedLotto(purchase.getLottos().size, purchase.getLottos())
-        return purchase
+        while (true) {
+            try {
+                val amount = inputView.purchaseAmount()
+                    ?: throw IllegalArgumentException("[ERROR] Amount must be integer.")
+                val purchase = LottoPurchase(amount)
+                outputView.showPurchasedLotto(purchase.getLottos().size, purchase.getLottos())
+                return purchase
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+
     }
 
     private fun inputWinningNum(): WinningLotto {
-        val winningNums = inputView.winningNumbers()
-        val bonusNum = inputView.bonusNumber()
-        return WinningLotto(winningNums, bonusNum)
+        while (true) {
+            try {
+                val winningNums = inputView.winningNumbers()
+                val bonusNum = inputView.bonusNumber()
+                return WinningLotto(winningNums, bonusNum)
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
     }
 
     private fun calcResult(purchase: LottoPurchase,
@@ -31,9 +44,11 @@ class LottoService (
             LottoResult(winningLotto.match(lotto), winningLotto.matchBonus(lotto))
         }
 
-        val resultStatic: Map<Int, Int> = results.groupingBy { it -> it.getPrize() }.eachCount()
+        val prizes = listOf(5_000, 50_000, 1_500_000, 30_000_000, 2_000_000_000)
+        val counted = results.groupingBy { it.getPrize() }.eachCount()
+        val resultStatic: Map<Int, Int> = prizes.associateWith { prize -> counted[prize] ?: 0 }
         val totalReward: Int = results.sumOf { it -> it.getPrize() }
-        val roi: Double = totalReward / purchase.getAmount().toDouble()
+        val roi: Double = (totalReward / purchase.getAmount().toDouble()) * 100
 
         return Pair(resultStatic, roi)
     }
