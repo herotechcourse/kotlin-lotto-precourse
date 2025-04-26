5c2a9ec9edeae1ac1f983402aa4200697c2f9977
bojana-petroska <bojana.petroska@yahoo.com>
2025-04-26 07:18:52 +0200

    feat(lottoGame): implement main game flow using InputValidator, LottoGame, and OutputFormatter

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,28 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: Implement the program
+    val purchaseAmount = InputValidator.getValidPurchaseAmount()
+    val numberOfTickets = purchaseAmount / 1000
+    println()
+    println("You have purchased $numberOfTickets tickets.")
+
+    val playerTickets = (1..numberOfTickets).map {
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        Lotto(numbers)
+    }
+
+    OutputFormatter.printTicketNumbers(playerTickets)
+
+    val lotteryNumbers = InputValidator.getValidLotteryNumbers()
+    val winningTicket = Lotto(lotteryNumbers)
+    val bonusNumber = InputValidator.getValidBonusNumber()
+
+    val game = LottoGame(playerTickets, winningTicket, bonusNumber)
+    game.play()
+
+    val results = game.getResults()
+
+    OutputFormatter.printLottoStatistics(results, game.getTotalWinnings(), purchaseAmount)
 }
