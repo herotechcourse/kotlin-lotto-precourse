f8169d5849c639c47f136aa0030ca14d6f94b0b5
ShrutiK23 <shruti.kulkarni@st.ovgu.de>
2025-04-26 19:39:39 +0200

    feat(calculate_prize_rank): Prizes for user is calculated
    - Each ticket of the user, is check with the winning numbers.
    - According to the number of matches, the prize is calculated.
    - The total winning amount is calculated by adding all the money against
      the prize ranks
    - The total return rate is also calculated by total winning amount
      divided by purchase amount multiplied by 100
    - The code logic is separated into Input view and output view

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,55 +1,48 @@
 package lotto
+
 import lotto.InputValidation.WinningNumbersValidation
 import camp.nextstep.edu.missionutils.Console
 import lotto.InputValidation.AmountValidation
 import lotto.InputValidation.BonusNumberValidation
 
-
-
+private val input = InputView()
 private val amountValidator = AmountValidation()
 private val winningNumbersvalidator = WinningNumbersValidation()
 private val bonusNumberValidation = BonusNumberValidation()
+private val output = OutputView()
 
 fun main() {
 
     println("Purchase amount for lottery tickets: ")
     try {
-        val amount = InputPurchaseAmount()
-
+        val amount = input.InputPurchaseAmount()
         val purchaseAmount = amountValidator.NullCheckNumber(amount)
         amountValidator.ValidateAmount(purchaseAmount)
 
+        println("Winning numbers (comma-separated):")
+        val numbers = input.InputWinningNumbers()
+        winningNumbersvalidator.NullCheckWinningNumber(numbers)
+        val winningNumbers = winningNumbersvalidator.SplitNumbersIntoList(numbers)
+        val lottoVariable = Lotto(winningNumbers)
 
-    println("Winning numbers (comma-separated):")
-    val numbers = InputWinningNumbers()
-    winningNumbersvalidator.NullCheckWinningNumber(numbers)
-    val winningNumbers = winningNumbersvalidator.SplitNumbersIntoList(numbers)
-    val lottoVariable = Lotto(winningNumbers)
+        println("Bonus number:")
+        val bonus_no = input.InputBonusNumber()
+        val bonus_number = amountValidator.NullCheckNumber(bonus_no)
+        bonusNumberValidation.ValidateBonusNumberInRange1to45(bonus_number)
+        bonusNumberValidation.ValidateBonusNumberUniqueness(bonus_number, winningNumbers)
 
-    println("Bonus number:")
-    val bonus_no = InputBonusNumber()
-    val bonus_number = amountValidator.NullCheckNumber(bonus_no)
-    bonusNumberValidation.ValidateBonusNumberInRange1to45(bonus_number)
-    bonusNumberValidation.ValidateBonusNumberUniqueness(bonus_number,winningNumbers)
 
-    val numberOfTickets = lottoVariable.CalculateNumberOfTickets(purchaseAmount)
-    val tickets = lottoVariable.GenerateLottoTickets(numberOfTickets)
-    lottoVariable.PrintLottoTickets(tickets, numberOfTickets)
+        val numberOfTickets = lottoVariable.CalculateNumberOfTickets(purchaseAmount)
+        val tickets = lottoVariable.GenerateLottoTickets(numberOfTickets)
+        output.PrintLottoTickets(tickets, numberOfTickets)
+
+        val lottoMachine = LottoMachine()
+        print("\nWinning Statistics\n" + "---\n")
+        var totalPrize = lottoMachine.MatchNumbers(bonus_number, tickets, winningNumbers)
+        val profit = lottoMachine.ProfitCalculation(totalPrize, purchaseAmount)
+        output.PrintReturnRate(profit)
+
     } catch (e: IllegalArgumentException) {
-        println(e.message)}
-}
-
-fun InputPurchaseAmount() : String{
-    val amount = Console.readLine()
-    return amount
-}
-
-fun InputWinningNumbers() : String{
-    val winning_numbers = Console.readLine()
-    return winning_numbers
-}
-
-fun InputBonusNumber(): String{
-    val bonus_number = Console.readLine()
-    return bonus_number
+        println(e.message)
+    }
 }
