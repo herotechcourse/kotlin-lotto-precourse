fcaeebd4cf3f0b847240c7b11fdf013d640b5a20
glotar <Glotar@mail.ru>
2025-04-26 11:52:57 +0200

    feature(application): calculate statistics
    add calculation of statistic (winnings to purchase amount in %)

--- src/main/kotlin/lotto/Cashier.kt
+++ src/main/kotlin/lotto/Cashier.kt
@@ -2,28 +2,31 @@ package lotto
 
 const val BONUS: Int = 7
 
-class Cashier (private val lotto: Lotto, private val bonusNumber: Int){
-    private val register: MutableMap<Rank, Int> =
-        enumValues<Rank>().associateWith { 0 }.toMutableMap()
-    fun checkWinningNumbers(tickets:List<List<Int>>): Map<Rank, Int>{
-        for (ticket in tickets ){
+class Cashier(private val lotto: Lotto, private val bonusNumber: Int) {
+    private val register: MutableMap<Rank, Int> = enumValues<Rank>().associateWith { 0 }.toMutableMap()
+
+    fun checkWinningNumbers(tickets: List<List<Int>>): Map<Rank, Int> {
+        for (ticket in tickets) {
             var numberOfMatches = lotto.numberOfMatches(ticket)
-            if (numberOfMatches == 5 && ticket.contains(bonusNumber))
-                numberOfMatches = BONUS
+            if (numberOfMatches == 5 && ticket.contains(bonusNumber)) numberOfMatches = BONUS
             updateRegister(numberOfMatches)
         }
         return register
     }
-    private fun updateRegister(numberOfMatches:Int){
-        when(numberOfMatches){
-            3 -> register[Rank.THREE] = register.getValue(Rank.THREE ) + 1
-            4 -> register[Rank.FOUR] = register.getValue(Rank.FOUR ) + 1
-            5 -> register[Rank.FIVE] = register.getValue(Rank.FIVE ) + 1
-            6 -> register[Rank.SIX] = register.getValue(Rank.SIX ) + 1
+
+    private fun updateRegister(numberOfMatches: Int) {
+        when (numberOfMatches) {
+            3 -> register[Rank.THREE] = register.getValue(Rank.THREE) + 1
+            4 -> register[Rank.FOUR] = register.getValue(Rank.FOUR) + 1
+            5 -> register[Rank.FIVE] = register.getValue(Rank.FIVE) + 1
+            6 -> register[Rank.SIX] = register.getValue(Rank.SIX) + 1
             BONUS -> register[Rank.BONUS] = register.getValue(Rank.BONUS) + 1
         }
     }
-    fun calculateStatistic(purchasePrice:Int):Double{
-        return 0.0
+
+    fun calculateStatistic(purchasePrice: Int): Double {
+        var winnings: Double = 0.0
+        for (entry in register) winnings += entry.key.winning * entry.value
+        return ((winnings / purchasePrice) * 100)
     }
 }
--- src/test/kotlin/lotto/CashierTest.kt
+++ src/test/kotlin/lotto/CashierTest.kt
@@ -4,21 +4,32 @@ import org.junit.jupiter.api.Test
 
 class CashierTest {
     @Test
-    fun `checkWinningNumbers updates the register in accordance with number of matches found`(){
-        val cashier = Cashier(Lotto(listOf(1,2,3,4,5,6)),7)
+    fun `checkWinningNumbers updates the register in accordance with number of matches found`() {
+        val cashier = Cashier(Lotto(listOf(1, 2, 3, 4, 5, 6)), 7)
         val tickets = listOf(
-            listOf(1,2,3,44,55,66),
-            listOf(1,2,3,4,5,66),
-            listOf(1,2,3,4,5,7),
-            listOf(1,2,3,4,6,7),
-            listOf(1,2,12,13,14,15),
-            listOf(12,13,14,15,16,17)
+            listOf(1, 2, 3, 44, 55, 66),
+            listOf(1, 2, 3, 4, 5, 66),
+            listOf(1, 2, 3, 4, 5, 7),
+            listOf(1, 2, 3, 4, 6, 7),
+            listOf(1, 2, 12, 13, 14, 15),
+            listOf(12, 13, 14, 15, 16, 17)
         )
         val register = cashier.checkWinningNumbers(tickets)
-        assert(register[3] == 1)
-        assert(register[4] == 0)
-        assert(register[5] == 1)
-        assert(register[6] == 0)
-        assert(register[7] == 2)
+        assert(register[Rank.THREE] == 1)
+        assert(register[Rank.FOUR] == 0)
+        assert(register[Rank.FIVE] == 1)
+        assert(register[Rank.SIX] == 0)
+        assert(register[Rank.BONUS] == 2)
+    }
+
+    @Test
+    fun `calculateStatistics returns winnings to purchase amount in percent`() {
+        val cashier = Cashier(Lotto(listOf(1, 2, 3, 4, 5, 6)), 7)
+        val tickets = listOf(
+            listOf(1, 2, 3, 44, 55, 66),
+        )
+        cashier.checkWinningNumbers(tickets)
+        assert(cashier.calculateStatistic(2000) == (5000.0 / 2000) * 100)
+        assert(cashier.calculateStatistic(10000) == (5000.0 / 10000) * 100)
     }
 }
