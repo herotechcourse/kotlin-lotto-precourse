9e4c93d31f3855c903f645eb98becd77aefcdb64
songpink <jejeje04@naver.com>
2025-04-26 12:20:30 +0900

    feat: implement the feature that print winning statistics

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,6 +1,7 @@
 package lotto.view
 
 import lotto.Lotto
+import lotto.model.Rank
 
 class OutputView {
     fun printUserLottos(userLottos: List<Lotto>) {
@@ -8,5 +9,19 @@ class OutputView {
         userLottos.forEach { userLotto ->
             println(userLotto.numbers.sorted())
         }
+    }
+
+    fun printWinningStatistics(countOfRanks: Map<Rank, Int>) {
+        println("Winning Statistics")
+        println("---")
+        val sortedCountOfRanks = countOfRanks.entries.sortedBy { it.key.winningMoney }
+        sortedCountOfRanks.forEach { countOfRank ->
+            val rank = countOfRank.key
+            println("%d Matches (%,d KRW) - %d ticket".format(rank.countOfMatch, rank.winningMoney, countOfRank.value))
+        }
+    }
+
+    fun printReturnRate(returnRate: Float) {
+        println("Total return rate is %.1f%%.".format(returnRate))
     }
 }
