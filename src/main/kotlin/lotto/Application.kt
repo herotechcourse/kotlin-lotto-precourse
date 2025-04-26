a933457f1548423e3b15a95ca95625aea3c4c2de
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2025-04-26 18:29:49 +0200

    feat(app): implement purchase input validation and ticket gerenation flow in Application.kt
    - Added retryInputUntilSuccess() helper to handle invalid user input with re-prompting.
    - Integrated purchase amount retrieval and validation using InputView and LottoService.
    - Generated Lotto tickets after successful input validation.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,21 @@
 package lotto
 
+import lotto.view.InputView
+import lotto.service.LottoService
+import lotto.exception.LottoInputException
+
+fun <T> retryInputUntilSuccess(block: () -> T): T {
+    while (true) {
+        try {
+            return block()
+        } catch (e: LottoInputException) {
+            println(e.message);
+        }
+    }
+}
+
 fun main() {
-    // TODO: Implement the program
+    val amount: Int =
+        retryInputUntilSuccess { InputView.getPurchaseAmount().also { LottoService.validatePurchaseAmount(it) } }
+    val tickets: List<Lotto> = LottoService.generateTickets(amount)
 }
