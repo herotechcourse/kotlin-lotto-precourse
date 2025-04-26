86854ce00318ead0f294b271993c2d2532d0e472
arash-039 <arrash18s@gmail.com>
2025-04-26 19:05:17 +0330

    feat(Application): completed main file for the application to run

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,31 @@
 package lotto
 
+const val PURCHASE_AMOUNT_TEXT = "Please enter the purchase amount."
+const val WINNING_NUMBERS_TEXT = "Please enter last week's winning numbers."
+const val BONUS_NUMBER_TEXT = "Please enter the bonus amount."
+
 fun main() {
-    // TODO: Implement the program
+    val inputView = InputView()
+    val outputView = OutputView()
+    val lottoProcessor = LottoProcessor()
+
+    while (true) {
+        try {
+            val purchaseAmount = InputView.getPurchaseAmount(inputView.inputReader(PURCHASE_AMOUNT_TEXT))
+            val tickets = lottoProcessor.ticketGenerator(purchaseAmount)
+            outputView.displayTickets(tickets)
+
+            val winningNumbers = InputView.getWinningNumbers(inputView.inputReader(WINNING_NUMBERS_TEXT))
+            val bonusNumber = InputView.getBonusNumber(inputView.inputReader(BONUS_NUMBER_TEXT))
+
+            val prizes = lottoProcessor.prizeCalculator(tickets, winningNumbers, bonusNumber)
+            val profitRate = lottoProcessor.profitRateCalculator(purchaseAmount, prizes)
+
+            outputView.displayStatistics(prizes)
+            outputView.displayProfitRate(profitRate)
+            break
+        } catch (e: IllegalArgumentException) {
+            println("${e.message}\nTry again\n" )
+        }
+    }
 }
