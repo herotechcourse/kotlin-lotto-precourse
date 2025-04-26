f509e6e075b28f45c40de6e50f5081228165ce44
김동영 <dongyi@gimdong-yeong-ui-MacBookAir.local>
2025-04-27 00:23:00 +0900

    feat(view): implement output view for each ticket numbers and winning statistics

---
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -0,0 +1,39 @@
+package lotto.view
+
+import lotto.Lotto
+import lotto.Rank
+
+object OutputView {
+
+    fun printNumberOfTickets(numberOfTickets: Int) {
+        println("You have purchased $numberOfTickets tickets.")
+    }
+
+    fun printTickets(tickets: List<Lotto>) {
+        tickets.forEach {
+            println(it.getNumbers().joinToString(prefix = "[", postfix = "]"))
+        }
+    }
+
+    private fun printRanks(rankTable: List<Int>) {
+        Rank.entries
+            .filter { it != Rank.ZERO }
+            .forEach { rank ->
+                val count = rankTable[rank.ordinal]
+                val prizeText = "%,d KRW".format(rank.prize)
+                println("${rank.label} ($prizeText) - $count tickets")
+            }
+    }
+
+    private fun printReturnRate(returnRate: Double) {
+        val rateText = "%,.1f%%".format(returnRate)
+        println("Total return rate is $rateText.")
+    }
+
+    fun printWinningStatistics(rankTable: List<Int>, returnRate: Double) {
+        println("Wining statistics")
+        println("---")
+        printRanks(rankTable)
+        printReturnRate(returnRate)
+    }
+}
