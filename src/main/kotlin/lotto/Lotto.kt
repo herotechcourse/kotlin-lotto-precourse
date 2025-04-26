d39412c4e8fd2355a239446f5df5bb30426719f5
phan <hanfeelhoo@naver.com>
2025-04-26 19:51:55 +0900

    fix(Lotto, WinningLotto): change wrong error condition when check unique

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,7 +4,7 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
         require(numbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
-        require(numbers.size != numbers.distinct().size) { "[ERROR] Lotto numbers must be unique." }
+        require(numbers.size == numbers.distinct().size) { "[ERROR] Lotto numbers must be unique." }
     }
 
     fun getNumbers(): List<Int> {
--- src/main/kotlin/lotto/WinningLotto.kt
+++ src/main/kotlin/lotto/WinningLotto.kt
@@ -9,7 +9,7 @@ class WinningLotto (
         require(bonusNumber != null) { "[ERROR]  Lotto number must be integer." }
         require(winningNumbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
         require(winningNumbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
-        require(winningNumbers.size != winningNumbers.distinct().size) { "[ERROR] Lotto numbers must be unique." }
+        require(winningNumbers.size == winningNumbers.distinct().size) { "[ERROR] Lotto numbers must be unique." }
         require(bonusNumber in (1..45)) { "[ERROR] Lotto numbers must be between 1 and 45." }
         require(winningNumbers.none { it == bonusNumber }) { "[ERROR] Lotto numbers must be unique." }
     }
