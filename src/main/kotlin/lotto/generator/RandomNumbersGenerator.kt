0cc74f231fa0c97e25980c4ec33ac1b4da3965f9
wjunseok <wminsoo1@naver.com>
2025-04-26 19:50:38 +0900

    refactor(randomnumbersgenerator): implement NumbersGenerator interface and move sorting logic
    - Implemented the NumbersGenerator interface to improve testing flexibility.
    - Moved the number sorting logic from LottoTicketMachine to RandomNumbersGenerator.

--- src/main/kotlin/lotto/generator/RandomNumbersGenerator.kt
+++ src/main/kotlin/lotto/generator/RandomNumbersGenerator.kt
@@ -5,11 +5,12 @@ import lotto.util.LottoConstants.COUNT
 import lotto.util.LottoConstants.END_INCLUSIVE
 import lotto.util.LottoConstants.START_INCLUSIVE
 
-class RandomNumbersGenerator {
+class RandomNumbersGenerator: NumbersGenerator {
 
-    fun generate(): List<Int> {
-        val numbersInRange = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT)
+    override fun generate(): List<Int> {
+        val numbersInRange = Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT).sorted()
 
+        require(numbersInRange.size == COUNT) { "[ERROR] Lotto numbers size must be $COUNT." }
         require(numbersInRange.size == numbersInRange.distinct().size) { "[ERROR] Lotto numbers must be unique." }
         for (number in numbersInRange) {
             require(number in START_INCLUSIVE..END_INCLUSIVE) { "[ERROR] Lotto number $number is out of the valid range (${START_INCLUSIVE}–${END_INCLUSIVE})." }
