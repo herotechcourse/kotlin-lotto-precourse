7ed0b8894382ab18457864be371e61c68ee5d503
김동영 <dongyi@gimdong-yeong-ui-MacBookAir.local>
2025-04-26 20:59:23 +0900

    feat(lotto): add label information to Rank enum

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,12 +1,12 @@
 package lotto
 
-enum class Rank(val prize: Long) {
-    ZERO(0),
-    THREE(5_000),
-    FOUR(50_000),
-    FIVE(1_500_000),
-    FIVE_BONUS(30_000_000),
-    SIX(2_000_000_000)
+enum class Rank(val label: String, val prize: Long) {
+    ZERO("0 Matches", 0),
+    THREE("3 Matches", 5_000),
+    FOUR("4 Matches", 50_000),
+    FIVE("5 Matches", 1_500_000),
+    FIVE_BONUS("5 Matches + Bonus Ball", 30_000_000),
+    SIX("6 Matches", 2_000_000_000)
 }
 
 class Lotto(private val numbers: List<Int>) {
