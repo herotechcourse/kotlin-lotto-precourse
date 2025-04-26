791d6ebbee184da78ae0acbacec379f4356372e7
phan <hanfeelhoo@naver.com>
2025-04-26 17:40:39 +0900

    fix(WinningLotto): add null check validation (null is not integer)

--- src/main/kotlin/lotto/WinningLotto.kt
+++ src/main/kotlin/lotto/WinningLotto.kt
@@ -1,14 +1,16 @@
 package lotto
 
 class WinningLotto (
-    private val winningNumbers: List<Int>,
-    private val bonusNumber: Int) {
+    private val winningNumbers: List<Int?>,
+    private val bonusNumber: Int?) {
 
     init {
+        require(winningNumbers.none { it == null }) { "[ERROR]  Lotto numbers must be integer." }
+        require(bonusNumber != null) { "[ERROR]  Lotto number must be integer." }
         require(winningNumbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
         require(winningNumbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
         require(winningNumbers.size != winningNumbers.distinct().size) { "[ERROR] Lotto numbers must be unique." }
-        require(bonusNumber in 1..45) { "[ERROR] Lotto numbers must be between 1 and 45." }
+        require(bonusNumber in (1..45)) { "[ERROR] Lotto numbers must be between 1 and 45." }
         require(winningNumbers.none { it == bonusNumber }) { "[ERROR] Lotto numbers must be unique." }
     }
 
