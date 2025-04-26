741387de7bf9be9a1f6fd32729bcc78692385e08
wjunseok <wminsoo1@naver.com>
2025-04-26 16:33:18 +0900

    test(mockwinninglotto): add MockWinningLotto to verify evaluateLotto invocation
    - Added a MockWinningLotto class implementing LottoEvaluator for testing purposes.
    - Tracks the number of times evaluateLotto is called.

---
+++ src/test/kotlin/lotto/MockWinningLotto.kt
@@ -0,0 +1,27 @@
+package lotto
+
+import lotto.util.LottoConstants.COUNT
+import lotto.util.LottoConstants.END_INCLUSIVE
+import lotto.util.LottoConstants.START_INCLUSIVE
+
+class MockWinningLotto(private val numbers: List<Int>, private val bonusNumber: Int): LottoEvaluator {
+
+    var callCount = 0
+        private set
+
+    init {
+        require(numbers.size == COUNT) { "[ERROR] WinningLotto must contain exactly 6 numbers." }
+        require(numbers.size == numbers.distinct().size) { "[ERROR] WinningLotto numbers must be unique." }
+        for (number in numbers) {
+            require(number in START_INCLUSIVE..END_INCLUSIVE) { "[ERROR] WinningLotto number must be between 1 and 45." }
+        }
+        require(bonusNumber in START_INCLUSIVE..END_INCLUSIVE) { "[ERROR] BonusNumber must be between 1 and 45." }
+        require(bonusNumber !in numbers) { "[ERROR] Bonus number must not be in the winning numbers." }
+    }
+
+    override fun evaluateLotto(lottoNumbers: List<Int>): LottoResultType {
+        callCount++
+        return LottoResultType.NO_PRIZE
+    }
+
+}
