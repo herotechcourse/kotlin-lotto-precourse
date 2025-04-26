8351e316b4b8aae623dabd50b9f25c5802ddf356
Regan Immanuel <reganimmanuelshawn@gmail.com>
2025-04-26 16:35:48 +0200

    feat: Purchase amount input and validation

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,19 @@
 package lotto
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     // TODO: Implement the program
+    val amount = readPuchaseAmount()
+    println("Purchase amount = $amount")
+}
+fun readPuchaseAmount(): Int {
+    println("Enter the Purchase Amount:")
+    val input = Console.readLine()
+    val amount = input.toIntOrNull()
+        ?: throw IllegalArgumentException("[Error] Input must be a valid integer.")
+
+    if (amount <= 0 || amount % 100 != 0) {
+        throw IllegalArgumentException("[Error] Amount must be a positive number divisible by 1000")
+    }
+    return amount
 }
