5377bd5c9fbc62b1bf9359b8d1b3befbb1479e6b
Sheyda <sheyda.sheydayi95@gmail.com>
2025-04-26 18:07:03 +0200

    feat: ask user to input amount of money

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,13 @@
 package lotto
 
 fun main() {
-    // TODO: Implement the program
+    println("Please enter the amount you want to spend:")
+    val amount = readlnOrNull()?.toIntOrNull()
+
+    if (amount == null || amount < 1000) {
+        println("Invalid amount. Please enter a valid number greater than or equal to 1000.")
+        return
+    }
+
+    println("You entered : $amount")
 }
