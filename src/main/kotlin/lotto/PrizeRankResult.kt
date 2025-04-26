72994a2317106f28106bb99d43e957165e05af3a
Jiihyun <cwlgusc@gmail.com>
2025-04-27 00:52:39 +0900

    refactor: change calculateProfitRate method parameter type

--- src/main/kotlin/lotto/PrizeRankResult.kt
+++ src/main/kotlin/lotto/PrizeRankResult.kt
@@ -2,8 +2,8 @@ package lotto
 
 class PrizeRankResult(val statistics: Map<PrizeRank, Int>) {
 
-    fun calculateProfitRate(purchaseAmount: Int): Double {
-        return (getTotalPrizeAmount() / purchaseAmount) * PERCENTAGE_MULTIPLIER
+    fun calculateProfitRate(lottoTickets: List<Lotto>): Double {
+        return (getTotalPrizeAmount() / getPurchasedAmount(lottoTickets)) * PERCENTAGE_MULTIPLIER
     }
 
     private fun getTotalPrizeAmount(): Double {
@@ -11,7 +11,12 @@ class PrizeRankResult(val statistics: Map<PrizeRank, Int>) {
             .toDouble()
     }
 
+    private fun getPurchasedAmount(lottoTickets: List<Lotto>): Int {
+        return lottoTickets.size * TICKET_PRICE
+    }
+
     companion object {
+        private const val TICKET_PRICE: Int = 1_000
         private const val PERCENTAGE_MULTIPLIER: Double = 100.0
     }
 }
