a2caaf75b0586f196f78d1456a1261421498da85
wjunseok <wminsoo1@naver.com>
2025-04-26 16:07:33 +0900

    refactor(lottoresulttype): move countMatches from OutputView to LottoResultType

--- src/main/kotlin/lotto/io/OutputView.kt
+++ src/main/kotlin/lotto/io/OutputView.kt
@@ -3,6 +3,7 @@ package lotto.io
 import lotto.Lotto
 import lotto.LottoResultType
 import lotto.LottoResultType.*
+import lotto.LottoResultType.Companion.countResultType
 
 object OutputView {
 
@@ -17,18 +18,14 @@ object OutputView {
     fun printResult(lottoResultTypes: List<LottoResultType>, amount: Int) {
         println("Winning Statistics")
         println("---")
-        println("3 Matches (5,000 KRW) – ${countMatches(lottoResultTypes, FIFTH_PRIZE)} tickets")
-        println("4 Matches (50,000 KRW) – ${countMatches(lottoResultTypes, FOURTH_PRIZE)} tickets")
-        println("5 Matches (1,500,000 KRW) – ${countMatches(lottoResultTypes, THIRD_PRIZE)} tickets")
-        println("5 Matches + Bonus Ball (30,000,000 KRW) – ${countMatches(lottoResultTypes, SECOND_PRIZE)} tickets")
-        println("6 Matches (2,000,000,000 KRW) – ${countMatches(lottoResultTypes, FIRST_PRIZE)} tickets")
+        println("3 Matches (5,000 KRW) – ${countResultType(lottoResultTypes, FIFTH_PRIZE)} tickets")
+        println("4 Matches (50,000 KRW) – ${countResultType(lottoResultTypes, FOURTH_PRIZE)} tickets")
+        println("5 Matches (1,500,000 KRW) – ${countResultType(lottoResultTypes, THIRD_PRIZE)} tickets")
+        println("5 Matches + Bonus Ball (30,000,000 KRW) – ${countResultType(lottoResultTypes, SECOND_PRIZE)} tickets")
+        println("6 Matches (2,000,000,000 KRW) – ${countResultType(lottoResultTypes, FIRST_PRIZE)} tickets")
 
         val rate = LottoResultType.calculate(lottoResultTypes).toDouble() / (amount * 1000) * 100
         println("Total return rate is ${String.format("%.1f", rate)}%.")
-    }
-
-    private fun countMatches(lottoResultTypes: List<LottoResultType>, lottoResultType: LottoResultType): Int {
-        return lottoResultTypes.count() { matchResult -> matchResult == lottoResultType }
     }
 
 }
