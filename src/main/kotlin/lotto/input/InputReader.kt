1525c1151fb59a5ecd25d4ddad8a746ac761a6fa
김동영 <dongyi@gimdong-yeong-ui-MacBookAir.local>
2025-04-26 16:05:01 +0900

    feat(input): implement input reader for purchase amount, winning numbers, and bonus number

--- src/main/kotlin/lotto/input/InputReader.kt
+++ src/main/kotlin/lotto/input/InputReader.kt
@@ -1,4 +1,45 @@
 package lotto.input
 
+import camp.nextstep.edu.missionutils.Console
+import lotto.util.Split
+import lotto.validation.Input
+import lotto.view.InputView
+
+const val ticketCost = 1000
+const val start = 1
+const val end = 45
+
 object InputReader {
+
+    fun purchaseAmount(): Int {
+        InputView.purchaseAmount()
+        val rawInput = Console.readLine()
+        Input.isEmpty(rawInput)
+        Input.isInteger(rawInput)
+        Input.isPositiveInteger(rawInput.toInt())
+        Input.isDivisible(rawInput.toInt(), ticketCost)
+        return rawInput.toInt()
+    }
+
+    fun winningNumbers(): List<Int> {
+        InputView.winningNumbers()
+        val rawInput = Console.readLine()
+        Input.isEmpty(rawInput)
+        val rawNumbers = Split.byComma(rawInput).map { it.trim() }
+        Input.isEachInteger(rawNumbers)
+        val numbers = rawNumbers.map { it.toInt() }
+        Input.isEachInRange(numbers, start, end)
+        Input.winningNumbersDuplication(numbers)
+        return numbers
+    }
+
+    fun bonusNumber(): Int {
+        InputView.bonusNumber()
+        val rawInput = Console.readLine()
+        Input.isEmpty(rawInput)
+        Input.isInteger(rawInput)
+        Input.isInRange(rawInput.toInt(), start, end)
+        return rawInput.toInt()
+    }
+
 }
