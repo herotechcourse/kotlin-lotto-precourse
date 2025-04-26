738227bb8d17b328aea34da4bfc597025e61afd7
rwaeng <rwaeng0421@gmail.com>
2025-04-26 16:11:46 +0900

    feat(InputView): add an input function for winning numbers

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -16,4 +16,16 @@ object InputView {
             getPurchaseAmount()
         }
     }
+
+    fun getWinningNumbers(): List<Int> {
+        return try {
+            println(InputMessage.WINNING_NUMBER.message)
+            val input: String = Console.readLine()
+
+            return Validator.validateWinningNumbers(input)
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            getWinningNumbers()
+        }
+    }
 }
