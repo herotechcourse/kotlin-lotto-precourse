ae15605674da851a46a7060039174681cc36f447
권장순 <jsoonworld@gmail.com>
2025-04-26 09:46:45 +0900

    refactor(BonusNumber) : apply constants for minimum and maximum bonus number

--- src/main/kotlin/lotto/domain/BonusNumber.kt
+++ src/main/kotlin/lotto/domain/BonusNumber.kt
@@ -5,7 +5,7 @@ import lotto.Lotto
 class BonusNumber(private val number: Int) {
 
     init {
-        require(number in 1..45) { BONUS_NUMBER_OUT_OF_RANGE }
+        require(number in MIN_NUMBER..MAX_NUMBER) { BONUS_NUMBER_OUT_OF_RANGE }
     }
 
     fun validateNotDuplicateWith(lotto: Lotto) {
@@ -17,6 +17,9 @@ class BonusNumber(private val number: Int) {
     fun number(): Int = number
 
     companion object {
+        const val MIN_NUMBER = 1
+        const val MAX_NUMBER = 45
+
         const val BONUS_NUMBER_OUT_OF_RANGE = "[ERROR] Bonus number must be between 1 and 45."
         const val BONUS_NUMBER_DUPLICATED_WITH_WINNING_NUMBER = "[ERROR] Bonus number must not duplicate any winning number."
     }
