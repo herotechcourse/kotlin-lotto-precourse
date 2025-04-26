77c67cbf9b80b05959fd6d6e31ed7b9d0c46cb8b
sunishae <sunishae44@gmail.com>
2025-04-27 00:39:25 +0900

    feat: add printLottoTickets method to OutputView

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,8 +1,16 @@
 package lotto.view
 
+import lotto.Lotto
+
 object OutputView {
 
     fun printTicketCount(ticketCount: Int) {
         println("You have purchased $ticketCount tickets.")
+    }
+
+    fun printLottoTickets(tickets: List<Lotto>) {
+        for (ticket in tickets) {
+            println(ticket)
+        }
     }
 }
