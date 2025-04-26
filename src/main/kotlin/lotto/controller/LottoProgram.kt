6b57bdc98b13fb2240cf3837c5deba5eb7e52f58
moonseonghui <msh9980@naver.com>
2025-04-26 17:20:55 +0900

    refactor(InputHandler): extract input reading and validation flow into InputHandler
    - create readWithValidation to handle repeated input-validate-transform pattern
    - Move readValidPurchaseAmount, readValidLotto, readValidBonusNumber, readWinningLotto into InputHandler

--- src/main/kotlin/lotto/controller/LottoProgram.kt
+++ src/main/kotlin/lotto/controller/LottoProgram.kt
@@ -1,69 +1,20 @@
 package lotto.controller
 
-import lotto.Lotto
 import lotto.constants.Constants
-import lotto.constants.Messages
 import lotto.model.LottoIssuer
 import lotto.model.LottoResult
 import lotto.model.LottoStorage
-import lotto.model.WinningLotto
-import lotto.validator.ValidationBundle
-import lotto.view.InputView
+import lotto.view.InputHandler
 import lotto.view.OutputView
 
 class LottoProgram {
     fun run() {
-        val purchaseAmount = readValidPurchaseAmount()
+        val purchaseAmount = InputHandler.readValidPurchaseAmount()
         val lottoStorage = issueLottery(purchaseAmount)
-        val winningLotto = readWinningLotto()
+        val winningLotto = InputHandler.readWinningLotto()
 
         val lottoResult = LottoResult(lottoStorage, winningLotto)
         OutputView.winningStatistics(lottoResult, purchaseAmount)
-    }
-}
-
-fun readWinningLotto(): WinningLotto {
-    val winningLotto = readValidLotto()
-    OutputView.newline()
-    val finalWinningLotto = readValidBonusNumber(winningLotto)
-    return finalWinningLotto
-}
-
-fun readValidPurchaseAmount(): Int {
-    while (true) {
-        try {
-            val input = InputView.promptAndReadLine(Messages.Prompt.PURCHASE_AMOUNT)
-            ValidationBundle.purchaseAmount(input)
-            return input.toInt()
-        } catch (e: IllegalArgumentException) {
-            OutputView.error(e.message)
-        }
-    }
-}
-
-fun readValidLotto(): Lotto {
-    while (true) {
-        try {
-            val input = InputView.promptAndReadLine(Messages.Prompt.WINNING_NUMBERS)
-            ValidationBundle.winningNumbers(input)
-            val numbers = input.split(Constants.SEPARATOR).map { it.trim().toInt() }
-            return Lotto(numbers)
-        } catch (e: IllegalArgumentException) {
-            OutputView.error(e.message)
-        }
-    }
-}
-
-fun readValidBonusNumber(mainLotto: Lotto): WinningLotto {
-    while (true) {
-        try {
-            val input = InputView.promptAndReadLine(Messages.Prompt.BONUS_NUMBER)
-            ValidationBundle.bonusNumber(input)
-            val bonusNumber = input.toInt()
-            return WinningLotto(mainLotto, bonusNumber)
-        } catch (e: IllegalArgumentException) {
-            OutputView.error(e.message)
-        }
     }
 }
 
