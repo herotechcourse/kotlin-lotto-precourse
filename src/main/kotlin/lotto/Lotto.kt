1f8b6608eb4aa6108393aab6055fb2429b1323dc
wjunseok <wminsoo1@naver.com>
2025-04-26 15:13:57 +0900

    feat(lotto): add validation for Lotto numbers in init block
    - Ensures the Lotto has exactly 6 numbers.
    - Validates that all numbers are unique.
    - Checks that all numbers are within the range 1 to 45.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,16 @@
 package lotto
 
+import lotto.util.LottoConstants.COUNT
+import lotto.util.LottoConstants.END_INCLUSIVE
+import lotto.util.LottoConstants.START_INCLUSIVE
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.size == COUNT) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.size == numbers.distinct().size) { "[ERROR] Lotto numbers must be unique." }
+        for (number in numbers) {
+            require(number in START_INCLUSIVE..END_INCLUSIVE) { "[ERROR] Lotto number must be between 1 and 45." }
+        }
     }
 
-    // TODO: Implement additional functions
 }
