10dffa5a447d8530d9ae8633901be9a34dcb0128
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-04-26 17:04:17 +0200

    feat: Add OutputView singleton to display output

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -6,6 +6,20 @@ fun main() {
     println(InputView.inputPurchaseAmount())
     println(InputView.inputLottoNumber())
     println(InputView.inputBonusNumber())
+
+    val lotto1 = Lotto(listOf(1, 2, 3, 4, 5, 6))
+    val lotto2 = Lotto(listOf(11, 12, 13, 14, 15, 16))
+    OutputView.printTickets(listOf(lotto1, lotto2))
+    OutputView.printResultStatistics()
+    OutputView.printProfitRate(62.2)
+}
+
+enum class WinningRanks(val rank: String) {
+    FIRST("3 Matches (5,000 KRW)"),
+    SECOND("4 Matches (50,000 KRW)"),
+    THIRD("5 Matches (1,500,000 KRW)"),
+    FOURTH("5 Matches + Bonus Ball (30,000,000 KRW)"),
+    FIFTH("6 Matches (2,000,000,000 KRW)");
 }
 
 object InputView {
@@ -27,5 +41,25 @@ object InputView {
         println("Please enter the bonus number.")
         val bonusNumber = Console.readLine().toInt()
         return bonusNumber
+    }
+}
+
+object OutputView {
+    fun printTickets(lottoTickets: List<Lotto>) {
+        for (lotto in lottoTickets) {
+            println(lotto.getNumbers().joinToString(prefix = "[", postfix = "]", separator = ", "))
+        }
+    }
+
+    fun printResultStatistics() {
+        println(WinningRanks.FIRST.rank)
+        println(WinningRanks.SECOND.rank)
+        println(WinningRanks.THIRD.rank)
+        println(WinningRanks.FOURTH.rank)
+        println(WinningRanks.FIFTH.rank)
+    }
+
+    fun printProfitRate(profitRate: Double) {
+        println("Total return rate is $profitRate%.")
     }
 }
