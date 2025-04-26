e5d366d1bbf9f7ba07c415b3c0df5ed8d92448c5
Farhana Ahmed Fima <farhanaahmedfima@mac.speedport.ip>
2025-04-26 22:11:35 +0200

    feat: implement InputValidator to validate purchase amount

--- src/main/kotlin/lotto/utils/InputValidator.kt
+++ src/main/kotlin/lotto/utils/InputValidator.kt
@@ -1,4 +1,28 @@
 package lotto.utils
 
-class InputValidator {
+interface InputValidator {
+    fun validateInput(inputString: String): Int
+}
+
+class InputValidatorImpl : InputValidator {
+    override fun validateInput(inputString: String): Int {
+        try {
+            val number = inputString.toInt()
+            if (number < 0) {
+                throw IllegalArgumentException(POSITIVE_INTEGER_ERROR_MESSAGE)
+            }
+            if (number % DIVISOR != 0) {
+                throw IllegalArgumentException(DIVISIBLE_BY_THOUSAND_ERROR_MESSAGE)
+            }
+            return number
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException(POSITIVE_INTEGER_ERROR_MESSAGE)
+        }
+    }
+
+    companion object {
+        private const val POSITIVE_INTEGER_ERROR_MESSAGE = "Amount must be a positive integer number."
+        private const val DIVISIBLE_BY_THOUSAND_ERROR_MESSAGE = "Amount must be divisible by 1,000."
+        private const val DIVISOR = 1000
+    }
 }
