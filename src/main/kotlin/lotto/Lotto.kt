57527a49be7e28b537fe45d4316f92c57e96eea1
wjunseok <wminsoo1@naver.com>
2025-04-26 17:47:25 +0900

    refactor(lotto): implement LottoMatchable interface for Lotto to support testing
    - Refactored Lotto class to implement LottoMatchable interface.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,7 +4,7 @@ import lotto.util.LottoConstants.COUNT
 import lotto.util.LottoConstants.END_INCLUSIVE
 import lotto.util.LottoConstants.START_INCLUSIVE
 
-class Lotto(private val numbers: List<Int>) {
+class Lotto(private val numbers: List<Int>): LottoMatchable{
     init {
         require(numbers.size == COUNT) { "[ERROR] Lotto must contain exactly 6 numbers." }
         require(numbers.size == numbers.distinct().size) { "[ERROR] Lotto numbers must be unique." }
@@ -13,7 +13,7 @@ class Lotto(private val numbers: List<Int>) {
         }
     }
 
-    fun matchLotto(lottoEvaluator: LottoEvaluator): LottoResultType {
+    override fun matchLotto(lottoEvaluator: LottoEvaluator): LottoResultType {
         return lottoEvaluator.evaluateLotto(this.numbers)
     }
 
