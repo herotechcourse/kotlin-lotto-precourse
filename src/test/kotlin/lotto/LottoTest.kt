d56021b7f75b982f4a3d111ad93da0a50ddc9767
Taekeundo <pleasepose@gmail.com>
2025-04-26 18:30:45 +0200

    test(domain): add failing test for Lotto class
    - Tests are now RED, because Lotto class hasn't implemented the required functions yet
    - Add unit tests for Lotto creation validation: count, duplication, range
    - Add unit tests for Lotto matching functions: countMatchedNumbers, isBonusNumber
    - Refactor and intergrate previous TDD unit tests:
      Validation logic for creation was tested in ValidateLottoNumberTest before
      Matching logic was tested in matchLottosTest
    - Refactor to move responsibilities into Lotto domain class(OOP,DDD)

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -2,22 +2,62 @@ package lotto
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.Assertions.assertEquals
 
 class LottoTest {
+
     @Test
-    fun `throws an exception when lotto numbers exceed six`() {
+    fun `givenValidSixNumbers_whenCreateLotto_thenSuccess`() {
+        val numbers = listOf(1, 2, 3, 4, 5, 6)
+        val lotto = Lotto(numbers)
+        assertEquals(6, lotto.getNumbers().size)
+    }
+
+    @Test
+    fun `givenLessThanSixNumbers_whenCreateLotto_thenThrowException`() {
+        val numbers = listOf(1, 2, 3, 4, 5)
+        assertThrows<IllegalArgumentException> {
+            Lotto(numbers)
+        }
+    }
+
+    @Test
+    fun `givenMoreThanSixNumbers_whenCreateLotto_thenThrowException`() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
         }
     }
 
-    // TODO: Implement production code to pass the test
     @Test
-    fun `throws an exception when lotto numbers contain duplicates`() {
+    fun `givenDuplicatedNumbers_whenCreateLotto_thenThrowException`() {
+        val numbers = listOf(1, 2, 3, 4, 5, 5)
         assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 5))
+            Lotto(numbers)
         }
     }
 
-    // TODO: Implement tests based on the added features
+    @Test
+    fun `givenOutOfRangeNumbers_whenCreateLotto_thenThrowException`() {
+        val numbers = listOf(0, 1, 2, 3, 4, 5)
+        assertThrows<IllegalArgumentException> {
+            Lotto(numbers)
+        }
+    }
+
+    @Test
+    fun `givenWinningNumbers_whenCountMatchedNumbers_thenReturnCorrectCount`() {
+        val numbers = listOf(1, 2, 3, 43, 44, 45)
+        val lotto = Lotto(numbers)
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val matchCount = lotto.countMatchedNumbers(winningNumbers)
+        assertEquals(3, matchCount)
+    }
+
+    @Test
+    fun `givenBonusNumber_whenCheckIsBonusNumber_thenReturnTrue`() {
+        val numbers = listOf(1, 2, 3, 10, 11, 12)
+        val lotto = Lotto(numbers)
+        val checkBonus = lotto.isBonusNumber(10)
+        assertEquals(true, checkBonus)
+    }
 }
