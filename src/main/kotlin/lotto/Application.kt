bbe41ec5db38f7b5da74a4476d1001c3e1753f4b
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-04-26 20:56:14 +0200

    feat: add main application setup for Lotto program

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,32 @@
 package lotto
 
+import lotto.data.repo.LottoRepositoryImpl
+import lotto.data.service.LottoNumberService
+import lotto.domain.model.WinningNumbers
+import lotto.domain.usecases.CalculateResultUseCase
+import lotto.domain.usecases.ComputeProfitUseCase
+import lotto.domain.usecases.PurchaseLottoUseCase
+import lotto.presentation.controller.LottoController
+import lotto.presentation.view.InputView
+
 fun main() {
-    // TODO: Implement the program
+  val lottoNumberService = LottoNumberService()
+  val lottoRepository = LottoRepositoryImpl(lottoNumberService)
+
+  val purchaseLottoUseCase = PurchaseLottoUseCase(lottoRepository)
+  val calculateResultUseCase = CalculateResultUseCase(lottoRepository)
+  val computeProfitUseCase = ComputeProfitUseCase()
+
+  val controller = LottoController(
+    purchaseLottoUseCase,
+    calculateResultUseCase,
+    computeProfitUseCase
+  )
+
+  val purchaseAmount = InputView.readPurchaseAmount()
+
+  val winningNumbers = InputView.readWinningNumbers()
+  val bonusNumber = InputView.readBonusNumber(winningNumbers)
+
+  controller.run(purchaseAmount, WinningNumbers(winningNumbers , bonusNumber ))
 }
