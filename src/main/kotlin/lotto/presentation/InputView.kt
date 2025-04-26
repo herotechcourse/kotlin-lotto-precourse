a0ca5ab2283b804fdc570c34ce727d0de88660e7
Farhana Ahmed Fima <farhanaahmedfima@mac.speedport.ip>
2025-04-26 15:54:06 +0200

    feat: take user input for purchase amount

--- src/main/kotlin/lotto/presentation/InputView.kt
+++ src/main/kotlin/lotto/presentation/InputView.kt
@@ -1,4 +1,19 @@
 package lotto.presentation
 
-class InputView {
+import camp.nextstep.edu.missionutils.Console
+
+interface InputView {
+    fun promptInputForPurchaseAmount(): String
+}
+
+class InputViewImpl : InputView {
+    override fun promptInputForPurchaseAmount(): String {
+        println(PURCHASE_AMOUNT_PROMPT)
+        return Console.readLine()
+    }
+
+    companion object {
+        private const val PURCHASE_AMOUNT_PROMPT = "Please enter the purchase amount."
+    }
+
 }
