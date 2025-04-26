244c329dc1f24d65cf6c6036fff40c7db77c1122
seyeok94 <seyeok94@mi.fu-berlin.de>
2025-04-26 23:33:26 +0200

    implement user input amount prompt

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,19 @@
 package lotto
+import camp.nextstep.edu.missionutils.Console
+import java.lang.Integer.parseInt
 
 fun main() {
-    // TODO: Implement the program
+    // prompt user for purchase amount
+    println("Please enter the purchase amount.")
+    var amountInput = Console.readLine().toInt()
+    var amount = parseAmount(amountInput)
+    println(amount)
+}
+
+fun parseAmount(input: Int): Int {
+    // return input if amount if divisible by 1,000
+    if (input % 1000 != 0) {
+        throw IllegalArgumentException("The amount must be divisible by 1,000.")
+    }
+    return input
 }
