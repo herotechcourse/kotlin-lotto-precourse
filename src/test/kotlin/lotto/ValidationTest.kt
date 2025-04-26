22c1dfb994c5fb2b604aac9202ca6196af05fcb7
김동영 <dongyi@gimdong-yeong-ui-MacBookAir.local>
2025-04-26 13:47:39 +0900

    test(validation): add tests for each case of input validation

---
+++ src/test/kotlin/lotto/ValidationTest.kt
@@ -0,0 +1,50 @@
+package lotto
+
+import lotto.validation.Input
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class ValidationTest {
+
+    @Test
+    fun `input must be not empty`() {
+        assertThrows<IllegalArgumentException> {
+            Input.isEmpty("")
+        }
+    }
+
+    @Test
+    fun `input must be integer`() {
+        assertThrows<IllegalArgumentException> {
+            Input.isInteger("abc")
+        }
+    }
+
+    @Test
+    fun `input must be positive integer`() {
+        assertThrows<IllegalArgumentException> {
+            Input.isPositiveInteger(-1)
+        }
+    }
+
+    @Test
+    fun `input must be divisible by 1000`() {
+        assertThrows<IllegalArgumentException> {
+            Input.isDivisible(800, 1000)
+        }
+    }
+
+    @Test
+    fun `numbers must be in range from 1 to 45`() {
+        assertThrows<IllegalArgumentException> {
+            Input.isInRange(50, 1, 45)
+        }
+    }
+
+    @Test
+    fun `each input must be integer`() {
+        assertThrows<IllegalArgumentException> {
+            Input.isEachInteger(listOf("1", "2", "a"))
+        }
+    }
+}
