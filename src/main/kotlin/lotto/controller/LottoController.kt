399acd8c117cf0b0e0f82120c9b1efd5a364d158
Farhana Ahmed Fima <farhanaahmedfima@mac.speedport.ip>
2025-04-26 23:40:04 +0200

    feat: update LottoController to process input amount

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,4 +1,23 @@
 package lotto.controller
 
-class LottoController {
+import lotto.presentation.InputView
+import lotto.utils.InputValidator
+
+class LottoController(
+    private val inputValidator: InputValidator,
+    private val inputView: InputView,
+) {
+    private fun processPurchaseAmount(): Int {
+        val input = inputView.promptInputForPurchaseAmount()
+        return try {
+            inputValidator.validateInput(input)
+        } catch (e: IllegalArgumentException) {
+            processPurchaseAmount()
+        }
+    }
+
+    fun run() {
+        val purchaseAmount = processPurchaseAmount()
+
+    }
 }
