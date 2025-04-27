1d4ba1a563a1488c60e4f6e110fb363be85002c4
Khush Bakht Naveed <khush.naveed@dci-student.org>
2025-04-27 22:00:40 +0200

    Input Winning Numbers

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -14,4 +14,12 @@ fun main() {
     println("\nYou have purchased $ticketCount tickets.")
     tickets.forEach { println(it.getNumbers()) }
 
+    println("\nPlease enter last week's winning numbers (comma-separated).")
+    val winningNumbers = readLine()!!.split(",").map { it.trim().toInt() }
+    InputValidator.validateWinningNumbers(winningNumbers)
+
+    println("Please enter the bonus number.")
+    val bonus = readLine()!!.toInt()
+    InputValidator.validateBonusNumber(bonus, winningNumbers.toSet())
+
 }
