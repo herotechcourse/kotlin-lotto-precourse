c1f542cefc66f6794aeb76b8f70a183199b8a900
leejuan1 <sportleejuan@naver.com>
2025-04-27 23:06:36 +0900

    fead:add features

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,11 +1,12 @@
 package lotto
 
+import lotto.Lotto.Companion.CreateLottoList
 
 
 fun main() {
-
+    print("Please enter the purchase amount.")
     val price = readLine()!!.toInt()
-    val numbers = readln().split(",").map {it.toInt() }
+
 
 //    for (i in numbers) {
 //        if (numbers[i] > 45)
@@ -22,19 +23,51 @@ fun main() {
         IllegalArgumentException();
     }
      val lottocount=price/1000
+    print("Please enter last week's winning numbers.")
+    val numbers = readln().split(",").map {it.toInt() }
+
+    print("Please enter the bonus number.")
+    val BonusNumber=readLine()!!.toInt()
+    println("You have purchased "+lottocount.toString()+" tickets..")
+
 
 
     val juan=Lotto(listOf(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5]))
-    val CreatedRandomLottos=juan.CreateLottoList(lottocount)
+    val CreatedRandomLottos=CreateLottoList(lottocount)//static으로 바꾼 이유..
     val sortedNumbers=juan.getNumbers()
+    var first=0
+    var second=0
+    var third=0
+    var fourth=0
+    var fifth=0
 
-    for(i in CreatedRandomLottos){
 
+    for (lotto in CreatedRandomLottos) {
+        println(lotto.getNumbers())
     }
 
+    for(i in 0..lottocount-1){
+        val correctLotto=juan.CorrectNumbers(CreatedRandomLottos[i].getNumbers(),sortedNumbers,BonusNumber)
+        when(correctLotto){
+            3->fifth+=1
+            4->fourth+=1
+            5->third+=1
+            6->second+=1
+            7->first+=1
+        }
+    }
 
+    println("Winning Statistics")
+    println("---")
+    println("3 Matches (5,000 KRW) - "+fifth.toString()+"tickets")
+    println("4 Matches (50,000 KRW) - "+fourth.toString()+"tickets")
+    println("5 Matches (1,500,000 KRW) - "+third.toString()+"tickets")
+    println("5 Matches + Bonus Ball (30,000,000 KRW) "+second.toString()+"tickets")
+    println("6 Matches (2,000,000,000 KRW) - "+first.toString()+"tickets")
 
-    print(sortedNumbers)
+val sum = (((fifth*5000)+(fourth*50000)+(third*1500000)+(second*30000000)+(first*2000000000)).toDouble()/price)*100
+    println("Total return rate is "+sum.toString()+"%")
 
+    
     }
 
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,6 +2,8 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
 import java.util.Random
+import kotlin.collections.plusAssign
+import kotlin.text.get
 
 class Lotto(private val numbers: List<Int>) {
     init {
@@ -12,21 +14,41 @@ class Lotto(private val numbers: List<Int>) {
         return numbers.sorted()
     }
 
-    fun CreateLottoList(count: Int):List<Lotto>{
+companion object {
+    fun CreateLottoList(count: Int): List<Lotto> {
         //val randomLotto: List<Int> = listOf()
-        val randomLotto=mutableListOf<Lotto>()
-        for(i in 1..count){
+        val randomLotto = mutableListOf<Lotto>()
+        for (i in 1..count) {
             val RandomlottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-             randomLotto.add(Lotto(RandomlottoNumbers))
+            val RandomlottoNumberssort = RandomlottoNumbers.sorted()
+            randomLotto.add(Lotto(RandomlottoNumberssort))
         }
         return randomLotto
     }
-    fun CorrectNumbers(){
+}
 
+    fun CorrectNumbers(lottolist: List<Int>,lottocorrectlist:List<Int>,bonusnumber: Int): Int {
+        var count=0
+    for(i in lottolist.indices){//수정해야함.
+      for(j in lottocorrectlist.indices){
+       count=OutputCount(count,lottolist[i],lottocorrectlist[j])
+      }
+        if(lottolist[i] == bonusnumber) {
+            count += 1
+        }
+   }
+        return count
     }
-    fun BonusNumbers(){
 
+    fun OutputCount(counting:Int,lottolistin:Int ,lottocorrectlistin:Int):Int{
+        var counted=counting
+        if(lottolistin== lottocorrectlistin){
+            counted+=1
+        }
+        return counted
     }
+
+
 
     fun Calculator(){
 
