1bd8e0ac67f09aa91e0c8bdd1defeb63baf7b031
Junyou Park <pjy008008@gmail.com>
2025-04-27 09:01:39 +0900

    refactor(ui): remove static methods from InputView and OutputView, injecting ConsoleIoInterface
    - Moving the functions previously within the Companion Object to become instance methods of `InputView` and `OutputView`.
    - Modifying the constructors of both classes to accept a `ConsoleIoInterface` dependency.

--- src/main/kotlin/lotto/ui/InputView.kt
+++ src/main/kotlin/lotto/ui/InputView.kt
@@ -1,78 +1,75 @@
 package lotto.ui
 
-import camp.nextstep.edu.missionutils.Console
 import lotto.Lotto
 import lotto.util.Constants
 
-class InputView {
-    companion object {
-        private fun <T> readValidatedInput(prompt: String, parseAndValidate: () -> T): T {
-            while (true) {
-                println(prompt)
-                try {
-                    return parseAndValidate()
-                } catch (e: IllegalArgumentException) {
-                    println(e.message)
-                }
-            }
-        }
+class InputView(private val console: ConsoleIoInterface) {
 
-        private fun readInput(): String = Console.readLine()
-
-        fun getPurchaseAmount(): Int {
-            return readValidatedInput("Please enter the purchase amount.") {
-                val amount = validatePurchaseAmount(readInput())
-                amount
+    private fun <T> readValidatedInput(prompt: String, parseAndValidate: () -> T): T {
+        while (true) {
+            console.print(prompt)
+            try {
+                return parseAndValidate()
+            } catch (e: IllegalArgumentException) {
+                e.message?.let { console.print(it) }
             }
-        }
-
-        fun getWinningNumbers(): Lotto {
-            return readValidatedInput("\nPlease enter last week's winning numbers.") {
-                val winningNumbers = parseWinningNumbers(readInput())
-                Lotto(winningNumbers)
-            }
-        }
-
-        fun getBonusNumber(winningNumbers: Lotto): Int {
-            return readValidatedInput("\nPlease enter the bonus number.") {
-                val number = validateBonusNumber(readInput(), winningNumbers.getNumbers())
-                number
-            }
-        }
-
-        private fun validatePurchaseAmount(input: String): Int {
-            val amount = input.toIntOrNull()
-                ?: throw IllegalArgumentException("${Constants.ERROR_PREFIX} Input must be a valid number.")
-
-            if (amount < Constants.MIN_PURCHASE_AMOUNT) {
-                throw IllegalArgumentException("${Constants.ERROR_PREFIX} Purchase amount must be at least ${Constants.MIN_PURCHASE_AMOUNT} ${Constants.CURRENCY_UNIT}.")
-            }
-            if (amount % Constants.PURCHASE_AMOUNT_UNIT != 0) {
-                throw IllegalArgumentException("${Constants.ERROR_PREFIX} Purchase amount must be in units of ${Constants.PURCHASE_AMOUNT_UNIT} ${Constants.CURRENCY_UNIT}.")
-            }
-            return amount
-        }
-
-        private fun parseWinningNumbers(input: String): List<Int> {
-            val numbers = input
-                .split(",")
-                .map {
-                    it.trim().toIntOrNull()
-                        ?: throw IllegalArgumentException("${Constants.ERROR_PREFIX} All Lotto numbers must be numbers.")
-                }
-            return numbers
-        }
-
-        private fun validateBonusNumber(input: String, winningNumbers: List<Int>): Int {
-            val number = input.toIntOrNull()
-                ?: throw IllegalArgumentException("${Constants.ERROR_PREFIX} Bonus number must be number.")
-            if (number !in Constants.MIN_LOTTO_NUMBER..Constants.MAX_LOTTO_NUMBER) {
-                throw IllegalArgumentException("${Constants.ERROR_PREFIX} Bonus number must be between ${Constants.MIN_LOTTO_NUMBER} and ${Constants.MAX_LOTTO_NUMBER}.")
-            }
-            if (number in winningNumbers) {
-                throw IllegalArgumentException("${Constants.ERROR_PREFIX} Bonus number must not be a duplicate of the winning numbers.")
-            }
-            return number
         }
     }
+
+    fun getPurchaseAmount(): Int {
+        return readValidatedInput("Please enter the purchase amount.") {
+            val amount = validatePurchaseAmount(console.read())
+            amount
+        }
+    }
+
+    fun getWinningNumbers(): Lotto {
+        return readValidatedInput("\nPlease enter last week's winning numbers.") {
+            val winningNumbers = parseWinningNumbers(console.read())
+            Lotto(winningNumbers)
+        }
+    }
+
+    fun getBonusNumber(winningNumbers: Lotto): Int {
+        return readValidatedInput("\nPlease enter the bonus number.") {
+            val number = validateBonusNumber(console.read(), winningNumbers.getNumbers())
+            number
+        }
+    }
+
+    private fun validatePurchaseAmount(input: String): Int {
+        val amount = input.toIntOrNull()
+            ?: throw IllegalArgumentException("${Constants.ERROR_PREFIX} Input must be a valid number.")
+
+        if (amount < Constants.MIN_PURCHASE_AMOUNT) {
+            throw IllegalArgumentException("${Constants.ERROR_PREFIX} Purchase amount must be at least ${Constants.MIN_PURCHASE_AMOUNT} ${Constants.CURRENCY_UNIT}.")
+        }
+        if (amount % Constants.PURCHASE_AMOUNT_UNIT != 0) {
+            throw IllegalArgumentException("${Constants.ERROR_PREFIX} Purchase amount must be in units of ${Constants.PURCHASE_AMOUNT_UNIT} ${Constants.CURRENCY_UNIT}.")
+        }
+        return amount
+    }
+
+    private fun parseWinningNumbers(input: String): List<Int> {
+        val numbers = input
+            .split(",")
+            .map {
+                it.trim().toIntOrNull()
+                    ?: throw IllegalArgumentException("${Constants.ERROR_PREFIX} All Lotto numbers must be numbers.")
+            }
+        return numbers
+    }
+
+    private fun validateBonusNumber(input: String, winningNumbers: List<Int>): Int {
+        val number = input.toIntOrNull()
+            ?: throw IllegalArgumentException("${Constants.ERROR_PREFIX} Bonus number must be number.")
+        if (number !in Constants.MIN_LOTTO_NUMBER..Constants.MAX_LOTTO_NUMBER) {
+            throw IllegalArgumentException("${Constants.ERROR_PREFIX} Bonus number must be between ${Constants.MIN_LOTTO_NUMBER} and ${Constants.MAX_LOTTO_NUMBER}.")
+        }
+        if (number in winningNumbers) {
+            throw IllegalArgumentException("${Constants.ERROR_PREFIX} Bonus number must not be a duplicate of the winning numbers.")
+        }
+        return number
+    }
+
 }
--- src/main/kotlin/lotto/ui/OutputView.kt
+++ src/main/kotlin/lotto/ui/OutputView.kt
@@ -5,47 +5,46 @@ import lotto.domain.PurchasedLottos
 import lotto.domain.WinningStats
 import lotto.util.Constants
 
-class OutputView {
-    companion object {
-        fun printPurchasedLottos(purchasedLottos: PurchasedLottos) {
-            val ticketAmount = purchasedLottos.count()
-            println("\nYou have purchased $ticketAmount tickets.")
-            purchasedLottos.getAllTickets().forEach { ticket ->
-                println(ticket)
-            }
-        }
-
-        fun printWinningStats(winningStats: WinningStats) {
-            println("\nWinning Statistics")
-            println("---")
-            WinningRank.entries
-                .filter { it != WinningRank.MISS }
-                .sortedBy { it.prize }
-                .forEach { rank ->
-                    val count = winningStats.stats.getOrDefault(rank, 0)
-                    val matchDescription = getMatchDescription(rank)
-                    println("$matchDescription – $count tickets")
-                }
-        }
-
-        private fun getMatchDescription(rank: WinningRank): String {
-            val matchDescription = when (rank) {
-                WinningRank.FIFTH -> "3 Matches (${Constants.FORMATTED_PRIZE_FIFTH} ${Constants.CURRENCY_UNIT})"
-                WinningRank.FOURTH -> "4 Matches (${Constants.FORMATTED_PRIZE_FOURTH} ${Constants.CURRENCY_UNIT})"
-                WinningRank.THIRD -> "5 Matches (${Constants.FORMATTED_PRIZE_THIRD} ${Constants.CURRENCY_UNIT})"
-                WinningRank.SECOND -> "5 Matches + Bonus Ball (${Constants.FORMATTED_PRIZE_SECOND} ${Constants.CURRENCY_UNIT})"
-                WinningRank.FIRST -> "6 Matches (${Constants.FORMATTED_PRIZE_FIRST} ${Constants.CURRENCY_UNIT})"
-                else -> ""
-            }
-            return matchDescription
-        }
-
-        fun printProfitRate(winningStats: WinningStats, purchaseAmount: Int) {
-            val totalPrize = winningStats.stats.entries.sumOf { (rank, count) ->
-                rank.prize * count
-            }
-            val profitRate = (totalPrize.toDouble() / purchaseAmount) * 100
-            println("Total return rate is ${profitRate}%.")
+class OutputView(private val console: ConsoleIoInterface) {
+    fun printPurchasedLottos(purchasedLottos: PurchasedLottos) {
+        val ticketAmount = purchasedLottos.count()
+        console.print("\nYou have purchased $ticketAmount tickets.")
+        purchasedLottos.getAllTickets().forEach { ticket ->
+            console.print(ticket.toString())
         }
     }
+
+    fun printWinningStats(winningStats: WinningStats) {
+        console.print("\nWinning Statistics")
+        console.print("---")
+        WinningRank.entries
+            .filter { it != WinningRank.MISS }
+            .sortedBy { it.prize }
+            .forEach { rank ->
+                val count = winningStats.stats.getOrDefault(rank, 0)
+                val matchDescription = getMatchDescription(rank)
+                console.print("$matchDescription – $count tickets")
+            }
+    }
+
+    private fun getMatchDescription(rank: WinningRank): String {
+        val matchDescription = when (rank) {
+            WinningRank.FIFTH -> "3 Matches (${Constants.FORMATTED_PRIZE_FIFTH} ${Constants.CURRENCY_UNIT})"
+            WinningRank.FOURTH -> "4 Matches (${Constants.FORMATTED_PRIZE_FOURTH} ${Constants.CURRENCY_UNIT})"
+            WinningRank.THIRD -> "5 Matches (${Constants.FORMATTED_PRIZE_THIRD} ${Constants.CURRENCY_UNIT})"
+            WinningRank.SECOND -> "5 Matches + Bonus Ball (${Constants.FORMATTED_PRIZE_SECOND} ${Constants.CURRENCY_UNIT})"
+            WinningRank.FIRST -> "6 Matches (${Constants.FORMATTED_PRIZE_FIRST} ${Constants.CURRENCY_UNIT})"
+            else -> ""
+        }
+        return matchDescription
+    }
+
+    fun printProfitRate(winningStats: WinningStats, purchaseAmount: Int) {
+        val totalPrize = winningStats.stats.entries.sumOf { (rank, count) ->
+            rank.prize * count
+        }
+        val profitRate = (totalPrize.toDouble() / purchaseAmount) * 100
+        console.print("Total return rate is ${profitRate}%.")
+    }
+
 }
