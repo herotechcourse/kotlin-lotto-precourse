a617d6169e90d1cae60fe328f8d938d4f0be7f2a
tharisha <lekamgetharisha@gmail.com>
2025-04-28 00:47:05 +0200

    feat(app): finalize program

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,14 +1,14 @@
 package lotto
 
 fun main() {
-    val purchaseAmount = InputView.getPurchaseAmount();
+    val purchaseAmount = InputView.getPurchaseAmount()
     val purchasedLottos = LottoMachine.purchaseLottos(purchaseAmount)
 
     OutputView.printLottos(purchasedLottos)
 
-    val winningNumbers = InputView.getWinningNumbers();
+    val winningNumbers = InputView.getWinningNumbers()
     val winningLottoTicket = Lotto(winningNumbers)
-    val bonusNumberInput = InputView.getBonusNumber();
+    val bonusNumberInput = InputView.getBonusNumber()
     val winningLotto = WinningLotto(winningLottoTicket, bonusNumberInput)
 
     val result = LottoResult(purchasedLottos, winningLotto)
