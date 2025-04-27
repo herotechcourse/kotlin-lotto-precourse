0f23f89d24d855cf1aa995fea01ddbf0cafd76ca
gokcesen <sengkce@gmail.com>
2025-04-27 11:09:13 +0200

    feat(prize-ranking): add enum values for lottery prizes

---
+++ src/main/kotlin/lotto/PrizeRanking.kt
@@ -0,0 +1,11 @@
+package lotto
+
+enum class PrizeRanking(val prize: Long) {
+    FIRST(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FOURTH(50_000),
+    FIFTH(5_000),
+    NONE(0);
+
+}
