0b36bd8a4b3a3d02d22d671325b3bf53f3983970
minjung <minjung0768@gmail.com>
2025-04-27 22:48:31 +0900

    feat(validator): implement validation for bonus number and refine exception message for numeric validation
    - Validate that the bonus number is unique compared to the winning numbers
    - Ensure all numbers are within the range of 1-45

--- src/main/kotlin/lotto/LottoValidator.kt
+++ src/main/kotlin/lotto/LottoValidator.kt
@@ -10,13 +10,19 @@ class LottoValidator {
     fun validateWinningNumbers(winningNumbers: List<String>) {
         validateAllNumbersAreNumeric(winningNumbers)
         validateSizeIsSix(winningNumbers)
-        validateRange1to45(winningNumbers)
-        validateNoDuplicates(winningNumbers)
+        validateRange1to45ForList(winningNumbers)
+        validateNoDuplicatesInList(winningNumbers)
     }
 
-    private fun validateIsNumeric(amount: String) {
-        if (amount.toIntOrNull() == null) {
-            throw IllegalArgumentException("[ERROR] Purchase amount must be a number.")
+    fun validateBonusNumber(bonusNumber:String, winningNumbers: List<Int>) {
+        validateIsNumeric(bonusNumber)
+        validateRange1to45ForSingle(bonusNumber)
+        validateNoDuplicatesForBonus(bonusNumber, winningNumbers)
+    }
+
+    private fun validateIsNumeric(input: String) {
+        if (input.toIntOrNull() == null) {
+            throw IllegalArgumentException("[ERROR] Input must be a number.")
         }
     }
 
@@ -44,15 +50,27 @@ class LottoValidator {
         }
     }
 
-    private fun validateRange1to45(winningNumbers: List<String>) {
+    private fun validateRange1to45ForList(winningNumbers: List<String>) {
         if (winningNumbers.any { it.toInt() < 1 } || winningNumbers.any { it.toInt() > 45 }) {
             throw IllegalArgumentException("[ERROR] Winning numbers must be in the range of 1 to 45.")
         }
     }
 
-    private fun validateNoDuplicates(winningNumbers: List<String>) {
+    private fun validateNoDuplicatesInList(winningNumbers: List<String>) {
         if (winningNumbers.distinct().size != winningNumbers.size) {
             throw IllegalArgumentException("[ERROR] Winning numbers must be unique.")
+        }
+    }
+
+    private fun validateRange1to45ForSingle(bonusNumber: String) {
+        if (bonusNumber.toInt() < 1 || bonusNumber.toInt() > 45 ) {
+            throw IllegalArgumentException("[ERROR] Bonus number must be in the range of 1 to 45.")
+        }
+    }
+
+    private fun validateNoDuplicatesForBonus(bonusNumber: String, winningNumbers: List<Int>) {
+        if (winningNumbers.contains(bonusNumber.toInt())) {
+            throw IllegalArgumentException("[ERROR] Bonus number must be not contain in winning numbers.")
         }
     }
 }
