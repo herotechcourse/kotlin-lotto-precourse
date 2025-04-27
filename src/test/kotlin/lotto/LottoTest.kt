0290734b83d87792862f0a41b4ab51223186463a
madhumitha murali <mithaeie@gmail.com>
2025-04-27 17:16:47 +0200

    test(checkLottery):unit test case for check lottery function

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.assertj.core.api.Assertions.assertThat
 
 class LottoTest {
     @Test
@@ -11,7 +12,6 @@ class LottoTest {
         }
     }
 
-    // TODO: Implement production code to pass the test
     @Test
     fun `throws an exception when lotto numbers contain duplicates`() {
         assertThrows<IllegalArgumentException> {
@@ -19,5 +19,30 @@ class LottoTest {
         }
     }
 
-    // TODO: Implement tests based on the added features
+    @Test
+    fun `test checkLottery for various match scenarios`() {
+        val numbers: List<Int> = listOf(1, 2, 3, 4, 5, 6)
+        val totalTicketList = mutableListOf(
+            mutableListOf(1, 2, 3, 4, 5, 6),
+            mutableListOf(1, 2, 3, 4, 5, 7),
+            mutableListOf(1, 2, 3, 4, 5, 6),
+            mutableListOf(1, 2, 3, 4, 5, 8),
+            mutableListOf(1, 2, 3, 4, 9, 10),
+            mutableListOf(1, 2, 3, 7, 8, 9),
+            mutableListOf(1, 2, 3, 4, 5, 6)
+        )
+
+        val bonus = 7
+        val result = Lotto(numbers).checkLottery(totalTicketList, bonus)
+
+        assertThat(result).containsExactlyInAnyOrderEntriesOf(
+            mapOf(
+                LottoEnum.SIX to 3,
+                LottoEnum.FIVE to 1,
+                LottoEnum.FIVE_BONUS to 1,
+                LottoEnum.FOUR to 1,
+                LottoEnum.THREE to 1
+            )
+        )
+    }
 }
