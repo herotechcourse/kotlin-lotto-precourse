817c1d21fc6ad2fd04cd04769bf059ccb58eacd7
YongLeeCode <yonghwan311@gmail.com>
2025-04-27 21:59:02 +0900

    feat(winningLotto): implement winning lotto class

--- src/main/kotlin/lotto/WinningLotto.kt
+++ src/main/kotlin/lotto/WinningLotto.kt
@@ -1,15 +1,22 @@
 package lotto
 
 class WinningLotto(
-    private val winningNumbers: Lotto,
+    private val winningNumbers: List<Int>,
     private val bonusNumber: Int
 ) {
     init {
         require(bonusNumber in 1..45) { "[ERROR] Bonus number must be between 1 and 45." }
-        require(!winningNumbers.matchNumbers().contains(bonusNumber)) { "[ERROR] Winning numbers must not contain bonus number." }
+        require(!winningNumbers.contains(bonusNumber)) { "[ERROR] Winning numbers must not contain bonus number." }
 
-        require(winningNumbers.matchNumbers().size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
-        require(winningNumbers.matchNumbers().distinct().size == 6) { "[ERROR] Lotto numbers must be unique." }
-        require(winningNumbers.matchNumbers().all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
+        require(winningNumbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(winningNumbers.distinct().size == 6) { "[ERROR] Lotto numbers must be unique." }
+        require(winningNumbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
+    }
+
+    fun getWinningNumbers(): List<Int> {
+        return winningNumbers
+    }
+    fun getBonusNumber(): Int {
+        return bonusNumber
     }
 }
