03981b41b8d232c904f7d84f326f295a5fda7977
권장순 <jsoonworld@gmail.com>
2025-04-27 09:07:13 +0900

    refactor(Lottos) : change matchAgainst parameter type to Lotto

--- src/main/kotlin/lotto/domain/Lottos.kt
+++ src/main/kotlin/lotto/domain/Lottos.kt
@@ -3,8 +3,8 @@ package lotto.domain
 import lotto.Lotto
 
 class Lottos(private val lottos: List<Lotto>) {
-    fun matchAgainst(winningNumbers: List<Int>, bonusNumber: BonusNumber): LottoResults {
-        val matchResults = lottos.map { it.toMatchResult(winningNumbers, bonusNumber.number()) }
+    fun matchAgainst(winningNumbers: Lotto, bonusNumber: BonusNumber): LottoResults {
+        val matchResults = lottos.map { it.toMatchResult(winningNumbers.numbers(), bonusNumber.number()) }
         return LottoResults(matchResults)
     }
 }
