50021c175bfdca8bb2bf7e6fc6ac1ff4db845c30
Sunho12 <rhkrtjsgh1210@gmail.com>
2025-04-27 12:32:55 +0900

    fix: Add duplicate number validation within Lotto class

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,8 +1,17 @@
 package lotto
 
+import lotto.util.InputValidator
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+
+        val uniqueNums = numbers.toSet()
+        if (uniqueNums.size != numbers.size) {
+            throw IllegalArgumentException("[ERROR] Duplicate numbers.")
+        }
+
+        require(numbers.all { it in 1..45}) { "[ERROR] Invalid number in range [1, 45]."}
     }
 
     fun countMatchingNumbers(otherNumbers: List<Int>): Int {
