e916e40699302181d5711872994b7f041b154dfd
madhumitha murali <mithaeie@gmail.com>
2025-04-27 16:31:51 +0200

    feat(generateTickets):Call generateTicket method of Random class

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,6 +2,10 @@ package lotto
 
 fun main() {
     val input=Input()
+    val output = Output()
 
     val noOfTickets = input.getPurchaseAmount()
+    val tickets = Random(noOfTickets).generateTickets()
+
+    output.displayTickets(tickets)
 }
