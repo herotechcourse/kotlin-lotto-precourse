98bd82298ba66767347a88a07c941d354ea30e37
DOL-DOM <haesolj@naver.com>
2025-04-28 01:06:11 +0900

    feat: InputView.kt to get last week's winning numbers

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -13,16 +13,11 @@ object InputView {
             ?: throw IllegalArgumentException("[ERROR] Please enter a valid number.")
     }
 //get which number won last week
-    fun readWinningNumbers(): List<Int> {
-        println("Please enter last week's winning numbers.")
-        val input = Console.readLine()
-        return input.split(",")
-            .map { it.trim() }
-            .map {
-                it.toIntOrNull()
-                    ?: throw IllegalArgumentException("[ERROR] Winning numbers must be numbers.")
-            }
-    }
+fun readWinningNumbers(): List<Int> {
+    println("Please enter last week's winning numbers.")
+    val input = Console.readLine()
+    return parseNumbers(input)
+}
 //bonus input
     fun readBonusNumber(): Int {
         println("Please enter the bonus number.")
@@ -30,4 +25,13 @@ object InputView {
         return input.toIntOrNull()
             ?: throw IllegalArgumentException("[ERROR] Bonus number must be a valid number.")
     }
+    private fun parseNumbers(input: String): List<Int> {
+        return input.split(",")
+            .map { it.trim() }
+            .map {
+                it.toIntOrNull()
+                    ?: throw IllegalArgumentException("[ERROR] Winning numbers must be valid numbers.")
+            }
+    }
+
 }
