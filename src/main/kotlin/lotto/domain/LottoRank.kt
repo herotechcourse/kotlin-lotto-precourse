bfbf8ee24798dd9ec147128742e24f19563ffa83
yeonupark <idepix@naver.com>
2025-04-27 21:44:09 +0900

    feat(domain): add LottoRank enum class to configure the matches list

--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -1,2 +1,36 @@
-package lotto
+package lotto.domain
 
+enum class LottoRank (
+    private val matchCount: Int,
+    private val prizeMoney: Int
+) {
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    NONE(0, 0);
+
+    fun getResultMessage(count: Int): String {
+        return when (this) {
+            SECOND -> "5 Matches + Bonus Ball (${prizeMoney.format()}} KRW) – $count tickets"
+            else -> "$matchCount Matches (${prizeMoney.format()} KRW) – $count tickets"
+        }
+    }
+
+    companion object {
+
+        fun findBy(matchCount: Int, bonusMatched: Boolean): LottoRank {
+            return when {
+                matchCount == 6 -> FIRST
+                matchCount == 5 && bonusMatched -> SECOND
+                matchCount == 5 -> THIRD
+                matchCount == 4 -> FOURTH
+                matchCount == 3 -> FIFTH
+                else -> NONE
+            }
+        }
+    }
+
+    private fun Int.format() = "%,d".format(this)
+}
