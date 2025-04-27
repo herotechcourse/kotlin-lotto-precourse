bc021e14371c07d87ccaf20b4950b70765edcb01
yeonupark <idepix@naver.com>
2025-04-28 02:06:11 +0900

    feat(domain): implement lottery machine logic in main function

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,20 +1,40 @@
 package lotto
 
+import lotto.utils.getValidatedInput
 import lotto.domain.InputValidator
 import lotto.messages.PromptMessages
 import lotto.view.InputView
+import lotto.view.OutputView
 
 fun main() {
 
-    var purchaseAmount: Int
-    while (true) {
-        val input = InputView().readOneLine(PromptMessages.PURCHASE_AMOUNT.message)
-        try {
-            purchaseAmount = InputValidator().validatePurchaseInput(input)
-            break
-        } catch (e: IllegalArgumentException) {
-            println("Error: ${e.message}")
-        }
-    }
+    val inputView = InputView()
+    val validator = InputValidator()
 
+    val purchaseAmount = getValidatedInput(
+        prompt = PromptMessages.PURCHASE_AMOUNT.message,
+        readInput = { inputView.readOneLine(it) },
+        validate = { validator.validatePurchaseInput(it) }
+    )
+
+    val lottoMachine = LottoMachine(purchaseAmount)
+    val outputView = OutputView(lottoMachine)
+
+    outputView.printTicketCount()
+    outputView.printLottoNumbers()
+
+    val winningNumbers = getValidatedInput(
+        prompt = PromptMessages.WINNING_NUMBERS.message,
+        readInput = { inputView.readOneLine(it) },
+        validate = { validator.validateWinningNumbers(it) }
+    )
+
+    val bonusNumber = getValidatedInput(
+        prompt = PromptMessages.BONUS_NUMBER.message,
+        readInput = { inputView.readOneLine(it) },
+        validate = { validator.validateBonusNumber(it, winningNumbers) }
+    )
+
+    outputView.printWinningStatistics(winningNumbers, bonusNumber)
+    outputView.printReturnRate()
 }
