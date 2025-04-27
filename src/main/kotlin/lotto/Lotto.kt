4b7e143bef6a4aa43419b881e624874681d05eb6
VittorioDeMarzi <vittoriodm7@gmail.com>
2025-04-27 13:23:28 +0200

    feat(lotto): add constructor validations uniqueness, and number range
    Added require checks in Lotto constructor to ensure that the numbers list contains exactly 6 unique numbers, all within the range of 1 to 45.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,6 +4,8 @@ class Lotto(private val numbers: List<Int>) {
 
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.distinct().size == 6) { "[ERROR] Winning numbers must be unique." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] Winning numbers must be between 1 and 45." }
     }
 
     fun getNumbers(): List<Int> = numbers.toList()
