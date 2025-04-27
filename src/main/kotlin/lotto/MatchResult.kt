2938be03b111c0932e76462982d7448ddb687d0b
YongLeeCode <yonghwan311@gmail.com>
2025-04-27 22:00:23 +0900

    feat(match result): implement MatchResult enum for getting lotto result

---
+++ src/main/kotlin/lotto/MatchResult.kt
@@ -0,0 +1,23 @@
+package lotto
+
+enum class MatchResult(val matchCount: Int, val bonusResult: Boolean, val prize: Int) {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    NONE(0, false, 0);
+
+    companion object {
+        fun of(matchCount: Int, matchBonus: Boolean): MatchResult {
+            return when {
+                matchCount == 6 -> FIRST
+                matchCount == 5 && matchBonus -> SECOND
+                matchCount == 5 -> THIRD
+                matchCount == 4 -> FOURTH
+                matchCount == 3 -> FIFTH
+                else -> NONE
+            }
+        }
+    }
+}
