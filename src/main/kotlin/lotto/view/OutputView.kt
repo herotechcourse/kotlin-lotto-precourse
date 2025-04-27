5f846a8c92e888bb6cc24c0ed8218a05416beecd
Taekeundo <pleasepose@gmail.com>
2025-04-27 22:50:56 +0200

    feat(view): implement OutputView
    - For printing lottos, match results, and profit rate
    - View or Formatting only: Not contain any logic or codition
    - Apply en datch(\u2013) to match result format
    
    Responsibility
    - Print purchase lottos
    - Print formatted match results
    - Print totla profit rate

---
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -0,0 +1,36 @@
+package lotto.view
+
+import lotto.Lotto
+import lotto.domain.Rank
+import lotto.domain.MatchResult
+
+object OutputView {
+    fun printPurchaseLottos(lottos: List<Lotto>) {
+        println("You have purchased ${lottos.size} tickets.")
+        lottos.forEach { lotto ->
+            println(lotto.getNumbers())
+        }
+    }
+
+    fun printMatchResult(matchResult: MatchResult) {
+        println("\nWinning Statistics")
+        println("---")
+        Rank.values()
+            .filter { it != Rank.MISS }
+            .sortedBy { it.prize }
+            .forEach { rank ->
+                println(formatMatchResultLine(rank, matchResult.getCount(rank)))
+            }
+    }
+
+    fun printProfitRate(profitRate: Double) {
+        println("Total return rate is ${profitRate}%.")
+    }
+
+    // \u2013: (en dash) "–", not same with short hyphen ("-")
+    private fun formatMatchResultLine(rank: Rank, count: Int): String {
+        return "${rank.getDescription()} (${rank.prize.toKRWFormat()}) \u2013 ${count} tickets"
+    }
+
+    private fun Int.toKRWFormat(): String = "%,d KRW".format(this)
+}
