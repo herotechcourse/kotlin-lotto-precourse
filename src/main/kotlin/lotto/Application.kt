92ad831e8919339503f22a4e34772e43eebde6cc
Tochukwu <Tochukwu.O-Munonye@fcmb.com>
2025-04-27 20:12:41 +0200

    feat: Add Lotto class and LottoService to generate random unique tickets

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     Application().run()
@@ -9,9 +10,11 @@ fun main() {
 class Application {
 
     private val inputView = InputView()
+    private val lottoService = LottoService()
 
     fun run() {
         val amount = inputView.readPurchaseAmount()
+        val tickets = lottoService.generateTickets(amount)
     }
 }
 
@@ -33,5 +36,18 @@ class InputView {
         val amount = input.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] Amount must be a number")
         if (amount % 100 != 0)  throw IllegalArgumentException("[ERROR] Amount must be divisible by 1,000.")
         return amount
+    }
+}
+
+class LottoService {
+
+    fun generateTickets(amount: Int): List<Lotto> {
+        val ticketCount = amount / 1000
+        return List(ticketCount) { generateTicket() }
+    }
+
+    private fun generateTicket(): Lotto {
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+        return Lotto(numbers)
     }
 }
