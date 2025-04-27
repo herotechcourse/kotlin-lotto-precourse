74f9f9328d0c9720b28c617f139e4461e1e05fe1
haebyun <bhk4392@gmail.com>
2025-04-27 19:56:08 +0900

    test: add additional unit tests for Lotto validation and matchCount

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -11,7 +12,6 @@ class LottoTest {
         }
     }
 
-    // TODO: Implement production code to pass the test
     @Test
     fun `throws an exception when lotto numbers contain duplicates`() {
         assertThrows<IllegalArgumentException> {
@@ -19,5 +19,40 @@ class LottoTest {
         }
     }
 
-    // TODO: Implement tests based on the added features
+    @Test
+    fun `throws an exception when lotto numbers are less than six`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5)) // 5개
+        }
+    }
+
+    @Test
+    fun `throws an exception when lotto numbers are out of range`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(0, 2, 3, 4, 5, 6))
+        }
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 46))
+        }
+    }
+
+    @Test
+    fun `returns correct matching count when compared with another Lotto`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val other = Lotto(listOf(4, 5, 6, 7, 8, 9))
+
+        val matchCount = lotto.matchCount(other)
+
+        assertThat(matchCount).isEqualTo(3)  //4, 5, 6
+    }
+
+    @Test
+    fun `returns zero when no numbers match`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val other = Lotto(listOf(7, 8, 9, 10, 11, 12))
+
+        val matchCount = lotto.matchCount(other)
+
+        assertThat(matchCount).isEqualTo(0)
+    }
 }
