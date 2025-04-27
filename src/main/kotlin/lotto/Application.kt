1b96e234866dc2a68fe21c8a94c37a8137a83624
asena <asena@asenaui-MacBookAir.local>
2025-04-28 01:07:50 +0900

    feat(input): implement read user input of purchase amount, winning numbers, bonus number with validation check

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,47 @@
 package lotto
+import lotto.view.InputView
+
+class Application {
+    private val inputView = InputView()
+
+    fun run() {
+        val money = readPurchaseWithRetry()
+        val winningNumbers = readWinningNumbersWithRetry()
+        val bonusNumber = readBonusNumberWithRetry(winningNumbers)
+
+    }
+
+    private fun readPurchaseWithRetry(): Int {
+        while (true) {
+            try {
+                return inputView.readPurchaseAmount()
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun readWinningNumbersWithRetry(): List<Int> {
+        while (true) {
+            try {
+                return inputView.readWinningNumbers()
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun readBonusNumberWithRetry(winningNumbers: List<Int>): Int {
+        while (true) {
+            try {
+                return inputView.readBonusNumber(winningNumbers)
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+}
 
 fun main() {
-    // TODO: Implement the program
+    Application().run()
 }
