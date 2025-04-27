595c5f151d825db4a1b693ae60d77d34bbb14821
Tochukwu <Tochukwu.O-Munonye@fcmb.com>
2025-04-27 19:56:39 +0200

    feat: Implement InputView to read and validate purchase amount

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,37 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: Implement the program
+    Application().run()
+}
+
+class Application {
+
+    private val inputView = InputView()
+
+    fun run() {
+        val amount = inputView.readPurchaseAmount()
+    }
+}
+
+class InputView {
+
+    fun readPurchaseAmount(): Int {
+        while (true) {
+            try {
+                println("Please enter the purchase amount.")
+                val input = Console.readLine() ?: throw IllegalArgumentException("[ERROR] Input cannot be null.")
+                return validateAmount(input)
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun validateAmount(input: String): Int {
+        val amount = input.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] Amount must be a number")
+        if (amount % 100 != 0)  throw IllegalArgumentException("[ERROR] Amount must be divisible by 1,000.")
+        return amount
+    }
 }
