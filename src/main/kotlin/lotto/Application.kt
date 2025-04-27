6572448476af527e6c5e3fe89ad51f66e8a66987
arash-039 <arrash18s@gmail.com>
2025-04-27 17:23:04 +0330

    refactor(Application): refactored main() to include processLotto

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,24 +5,11 @@ const val WINNING_NUMBERS_TEXT = "Please enter last week's winning numbers."
 const val BONUS_NUMBER_TEXT = "Please enter the bonus amount."
 
 fun main() {
-    val inputView = InputView()
-    val outputView = OutputView()
     val lottoProcessor = LottoProcessor()
 
     while (true) {
         try {
-            val purchaseAmount = InputView.getPurchaseAmount(inputView.inputReader(PURCHASE_AMOUNT_TEXT))
-            val tickets = lottoProcessor.ticketGenerator(purchaseAmount)
-            outputView.displayTickets(tickets)
-
-            val winningNumbers = InputView.getWinningNumbers(inputView.inputReader(WINNING_NUMBERS_TEXT))
-            val bonusNumber = InputView.getBonusNumber(inputView.inputReader(BONUS_NUMBER_TEXT))
-
-            val prizes = lottoProcessor.prizeCalculator(tickets, winningNumbers, bonusNumber)
-            val profitRate = lottoProcessor.profitRateCalculator(purchaseAmount, prizes)
-
-            outputView.displayStatistics(prizes)
-            outputView.displayProfitRate(profitRate)
+            lottoProcessor.processLotto(InputView.getPurchaseAmount(InputView.inputReader(PURCHASE_AMOUNT_TEXT)))
             break
         } catch (e: IllegalArgumentException) {
             println("${e.message}\nTry again\n" )
