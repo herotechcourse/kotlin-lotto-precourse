67ca4f8b0a09a5db0003e004e916d16aad3a02c4
seunzu <0123suh@gmail.com>
2025-04-28 02:27:08 +0900

    refactor(view): simplify input validation by removing unnecessary try-catch

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,35 +1,22 @@
 package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
-import lotto.exception.ApplicationException
 import lotto.util.Validator
 
 object InputView {
 
     fun readPurchaseAmount(): Int {
         println("Please enter the purchase amount.")
-        return readInput { Validator.validateAmount(Console.readLine()) }
+        return Validator.validateAmount(Console.readLine())
     }
 
     fun readWinningNumbers(): List<Int> {
         println("Please enter last week's winning numbers.")
-        return readInput { Validator.validateWinningNumbers(Console.readLine()) }
+        return Validator.validateWinningNumbers(Console.readLine())
     }
 
     fun readBonusNumber(): Int {
         println("Please enter the bonus number.")
-        return readInput { Validator.validateBonusNumber(Console.readLine()) }
-    }
-
-    private inline fun <T> readInput(block: () -> T): T {
-        while (true) {
-            try {
-                return block()
-            } catch (e: ApplicationException) {
-                println(e.message)
-            } catch (e: IllegalArgumentException) {
-                println("[ERROR] Invalid input. Please try again.")
-            }
-        }
+        return Validator.validateBonusNumber(Console.readLine())
     }
 }
