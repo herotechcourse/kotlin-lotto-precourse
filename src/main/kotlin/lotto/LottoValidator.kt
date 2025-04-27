4921e0174c0ce4ce5fd0c936d6240ec29ad79473
minjung <minjung0768@gmail.com>
2025-04-27 19:57:30 +0900

    feat(validator): implement validation for winning numbers
    - Validate that the user input is unique
    - Ensure all numbers are within the range of 1-45
    - Check that exactly 6 numbers are provided

--- src/main/kotlin/lotto/LottoValidator.kt
+++ src/main/kotlin/lotto/LottoValidator.kt
@@ -7,6 +7,13 @@ class LottoValidator {
         validateIsDivisibleBy1000(amount)
     }
 
+    fun validateWinningNumbers(winningNumbers: List<String>) {
+        validateAllNumbersAreNumeric(winningNumbers)
+        validateSizeIsSix(winningNumbers)
+        validateRange1to45(winningNumbers)
+        validateNoDuplicates(winningNumbers)
+    }
+
     private fun validateIsNumeric(amount: String) {
         if (amount.toIntOrNull() == null) {
             throw IllegalArgumentException("[ERROR] Purchase amount must be a number.")
@@ -22,6 +29,30 @@ class LottoValidator {
     private fun validateIsDivisibleBy1000(amount: String) {
         if (amount.toInt() % 1000 != 0) {
             throw IllegalArgumentException("[ERROR] Purchase amount must be divisible by 1,000.")
+        }
+    }
+
+    private fun validateAllNumbersAreNumeric(winningNumbers: List<String>) {
+        if (winningNumbers.any { it.toIntOrNull() == null }) {
+            throw IllegalArgumentException("[ERROR] Winning numbers must be numbers.")
+        }
+    }
+
+    private fun validateSizeIsSix(winningNumbers: List<String>) {
+        if (winningNumbers.size != 6) {
+            throw IllegalArgumentException("[ERROR] Winning numbers size should be 6.")
+        }
+    }
+
+    private fun validateRange1to45(winningNumbers: List<String>) {
+        if (winningNumbers.any { it.toInt() < 1 } || winningNumbers.any { it.toInt() > 45 }) {
+            throw IllegalArgumentException("[ERROR] Winning numbers must be in the range of 1 to 45.")
+        }
+    }
+
+    private fun validateNoDuplicates(winningNumbers: List<String>) {
+        if (winningNumbers.distinct().size != winningNumbers.size) {
+            throw IllegalArgumentException("[ERROR] Winning numbers must be unique.")
         }
     }
 }
