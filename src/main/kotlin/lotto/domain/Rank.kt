1d126345226ce012d8e5f76f27158edd45447a64
anshu2323 <abshahi2311@icloud.com>
2025-04-27 16:27:22 +0200

    feat(domain): define Rank enum for lotto result ranking

--- src/main/kotlin/lotto/domain/Rank.kt
+++ src/main/kotlin/lotto/domain/Rank.kt
@@ -1,2 +1,17 @@
 package lotto.domain
 
+enum class Rank(val match: Int, val prize: Int, val requiresBonus: Boolean = false) {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000);
+
+    companion object {
+        fun from(matchCount: Int, bonusMatch: Boolean): Rank? {
+            return values().find {
+                it.match == matchCount && (it.requiresBonus == bonusMatch || !it.requiresBonus)
+            }
+        }
+    }
+}
