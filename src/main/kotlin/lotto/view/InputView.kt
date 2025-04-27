01f2037128da1e5111a02b8606ea0598ffcaa32d
Taekeundo <pleasepose@gmail.com>
2025-04-28 00:47:29 +0200

    refactor(view): delegat valida to InputValidator
    - Ensure InputView only handles I/O without any business logic

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -5,18 +5,23 @@ import camp.nextstep.edu.missionutils.Console
 object InputView {
     fun readPurchaseAmount(): Int {
         println("Please enter the purchase amount.")
-        return Console.readLine().toInt()
+        val input = Console.readLine()
+        InputValidator.validatePurchaseAmount(input)
+        return input.toInt()
     }
 
     fun readWinningNumbers(): List<Int> {
         println("Please enter last week's winning numbers.")
-        return Console.readLine()
-            .split(",")
+        val input = Console.readLine()
+        InputValidator.validateWinningNumbers(input)
+        return input.split(",")
             .map { it.trim().toInt() }
     }
 
     fun readBonusNumber(): Int {
         println("Please enter the bonus number.")
-        return Console.readLine().toInt()
+        val input = Console.readLine()
+        InputValidator.validateBonusNumber(input)
+        return input.toInt()
     }
 }
