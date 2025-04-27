0d1246aa4e95fc2e13b1195604cdd437792e140c
Mariia Kulikova <111587633+mariyakulikova@users.noreply.github.com>
2025-04-27 16:43:12 +0200

    feat(lotto): check for unique numbers and numbers in range

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,13 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.size == numbers.toSet().size) { "[ERROR] Lotto numbers must be unique." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] Winning numbers must be between 1 and 45." }
     }
 
     fun getNumbers(): List<Int> = numbers.toList()
+
+    override fun toString(): String {
+        return numbers.toString()
+    }
 }
