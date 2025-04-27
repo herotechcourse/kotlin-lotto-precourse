64869a4c235241e326f15ac09aab38e9c7bd8df0
sominyun <somin455@naver.com>
2025-04-28 03:21:57 +0900

    feat: print winning statistics output

--- src/main/kotlin/lotto/ui/OutputView.kt
+++ src/main/kotlin/lotto/ui/OutputView.kt
@@ -1,10 +1,13 @@
 package lotto.ui
 
 import lotto.Lotto
+import lotto.LottoResult
 import lotto.PurchaseAmount
 
 object OutputView {
     private const val PURCHASED_TICKETS_MESSAGE = "You have purchased %d tickets."
+    private const val WINNING_STATICS_OUTPUT_MESSAGE = "Winning Statistics\n" + "---"
+    private const val TICKET_RESULT_MESSAGE = "%s (%s KRW) – %d tickets"
 
     fun printLotteryTicketsOutputMessage(purchaseAmount: PurchaseAmount, lottoList: List<Lotto>) {
         println()
@@ -13,5 +16,16 @@ object OutputView {
             println(lotto.getNumbers())
         }
         println()
+    }
+
+    fun printWinningStatisticsOutputMessage(lottoResult: LottoResult) {
+        println(WINNING_STATICS_OUTPUT_MESSAGE)
+        lottoResult.getResult().forEach { result ->
+            println(
+                TICKET_RESULT_MESSAGE.format(
+                    result.key.message, String.format("%,d", result.key.prize), result.value
+                )
+            )
+        }
     }
 }
