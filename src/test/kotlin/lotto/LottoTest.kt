0aecb22e3e75f9d902b326293f36d7b3852cb18f
Mariia Kulikova <111587633+mariyakulikova@users.noreply.github.com>
2025-04-27 16:42:26 +0200

    test(lotto): add missing tests
    test for unique numbers in lotto ticket
    test for number are in range

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -8,16 +9,24 @@ class LottoTest {
     fun `throws an exception when lotto numbers exceed six`() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
-        }
+        } .also { e -> assertTrue(e.message!!.startsWith(ERROR_MESSAGE)) }
     }
 
-    // TODO: Implement production code to pass the test
     @Test
     fun `throws an exception when lotto numbers contain duplicates`() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 5))
-        }
+        } .also { e -> assertTrue(e.message!!.startsWith(ERROR_MESSAGE)) }
     }
 
-    // TODO: Implement tests based on the added features
+    @Test
+    fun `throws an exception when lotto numbers contain numbers out of range from 1 to 45`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3333, 4, 5, 6))
+        } .also { e -> assertTrue(e.message!!.startsWith(ERROR_MESSAGE)) }
+    }
+
+    companion object {
+        private const val ERROR_MESSAGE: String = "[ERROR]"
+    }
 }
