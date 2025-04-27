d4adca97b388d7078852a340c6e083569dd92689
MukhailoK <kolotajlo17@gmail.com>
2025-04-27 19:20:02 +0200

    feat(lotto): implement Lotto model with validation and tests

---
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -0,0 +1,17 @@
+package lotto.model
+
+class Lotto(numbers: List<Int>) {
+    val numbers: List<Int>
+
+    init {
+        require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.toSet().size == 6) { "[ERROR] Lotto numbers must be unique." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
+
+        this.numbers = numbers.sorted()
+    }
+
+    override fun toString(): String {
+        return numbers.toString()
+    }
+}
