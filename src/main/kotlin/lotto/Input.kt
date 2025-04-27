f300efab3799f018028a706b15a3e58caef36077
madhumitha murali <mithaeie@gmail.com>
2025-04-27 16:21:21 +0200

    feat(getPurchaseAmount):get purchase amount and calculate number of tickets

---
+++ src/main/kotlin/lotto/Input.kt
@@ -0,0 +1,37 @@
+package lotto
+import camp.nextstep.edu.missionutils.Console
+
+class Input {
+
+    //1. find number of tickets that can be purchased with the purchase amount entered
+    fun getPurchaseAmount(): Int {
+        while (true) {
+            try {
+                println("Please enter the purchase amount: ")
+                val input = Console.readLine()?.toIntOrNull()
+                    ?: throw IllegalArgumentException("Purchase amount must be a valid number.")
+
+                validatePurchaseAmount(input)
+
+                val tickets = input / Input.DIVISIBLE_AMOUNT
+                println("\nYou have purchased $tickets tickets.")
+                return tickets
+            } catch (e: IllegalArgumentException) {
+                println("[ERROR] ${e.message}")
+            }catch (e: IllegalStateException) {
+                println("[testERROR] ${e.message}")
+            }
+        }
+
+    }
+
+    private fun validatePurchaseAmount(input: Int) {
+        require(input > 0) { "Purchase amount must be greater than zero." }
+        require(input % Input.DIVISIBLE_AMOUNT == 0) { "Purchase amount must be divisible by $Input.DIVISIBLE_AMOUNT KRW." }
+    }
+
+    companion object {
+        const val DIVISIBLE_AMOUNT = 1000
+
+    }
+}
