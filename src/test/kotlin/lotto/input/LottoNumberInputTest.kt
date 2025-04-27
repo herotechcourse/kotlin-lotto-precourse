85e26d41580a70a2b1a023730d207a40e98e7f9c
YongLeeCode <yonghwan311@gmail.com>
2025-04-27 18:01:34 +0900

    test(lotto): add tests for validating lotto number inputs

--- src/test/kotlin/lotto/input/LottoNumberInputTest.kt
+++ src/test/kotlin/lotto/input/LottoNumberInputTest.kt
@@ -1,38 +1,97 @@
 package lotto.input
 
 import lotto.Lotto
+import lotto.WinningLotto
 import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class LottoNumberInputTest {
     @Test
-    fun `should create Lotto when given 6 unique numbers between 1 and 45`() {
+    fun `should throw exception when Lotto is not given 6 unique numbers`() {
+        // given
+        val lottoNumbers: List<Int> = listOf(1, 2, 3, 4, 5, 6)
+
+        // when
+        val exception = assertThrows<IllegalArgumentException> {
+            Lotto(lottoNumbers)
+        }
+
+        // then
+        assertTrue(exception.message!!.contains("[ERROR]"))
 
     }
 
     @Test
     fun `should throw exception when less than 6 numbers are given`() {
+        // given
+        val lottoNumbers: List<Int> = listOf(1, 2, 3, 4, 5)
 
+        // when
+        val exception = assertThrows<IllegalArgumentException> {
+            Lotto(lottoNumbers)
+        }
+
+        // then
+        assertTrue(exception.message!!.contains("[ERROR]"))
     }
 
     @Test
     fun `should throw exception when duplicate numbers are given`() {
+        // given
+        val lottoNumbers: List<Int> = listOf(1, 2, 3, 4, 6, 6)
 
+        // when
+        val exception = assertThrows<IllegalArgumentException> {
+            Lotto(lottoNumbers)
+        }
+
+        // then
+        assertTrue(exception.message!!.contains("[ERROR]"))
     }
 
     @Test
     fun `should throw exception when numbers are out of range`() {
+        // given
+        val lottoNumbers: List<Int> = listOf(1, 2, 3, 4, 5, 46)
 
+        // when
+        val exception = assertThrows<IllegalArgumentException> {
+            Lotto(lottoNumbers)
+        }
+
+        // then
+        assertTrue(exception.message!!.contains("[ERROR]"))
     }
 
     @Test
     fun `should throw exception when bonus number is out of range`() {
+        // given
+        val lotto: Lotto = Lotto(listOf(1,2,3,4,5,6))
+        val bonusNumber: Int = 46
 
+        // when
+        val exception = assertThrows<IllegalArgumentException> {
+            WinningLotto(lotto, bonusNumber)
+        }
+
+        // then
+        assertTrue(exception.message!!.contains("[ERROR]"))
     }
 
     @Test
     fun `should throw exception when bonus number is duplicated with main numbers`() {
+        // given
+        val lotto: Lotto = Lotto(listOf(1,2,3,4,5,6))
+        val bonusNumber: Int = 6
 
+        // when
+        val exception = assertThrows<IllegalArgumentException> {
+            WinningLotto(lotto, bonusNumber)
+        }
+
+        // then
+        assertTrue(exception.message!!.contains("[ERROR]"))
     }
 }
