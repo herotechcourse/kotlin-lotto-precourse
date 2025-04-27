8b94801667c6f207f90a6bdfdaa50b8f769d2a6b
madhumitha murali <mithaeie@gmail.com>
2025-04-27 17:30:53 +0200

    feat(displayWinningStatistics): Implement function to display winning statistics along with the count of ticket

--- src/main/kotlin/lotto/Output.kt
+++ src/main/kotlin/lotto/Output.kt
@@ -2,6 +2,16 @@ package lotto
 
 class Output {
 
+    //5. Display the winning statistics with the number of matches in each ticket
+    fun displayWinningStatistics(resultCount: MutableMap<LottoEnum, Int>) {
+        println("Winning Statistics")
+        println("---")
+
+        LottoEnum.entries.forEach { result ->
+            println("${result.Output} – ${resultCount.getValue(result)} tickets")
+        }
+    }
+
     fun displayTickets(tickets: MutableList<MutableList<Int>>) {
         tickets.forEach { ticket ->
             println(ticket)
