dd086cc07290c6abffec79be2d8b6de684ea9abc
minjung <minjung0768@gmail.com>
2025-04-28 02:50:04 +0900

    feat(output): implement displayPrizeDistribution, displayTotalReturnRate

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -8,10 +8,22 @@ class OutputView {
         lottoTickets.forEach { println(it.numberList) }
     }
 
+    fun displayPrizeDistribution(prizeCountList: MutableMap<Prize, Int>) {
+        println(Messages.WINNING_STATISTICS)
+        prizeCountList.forEach{
+            println("${it.key.condition} (${it.key.getFormattedMoney()} KRW) – ${it.value} tickets")
+        }
+    }
+
+    fun displayTotalReturnRate(numberOfTickets: Int, totalPrizeMoney:Int) {
+        println(Messages.TOTAL_RETURN_RATE_PREFIX + (totalPrizeMoney / (numberOfTickets * 1000.0)) * 100 + Messages.RETURN_RATE_UNIT)
+    }
+
     companion object Messages {
         const val PURCHASED_TICKETS_PREFIX = "/nYou have purchased "
         const val TICKET_UNIT = " tickets/n"
         const val WINNING_STATISTICS = "Winning Statistics\n---"
         const val TOTAL_RETURN_RATE_PREFIX = "Total return rate is "
+        const val RETURN_RATE_UNIT = "%."
     }
 }
