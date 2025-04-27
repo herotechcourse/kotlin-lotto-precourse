58296589c84d5fe96d22b29102e088dc011dcd45
seunzu <0123suh@gmail.com>
2025-04-28 01:54:30 +0900

    feat(controller): implement flow controller

---
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -0,0 +1,20 @@
+package lotto.controller
+
+import lotto.service.LottoService
+import lotto.view.InputView
+import lotto.view.OutputView
+
+class LottoController {
+
+    fun run() {
+        val purchaseAmount = InputView.readPurchaseAmount()
+        val lottoTickets = LottoService.purchaseTickets(purchaseAmount)
+        OutputView.printTickets(lottoTickets.getTickets())
+
+        val winningNumbers = InputView.readWinningNumbers()
+        val bonusNumber = InputView.readBonusNumber()
+
+        val result = LottoService.calculateResult(lottoTickets, winningNumbers, bonusNumber)
+        OutputView.printResult(result)
+    }
+}
