0c9061ba7652ccd998de8d259738aef92f73eb03
VittorioDeMarzi <vittoriodm7@gmail.com>
2025-04-27 17:46:14 +0200

    Refactor Prize enum: change winningPrize from String to Int for better data handling

--- src/main/kotlin/lotto/core/Prize.kt
+++ src/main/kotlin/lotto/core/Prize.kt
@@ -1,10 +1,15 @@
 package lotto.core
 
-enum class Prize(matches: Int, winningPrize: String, hasBonus: Boolean, val display: String) {
-    FIRST(matches = 6, winningPrize = "2,000,000,000 KRW", hasBonus = false, "6 Matches (2,000,000,000 KRW)"),
-    SECOND(matches = 5, winningPrize = "30,000,000 KRW", hasBonus = true, "5 Matches + Bonus Ball (30,000,000 KRW)"),
-    THIRD(matches = 5, winningPrize = "1,500,000 KRW", hasBonus = false, "5 Matches (1,500,000 KRW)"),
-    FOURTH(matches = 4, winningPrize = "50,000 KRW", hasBonus = false, "4 Matches (50,000 KRW)"),
-    FIFTH(matches = 3, winningPrize = "5,000 KRW", hasBonus = false, "3 Matches (5,000 KRW)"),
-    NONE(matches = 0, winningPrize = "0 KRW", hasBonus = false, "")
+enum class Prize(
+    val matches: Int,
+    val winningPrize: Int,
+    val hasBonus: Boolean,
+    val display: String) {
+
+    FIRST(matches = 6, winningPrize = 2_000_000_000, hasBonus = false, "6 Matches (2,000,000,000 KRW)"),
+    SECOND(matches = 5, winningPrize = 30_000_000, hasBonus = true, "5 Matches + Bonus Ball (30,000,000 KRW)"),
+    THIRD(matches = 5, winningPrize = 1_500_000, hasBonus = false, "5 Matches (1,500,000 KRW)"),
+    FOURTH(matches = 4, winningPrize = 50_000, hasBonus = false, "4 Matches (50,000 KRW)"),
+    FIFTH(matches = 3, winningPrize = 5_000, hasBonus = false, "3 Matches (5,000 KRW)"),
+    NONE(matches = 0, winningPrize = 0, hasBonus = false, "")
 }
