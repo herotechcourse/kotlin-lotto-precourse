534a0a95fcbc60dcccff66292c8574a42a23a68f
Raheleh bayat <raheleh.bayat.92@gmail.com>
2025-04-27 10:31:37 +0200

    test: Add unit tests for Lotto and LottoService classes

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.Assertions.*
 
 class LottoTest {
     @Test
@@ -19,5 +20,34 @@ class LottoTest {
         }
     }
 
-    // TODO: Implement tests based on the added features
+    @Test
+    fun `throws an exception when lotto numbers are less than six`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5))
+        }
+    }
+
+    @Test
+    fun `throws an exception when lotto numbers are out of range`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(0, 2, 3, 4, 5, 6)) // 0 is invalid
+        }
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 46)) // 46 is invalid
+        }
+    }
+
+    @Test
+    fun `creates a valid Lotto when six unique numbers between 1 and 45 are given`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val numbers = lotto.getNumbers()
+        assertEquals(6, numbers.size)
+        assertEquals(listOf(1, 2, 3, 4, 5, 6), numbers) // should be sorted
+    }
+
+    @Test
+    fun `lotto numbers should always be sorted`() {
+        val lotto = Lotto(listOf(6, 1, 5, 2, 4, 3))
+        assertEquals(listOf(1, 2, 3, 4, 5, 6), lotto.getNumbers())
+    }
 }
