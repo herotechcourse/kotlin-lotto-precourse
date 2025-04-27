ee29181078ab878b13391fe27eec0b29b9ba4986
knopgm <knop.gm@gmail.com>
2025-04-27 14:34:10 +0200

    test: strengthen ticket validation by adding more tests and revert prize calculation "fix"

--- src/main/kotlin/lotto/TicketChecker.kt
+++ src/main/kotlin/lotto/TicketChecker.kt
@@ -8,9 +8,7 @@ data class TicketResults(
 
 class TicketChecker() {
 
-    fun getPrize(winningNumbers: List<Int>, bonusNumber: Int, matchedNumbersQuantity: Int): Prize {
-        val hasBonusNumber = bonusNumber in winningNumbers
-
+    fun getPrize(hasBonusNumber: Boolean, matchedNumbersQuantity: Int): Prize {
         val prize = when (matchedNumbersQuantity) {
             3 -> Prize.FIFTH
             4 -> Prize.FOURTH
@@ -23,18 +21,23 @@ class TicketChecker() {
     }
 
     fun checkTicket(lotto: Lotto, winningNumbers: List<Int>, bonusNumber: Int): TicketResults {
-        val matchedNumbers = lotto.numbers.count { it in winningNumbers }
-        val hasBonusNumber = bonusNumber in winningNumbers
+        val matchedNumbersQuantity = lotto.numbers.count { it in winningNumbers }
+        val hasBonusNumber = bonusNumber in lotto.numbers
 
-        val resultPrize = getPrize(winningNumbers, bonusNumber, matchedNumbers)
-        val oneTicketResult = TicketResults(matchedNumbers, hasBonusNumber, resultPrize)
+        val resultPrize = getPrize(hasBonusNumber, matchedNumbersQuantity)
+        val oneTicketResult = TicketResults(matchedNumbersQuantity, hasBonusNumber, resultPrize)
 
         return oneTicketResult
     }
 
-    fun calculateTicketsResults(lottoList: List<Lotto>, winningNumbers: List<Int>, bonusNumber: Int): List<TicketResults> {
+    fun calculateTicketsResults(
+        lottoList: List<Lotto>,
+        winningNumbers: List<Int>,
+        bonusNumber: Int
+    ): List<TicketResults> {
         val allResults = lottoList.map { lotto -> checkTicket(lotto, winningNumbers, bonusNumber) }
-        val winningResults: List<TicketResults> = allResults.filter { it.prize != Prize.NO_PRIZE }
+        val winningResults: List<TicketResults> =
+            allResults.filter { it.prize != Prize.NO_PRIZE }.sortedByDescending { it.prize }
 
         return winningResults
     }
--- src/test/kotlin/lotto/TicketCheckerTest.kt
+++ src/test/kotlin/lotto/TicketCheckerTest.kt
@@ -19,51 +19,47 @@ class TicketCheckerTest {
 
     @Test
     fun `assert that getPrize is called with its arguments`() {
-        val winningNumbers = listOf(1, 2, 3, 14, 25, 8)
-        val bonusNumber = 7
+        val hasBonusNumber = false
         val matchedNumbersQuantity = 3
         val ticketChecker = TicketChecker()
         assertSimpleTest {
-            ticketChecker.getPrize(winningNumbers, bonusNumber, matchedNumbersQuantity)
+            ticketChecker.getPrize(hasBonusNumber, matchedNumbersQuantity)
         }
     }
 
     @Test
     fun `assert that when matchedNumbersQuantity is equal 3, getPrize returns FIFTH`() {
-        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
-        val bonusNumber = 7
+        val hasBonusNumber = false
         val matchedNumbersQuantity = 3
         val ticketChecker = TicketChecker()
         val prize = Prize.FIFTH
         assertEquals(
             prize,
-            ticketChecker.getPrize(winningNumbers, bonusNumber, matchedNumbersQuantity)
+            ticketChecker.getPrize(hasBonusNumber, matchedNumbersQuantity)
         )
     }
 
     @Test
     fun `assert that when matchedNumbersQuantity is equal 0, getPrize returns NO_PRIZE`() {
-        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
-        val bonusNumber = 7
+        val hasBonusNumber = false
         val matchedNumbersQuantity = 0
         val ticketChecker = TicketChecker()
         val prize = Prize.NO_PRIZE
         assertEquals(
             prize,
-            ticketChecker.getPrize(winningNumbers, bonusNumber, matchedNumbersQuantity)
+            ticketChecker.getPrize(hasBonusNumber, matchedNumbersQuantity)
         )
     }
 
     @Test
     fun `assert that when matchedNumbersQuantity is equal 5 and has the bonus number included on the lotto ticket, getPrize returns SECOND`() {
-        val winningNumbers = listOf(1, 2, 3, 4, 5, 7)
-        val bonusNumber = 7
+        val hasBonusNumber = true
         val matchedNumbersQuantity = 5
         val ticketChecker = TicketChecker()
         val prize = Prize.SECOND
         assertEquals(
             prize,
-            ticketChecker.getPrize(winningNumbers, bonusNumber, matchedNumbersQuantity)
+            ticketChecker.getPrize(hasBonusNumber, matchedNumbersQuantity)
         )
     }
 
@@ -73,11 +69,67 @@ class TicketCheckerTest {
         val lottoTickets = lottoGenerator.generateMultiples(2)
         val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
         val bonusNumber = 7
-        val prizeList = mutableListOf<Prize>()
+        val expected = listOf<TicketResults>()
         val ticketChecker = TicketChecker()
         assertEquals(
-            prizeList,
-            ticketChecker.calculateTicketsResults(lottoTickets, winningNumbers, bonusNumber )
+            expected,
+            ticketChecker.calculateTicketsResults(lottoTickets, winningNumbers, bonusNumber)
         )
     }
+
+    @Test
+    fun `assert that 6 matching numbers without bonus returns FIRST prize`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 9
+        val expected = TicketResults(6, false, Prize.FIRST)
+        val ticketChecker = TicketChecker()
+        val ticketResult = ticketChecker.checkTicket(lotto, winningNumbers, bonusNumber)
+        assertEquals(expected, ticketResult)
+    }
+
+    @Test
+    fun `assert that 5 matching numbers without bonus returns THIRD prize`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 45))
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 9
+        val expected = TicketResults(5, false, Prize.THIRD)
+        val ticketChecker = TicketChecker()
+        val ticketResult = ticketChecker.checkTicket(lotto, winningNumbers, bonusNumber)
+        assertEquals(expected, ticketResult)
+    }
+
+    @Test
+    fun `assert that 5 matching numbers with bonus returns SECOND prize`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 9))
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 9
+        val expected = TicketResults(5, true, Prize.SECOND)
+        val ticketChecker = TicketChecker()
+        val ticketResult = ticketChecker.checkTicket(lotto, winningNumbers, bonusNumber)
+        assertEquals(expected, ticketResult)
+    }
+
+    @Test
+    fun `assert that 4 matching numbers with bonus returns FOURTH prize`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 11, 10))
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 9
+        val expected = TicketResults(4, false, Prize.FOURTH)
+        val ticketChecker = TicketChecker()
+        val ticketResult = ticketChecker.checkTicket(lotto, winningNumbers, bonusNumber)
+        assertEquals(expected, ticketResult)
+    }
+
+    @Test
+    fun `assert that 3 matching numbers with bonus returns FIFTH prize`() {
+        val lotto = Lotto(listOf(1, 2, 3, 12, 11, 10))
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 9
+        val expected = TicketResults(3, false, Prize.FIFTH)
+        val ticketChecker = TicketChecker()
+        val ticketResult = ticketChecker.checkTicket(lotto, winningNumbers, bonusNumber)
+        assertEquals(expected, ticketResult)
+    }
+
 }
