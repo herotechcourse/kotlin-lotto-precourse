91253d21f31bfba8605182aedb4e4e54e5d2f057
sominyun <somin455@naver.com>
2025-04-28 03:09:31 +0900

    feat: add lotto error message

--- src/main/kotlin/lotto/business/LottoErrorMessage.kt
+++ src/main/kotlin/lotto/business/LottoErrorMessage.kt
@@ -3,6 +3,10 @@ package lotto.business
 enum class LottoErrorMessage(private val errorMessage: String) {
     PURCHASE_AMOUNT_NOT_INTEGER("The purchase amount must be an integer."),
     INVALID_PURCHASE_AMOUNT_UNIT("Purchase amount must be a positive integer and a multiple of 1000."),
+
+    LOTTO_NUMBER_DUPLICATED("Lotto numbers cannot be duplicated"),
+    INVALID_LOTTO_NUMBER_RANGE("Lotto numbers must be between 1 and 45."),
+    INVALID_LOTTO_NUMBER_SIZE("Lotto must contain exactly 6 numbers."),
     ;
 
     val message: String
