fe7a6c2a4d56ebd5b46a1dc3d5d02f34a2bba4d7
ppour-ba <pouriya.pourbahrami@gmail.com>
2025-04-27 16:50:45 +0200

    feat(application): add main function to start LottoController

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,10 +1,7 @@
 package lotto
 
-import lotto.controller.InputHandler
-import lotto.domain.LottoMachine
-import lotto.view.OutputView
+import lotto.controller.LottoController
 
 fun main() {
-    val  lottoMachine = LottoMachine(InputHandler.requestPurchaseAmount())
-    OutputView.printTickets(lottoMachine.tickets)
+    LottoController.start()
 }
