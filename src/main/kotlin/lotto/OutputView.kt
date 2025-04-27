48f978da093bc232c42772f654101c7f45d22a87
arash-039 <arrash18s@gmail.com>
2025-04-27 15:57:28 +0330

    refactor(OutputView): made changes to output view
    - Added methods to companion object
    
    - merger displayProfitRate() with displayStatistics()

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -1,24 +1,24 @@
 package lotto
 
 class OutputView {
-    fun displayTickets(tickets: List<Lotto>) {
-        println("You have purchased ${tickets.size} tickets.")
+    companion object {
+        fun displayTickets(tickets: List<Lotto>) {
+            println("You have purchased ${tickets.size} tickets.")
 
-        tickets.forEach { println(it.getNumbers()) }
-    }
-
-    fun displayStatistics(prizeCategories: Map<Prize, Int>) {
-        println("Lotto result statistics:")
-
-        Prize.entries.reversed().forEach { prize ->
-            if (prize != Prize.NONE) {
-                val count = prizeCategories.getOrDefault(prize, 0)
-                println("${prize.description} – $count tickets")
-            }
+            tickets.forEach { println(it.getNumbers()) }
         }
-    }
 
-    fun displayProfitRate(profitRate: Double) {
-        println("Total return rate is %.1f%%.".format(profitRate))
+        fun displayStatistics(prizeCategories: Map<Prize, Int>, profitRate: Double) {
+            println("Lotto result statistics:")
+
+            Prize.entries.reversed().forEach { prize ->
+                if (prize != Prize.NONE) {
+                    val count = prizeCategories.getOrDefault(prize, 0)
+                    println("${prize.description} – $count tickets")
+                }
+            }
+
+            println("Total return rate is %.1f%%.".format(profitRate))
+        }
     }
 }
