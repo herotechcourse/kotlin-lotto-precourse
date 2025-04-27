ed1765790aa65688fa2c91c0022bd335ab947efc
rwaeng <rwaeng0421@gmail.com>
2025-04-27 17:04:30 +0900

    feat(Application): implement lotto game flow

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,20 @@
 package lotto
 
+import lotto.view.InputView
+import lotto.view.OutputView
+
 fun main() {
-    // TODO: Implement the program
+    val amount: Int = InputView.getPurchaseAmount()
+
+    val lottos: List<Lotto> = LottoGenerator.generate(amount)
+    OutputView.printTickets(lottos)
+
+    val winningNumbers: List<Int> = InputView.getWinningNumbers()
+    val bonusNumber: Int = InputView.getBonusNumber()
+
+    val ranker = LottoRanker(lottos, winningNumbers, bonusNumber)
+
+    val statistics = ranker.rankLotto()
+    val profit = ranker.calculateProfit(statistics)
+    OutputView.printStatistics(statistics, profit)
 }
