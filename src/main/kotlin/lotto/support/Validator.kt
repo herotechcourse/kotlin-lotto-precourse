42fca1eabe4af115a3f892185e15db994c0c15e7
ppour-ba <pouriya.pourbahrami@gmail.com>
2025-04-27 16:58:04 +0200

    refactor(validator): improve Validator checks for winning ticket and bonus number
    - Add size and duplicate checks for winning ticket
    - Fix range validation logic for winning numbers and bonus number
    - Adjust validateBonusNumber to check for duplicates against ticket

--- src/main/kotlin/lotto/support/Validator.kt
+++ src/main/kotlin/lotto/support/Validator.kt
@@ -3,21 +3,20 @@ package lotto.support
 import lotto.support.Message
 
 object Validator {
-    private const val TICKET_PRICE= 1000
-    private const val MAX_LOTTO_NUMBER = 45
-    private const val MIN_LOTTO_NUMBER = 1
-
     fun validatePurchaseAmount(amount: Int){
         require(amount >= TICKET_PRICE) { Message.ERROR_AMOUNT_TOO_SMALL }
         require(amount % TICKET_PRICE == 0) { Message.ERROR_AMOUNT_NOT_DIVISIBLE }
     }
 
-    fun validateWinningTicket(winner: List<Int>){
-        for (number in winner)
-            require(number <= MAX_LOTTO_NUMBER || number >= MIN_LOTTO_NUMBER) { Message.ERROR_WINNER_RANGE }
+    fun validateWinningTicket(ticket: List<Int>){
+        require(ticket.size == TICKET_NUMBERS_COUNT) {Message.ERROR_WINNING_NUMBERS_COUNT}
+        require(ticket.toSet().size == TICKET_NUMBERS_COUNT) { Message.ERROR_WINNING_NUMBERS_DUPLICATE }
+        for (number in ticket)
+            require(number in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) { Message.ERROR_WINNER_RANGE }
     }
 
-   fun validateBonusNumber(number: Int){
-       require(number <= MAX_LOTTO_NUMBER || number >= MIN_LOTTO_NUMBER) { Message.ERROR_BONUS_RANGE }
+   fun validateBonusNumber(bonus: Int, ticket: List<Int>){
+       require(bonus in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) { Message.ERROR_BONUS_RANGE }
+       require(bonus !in ticket) { Message.ERROR_BONUS_DUPLICATE }
    }
 }
