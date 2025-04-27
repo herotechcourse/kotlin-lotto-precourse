51246b960b024cbcdd72880e47181a4fab08c192
sominyun <somin455@naver.com>
2025-04-28 03:12:32 +0900

    feat: get winner numbers input

--- src/main/kotlin/lotto/ui/InputView.kt
+++ src/main/kotlin/lotto/ui/InputView.kt
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console
 
 object InputView {
     private const val PURCHASE_AMOUNT_INPUT_MESSAGE = "Please enter the purchase amount."
+    private const val WINNING_NUMBERS_INPUT_MESSAGE = "Please enter last week's winning numbers."
 
     fun getPurchaseAmount(): String {
         println(PURCHASE_AMOUNT_INPUT_MESSAGE)
+        return Console.readLine()
+    }
+
+    fun getWinningNumbers(): String {
+        println(WINNING_NUMBERS_INPUT_MESSAGE)
         return Console.readLine()
     }
 }
