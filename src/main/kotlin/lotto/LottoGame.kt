2ce2a9682b9ca66d99010f79673d6cc24d684aaf
minjung <minjung0768@gmail.com>
2025-04-28 03:03:19 +0900

    feat(game): implement displayResult method to show prize distribution and total return rate
    Implemented the displayResult method to display lottery game results

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -17,6 +17,8 @@ class LottoGame (
 
         initWinningNumbers()
         initBonusNumber()
+
+        displayResult(lottoList)
     }
 
     private fun getNumberOfTickets(): Int = repeatUntilSuccess {
@@ -41,13 +43,13 @@ class LottoGame (
     private fun initWinningNumbers() = repeatUntilSuccess {
         val input: List<String> = inputView.readWinningNumbers()
         lottoValidator.validateWinningNumbers(input)
-        winningCombination = WinningCombination(input.map { it.toInt() }, 0)
+        winningCombination = WinningCombination(input.map { it.toInt() }, 0, 0)
     }
 
     private fun initBonusNumber() = repeatUntilSuccess {
         val input = inputView.readBonusNumber()
         lottoValidator.validateBonusNumber(input, winningCombination.winningNumberList)
-        winningCombination = WinningCombination(winningCombination.winningNumberList, input.toInt())
+        winningCombination = WinningCombination(winningCombination.winningNumberList, input.toInt(), 0)
     }
 
     private fun <T> repeatUntilSuccess(action: () -> T): T {
@@ -56,7 +58,14 @@ class LottoGame (
                 return action()
             } catch (e: IllegalArgumentException) {
                 println(e.message)
+                println()
             }
         }
+    }
+
+    private fun displayResult(lottoTickets: List<Lotto>) {
+        val prizeCountList = winningCombination.match(lottoTickets)
+        outputView.displayPrizeDistribution(prizeCountList)
+        outputView.displayTotalReturnRate(lottoTickets.size, winningCombination.getTotalPrize())
     }
 }
