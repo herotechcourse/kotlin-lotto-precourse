7146f34d74e14befe270109135a212071dd9c9a3
Marc Rodenbusch <87020460+MacMosby@users.noreply.github.com>
2025-04-27 18:21:06 +0200

    feature(Lotto): Add function to validate ticket numbers
    Add validateNumbers() to:
    - Validate that ticket numbers are all unique
    Add validateNumbers() to Lotto init block to:
    - Validate that only valid numbers initialize a new Lotto ticket

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,15 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        validateNumbers(this.numbers)
     }
 
     // TODO: Implement additional functions
+    fun validateNumbers(numbers: List<Int>) {
+        for (number in numbers) {
+            if (numbers.indexOf(number) != numbers.lastIndexOf(number)) {
+                throw IllegalArgumentException("[ERROR] Lotto numbers must be unique.")
+            }
+        }
+    }
 }
