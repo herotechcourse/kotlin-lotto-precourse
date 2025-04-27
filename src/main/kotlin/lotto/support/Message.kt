34609f3a0917c6f290641c45acc90c9c2de5602f
ppour-ba <pouriya.pourbahrami@gmail.com>
2025-04-27 10:15:53 +0200

    feat(outputview): add printTickets function to display purchased tickets
    -fix(message): correct ticket purchase message format for String.format

--- src/main/kotlin/lotto/support/Message.kt
+++ src/main/kotlin/lotto/support/Message.kt
@@ -6,6 +6,8 @@ object Message {
     const val ERROR_AMOUNT_NOT_DIVISIBLE = "[ERROR] Purchase amount must be divisible by 1,000."
     const val ERROR_INPUT_NOT_INT = "[ERROR] Input must be a number."
     const val UNKNOWN_ERROR = "[ERROR] Unknown error occurred."
+    const val ERROR_LOTTO_NUMBERS_COUNT = "[ERROR] Lotto must contain exactly 6 numbers."
     // Prompt Messages
     const val PURCHASE_AMOUNT_PROMPT = "Please enter the purchase amount."
+    const val PURCHASED_TICKETS_MESSAGE = "You have purchased %d tickets."
 }
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,11 +1,21 @@
 package lotto.view
 
 import lotto.support.Message
+import lotto.Lotto
 
 object OutputView {
+
     fun printPurchaseAmountPrompt() {
         println(Message.PURCHASE_AMOUNT_PROMPT)
     }
+
+    fun printTickets(tickets: List<Lotto>){
+        println()
+        println(Message.PURCHASED_TICKETS_MESSAGE.format(tickets.size))
+        for (ticket in tickets)
+            println(ticket)
+    }
+
     fun printErrorMessage(message: String?) {
         println(message ?: Message.UNKNOWN_ERROR)
     }
