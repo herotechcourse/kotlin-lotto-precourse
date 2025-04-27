1faa7c9a7e9d7ddd2462dd824e92159459eea458
DOL-DOM <haesolj@naver.com>
2025-04-28 00:30:17 +0900

    feat: Add functions to Lotto for number sorting and matching, bonus

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,5 +5,13 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
     }
 
-    // TODO: Implement additional functions
+    fun getNumbers(): List<Int> = numbers.sorted()
+
+    fun matchCount(winningNumbers: Set<Int>): Int {
+        return numbers.count { it in winningNumbers }
+    }
+
+    fun containsBonusNumber(bonusNumber: Int): Boolean {
+        return bonusNumber in numbers
+    }
 }
