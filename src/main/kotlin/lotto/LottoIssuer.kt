d4ba50c668f1748da444d1536d99c52dd7d0727c
Mans228 <zhenismansur@gmail.com>
2025-04-27 18:19:54 +0200

    refactor(issue lotto): rename lotto issuer

--- src/main/kotlin/lotto/LottoIssuer.kt
+++ src/main/kotlin/lotto/LottoIssuer.kt
@@ -1,12 +1,8 @@
 package lotto
 
-class LottoMachine {
+class LottoIssuer {
 
     fun issueLottos(purchaseAmount: Int): List<Lotto> {
-        require(purchaseAmount >= 1000 && purchaseAmount % 1000 == 0) {
-            "[ERROR] Purchase amount must be a multiple of 1,000."
-        }
-
         val ticketCount = purchaseAmount / 1000
 
         return List(ticketCount) { Lotto.generateRandomLotto() }
