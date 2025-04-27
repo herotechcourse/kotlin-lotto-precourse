6736525ba10c2fcf66599c9f0d560678ebafd512
sominyun <somin455@naver.com>
2025-04-28 03:07:55 +0900

    feat: add PurchaseAmount getter

--- src/main/kotlin/lotto/PurchaseAmount.kt
+++ src/main/kotlin/lotto/PurchaseAmount.kt
@@ -3,9 +3,14 @@ package lotto
 import lotto.business.LottoErrorMessage
 
 class PurchaseAmount(private val purchaseAmount: Int) {
+    val ticketCount
+        get() = purchaseAmount / 1000
+
     init {
         require(purchaseAmount > 0 && purchaseAmount % 1000 == 0) { LottoErrorMessage.INVALID_PURCHASE_AMOUNT_UNIT.message }
     }
+
+    fun getPurchaseAmount() = purchaseAmount
 
     companion object {
         fun from(purchaseAmount: String): PurchaseAmount {
