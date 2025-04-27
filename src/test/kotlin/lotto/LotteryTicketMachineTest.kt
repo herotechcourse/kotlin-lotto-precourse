6aea8cc61794335583ecbe515a3331f334112683
Marc Rodenbusch <87020460+MacMosby@users.noreply.github.com>
2025-04-27 16:54:45 +0200

    test(LotteryTicketMachineTest): Add tests to validate winning numbers and bonus number
    Add tests to validate winning numbers:
    - Validate that exactly 6 winning numbers are provided
    - Validate that winning numbers do not have duplicates
    - Validate that winning numbers are in the range from 1 to 45
    Add tests to validate bonus number:
    - Validate that bonus number is in the range from 1 to 45
    - Validate that bonus number does not have duplicate in winning numbers

--- src/test/kotlin/lotto/LotteryTicketMachineTest.kt
+++ src/test/kotlin/lotto/LotteryTicketMachineTest.kt
@@ -10,4 +10,56 @@ class LotteryTicketMachineTest {
             LotteryTicketMachine.validatePurchaseAmount(1500)
         }
     }
+
+    @Test
+    fun `throws exception when more than six winning numbers are provided`() {
+        assertThrows<IllegalArgumentException> {
+            LotteryTicketMachine.validateWinningNumbers(listOf(1, 2, 3, 4, 5, 6, 7))
+        }
+    }
+
+    @Test
+    fun `throws exception when less than six winning numbers are provided`() {
+        assertThrows<IllegalArgumentException> {
+            LotteryTicketMachine.validateWinningNumbers(listOf(1, 2, 3, 4, 5))
+        }
+    }
+
+    @Test
+    fun `throws exception when winning numbers have duplicate`() {
+        assertThrows<IllegalArgumentException> {
+            LotteryTicketMachine.validateWinningNumbers(listOf(1, 2, 3, 4, 5, 5))
+        }
+    }
+
+    @Test
+    fun `throws exception when winning numbers are not in range from 1 to 45`() {
+        assertThrows<IllegalArgumentException> {
+            LotteryTicketMachine.validateWinningNumbers(listOf(1, 2, 3, 4, 5, 46))
+        }
+    }
+
+    @Test
+    fun `does not throw exception when winning numbers are six, in range from 1 to 45 and do not have duplicates`() {
+        LotteryTicketMachine.validateWinningNumbers(listOf(1, 2, 3, 4, 5, 6))
+    }
+
+    @Test
+    fun `throws exception when bonus number is not in range from 1 to 45`() {
+        assertThrows<IllegalArgumentException> {
+            LotteryTicketMachine.validateBonusNumber(listOf(1, 2, 3, 4, 5, 6), 46)
+        }
+    }
+
+    @Test
+    fun `throws exception when bonus number is already in winning numbers`() {
+        assertThrows<IllegalArgumentException> {
+            LotteryTicketMachine.validateBonusNumber(listOf(1, 2, 3, 4, 5, 6), 6)
+        }
+    }
+
+    @Test
+    fun `does not throw exception when bonus number is in range from 1 to 45 and does not have a duplicate in winning numbers`() {
+        LotteryTicketMachine.validateBonusNumber(listOf(1, 2, 3, 4, 5, 6), 7)
+    }
 }
