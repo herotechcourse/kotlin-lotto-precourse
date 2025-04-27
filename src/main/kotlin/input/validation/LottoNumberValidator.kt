f4293d70634015dc6e3802607eb9c8fcb8c266fc
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-27 18:27:26 +0900

    feat(input): add number validation for lotto number

--- src/main/kotlin/input/validation/LottoNumberValidator.kt
+++ src/main/kotlin/input/validation/LottoNumberValidator.kt
@@ -1,2 +1,12 @@
 package input.validation
 
+internal const val INVALID_LOTTO_NUMBER_MSG = "Numbers must be in the range from 1 to 45."
+
+internal const val MIN_LOTTO_NUMBER = 1
+internal const val MAX_LOTTO_NUMBER = 45
+
+
+fun validateLottoNumber(num: Int) {
+    if(num !in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER)
+        throw IllegalArgumentException(INVALID_LOTTO_NUMBER_MSG)
+}
