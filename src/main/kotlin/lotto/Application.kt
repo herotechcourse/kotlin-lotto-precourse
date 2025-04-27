8e59f271e70b0a1c4deb5143277b40180d703003
hiyoons <arin9825@naver.com>
2025-04-27 22:03:16 +0900

    feat(Application): implement lotto main

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,4 +2,9 @@ package lotto
 
 fun main() {
     // TODO: Implement the program
+    val inputView = InputView()
+    val ticketGenerate = inputView.run()
+    println()
+    val lotto=Lotto(inputView.readWinningNumbers())
+    lotto.run(ticketGenerate,inputView.getPurchase())
 }
