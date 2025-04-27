c2145781d90bb5cf905509a7fe9fc51b144e2dd1
NGOC NGUYEN <thaingoc273@gmail.com>
2025-04-27 17:01:11 +0200

    feature: test implementation

--- src/test/kotlin/lotto/ApplicationTest.kt
+++ src/test/kotlin/lotto/ApplicationTest.kt
@@ -74,6 +74,41 @@ class ApplicationTest : NsTest() {
             listOf(1, 3, 5, 14, 22, 45),
         )
     }
+
+    @Test
+    fun `feature test second price`() {
+        assertRandomUniqueNumbersInRangeTest(
+            {
+                run("8000", "1,2,3,4,5,6", "7")
+                assertThat(output()).contains(
+                    "You have purchased 8 tickets.",
+                    "[1, 2, 3, 4, 5, 7]",
+                    "[3, 5, 11, 16, 32, 38]",
+                    "[7, 11, 16, 35, 36, 44]",
+                    "[1, 8, 11, 31, 41, 42]",
+                    "[13, 14, 16, 38, 42, 45]",
+                    "[7, 11, 30, 40, 42, 43]",
+                    "[2, 13, 22, 32, 38, 45]",
+                    "[1, 3, 5, 14, 22, 45]",
+                    "3 Matches (5,000 KRW) – 1 tickets",
+                    "4 Matches (50,000 KRW) – 0 tickets",
+                    "5 Matches (1,500,000 KRW) – 0 tickets",
+                    "5 Matches + Bonus Ball (30,000,000 KRW) – 1 tickets",
+                    "6 Matches (2,000,000,000 KRW) – 0 tickets",
+                    "Total return rate is 375062.5%.",
+                )
+            },
+            listOf(1, 2, 3, 4, 5, 7),
+            listOf(3, 5, 11, 16, 32, 38),
+            listOf(7, 11, 16, 35, 36, 44),
+            listOf(1, 8, 11, 31, 41, 42),
+            listOf(13, 14, 16, 38, 42, 45),
+            listOf(7, 11, 30, 40, 42, 43),
+            listOf(2, 13, 22, 32, 38, 45),
+            listOf(1, 3, 5, 14, 22, 45),
+        )
+    }
+
     @Test
     fun `exception test`() {
         assertSimpleTest {
---
+++ src/test/kotlin/lotto/LottoTicketGenerationTest.kt
@@ -0,0 +1,26 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+
+class LottoTicketGenerationTest{
+
+    @Test
+    fun `feature test ticket generation`() {
+
+        assertRandomUniqueNumbersInRangeTest(
+            {
+                val lottoTickets = LottoTicketGeneration.generateTicket(2)
+                assertThat(lottoTickets[0].getTicket()).isEqualTo(listOf(1, 2, 3, 4, 5, 6))
+                assertThat(lottoTickets[1].getTicket()).isEqualTo(listOf(10, 11, 12, 13, 14, 15))
+            },
+            listOf(1, 3, 2, 4, 5, 6),
+            listOf(10, 11, 12, 13, 14, 15),
+        )
+    }
+
+}
