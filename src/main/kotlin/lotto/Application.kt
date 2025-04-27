78705d594e4234e968bc86b99db47fddbf6842b3
seunzu <0123suh@gmail.com>
2025-04-28 02:48:47 +0900

    refactor(exception): remove ApplicationException and handle exceptions with IllegalArgumentException only

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,13 +1,10 @@
 package lotto
 
 import lotto.controller.LottoController
-import lotto.exception.ApplicationException
 
 fun main() {
     try {
         LottoController().run()
-    } catch (e: ApplicationException) {
-        println(e.message)
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
--- src/main/kotlin/lotto/exception/ApplicationException.kt
+++
@@ -1,6 +0,0 @@
-package lotto.exception
-
-class ApplicationException(private val errorCode: ErrorCode) : IllegalArgumentException() {
-    override val message: String
-        get() = errorCode.getMessage()
-}
