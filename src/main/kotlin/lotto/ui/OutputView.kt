90cba8ca8424db25ae563b720916d91e8b2e9e41
sominyun <somin455@naver.com>
2025-04-28 03:22:35 +0900

    feat: print profit rate output

--- src/main/kotlin/lotto/ui/OutputView.kt
+++ src/main/kotlin/lotto/ui/OutputView.kt
@@ -8,6 +8,7 @@ object OutputView {
     private const val PURCHASED_TICKETS_MESSAGE = "You have purchased %d tickets."
     private const val WINNING_STATICS_OUTPUT_MESSAGE = "Winning Statistics\n" + "---"
     private const val TICKET_RESULT_MESSAGE = "%s (%s KRW) – %d tickets"
+    private const val PROFIT_RATE_MESSAGE = "Total return rate is %.1f%%."
 
     fun printLotteryTicketsOutputMessage(purchaseAmount: PurchaseAmount, lottoList: List<Lotto>) {
         println()
@@ -27,5 +28,9 @@ object OutputView {
                 )
             )
         }
+    }
+
+    fun printProfitRateOutputMessage(profitRate: Float) {
+        println(PROFIT_RATE_MESSAGE.format(profitRate))
     }
 }
