6e731420f042c004fc2e178187e0a734fefa4f3c
Santiago Tena Hernandez <stenahe@gmail.com>
2025-04-27 20:49:19 +0200

    fix(LottoResult): correct spelling mistake

--- src/main/kotlin/lotto/LottoResult.kt
+++ src/main/kotlin/lotto/LottoResult.kt
@@ -3,17 +3,17 @@ package lotto
 import lotto.constants.Prizes
 
 class LottoResult {
-    var winningStatics: MutableMap<String, Int> = mutableMapOf()
+    var winningStatistics: MutableMap<String, Int> = mutableMapOf()
         private set
     var returnRate: Double = 0.0
         private set
 
     init {
-        this.winningStatics["three"] = 0
-        this.winningStatics["four"] = 0
-        this.winningStatics["five"] = 0
-        this.winningStatics["six"] = 0
-        this.winningStatics["bonus"] = 0
+        this.winningStatistics["three"] = 0
+        this.winningStatistics["four"] = 0
+        this.winningStatistics["five"] = 0
+        this.winningStatistics["six"] = 0
+        this.winningStatistics["bonus"] = 0
     }
 
     fun generateWinningStatistics(userInput: UserInput, ticketPurchase: TicketPurchase) {
@@ -28,24 +28,24 @@ class LottoResult {
         val matches = ticket.getNumbers().count { it in userInput.winningNumbers }
 
         when (matches) {
-            3 -> this.winningStatics["three"] = this.winningStatics.getOrDefault("three", 0) + 1
-            4 -> this.winningStatics["four"] = this.winningStatics.getOrDefault("four", 0) + 1
+            3 -> this.winningStatistics["three"] = this.winningStatistics.getOrDefault("three", 0) + 1
+            4 -> this.winningStatistics["four"] = this.winningStatistics.getOrDefault("four", 0) + 1
             5 -> handleFiveMatches(userInput, ticket)
-            6 -> this.winningStatics["six"] = this.winningStatics.getOrDefault("six", 0) + 1
+            6 -> this.winningStatistics["six"] = this.winningStatistics.getOrDefault("six", 0) + 1
         }
     }
 
     private fun handleFiveMatches(userInput: UserInput, ticket: Lotto) {
         if (userInput.bonusNumber in ticket.getNumbers())
-            this.winningStatics["bonus"] = this.winningStatics.getOrDefault("bonus", 0) + 1
+            this.winningStatistics["bonus"] = this.winningStatistics.getOrDefault("bonus", 0) + 1
         else
-            this.winningStatics["five"] = this.winningStatics.getOrDefault("five", 0) + 1
+            this.winningStatistics["five"] = this.winningStatistics.getOrDefault("five", 0) + 1
     }
 
     fun calculateReturnRate(userInput: UserInput) {
         var totalPrize = 0
 
-        for ((key, count) in this.winningStatics) {
+        for ((key, count) in this.winningStatistics) {
             totalPrize += when (key) {
                 "three" -> count * Prizes.FIFTH.prizeMoney
                 "four" -> count * Prizes.FOURTH.prizeMoney
