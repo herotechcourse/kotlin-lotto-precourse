b874fbeecde655b04d798fcdd84fef5c7d729761
rwaeng <rwaeng0421@gmail.com>
2025-04-27 17:26:27 +0900

    feat(Lotto): add validation of Lotto class

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,9 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.toSet().size == 6) {
+            "[ERROR] Lotto numbers must not contain duplicates."
+        }
     }
 
     fun getNumbers(): List<Int> = numbers
