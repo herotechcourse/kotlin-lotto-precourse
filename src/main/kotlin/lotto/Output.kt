ea7e7821bbb60864e422ed10060cedff8d0fe682
madhumitha murali <mithaeie@gmail.com>
2025-04-27 16:29:44 +0200

    feat(generateTickets):Implement method to display the generated tickets

--- src/main/kotlin/lotto/Output.kt
+++ src/main/kotlin/lotto/Output.kt
@@ -1,4 +1,11 @@
 package lotto
 
 class Output {
+
+    fun displayTickets(tickets: MutableList<MutableList<Int>>) {
+        tickets.forEach { ticket ->
+            println(ticket)
+
+        }
+    }
 }
