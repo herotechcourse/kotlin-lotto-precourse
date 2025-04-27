670c95204dcddbad120ccd8fed833e21cf737c6f
sungkyum-kim(Tebah) <sungkyum1@gmail.com>
2025-04-27 19:38:24 +0900

    refactor(game rules): replace filter with all for number range validation

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,7 +4,7 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "Lotto must contain exactly 6 numbers." }
         require(numbers.toSet().size == 6) { "Lotto numbers must be unique." }
-        require(numbers.filter { it in 1..45 }.size == 6) { "Lotto numbers must be in the range 1 to 45" }
+        require(numbers.all { it in 1..45 }) { "Lotto numbers must be in the range 1 to 45" }
     }
 
     fun contains(bonusNumber: Int): Boolean {
