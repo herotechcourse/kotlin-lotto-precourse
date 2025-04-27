90f98c7a3d07bfc2dcf545019f26bc6593e7251d
YongLeeCode <yonghwan311@gmail.com>
2025-04-27 22:01:27 +0900

    feat(profit): implement profit calculator

---
+++ src/main/kotlin/lotto/ProfitCaculator.kt
@@ -0,0 +1,13 @@
+package lotto
+
+object ProfitCaculator {
+    private val TICKET_PRICE: Int = 1000
+    fun calculate(map: Map<MatchResult, Int>, purchaseAmount: Int): String {
+        val totalProfit = map.entries.sumOf { (result, count) -> result.prize.toLong() * count }
+        return format((totalProfit.toDouble() / (purchaseAmount * TICKET_PRICE)) * 100)
+    }
+
+    private fun format(rate: Double): String {
+        return String.format("%.1f", rate)
+    }
+}
