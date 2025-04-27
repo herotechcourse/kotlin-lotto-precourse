da7339e03d75988f17e6ebdfa9154117a015a54b
seunzu <0123suh@gmail.com>
2025-04-28 01:49:13 +0900

    feat(validator): create Validator for input validations

---
+++ src/main/kotlin/lotto/util/Validator.kt
@@ -0,0 +1,37 @@
+package lotto.util
+
+import lotto.exception.ApplicationException
+import lotto.exception.InputException
+
+object Validator {
+
+    fun validateAmount(input: String): Int {
+        val number = input.toIntOrNull()
+            ?: throw ApplicationException(InputException.INVALID_INTEGER)
+        require(number >= 1000) { throw ApplicationException(InputException.AMOUNT_TOO_SMALL) }
+        require(number % 1000 == 0) { throw ApplicationException(InputException.AMOUNT_NOT_DIVISIBLE) }
+        return number
+    }
+
+    fun validateWinningNumbers(input: String): List<Int> {
+        val numbers = input.split(",").map {
+            it.trim().toIntOrNull() ?: throw ApplicationException(InputException.INVALID_INTEGER)
+        }
+        if (numbers.size != 6 || numbers.toSet().size != 6) {
+            throw ApplicationException(InputException.INVALID_WINNING_NUMBERS)
+        }
+        if (numbers.any { it !in 1..45 }) {
+            throw ApplicationException(InputException.NUMBER_OUT_OF_RANGE)
+        }
+        return numbers
+    }
+
+    fun validateBonusNumber(input: String): Int {
+        val number = input.toIntOrNull()
+            ?: throw ApplicationException(InputException.INVALID_INTEGER)
+        if (number !in 1..45) {
+            throw ApplicationException(InputException.NUMBER_OUT_OF_RANGE)
+        }
+        return number
+    }
+}
