eec01350c812d450a9494a7e6d34e1e40ff6872e
anshu2323 <abshahi2311@icloud.com>
2025-04-27 16:25:47 +0200

    feat(domain): add LottoMachine to generate random lotto tickets

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -1,2 +1,12 @@
 package lotto.domain
 
+import camp.nextstep.edu.missionutils.Randoms
+
+object LottoMachine {
+    fun generateTickets(count: Int): List<LottoTicket> {
+        return List(count) {
+            val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+            LottoTicket(Lotto(numbers))
+        }
+    }
+}
