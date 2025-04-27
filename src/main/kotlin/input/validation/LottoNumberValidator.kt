2c1df75a37ca529ee1a991a925f28034d1837b35
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-27 18:29:36 +0900

    feat(input): add validate and return function for lotto number

--- src/main/kotlin/input/validation/LottoNumberValidator.kt
+++ src/main/kotlin/input/validation/LottoNumberValidator.kt
@@ -5,7 +5,10 @@ internal const val INVALID_LOTTO_NUMBER_MSG = "Numbers must be in the range from
 internal const val MIN_LOTTO_NUMBER = 1
 internal const val MAX_LOTTO_NUMBER = 45
 
-
+fun validateAndReturnLottoNumber(num: Int): Int{
+    validateLottoNumber(num)
+    return num
+}
 fun validateLottoNumber(num: Int) {
     if(num !in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER)
         throw IllegalArgumentException(INVALID_LOTTO_NUMBER_MSG)
