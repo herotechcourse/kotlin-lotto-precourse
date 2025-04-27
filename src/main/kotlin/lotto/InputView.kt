d2b86ea8d0dc238a7112e4a0aad609284866426c
minjung <minjung0768@gmail.com>
2025-04-27 22:52:39 +0900

    feat(input): implement bonus number input

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -11,8 +11,14 @@ class InputView {
         return readln().split(",").map {it.trim()}
     }
 
+    fun readBonusNumber(): String {
+        println(RequestMessages.BONUS_NUMBER)
+        return readln()
+    }
+
     companion object RequestMessages {
         const val AMOUNT = "Please enter the purchase amount."
         const val WINNING_NUMBERS = "Please enter last week's winning numbers."
+        const val BONUS_NUMBER = "Please enter the bonus number."
     }
 }
