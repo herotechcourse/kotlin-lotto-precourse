d089d36b7b3399b2b09bea32779f97ad791dfad1
songpink <jejeje04@naver.com>
2025-04-27 12:26:46 +0900

    refactor: separate the constants

--- src/main/kotlin/lotto/model/random/DefaultRandomNumbersGenerator.kt
+++ src/main/kotlin/lotto/model/random/DefaultRandomNumbersGenerator.kt
@@ -1,9 +1,12 @@
 package lotto.model.random
 
 import camp.nextstep.edu.missionutils.Randoms
+import lotto.Lotto.Companion.LOTTO_NUMBER_SIZE
+import lotto.Lotto.Companion.MAXIMUM_LOTTO_NUMBER
+import lotto.Lotto.Companion.MINIMUM_LOTTO_NUMBER
 
 class DefaultRandomNumbersGenerator: RandomNumbersGenerator {
     override fun generate(): List<Int> {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        return Randoms.pickUniqueNumbersInRange(MINIMUM_LOTTO_NUMBER, MAXIMUM_LOTTO_NUMBER, LOTTO_NUMBER_SIZE)
     }
 }
