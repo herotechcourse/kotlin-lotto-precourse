1fea736a936d128192b9445a8df57b925cc90b19
sungkyum-kim(Tebah) <sungkyum1@gmail.com>
2025-04-27 20:33:59 +0900

    refactor(game rules): extract constants for Budget and Lotto class

--- src/main/kotlin/lotto/Budget.kt
+++ src/main/kotlin/lotto/Budget.kt
@@ -2,10 +2,14 @@ package lotto
 
 data class Budget(val amount: Int) {
     init {
-        require(amount % 1000 == 0) { "The purchase amount must be divisible by 1,000." }
+        require(amount % LOTTO_PRICE == 0) { "The purchase amount must be divisible by 1,000." }
     }
 
     fun ticketCounts(): Int {
-        return amount / 1000
+        return amount / LOTTO_PRICE
+    }
+
+    companion object {
+        private const val LOTTO_PRICE = 1000
     }
 }
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,9 +2,9 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "Lotto must contain exactly 6 numbers." }
-        require(numbers.toSet().size == 6) { "Lotto numbers must be unique." }
-        require(numbers.all { it in 1..45 }) { "Lotto numbers must be in the range 1 to 45" }
+        require(numbers.size == NUMBER_COUNT) { "Lotto must contain exactly 6 numbers." }
+        require(numbers.toSet().size == NUMBER_COUNT) { "Lotto numbers must be unique." }
+        require(numbers.all { it in MIN_NUMBER..MAX_NUMBER }) { "Lotto numbers must be in the range 1 to 45" }
     }
 
     fun contains(bonusNumber: Int): Boolean {
@@ -17,5 +17,11 @@ class Lotto(private val numbers: List<Int>) {
 
     fun getNumbers(): List<Int> {
         return numbers
+    }
+
+    companion object {
+        private const val NUMBER_COUNT = 6
+        private const val MIN_NUMBER = 1
+        private const val MAX_NUMBER = 45
     }
 }
