8873118e60dbf09bea9769f65856adf00adc79a8
USER <rhrudtn0314@gmail.com>
2025-04-28 04:18:26 +0900

    feat(readAmount): implement function 'readAmount'
    - Implemented function 'readAmount' that get input of money and return ticket's number

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,27 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+
 fun main() {
     // TODO: Implement the program
+
+    val amount = readAmount()
+    val ticketCount = amount / 1000
+    println("You have purchased $ticketCount tickets.")
+
+
+
+}
+
+// 입력 관련 함수. 값 입력받고 복권개수 리턴
+fun readAmount(): Int {
+    println("Please enter the purchase amount.")
+    val input = Console.readLine()
+    val amount = input.toIntOrNull()
+    require(amount != null && amount % 1000 == 0) {
+        "[ERROR] Amount must be a multiple of 1000."
+    }
+    return amount
 }
