a8c306a9a8c3d277b23d55fae51ec3a69f21ecd9
DOL-DOM <haesolj@naver.com>
2025-04-28 00:45:19 +0900

    feat: Implement purchase amount input in Application.kt

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,11 @@
 package lotto
+import lotto.view.InputView
 
 fun main() {
     // TODO: Implement the program
+    val purchaseAmount = InputView.readPurchaseAmount()
+    println("입력한 구매 금액: $purchaseAmount")
+
+
+
 }
