14f353f8068fcf2eb1737a32111f05e8024329cf
seyeok94 <seyeok94@mi.fu-berlin.de>
2025-04-28 00:46:07 +0200

    implement lottery logic and total return rate

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -27,6 +27,52 @@ fun main() {
     // prompt user for the bonus number
     println("\nPlease enter the bonus number.")
     val bonusNumber = Console.readLine().toInt()
+
+    // initialise key value pair with 0
+    val results = mutableMapOf<String, Int>()
+    results["3"] = 0
+    results["4"] = 0
+    results["5"] = 0
+    results["5_bonus"] = 0
+    results["6"] = 0
+
+    // update values using the functions in Lotto class
+    for (lotto in lottoNumbers) {
+        val matchCount = lotto.countMatch(winningNumbers)
+        val hasBonus = lotto.containsBonus(bonusNumber)
+
+        if (matchCount == 6) {
+            results["6"] = results["6"]!! + 1
+        } else if (matchCount == 5 && hasBonus) {
+            results["5_bonus"] = results["5_bonus"]!! + 1
+        } else if (matchCount == 5) {
+            results["5"] = results["5"]!! + 1
+        } else if (matchCount == 4) {
+            results["4"] = results["4"]!! + 1
+        } else if (matchCount == 3) {
+            results["3"] = results["3"]!! + 1
+        }
+    }
+
+    println("\nWinning Statistics")
+    println("---")
+    println("3 Matches (5,000 KRW) – ${results["3"]} tickets")
+    println("4 Matches (50,000 KRW) – ${results["4"]} tickets")
+    println("5 Matches (1,500,000 KRW) – ${results["5"]} tickets")
+    println("5 Matches + Bonus Ball (30,000,000 KRW) – ${results["5_bonus"]} tickets")
+    println("6 Matches (2,000,000,000 KRW) – ${results["6"]} tickets")
+
+    // calculate total return rate
+    val totalPrizeMoney =
+        results["3"]!! * 5000 +
+                results["4"]!! * 50000 +
+                results["5"]!! * 1500000 +
+                results["5_bonus"]!! * 30000000 +
+                results["6"]!! * 2000000000
+
+    val returnRate = (totalPrizeMoney.toDouble() / amountInput) * 100
+
+    println("Total return rate is ${String.format("%.1f", returnRate)}%.")
 }
 
 fun parseAmount(input: Int): Int {
@@ -37,11 +83,11 @@ fun parseAmount(input: Int): Int {
     return input/1000
 }
 
-fun generateLotto(tickets: Int): List<List<Int>> {
+fun generateLotto(tickets: Int): List<Lotto> {
     // generate 6 random numbers between 1 and 45 based on the number of tickets
-    val result = mutableListOf<List<Int>>()
+    val result = mutableListOf<Lotto>()
     repeat(tickets) {
-        result.add(Randoms.pickUniqueNumbersInRange(1, 45, 6))
+        result.add(Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)))
     }
     return result
 }
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,5 +5,23 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
     }
 
-    // TODO: Implement additional functions
+    // return matching numbers
+    fun countMatch(winningNumbers: List<Int>): Int {
+        var matchCount = 0
+        for (number in numbers) {
+            if (winningNumbers.contains(number)) {
+                matchCount += 1
+            }
+        }
+        return matchCount
+    }
+
+    // check bonus number in the lotto
+    fun containsBonus(bonus: Int): Boolean {
+        return numbers.contains(bonus)
+    }
+    // sorts the numbers in ascending order, then returns them as a string.
+    override fun toString(): String {
+        return numbers.sorted().toString()
+    }
 }
