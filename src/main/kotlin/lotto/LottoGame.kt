9111453b5c9079dc9307bb5670f476127035a7d9
minjung <minjung0768@gmail.com>
2025-04-27 20:39:09 +0900

    feat(game): add winningNumbers field and implement initWinningNumbers method
    - Add a field to store the winning numbers entered by the user
    - Implemented initWinningNumbers method to prompt and validate the winning numbers input

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -4,8 +4,11 @@ class LottoGame (
     private val inputView: InputView,
     private val lottoValidator: LottoValidator
 ) {
+    private var winningNumbers = emptyList<String>()
+
     fun start(){
         getNumberOfTickets()
+        initWinningNumbers()
     }
 
     private fun getNumberOfTickets(): Int{
@@ -14,6 +17,18 @@ class LottoGame (
                 val amount = inputView.readPurchaseAmount()
                 lottoValidator.validatePurchaseAmount(amount)
                 amount.toInt() / 1000
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun initWinningNumbers() {
+        while (true) {
+            try {
+                val input: List<String> = inputView.readWinningNumbers()
+                lottoValidator.validateWinningNumbers(input)
+                winningNumbers = input
             } catch (e: IllegalArgumentException) {
                 println(e.message)
             }
