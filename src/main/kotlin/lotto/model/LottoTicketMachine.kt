6146d21cdb840ceb02a0c7947f7b0f81eaebbc96
soeunnPark <carol0724@naver.com>
2025-04-27 14:43:03 +0900

    refactor(lottoTicketMachine): sort lotto numbers

--- src/main/kotlin/lotto/model/LottoTicketMachine.kt
+++ src/main/kotlin/lotto/model/LottoTicketMachine.kt
@@ -7,7 +7,7 @@ class LottoTicketMachine {
     fun buyLotto(quantity: Int) : List<Lotto> {
         val lottoTicket : MutableList<Lotto> = mutableListOf()
         for(i in 1..quantity) {
-            lottoTicket.add(Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)))
+            lottoTicket.add(Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()))
         }
         return lottoTicket
     }
