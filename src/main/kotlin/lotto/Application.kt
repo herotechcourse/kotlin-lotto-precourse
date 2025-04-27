2f371dccb8d28322e1efd5b04f9dade09954b92f
Khush Bakht Naveed <khush.naveed@dci-student.org>
2025-04-27 21:50:28 +0200

    get user input of purchase amount

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,4 +2,8 @@ package lotto
 
 fun main() {
     // TODO: Implement the program
+    println("Please enter the purchase amount.")
+    val amount = readLine()!!.toInt()
+    InputValidator.validatePurchaseAmount(amount)
+    val ticketCount = amount / 1000
 }
---
+++ src/main/kotlin/lotto/InputValidator.kt
@@ -0,0 +1,21 @@
+package lotto
+
+object InputValidator {
+    fun validatePurchaseAmount(amount: Int) {
+        if (amount % 1000 != 0) {
+            throw IllegalArgumentException("[ERROR] Amount must be divisible by 1000.")
+        }
+    }
+
+    fun validateWinningNumbers(numbers: List<Int>) {
+        if (numbers.size != 6 || numbers.any { it !in 1..45 } || numbers.toSet().size != 6) {
+            throw IllegalArgumentException("[ERROR] Winning numbers must be 6 unique numbers between 1 and 45.")
+        }
+    }
+
+    fun validateBonusNumber(bonus: Int, winningNumbers: Set<Int>) {
+        if (bonus !in 1..45 || bonus in winningNumbers) {
+            throw IllegalArgumentException("[ERROR] Bonus number must be between 1 and 45 and not duplicated.")
+        }
+    }
+}
