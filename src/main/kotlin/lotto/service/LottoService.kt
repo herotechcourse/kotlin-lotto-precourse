8f9493639e194cc96a0c2a3af43e8d5a196b61a3
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2025-04-27 10:56:37 +0200

    feat(service): add validation for winning numbers and bonus number
    - Added validation for winning numbers: ensures 6 unique numbers within the range of 1 to 45.
    - Added validation for bonus number: ensures the number is within the range of 1 to 45.

--- src/main/kotlin/lotto/service/LottoService.kt
+++ src/main/kotlin/lotto/service/LottoService.kt
@@ -14,4 +14,16 @@ object LottoService {
             val ticketNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
             Lotto(ticketNumbers)
     }
+
+    fun validateWinningNumbers(numbers: List<Int>) {
+        if (numbers.distinct().size != 6)
+            throw LottoInputException.InvalidWinningNumbers("Winning numbers must all be unique.")
+        if (numbers.any { it !in 1..45 })
+            throw LottoInputException.InvalidWinningNumbers("Numbers must be between 1 and 45.")
+    }
+
+    fun validateBonusNumber(bonus: Int) {
+        if (bonus !in 1..45)
+            throw LottoInputException.InvalidBonusNumber(bonus)
+    }
 }
