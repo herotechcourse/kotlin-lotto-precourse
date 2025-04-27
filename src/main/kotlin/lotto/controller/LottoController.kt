ef13e85bf8aa89d94bcedc1eefa8bacce6dd31cc
soeunnPark <carol0724@naver.com>
2025-04-27 15:44:28 +0900

    refactor(lottoController): separate controller logic

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -4,9 +4,7 @@ import lotto.Lotto
 import lotto.model.Game
 import lotto.model.LottoTicketMachine
 import lotto.model.WinningLotto
-import lotto.view.InputView
-import lotto.view.MatchResultDto
-import lotto.view.OutputView
+import lotto.view.*
 
 class LottoController() {
 
@@ -15,27 +13,39 @@ class LottoController() {
     private val lottoTicketMachine: LottoTicketMachine = LottoTicketMachine()
 
     fun start() {
-        outputView.askPurchaseAmount()
-        val purchaseAmount = inputView.askPurchaseAmount()
-
+        val purchaseAmount = getPurchaseAmount()
         val purchaseQuantity = purchaseAmount.amount / 1_000
 
         val lottoTicket = lottoTicketMachine.buyLotto(purchaseQuantity)
         outputView.printPurchasedLottoTicket(purchaseQuantity, lottoTicket)
 
-        outputView.askWinningNumbers()
-        val winningNumbers = inputView.askWinningNumbers()
-
-        outputView.askBonusNumber()
-        val bonusNumber = inputView.askBonusNumber()
-
-        val winningLotto = WinningLotto(Lotto(winningNumbers.winningNumbers), bonusNumber.bonusNumber)
-
+        val winningLotto = getWinningLotto()
         val game = Game(purchaseAmount.amount, lottoTicket, winningLotto)
-        val matchResult = game.match()
 
+        val matchResult = game.match()
         val matchResultDto = MatchResultDto.getMatchResultDto(matchResult)
         val returnRate = game.countReturnRate()
         outputView.printWinningStatistics(matchResultDto, returnRate)
+    }
+
+    private fun getWinningLotto(): WinningLotto {
+        val winningNumbers = getWinningNumbers()
+        val bonusNumber = getBonusNumber()
+        return WinningLotto(Lotto(winningNumbers.winningNumbers), bonusNumber.bonusNumber)
+    }
+
+    private fun getPurchaseAmount(): PurchaseAmountDto {
+        outputView.askPurchaseAmount()
+        return inputView.askPurchaseAmount()
+    }
+
+    private fun getWinningNumbers(): WinningNumbersDto {
+        outputView.askWinningNumbers()
+        return inputView.askWinningNumbers()
+    }
+
+    private fun getBonusNumber(): BonusNumberDto {
+        outputView.askBonusNumber()
+        return inputView.askBonusNumber()
     }
 }
