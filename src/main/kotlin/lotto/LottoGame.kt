3b090b4e556d4582cafdc2e8805359abdf0f9dd0
LeeJaeHyeok97 <hazardous10@naver.com>
2025-04-28 02:43:08 +0900

    refactor: separate LottoGame run logic into smaller methods

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -1,32 +1,56 @@
 package lotto
 
 class LottoGame {
-    fun run() { // Todo - method separate
-        OutputView().askPurchaseAmount()
-        val purchaseAmount = InputView().purchaseAmount()
+    fun run() {
+        val purchaseAmount = inputPurchaseAmount()
+        val lottos = generateLottos(purchaseAmount)
+        val winningLotto = inputWinningLotto()
+
+        val result = calculateResult(winningLotto, lottos, purchaseAmount)
+        printResult(result)
+    }
+
+    private fun printResult(result: Result) {
+        OutputView().printWinningStatics()
+        println(result)
+    }
+
+    private fun calculateResult(
+        winningLotto: WinningLotto,
+        lottos: Lottos,
+        purchaseAmount: String
+    ): Result {
+        return Result().apply { calculateResult(winningLotto, lottos, purchaseAmount) }
+    }
+
+    private fun inputWinningLotto(): WinningLotto {
+        OutputView().askWinningNumbers()
+        val lastWeekWinningNumbers = InputView().lastWeekWinningNumbers()
+        OutputView().askBonusNumber()
+        val lastWeekBonusNumber = InputView().lastWeekBonusNumber()
+
+        return WinningLotto().apply {
+            registerWinningNumbers(lastWeekWinningNumbers)
+            registerBonusNumber(lastWeekBonusNumber)
+        }
+    }
+
+    private fun generateLottos(purchaseAmount: String): Lottos {
         val ticketCount = calculateTicketCount(purchaseAmount)
         OutputView().printPurchasedCount(ticketCount)
         val lottos = LottoMachine().generate(ticketCount)
         OutputView().printLottos(lottos)
-
-        OutputView().askWinningNumbers()
-        val lastWeekWinningNumbers = InputView().lastWeekWinningNumbers()
-
-        val winningLotto = WinningLotto()
-        winningLotto.registerWinningNumbers(lastWeekWinningNumbers)
-        OutputView().askBonusNumber()
-        val lastWeekBonusNumber = InputView().lastWeekBonusNumber()
-        winningLotto.registerBonusNumber(lastWeekBonusNumber)
-
-        OutputView().printWinningStatics()
-        val result = Result()
-        result.calculateResult(winningLotto, lottos, purchaseAmount)
-        println(result)
+        return lottos
     }
 
-        private fun calculateTicketCount(purchaseAmount: String): Int {
-            return purchaseAmount.toInt() / LOTTO_PRICE
-        }
+    private fun inputPurchaseAmount(): String {
+        OutputView().askPurchaseAmount()
+        return InputView().purchaseAmount()
+    }
+
+    private fun calculateTicketCount(purchaseAmount: String): Int {
+        return purchaseAmount.toInt() / LOTTO_PRICE
+    }
 
     companion object {
         private const val LOTTO_PRICE = 1000
