b162b38eba03c1895ebd510ab8808b6547b15f34
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2025-04-27 18:51:06 +0200

    feat(view): enhance winning statistics display
    - Improved the printWinningStatistics method to show statistics for all prize ranks
    - Added logic to display the number of tickets matching each prize rank, including bonus
    - Updated formatting for clearer output

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,5 +1,8 @@
 package lotto.view
+
 import lotto.Lotto
+import lotto.domain.LottoResult
+import lotto.domain.PrizeRank
 
 object OutputView {
     fun printTickets(tickets: List<Lotto>) {
@@ -7,5 +10,25 @@ object OutputView {
         tickets.forEach { println(it) }
     }
 
+    fun printWinningStatistics(lottoResults: List<LottoResult>) {
+        println(Constants.STATISTICS_HEADER)
+        PrizeRank.entries
+            .filter { it != PrizeRank.NONE }
+            .sortedByDescending { it.matchCount }
+            .forEach { println(getStatisticMessageFor(it, lottoResults))}
+    }
+
     private fun getPurchaseMessage(count: Int) = "You have purchased $count tickets"
+
+    private fun getStatisticMessageFor(rank: PrizeRank, lottoResults: List<LottoResult>): String {
+        val count = lottoResults.count { it.prizeRank == rank }
+        val bonusMessage = if (rank.hasBonus) " + Bonus Ball" else ""
+        val ticketsPluralization = if (count == 1) "ticket" else "tickets"
+
+        return "${rank.matchCount} Matches$bonusMessage (${rank.prize} KRW) - ${count} $ticketsPluralization"
+    }
+
+    object Constants {
+        const val STATISTICS_HEADER = "Winning Statistics\n---"
+    }
 }
