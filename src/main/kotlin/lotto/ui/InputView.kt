d988504506466a22507e30a5b5bf07d124638dae
sominyun <somin455@naver.com>
2025-04-28 03:16:02 +0900

    feat: get bonus number input

--- src/main/kotlin/lotto/ui/InputView.kt
+++ src/main/kotlin/lotto/ui/InputView.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 object InputView {
     private const val PURCHASE_AMOUNT_INPUT_MESSAGE = "Please enter the purchase amount."
     private const val WINNING_NUMBERS_INPUT_MESSAGE = "Please enter last week's winning numbers."
+    private const val BONUS_NUMBER_INPUT_MESSAGE = "Please enter the bonus number."
 
     fun getPurchaseAmount(): String {
         println(PURCHASE_AMOUNT_INPUT_MESSAGE)
@@ -13,6 +14,11 @@ object InputView {
 
     fun getWinningNumbers(): String {
         println(WINNING_NUMBERS_INPUT_MESSAGE)
+        return Console.readLine()
+    }
+
+    fun getBonusNumber(): String {
+        println(BONUS_NUMBER_INPUT_MESSAGE)
         return Console.readLine()
     }
 }
