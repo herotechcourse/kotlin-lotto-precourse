9d869ded6ecc7f425f072c14bb2a7d1dd849cae7
sungkyum-kim(Tebah) <sungkyum1@gmail.com>
2025-04-27 19:24:59 +0900

    refactor(game rules): modify Lotto class error message

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,9 +2,9 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.size == 6) { "Lotto must contain exactly 6 numbers." }
         require(numbers.toSet().size == 6) { "Lotto numbers must be unique." }
-        require(numbers.filter { it in 1..45 }.size == 6) { "The numbers must be in the range 1 to 45" }
+        require(numbers.filter { it in 1..45 }.size == 6) { "Lotto numbers must be in the range 1 to 45" }
     }
 
     fun contains(bonusNumber: Int): Boolean {
