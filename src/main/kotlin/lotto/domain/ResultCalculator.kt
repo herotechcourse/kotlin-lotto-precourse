5635e3bb16ca080c8bc5cb996aa8a43e1ee76732
차정혁 <ckwjdgur1234@gmail.com>
2025-04-29 04:10:23 +0900

    style(result): formatted based on basic formatter

--- src/main/kotlin/lotto/domain/ResultCalculator.kt
+++ src/main/kotlin/lotto/domain/ResultCalculator.kt
@@ -1,7 +1,11 @@
 package lotto.domain
 
 object ResultCalculator {
-    fun calculateMatchedLottoCount(lottos: Lottos, winningNumbers: WinningNumbers, bonusNumber: BonusNumber): Map<LottoRank, Int> {
+    fun calculateMatchedLottoCount(
+        lottos: Lottos,
+        winningNumbers: WinningNumbers,
+        bonusNumber: BonusNumber
+    ): Map<LottoRank, Int> {
         return lottos.tickets
             .map { it.match(winningNumbers, bonusNumber) }
             .groupingBy { it }
