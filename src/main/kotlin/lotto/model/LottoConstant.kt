6677983b64a692045b9ea8e7a9d0232ef1e5cc01
jinny <ggab.challenge@gmail.com>
2025-04-28 21:24:10 +0900

    fix(winning-statistics): reorder LottoRank from FIFTH to FIRST for better readability

--- src/main/kotlin/lotto/model/LottoConstant.kt
+++ src/main/kotlin/lotto/model/LottoConstant.kt
@@ -5,9 +5,9 @@ const val LOTTO_NUMBER_END = 45
 const val LOTTO_NUMBER_COUNT = 6
 
 enum class LottoRank(val prize: Int, val comment: String) {
-    FIRST(2_000_000_000, "6 Matches"),
-    SECOND(30_000_000, "5 Matches + Bonus Ball"),
-    THIRD(1_500_000, "5 Matches"),
-    FOURTH(50_000, "4 Matches"),
     FIFTH(5_000, "3 Matches"),
+    FOURTH(50_000, "4 Matches"),
+    THIRD(1_500_000, "5 Matches"),
+    SECOND(30_000_000, "5 Matches + Bonus Ball"),
+    FIRST(2_000_000_000, "6 Matches"),
 }
