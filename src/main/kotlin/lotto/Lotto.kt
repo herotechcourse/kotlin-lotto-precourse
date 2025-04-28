493c0389091430e2b8d3ca53c750b3d04301aab5
김소운 <sozzykim@naver.com>
2025-04-28 21:28:39 +0900

    feat(lotto): implement Lotto core functions
    - Add getLottoNumbers() to get numbers
    - Add checkForWinningNumber() to count matched numbers
    - Add checkForBonusNumber() to check bonus number inclusion

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,21 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
     }
 
-    // TODO: Implement additional functions
+    fun getLottoNumbers () : List<Int> {
+        return numbers
+    }
+
+    fun checkForWinningNumber git(winningNumbers : List<Int>) : Int {
+        return numbers.count { it in winningNumbers }
+    }
+
+    fun checkForBonusNumber (bonusNumber : Int) : Boolean {
+        return bonusNumber in numbers
+    }
 }
