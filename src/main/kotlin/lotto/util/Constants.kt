98c3a46dec86ad56cfdaaa2c86003227bcc441c5
inaemin <inaemin@gmail.com>
2025-04-29 04:32:24 +0900

    feat: add constants

---
+++ src/main/kotlin/lotto/util/Constants.kt
@@ -0,0 +1,8 @@
+package lotto.util
+
+object Constants {
+    const val MIN_LOTTERY_NUM = 1
+    const val MAX_LOTTERY_NUM = 45
+    const val LOTTERY_NUMBER_COUNT = 6
+    const val PRICE_PER_LOTTO = 1000
+}
