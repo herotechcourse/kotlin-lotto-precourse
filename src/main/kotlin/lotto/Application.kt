bfd55132edfe307e9260a2566e853a76d35c4f79
sopa <sopa.sara@gmail.com>
2025-04-28 22:43:44 +0200

    feat(app): integrate WinningLotto and Rank to match tickets and assign ranks

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -10,4 +10,13 @@ fun main() {
 
     val winningNumbers = InputView.readWinningNumbers()
     val bonusNumber = InputView.readBonusNumber(winningNumbers)
+
+    val winningLotto = WinningLotto(winningNumbers, bonusNumber)
+
+    val ranks = tickets.map { ticket ->
+        val matchCount = winningLotto.matchCount(ticket)
+        val bonusMatched = winningLotto.isBonusMatched(ticket)
+        Ranks.of(matchCount, bonusMatched)
+    }
+
 }
