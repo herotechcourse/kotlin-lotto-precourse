4e34848fac3f1a5a509b5a7cb1c6842c10fdafaa
차정혁 <ckwjdgur1234@gmail.com>
2025-04-28 22:41:59 +0900

    test(winningnumbers): add test winning number not empty string

---
+++ src/test/kotlin/lotto/domain/WinningNumbersTest.kt
@@ -0,0 +1,14 @@
+package lotto.domain
+
+import lotto.Lotto
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class WinningNumbersTest {
+    @Test
+    fun `throws an exception when winning numbers is empty string`() {
+        assertThrows<IllegalArgumentException> {
+            WinningNumbers.from(listOf("1", "2", "3", "4", "5", ""))
+        }
+    }
+}
