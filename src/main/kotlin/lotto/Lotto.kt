d8f3c08aa7c879c4581558b072b09b9397e14792
Santiago Tena Hernandez <stenahe@gmail.com>
2025-04-28 02:19:02 +0200

    format(Lotto): error messages

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,12 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
-        require(numbers.size == numbers.toSet().size) { "[ERROR] All numbers must be unique" }
+        require(numbers.size == 6) {
+            "[ERROR] Lotto must contain exactly 6 numbers."
+        }
+        require(numbers.size == numbers.toSet().size) {
+            "[ERROR] All numbers must be unique"
+        }
     }
 
     fun getNumbers(): List<Int> {
