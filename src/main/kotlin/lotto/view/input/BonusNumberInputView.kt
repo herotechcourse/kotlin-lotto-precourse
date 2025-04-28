65b4ebc1fe68cbf7e196141cfda277cae535a1b0
minsang <minsang007@gmail.com>
2025-04-28 14:25:29 +0900

    fix(input): add Console import in BonusNumberInputView
    Add `import camp.nextstep.edu.missionutils.Console` to enable `Console.readLine()` usage in BonusNumberInputView.

--- src/main/kotlin/lotto/view/input/BonusNumberInputView.kt
+++ src/main/kotlin/lotto/view/input/BonusNumberInputView.kt
@@ -1,5 +1,7 @@
 package lotto.view.input
 
+import camp.nextstep.edu.missionutils.Console
+
 object BonusNumberInputView : InputView {
     override fun readInput(): String {
         println("Please enter the bonus number.")
