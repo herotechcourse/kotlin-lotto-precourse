4ad4ba5f643b5becbf6546c356bf5dca6139da05
ppour-ba <pouriya.pourbahrami@gmail.com>
2025-04-28 19:52:50 +0200

    refactor(Lotto): remove match-related state and improve ticket validation

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,34 +3,16 @@ import lotto.support.*
 import lotto.domain.Rank
 
 class Lotto(private val numbers: List<Int>) {
-    var matchCount: Int = 0
-        private set
-    var bonusMatched: Boolean = false
-        private set
-    var rank: Rank? = null
-        private set
-
-    init {
-        require(numbers.size == TICKET_NUMBERS_COUNT) { Message.ERROR_LOTTO_NUMBERS_COUNT }
+init {
+    require(numbers.toSet().size == TICKET_NUMBERS_COUNT) { Message.ERROR_WINNING_NUMBERS_DUPLICATE }
+    require(numbers.size == TICKET_NUMBERS_COUNT) { Message.ERROR_LOTTO_NUMBERS_COUNT }
     }
 
     override fun toString(): String {
         return numbers.sorted().toString()
     }
 
-    fun settMatchCount(points: Int) {
-        matchCount = points
-    }
-
-    fun markBonusMatched() {
-        bonusMatched = true
-    }
-
     fun getNumbers():List<Int> {
         return (numbers)
-    }
-
-    fun setRank(rank: Rank) {
-        this.rank = rank
     }
 }
