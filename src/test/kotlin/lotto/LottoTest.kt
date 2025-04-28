8b9ce3d84c77c710af746ee91fa204434ea93d6a
차정혁 <ckwjdgur1234@gmail.com>
2025-04-29 02:22:36 +0900

    test(lotto): test for match different ranks

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,7 +1,14 @@
 package lotto
 
+import lotto.domain.BonusNumber
+import lotto.domain.LottoGenerator
+import lotto.domain.LottoRank
+import lotto.domain.WinningNumbers
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 class LottoTest {
     @Test
@@ -29,5 +36,31 @@ class LottoTest {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(0, 2, 3, 4, 5, 6))
         }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideLottoAndExpectedRank")
+    fun `match with winning numbers and bonus number`(lottoNumbersAndExpectedRank: Pair<List<Int>, LottoRank>) {
+        val (lottoNumbers, expectedRank) = lottoNumbersAndExpectedRank
+        val lotto = Lotto(lottoNumbers)
+
+        assertThat(lotto.match(winningNumbers, bonusNumber)).isEqualTo(expectedRank)
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideLottoAndExpectedRank() = listOf(
+            listOf(1, 2, 3, 4, 5, 6) to LottoRank.FIRST,
+            listOf(1, 2, 3, 4, 5, 7) to LottoRank.SECOND,
+            listOf(1, 2, 3, 4, 5, 9) to LottoRank.THIRD,
+            listOf(1, 2, 3, 4, 8, 9) to LottoRank.FOURTH,
+            listOf(1, 2, 3, 4, 7, 9) to LottoRank.FOURTH,
+            listOf(1, 2, 3, 8, 9, 10) to LottoRank.FIFTH,
+            listOf(1, 2, 3, 7, 9, 10) to LottoRank.FIFTH,
+            listOf(1, 2, 7, 8, 9, 10) to LottoRank.NONE
+        )
+
+        private val winningNumbers = WinningNumbers(listOf(1, 2, 3, 4, 5, 6))
+        private val bonusNumber = BonusNumber(7)
     }
 }
