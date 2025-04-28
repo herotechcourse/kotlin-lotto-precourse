3beefe09aca07caab811dcfa3d2d24c741339ec7
GDMontsho <montshogoitseone@gmail.com>
2025-04-28 07:40:08 +0200

    feat: update outputview for readability and init Rank

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -1,11 +1,33 @@
 package lotto
 
+import kotlin.collections.groupingBy
+
 object OutputView {
+
     fun printTicketCount(count: Int) {
-        println("You have purchased $count tickets")
+        println("You have purchased $count tickets.")
     }
 
-    fun printTickets (tickets: List <Lotto> ) {
-        tickets.forEach {println(it.getNumbers())}
+    fun printTickets(tickets: List<Lotto>) {
+        tickets.forEach { println(it.getNumbers()) }
+    }
+
+    fun printResult(ranks: List<Rank>, amountSpent: Int) {
+        println("\nWinning Statistics")
+        println("*****************************************************")
+
+        val rankCount = ranks.groupingBy { it }.eachCount()
+
+        Rank.values()
+            .filter { it != Rank.NONE }
+            .forEach { rank ->
+                val bonusText = if (rank.requiresBonus) ", bonus matched" else ""
+                println("${rank.matchCount} matches$bonusText (${rank.prize} won) - ${rankCount[rank] ?: 0} times")
+            }
+
+        val totalPrize = ranks.sumOf { it.prize }
+        val profitRate = totalPrize.toDouble() / amountSpent * 100
+
+        println("Total return rate: ${"%.1f".format(profitRate)}%")
     }
 }
