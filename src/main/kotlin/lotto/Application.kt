7ee9c22f46fb790681e94f2d4b224678be12fc1f
haeun <jhe0903@naver.com>
2025-04-28 18:04:11 +0900

    feat(application) : implement Application main flow connecting input, logic and output

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,19 @@
 package lotto
 
 fun main() {
-    // TODO: Implement the program
+    val inputView = InputView().readAmount()
+
+    val count = inputView / 1000
+    val outputView = OutputView()
+    val lottos = LottoMachine().createTickets(count)
+
+    outputView.printResult(lottos)
+
+    val winningNum = InputView().readWinningNumbers()
+    val bonusNum = InputView().readBonusNumber()
+
+    val result = LottoResult().calculateStatics(winningNum, bonusNum, lottos)
+    val total = LottoResult().calculateTotal(result, inputView)
+
+    outputView.printStatics(result, total)
 }
