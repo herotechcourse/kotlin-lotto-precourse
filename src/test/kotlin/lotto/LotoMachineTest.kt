797aad0c9564d78b23c105812ed7f1ae8b97d7cf
atash1986 <msc.ashourzadeh@gmail.com>
2025-04-28 15:34:36 +0200

    test(LottoMachine): add test for the LottoMachine class
    - add test to verifies that the issueTickets()  generates the correct number of unique tickets

--- src/test/kotlin/lotto/LotoMachineTest.kt
+++ src/test/kotlin/lotto/LotoMachineTest.kt
@@ -14,4 +14,17 @@ class LottoMachineTest {
 
         assertEquals(5, numberOfTickets)
     }
+
+    @Test
+    fun `issueTickets should issue correct number of unique tickets`() {
+        val ticketsCount = 3
+        val lottoMachine=LottoMachine()
+        val tickets = lottoMachine.issueTickets(ticketsCount)
+
+        assertEquals(ticketsCount, tickets.size)
+        tickets.forEach { ticket ->
+            assertEquals(6, ticket.getNumbers().size)
+            assertEquals(6, ticket.getNumbers().toSet().size) 
+        }
+    }
 }
