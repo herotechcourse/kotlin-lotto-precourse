0ad3e5fa85c5795f55ac7386a1272039bb9e3087
Jiihyun <cwlgusc@gmail.com>
2025-04-28 22:52:01 +0900

    refactor: reorder PrizeRank enum declarations

--- src/main/kotlin/lotto/PrizeRank.kt
+++ src/main/kotlin/lotto/PrizeRank.kt
@@ -3,11 +3,11 @@ package lotto
 enum class PrizeRank(val matchingCount: Int, val prizeAmount: Int) {
 
     NONE(0, 0),
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
+    FIFTH(3, 5_000),
     FOURTH(4, 50_000),
-    FIFTH(3, 5_000);
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    FIRST(6, 2_000_000_000);
 
     companion object {
         fun findBy(matchingCount: Int, hasBonusNumber: Boolean): PrizeRank {
