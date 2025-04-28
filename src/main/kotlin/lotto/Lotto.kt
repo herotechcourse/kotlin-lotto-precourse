d8d07fd0748e3f4dace534e9359a1717f4e6af31
rlamsp <rlamsp234@naver.com>
2025-04-29 01:59:16 +0900

    Implement Lotto class

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,28 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.size == 6) { Constants.ERROR_LOTTO_SIZE }
+        require(numbers.all { it in 1..45 }) { Constants.ERROR_LOTTO_RANGE }
+        require(numbers.toSet().size == 6) { Constants.ERROR_LOTTO_DUPLICATE }
     }
 
-    // TODO: Implement additional functions
+    fun getNumbers(): List<Int> {
+        return numbers
+    }
+
+    fun countMatch(winningNumbers: List<Int>): Int {
+        return numbers.count { it in winningNumbers }
+    }
+
+    fun hasBonus(bonusNumber: Int): Boolean {
+        return bonusNumber in numbers
+    }
+
+    override fun toString(): String {
+        return numbers.joinToString(
+            separator = Constants.LOTTO_DELIMITER,
+            prefix = Constants.LOTTO_PREFIX,
+            postfix = Constants.LOTTO_POSTFIX
+        )
+    }
 }
