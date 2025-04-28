fba6916b74ffcc649de8fdcd955b46ac1f786fa6
puleugo <puleugo@gmail.com>
2025-04-28 18:12:47 +0900

    feat(domain): implement `WinningNumbers`

--- src/main/kotlin/lotto/domain/WinningNumbers.kt
+++ src/main/kotlin/lotto/domain/WinningNumbers.kt
@@ -1,5 +1,15 @@
 package lotto.domain
 
-class WinningNumbers(private val numbers: Set<Int>) {
+class WinningNumbers private constructor(private val numbers: Set<LottoNumber>) {
+    init {
+        require(numbers.size == 6) { "[ERR] Winning numbers must be 6 numbers." }
+    }
 
+    companion object {
+        fun of(numbers: List<Int>): WinningNumbers {
+            return WinningNumbers(
+                numbers.map { LottoNumber(it) }.toSet()
+            )
+        }
+    }
 }
