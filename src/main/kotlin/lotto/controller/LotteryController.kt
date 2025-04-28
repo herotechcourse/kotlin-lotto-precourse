a0181b16076b36fcdfcc3d039bb8db2ae59153bf
Emir Risyad <emirrsyd@gmail.com>
2025-04-28 21:21:41 +0200

    feat(LotteryController): implement class to orchestrate the game

---
+++ src/main/kotlin/lotto/controller/LotteryController.kt
@@ -0,0 +1,71 @@
+package lotto.controller
+
+import lotto.LotteryConstants
+import lotto.model.LotteryResult
+import lotto.model.LottoGenerator
+import lotto.model.WinningLotto
+import lotto.util.AmountParser
+import lotto.util.BonusNumberParser
+import lotto.util.WinningNumbersParser
+import lotto.view.InputView
+import lotto.view.OutputView
+
+class LotteryController(
+    private val inputView: InputView,
+    private val outputView: OutputView,
+    private val amountParser: AmountParser,
+    private val winningNumbersParser: WinningNumbersParser,
+    private val bonusNumberParser: BonusNumberParser,
+    private val lottoGenerator: LottoGenerator,
+) {
+    fun start() {
+        val purchaseAmount = getPurchaseAmount()
+        val ticketCount = purchaseAmount / LotteryConstants.TICKET_PRIZE
+        val tickets = lottoGenerator.generate(ticketCount)
+
+        outputView.printTicketCount(ticketCount)
+        outputView.printTickets(tickets)
+
+        val winningNumbers = getWinningNumbers()
+        val bonusNumber = getBonusNumber(winningNumbers)
+        val winningLotto = WinningLotto(winningNumbers, bonusNumber)
+
+
+        val result = LotteryResult(tickets, winningLotto)
+        outputView.printResult(result)
+        outputView.printReturnRate(result)
+    }
+
+    private fun getPurchaseAmount(): Int {
+        while (true) {
+            try {
+                val input = inputView.readPurchaseAmount()
+                return amountParser.parse(input)
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun getWinningNumbers(): List<Int> {
+        while (true) {
+            try {
+                val input = inputView.readWinningNumbers()
+                return winningNumbersParser.parse(input)
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun getBonusNumber(winningNumbers: List<Int>): Int {
+        while (true) {
+            try {
+                val input = inputView.readBonusNumber()
+                return bonusNumberParser.parse(input, winningNumbers)
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+}
