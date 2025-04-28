a922bd773129d0692a086fda9f33693970f6db07
minsang <minsang007@gmail.com>
2025-04-28 11:04:35 +0900

    feat(main): add main logic for input handling and scalability
    Add input handling flow to main func.
    And also use inputViewFactory and inputView for addtional inputviews.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,14 @@
 package lotto
 
+import lotto.dto.PurchaseAmount
+import lotto.factory.InputViewFactory
+
 fun main() {
     // TODO: Implement the program
+    val inputViewFactory = InputViewFactory()
+    val inputView = inputViewFactory.getInputView()
+    val purchaseAmount = PurchaseAmount(inputView.readInput())
+    println("PurchaseAmount: ${purchaseAmount.amountValue}")
+    println("TicketCounts: ${purchaseAmount.getTicketCount()}")
+
 }
