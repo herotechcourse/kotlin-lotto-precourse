4f2947955bbbc150aa3026c7945e6907f2dcfa61
ppour-ba <pouriya.pourbahrami@gmail.com>
2025-04-28 20:01:07 +0200

    refactor(LottoMachine): simplify as object with ticket generator function

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -1,19 +1,11 @@
 package lotto.domain
 
-import  lotto.Lotto
-import  lotto.support.*
-import  camp.nextstep.edu.missionutils.Randoms
+import lotto.Lotto
+import lotto.support.*
+import camp.nextstep.edu.missionutils.Randoms
 
-class LottoMachine (private val purchaseAmount: Int){
-    val tickets: List<Lotto>
-
-    init {
-        require(purchaseAmount >= TICKET_PRICE) { Message.ERROR_AMOUNT_TOO_SMALL }
-        require(purchaseAmount % TICKET_PRICE == 0) { Message.ERROR_AMOUNT_NOT_DIVISIBLE }
-        tickets = generateTickets()
-    }
-
-    private fun generateTickets(): List<Lotto> {
+object LottoMachine{
+    fun generateTickets(purchaseAmount: Int): List<Lotto> {
         val ticketCount = purchaseAmount / TICKET_PRICE
         return List(ticketCount) {
             Lotto(Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER,
