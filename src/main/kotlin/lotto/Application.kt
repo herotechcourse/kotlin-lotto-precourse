5d3ced823050c572ad2cb14f762ff38226950702
coreyol <coreycharm@gmail.com>
2025-04-29 06:48:27 +0900

    fix: main

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,7 +2,6 @@ package lotto
 
 fun main() {
     val amount = InputView.getUserPurchaseAmount()
-//    val amount = InputView.parsePurchaseAmount("1000")
 
     val numberOfTickets = amount / const.TICKET_PRICE
     val tickets = LottoTickets.generate(numberOfTickets)
@@ -10,7 +9,5 @@ fun main() {
 
     val winningNumbers = InputView.getUserWinningNumbers()
     val bonusNumbers = InputView.getUserBonusNumbers(winningNumbers)
-//    val winningNumbers = InputView.parseWinningNumbers("1,20,13,4,5,7")
-//    val bonusNumbers = InputView.parseBonusNumbers("8", winningNumbers)
     OutputView.printStatistics(tickets, winningNumbers, bonusNumbers)
 }
