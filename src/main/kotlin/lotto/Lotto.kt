a73c6fd16f5f7355a8825907a8acf5c83502623b
Emir Risyad <emirrsyd@gmail.com>
2025-04-28 09:40:37 +0200

    feat(Lotto): implement additional functions for Lotto

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,5 +5,15 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
     }
 
-    // TODO: Implement additional functions
+    fun countMatch(winningNumbers: List<Int>): Int {
+        return numbers.count { winningNumbers.contains(it) }
+    }
+
+    fun contains(number: Int): Boolean {
+        return numbers.contains(number)
+    }
+
+    fun getNumbers(): List<Int> {
+        return numbers
+    }
 }
