a8fc16f837c469b5fe3ddd9f5b0c3f1937266a45
minsang <minsang007@gmail.com>
2025-04-28 17:00:51 +0900

    refactor(factory): convert InputViewFactory from class to object
    Change InputViewFactory to a Kotlin object to enforce a singleton pattern and simplify instantiation.

--- src/main/kotlin/lotto/factory/InputViewFactory.kt
+++ src/main/kotlin/lotto/factory/InputViewFactory.kt
@@ -3,7 +3,7 @@ package lotto.factory
 import lotto.view.input.InputView
 import lotto.view.input.PurchaseAmountInputView
 
-class InputViewFactory {
+object InputViewFactory {
     fun getInputView(): InputView {
         return PurchaseAmountInputView
     }
