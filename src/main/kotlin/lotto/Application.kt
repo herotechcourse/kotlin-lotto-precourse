124bfa7e9afe9e338cc98fcc74b98dcd11066238
Bengel Adoma <bengelansah@gmail.com>
2025-04-28 18:45:44 +0200

    feat: Implement Application runner to orchestrate the Lotto program

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,24 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: Implement the program
+    Application().run()
+}
+
+class Application {
+    fun run() {
+        val purchaseAmount = InputView.readPurchaseAmount()
+        val lottoTickets = LottoMachine.issueTickets(purchaseAmount)
+
+        OutputView.printLottoTickets(lottoTickets)
+
+        val winningNumbers = InputView.readWinningNumbers()
+        val bonusNumber = InputView.readBonusNumber()
+
+        val winningLotto = WinningLotto(winningNumbers, bonusNumber)
+        val lottoResult = LottoResult.calculate(lottoTickets, winningLotto)
+
+        OutputView.printStatistics(lottoResult)
+    }
 }
