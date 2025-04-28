2b546df2e92c40c0447a2f04593345b4fd3740bf
kkonii <119216472+kkonii@users.noreply.github.com>
2025-04-28 15:57:41 +0900

    feat: validate winning numbers are withing valida range
    - and check for duplication and numbers count

--- src/main/kotlin/lotto/domain/WinningNumbers.kt
+++ src/main/kotlin/lotto/domain/WinningNumbers.kt
@@ -4,9 +4,16 @@ class WinningNumbers(private val numbers: List<Int>) {
     init {
         require(numbers.size == VALID_SIZE) { "[ERROR] Winning numbers must contain exactly $VALID_SIZE numbers." }
         require(numbers.size == numbers.distinct().size) { "[ERROR] Winning Numbers must not be duplicated." }
+        require(isValidRange(numbers)) { "[ERROR] Winning numbers must be between $START_INCLUSIVE and $END_INCLUSIVE." }
+    }
+
+    private fun isValidRange(numbers: List<Int>): Boolean {
+        return numbers.all { it in START_INCLUSIVE..END_INCLUSIVE }
     }
 
     companion object {
         private const val VALID_SIZE = 6
+        private const val START_INCLUSIVE = 1
+        private const val END_INCLUSIVE = 45
     }
 }
--- src/test/kotlin/lotto/domain/WinningNumbersTest.kt
+++ src/test/kotlin/lotto/domain/WinningNumbersTest.kt
@@ -1,24 +1,43 @@
 package lotto.domain
 
-import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 class WinningNumbersTest {
-    @Nested
-    inner class ValidationTest {
-        @Test
-        fun `throws an exception when lotto numbers exceed six`() {
-            assertThrows<IllegalArgumentException> {
-                WinningNumbers(listOf(1, 2, 3, 4, 5, 6, 7))
-            }
+    @Test
+    fun `throws an exception when winning numbers exceed six`() {
+        assertThrows<IllegalArgumentException> {
+            WinningNumbers(listOf(1, 2, 3, 4, 5, 6, 7))
         }
+    }
 
-        @Test
-        fun `throws an exception when lotto numbers are duplicated`() {
-            assertThrows<IllegalArgumentException> {
-                WinningNumbers(listOf(1, 2, 3, 4, 5, 5))
-            }
+    @Test
+    fun `throws an exception when winning numbers are duplicated`() {
+        assertThrows<IllegalArgumentException> {
+            WinningNumbers(listOf(1, 2, 3, 4, 5, 5))
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("makeInvalidNumbers")
+    @DisplayName("[Exception] throws when winning numbers are not between 1 and 45")
+    fun `throws an exception when winning numbers are not valid range`(numbers: List<Int>) {
+        assertThrows<IllegalArgumentException> {
+            WinningNumbers(numbers)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        private fun makeInvalidNumbers(): List<List<Int>> {
+            return listOf(
+                listOf(1, 2, 3, 4, 5, 46),
+                listOf(0, 20, 30, 40, 41, 42),
+                listOf(-5, 15, 25, 35, 45, 1)
+            )
         }
     }
 }
