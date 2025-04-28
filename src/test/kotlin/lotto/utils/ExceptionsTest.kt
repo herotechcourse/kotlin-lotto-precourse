46991c9d69a8175b72fba9a8dc694b9c39f3f2e4
siyeonSon <siyeon0209@naver.com>
2025-04-28 22:55:37 +0900

    feat: throw an exception if purchase amount is not divisible by 1,000

--- src/test/kotlin/lotto/utils/ExceptionsTest.kt
+++ src/test/kotlin/lotto/utils/ExceptionsTest.kt
@@ -1,3 +1,4 @@
+import lotto.utils.Constant.AMOUNT_NOT_DIVISIBLE_ERROR_MESSAGE
 import lotto.utils.Constant.PURCHASE_AMOUNT_INPUT_ERROR_MESSAGE
 import lotto.utils.Exceptions.validatePurchaseAmount
 import org.assertj.core.api.Assertions.assertThatThrownBy
@@ -12,4 +13,14 @@ class ExceptionsTest {
             .isInstanceOf(NumberFormatException::class.java)
             .hasMessageContaining(PURCHASE_AMOUNT_INPUT_ERROR_MESSAGE)
     }
+
+    @Test
+    fun `throw an exception if purchase amount is not divisible by 1,000`() {
+        assertThatThrownBy {
+            validatePurchaseAmount("1501").getOrThrow()
+        }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining(AMOUNT_NOT_DIVISIBLE_ERROR_MESSAGE)
+    }
+
 }
