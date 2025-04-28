8aad810f62616bf3a843c42ecf5436666212e255
Sheyda <sheyda.sheydayi95@gmail.com>
2025-04-28 19:20:11 +0200

    feat: create Lotto class with validation and matching logic

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,13 +1,25 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    println("Please enter the amount you want to spend:")
-    val amount = readlnOrNull()?.toIntOrNull()
+        val lotteryMachine = LotteryMachine()
 
-    if (amount == null || amount < 1000) {
-        println("Invalid amount. Please enter a valid number greater than or equal to 1000.")
-        return
+    while (amount == null || amount < 1000) {
+        println("Please enter the amount you want to spend:")
+        amount = readlnOrNull()?.toIntOrNull()
+
+        if (amount == null || amount < 1000) {
+            println("Invalid amount. Please enter a valid number greater than or equal to 1000.")
+        }
     }
+    val numberOfTickets = amount / 1000
+    println("You can buy $numberOfTickets ticket(s).")
 
-    println("You entered : $amount")
+    repeat(numberOfTickets) {
+        val ticket = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+            .sorted()
+
+        println("You entered: $amount")
+    }
 }
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,13 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.all { it in 1..45}) { "[ERROR] Numbers must be between 1 and 45." }
+        require(numbers.size == numbers.distinct().size) { "[ERROR] Numbers must be unique. "}
     }
 
-    // TODO: Implement additional functions
+    fun contains(number: Int): Boolean = numbers.contains(number)
+
+    fun countMatches(otherLotto: Lotto): Int = numbers.count { otherLotto.contains(it) }
+
+    override fun toString(): String = numbers.sorted().toString()
 }
