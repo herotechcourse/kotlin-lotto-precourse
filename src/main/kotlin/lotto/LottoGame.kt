d4b9b80c9e5b7353dd97405e30e70e6870df563a
Burcu <burcu@MacBook-Air-von-Burcu.local>
2025-04-28 10:57:49 +0200

    feat: add InputView class for reading user inputs

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -3,18 +3,19 @@ package lotto
 import lotto.view.InputView
 import lotto.view.OutputView
 
-class LottoGame() {
+class LottoGame(private val inputView: InputView = InputView()) {
+
   fun play() {
   
     // get purchase amount
-    
+    val purchaseAmount = inputView.readPurchaseAmount()
     // generate and display tickets 
   
     // get winning numbers
-    
+    val winningNumbers = inputView.readWinningNumbers()
     // get bonus
-
-	// calculate winnings
+    val bonusNumber = inputView.readBonusNumber()
+	  // calculate winnings
     
     // display result
   }
