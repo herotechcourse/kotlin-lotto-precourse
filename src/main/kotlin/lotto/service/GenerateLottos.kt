0d084146bf15a05f8de97f2be708572d4255f6a9
Taekeundo <pleasepose@gmail.com>
2025-04-28 03:15:11 +0200

    refactor(service): replace randem generation
    Using Randoms.pickUniqueNumbersInRange()
    - Update GenerateLottos to use camp.nextstep.edu.missionutils.Randoms API as required
    - Fixed feature test (ApplicationTest.kt) failure

--- src/main/kotlin/lotto/service/GenerateLottos.kt
+++ src/main/kotlin/lotto/service/GenerateLottos.kt
@@ -1,12 +1,12 @@
 package lotto.service
 
 import lotto.Lotto
+import camp.nextstep.edu.missionutils.Randoms
 
 object GenerateLottos {
     fun generate(count: Int): List<Lotto> {
         return List(count) {
-            val numbers = (1..45).shuffled()
-                .take(6)
+            val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
                 .sorted()
             Lotto(numbers)
         }
