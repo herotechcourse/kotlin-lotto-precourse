69db6ccd3bec47d79e12f6dc6e2cb180ba47e670
Mans228 <zhenismansur@gmail.com>
2025-04-28 15:38:03 +0200

    feat(application): add everything together

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,21 @@
 package lotto
 
+import lotto.view.InputView
+import lotto.view.OutputView
 fun main() {
-    // TODO: Implement the program
+    val purchaseAmount = InputView.getPurchaseAmount()
+    val lottoIssuer = LottoIssuer()
+    val lottos = lottoIssuer.issueLottos(purchaseAmount)
+
+    OutputView.printLottos(lottos)
+
+    val winningNumbers = InputView.getWinningNumbers()
+    val bonusNumber = InputView.getBonusNumber()
+
+    val winningResult = WinningResult(winningNumbers, bonusNumber)
+    val matchResult = winningResult.match(lottos)
+    val totalPrize = winningResult.calculateTotalPrize(matchResult)
+    val profitRate = winningResult.calculateProfitRate(totalPrize, purchaseAmount)
+
+    OutputView.printStatistics(matchResult, profitRate)
 }
