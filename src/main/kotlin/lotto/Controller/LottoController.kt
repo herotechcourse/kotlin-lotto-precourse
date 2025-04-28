5fed48143eea1a3336eea32521a60f86faec01dd
Sanghyunn <shawnpark98@naver.com>
2025-04-29 00:52:13 +0900

    feat : Implement LottoController

---
+++ src/main/kotlin/lotto/Controller/LottoController.kt
@@ -0,0 +1,21 @@
+package lotto.controller
+
+import lotto.model.Lotto
+import lotto.model.LottoGame
+import lotto.view.InputView
+import lotto.view.OutputView
+
+class LottoController {
+    fun run() {
+        val amount = InputView.readPurchaseAmount()
+        val game = LottoGame()
+        val lottos = game.purchase(amount)
+        OutputView.printPurchasedLottos(lottos)
+
+        val winningLotto = Lotto(InputView.readWinningNumbers())
+        val bonusNumber = InputView.readBonusNumber()
+
+        val result = game.getResult(lottos, winningLotto, bonusNumber)
+        OutputView.printWinningStatics(result, amount)
+    }
+}
