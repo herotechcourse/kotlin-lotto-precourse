01b70b1cbd9b038b6f680b92898fad85d00080fd
Sanghyunn <shawnpark98@naver.com>
2025-04-29 00:46:49 +0900

    feat : Implement OutputView

---
+++ src/main/kotlin/lotto/View/OutputView.kt
@@ -0,0 +1,39 @@
+package lotto.view
+
+import lotto.model.Lotto
+import lotto.model.LottoPrize
+
+object OutputView {
+    private val PRIZE_FORMAT = listOf(
+        "6 Matches (2,000,000,000 KRW)",
+        "5 Matches + Bonus Ball (30,000,000 KRW)",
+        "5 Matches (1,500,000 KRW)",
+        "4 Matches (50,000 KRW)",
+        "3 Matches (5,000 KRW)"
+    )
+
+    fun printPurchasedLottos(lottos: List<Lotto>) {
+        println("You have purchased ${lottos.size} tickets.")
+        lottos.forEach { lotto -> println(lotto.getNumbers()) }
+    }
+
+    fun printWinningStatics(result: List<Int>, amount: Int) {
+        println("Winning Statistics")
+        println("---")
+
+        for(i in 0 until result.size) {
+            println("${PRIZE_FORMAT[i]} - ${result[i]} tickets")
+        }
+
+        val returnRate = calculateReturnRate(result, amount)
+        println("Total return rate is ${"%.1f".format(returnRate)}%.")
+    }
+
+    private fun calculateReturnRate(result: List<Int>, amount: Int): Double {
+        var total = 0
+        for(i in 0 until result.size) {
+            total += (LottoPrize.values()[i].prize * result[i])
+        }
+        return (total.toDouble() / amount) * 100
+    }
+}
