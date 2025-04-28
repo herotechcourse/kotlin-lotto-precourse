36ed960da9d8e1ce040f8223fb9a0273161e2bb0
Hkunnath <honeyk.mec@gmail.com>
2025-04-28 20:44:33 +0200

    feat: implement Lotto class with validation, matching, and number retrieval functions

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,23 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.distinct().size == 6) { "[ERROR] Lotto numbers must be unique." }
+        require(numbers.all { it in 1..45 }){ "[ERROR] Lotto numbers must be between 1 and 45." }
     }
 
-    // TODO: Implement additional functions
+    override fun toString(): String {
+        return numbers.toString()
+    }
+
+    fun getLottoNumber(): List<Int> {
+        return numbers;
+    }
+
+    fun containsNumber(number: Int): Boolean{
+        return numbers.contains(number)
+    }
+
+    fun countMatchingNumbers(anotherTicket : Lotto): Int {
+        return numbers.count { it in anotherTicket.getLottoNumber() }
+    }
 }
