1ba3b3a9bfe1c6849f45e6ce0a1f62f9bef70b86
minsang <minsang007@gmail.com>
2025-04-28 14:05:40 +0900

    feat(lotto): add winning numbers input and display in main
    Use WinningNumbersInputView to read last week’s winning numbers

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,7 +1,10 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
 import lotto.dto.PurchaseAmount
+import lotto.dto.WinningNumbers
 import lotto.factory.InputViewFactory
+import lotto.view.input.WinningNumbersInputView
 
 fun main() {
     // TODO: Implement the program
@@ -12,12 +15,14 @@ fun main() {
     val ticketCounts = purchaseAmount.getTicketCount()
     println("TicketCounts: ${ticketCounts}")
 
-    var tickets= mutableListOf<Lotto>()
-    val lottos = Lottos(tickets,ticketCounts)
+    var tickets = mutableListOf<Lotto>()
+    val lottos = Lottos(tickets, ticketCounts)
 
     for (lotto in lottos.getTickets()) {
         println("Lotto: ${lotto.getNumbers()}")
     }
 
-
+    val winningNumbers = WinningNumbers(WinningNumbersInputView.inputWinningNumbers())
+    println("WinningNumbers: ${winningNumbers.getWinningNumbers()}")
 }
+
