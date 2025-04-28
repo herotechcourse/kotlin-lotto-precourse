b06c28be0ca631917d5a1663c3bb0d0ebd8500ba
YongLeeCode <yonghwan311@gmail.com>
2025-04-29 03:32:17 +0900

    refactor(winning lotto): fix exception message

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -9,7 +9,7 @@ class WinningLotto(
     init {
         require(bonusNumber in Constants.MIN_LOTTO_NUMBER..Constants.MAX_LOTTO_NUMBER) { "[ERROR] Bonus number must be between 1 and 45." }
         require(!winningNumbers.contains(bonusNumber)) { "[ERROR] Winning numbers must not contain bonus number." }
-        require(winningNumbers.size == Constants.LOTTO_SIZE) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(winningNumbers.size == Constants.LOTTO_SIZE) { "[ERROR] Winning Lotto must contain exactly 6 numbers." }
         require(winningNumbers.distinct().size == Constants.LOTTO_SIZE) { "[ERROR] Lotto numbers must be unique." }
         require(winningNumbers.all { it in Constants.MIN_LOTTO_NUMBER..Constants.MAX_LOTTO_NUMBER }) { "[ERROR] Lotto numbers must be between 1 and 45." }
     }
