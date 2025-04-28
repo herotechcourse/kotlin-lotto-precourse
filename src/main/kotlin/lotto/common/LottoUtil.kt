74cf961357ff9777aeea6746bb5347bef9fea774
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2025-04-29 05:43:50 +0900

    feat(LottoUtil): add utility functions for lotto number generation and profit calculations

--- src/main/kotlin/lotto/common/LottoUtil.kt
+++ src/main/kotlin/lotto/common/LottoUtil.kt
@@ -12,5 +12,10 @@ object LottoUtil {
 
     fun isValidLottoNumber(input: Int?): Boolean = input in MIN..MAX
     fun isValidLottoSize(input: List<Int?>): Boolean = input.size == COUNT
-
+    fun generateNumbers(): List<Int> = Randoms.pickUniqueNumbersInRange(MIN, MAX, COUNT)
+    fun getMatchCount(lotto: Lotto, winner: Winner) = winner.getNumbers().filter { lotto.contains(it) }.size
+    fun isMatchBonus(lotto: Lotto, bonus: Int): Boolean = lotto.contains(bonus)
+    fun calculateProfit(prizes: List<Prize>): Int = prizes.sumOf { it.getReward() }
+    fun calculateProfitRate(profit: Int, totalSpent: Int): Double =
+        Math.round((profit.toDouble() / totalSpent) * 100 * 10) / 10.0
 }
