3a89ca0085c2088a3ca472e13880b6fa5ea8b2a2
minsang <minsang007@gmail.com>
2025-04-28 14:11:39 +0900

    refactor(input): refactor WinningNumbersInputView implement InputView
    Update WinningNumbersInputView to inherit from InputView and override readInput,
    ensuring consistent interface usage and enabling factory-based input retrieval.

--- src/main/kotlin/lotto/view/input/WinningNumbersInputView.kt
+++ src/main/kotlin/lotto/view/input/WinningNumbersInputView.kt
@@ -2,9 +2,9 @@ package lotto.view.input
 
 import camp.nextstep.edu.missionutils.Console
 
-object WinningNumbersInputView {
-    fun inputWinningNumbers(): List<String> {
-        println("Please enter last week's winning numbers.")
-        return Console.readLine().split(",")
+object WinningNumbersInputView: InputView {
+    override fun readInput(): String {
+        println("Please enter the winning numbers.")
+        return Console.readLine()
     }
 }
