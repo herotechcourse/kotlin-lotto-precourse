aa02cfc8e37b96218bb8e1e00258e180e75045df
Jihun Kim <zh1229@naver.com>
2025-04-28 17:39:13 +0900

    feat(output-view): add printPurchaseCount and printLottoTickets methods

---
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -0,0 +1,15 @@
+package lotto.view
+
+import lotto.domain.LottoTicket
+
+class OutputView {
+    fun printPurchaseCount(count: Int) {
+        println("You have purchased $count tickets.")
+    }
+
+    fun printLottoTickets(tickets: List<LottoTicket>) {
+        tickets.forEach { ticket ->
+            println(ticket.getNumbers())
+        }
+    }
+}
