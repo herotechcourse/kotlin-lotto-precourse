e9fee6d99c7639c5a1b3bcf62fe48d8e0886ff0e
rlamsp <rlamsp234@naver.com>
2025-04-29 02:11:55 +0900

    Implement main code

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,23 @@
 package lotto
 
 fun main() {
-    // TODO: Implement the program
+    val purchaseAmount = InputView.readPurchaseAmount()
+    val ticketAmount = LottoTicketCalculator.calculateTicketAmount(purchaseAmount)
+    OutputView.printLine()
+    OutputView.printPurchasedTickets(ticketAmount)
+
+    val lottoList = LottoGenerator.createRandomLottos(ticketAmount)
+    OutputView.printLottoList(lottoList)
+    OutputView.printLine()
+
+    val winningLotto = InputView.readWinningLotto()
+    OutputView.printLine()
+    val bonusNumber = InputView.readBonusNumber(winningLotto.getNumbers())
+
+    val evaluation = LottoEvaluation(winningLotto.getNumbers(), bonusNumber)
+    val result = evaluation.evaluate(lottoList, purchaseAmount)
+
+    OutputView.printLine()
+    result.printStatistics()
+    result.printReturnRate()
 }
