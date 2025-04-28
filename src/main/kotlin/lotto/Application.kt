2a7c933ad9a62481e3e7dc18c7e5c68a5acd4eda
이민경(Minkyung Lee) <87940552+Minlee01@users.noreply.github.com>
2025-04-28 23:06:08 +0900

    [feat]1. user input

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,17 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: Implement the program
+
+    val amount = totalPurchaseAmount()
+    // get user Input
+    fun totalPurchaseAmount(): Int{
+        println("Please enter the purchase amount.")
+        val inputMoney = Console.readLine()
+        val amount = inputMoney.toIntOrNull() ?: throw IllegalArgumentException("Input must be a number")
+        if(amount <= 0 || amount % 1000 != 0) {
+        throw IllegalArgumentException("input amount has to be divisible by 1000")
+    }
+    return amount;
 }
