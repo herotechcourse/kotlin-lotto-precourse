5d25dbc281659c8456351813910c57d23af2b536
YongLeeCode <yonghwan311@gmail.com>
2025-04-29 02:13:45 +0900

    refactor(profit): apply constants for magic numbers

--- src/main/kotlin/lotto/service/ProfitCaculator.kt
+++ src/main/kotlin/lotto/service/ProfitCaculator.kt
@@ -1,12 +1,12 @@
 package lotto.service
 
+import lotto.common.Constants
 import lotto.domain.PrizeRank
 
 object ProfitCaculator {
-    private val TICKET_PRICE: Int = 1000
     fun calculate(map: Map<PrizeRank, Int>, purchaseAmount: Int): String {
         val totalProfit = map.entries.sumOf { (result, count) -> result.prize.toLong() * count }
-        return format((totalProfit.toDouble() / (purchaseAmount * TICKET_PRICE)) * 100)
+        return format((totalProfit.toDouble() / (purchaseAmount * Constants.TICKET_PRICE)) * 100)
     }
 
     private fun format(rate: Double): String {
