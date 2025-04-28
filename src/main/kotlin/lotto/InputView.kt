95d2e61a111467271a351305c35fb5ebb50743ca
rinarina0429 <forrina0429@gmail.com>
2025-04-28 21:34:42 +0900

    feat(inputview): add bonus number input reader

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -12,4 +12,9 @@ class InputView {
         println("Please enter last week's winning numbers.")
         return Console.readLine()
     }
+
+    fun readBonusNumber(): String {
+        println("Please enter the bonus number.")
+        return Console.readLine()
+    }
 }
