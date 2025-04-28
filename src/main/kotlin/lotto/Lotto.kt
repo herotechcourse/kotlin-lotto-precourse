4d42a1623cd732a3d903fb18a09a24d66a0a1b3f
minsang <minsang007@gmail.com>
2025-04-28 11:16:18 +0900

    feat(lotto): use LottoGenerator for number generation and add getNumbers function
    Refactor Lotto class to use LottoGenerator for generating lotto numbers by default.
    Ensure that each lotto ticket contains exactly 6 unique numbers.
    Add getNumbers function to provide access to the generated lotto numbers.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,15 @@
 package lotto
 
-class Lotto(private val numbers: List<Int>) {
+import camp.nextstep.edu.missionutils.Randoms
+import lotto.utils.LottoGenerator
+
+class Lotto(private val numbers: List<Int> = LottoGenerator.generateLottoNumbers()) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
     }
 
     // TODO: Implement additional functions
+    fun getNumbers(): List<Int> {
+        return numbers
+    }
 }
