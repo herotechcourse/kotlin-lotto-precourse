8ff1027514c00288dd5ae1eaa2803e19626a6381
이민경(Minkyung Lee) <87940552+Minlee01@users.noreply.github.com>
2025-04-28 23:28:20 +0900

    [feat] 3. 6 Random numbers are printed out per ticket

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,6 +6,7 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
     }
+    fun getNumbers(): List<Int> = numbers
 
     // TODO: Implement additional functions
 }
