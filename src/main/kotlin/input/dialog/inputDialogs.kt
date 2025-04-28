c621b0437c43596649e624cc390ef5aea28c401a
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-28 19:16:00 +0900

    fix(input) add println and lazy return so that each dialog print additional line spacing

--- src/main/kotlin/input/dialog/inputDialogs.kt
+++ src/main/kotlin/input/dialog/inputDialogs.kt
@@ -1,10 +1,20 @@
 package input.dialog
 
+import input.config.printLastWeekWinningNumberMsg
 import input.config.printTicketAmountMsg
-import input.config.printTicketResultMsgWithAmount
+import input.getLottoNumberArrayFromInput
 import input.getTicketAmountFromInput
 
 fun getTicketAmountFromDialog(): Int{
     printTicketAmountMsg()
-    return getTicketAmountFromInput()
+    val ticketAmount: Int = getTicketAmountFromInput()
+    println()
+    return ticketAmount
+}
+
+fun getWinningNumberFrpmDialog(): Array<Int>{
+    printLastWeekWinningNumberMsg()
+    val winningNumbers: Array<Int> = getLottoNumberArrayFromInput()
+    println()
+    return winningNumbers
 }
