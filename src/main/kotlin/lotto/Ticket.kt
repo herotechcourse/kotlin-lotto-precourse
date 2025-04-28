e167fb49a645ccdc3fb9250f5b4c9d89dbbf41e5
Petra Bencze <petra.bncz@gmail.com>
2025-04-28 20:47:22 +0200

    refactor(Ticket): add secondary constructor to be able to test

--- src/main/kotlin/lotto/Ticket.kt
+++ src/main/kotlin/lotto/Ticket.kt
@@ -3,6 +3,16 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 
 class Ticket {
-    val numbers: List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-    val sortedNumbers: MutableList<Int> = numbers.sorted().toMutableList()
+    val numbers: List<Int>
+    val sortedNumbers: MutableList<Int>
+
+    constructor() {
+        numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        sortedNumbers = numbers.sorted().toMutableList()
+    }
+
+    constructor(list: List<Int>) {
+        numbers = list
+        sortedNumbers = numbers.sorted().toMutableList()
+    }
 }
