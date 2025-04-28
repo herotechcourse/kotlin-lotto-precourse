e2b7157aec6aa8df02eb47889f7f11d750d1b91d
ppour-ba <pouriya.pourbahrami@gmail.com>
2025-04-28 21:01:57 +0200

    feat(OutputView): add functions to display prize results and profit rate

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -2,6 +2,8 @@ package lotto.view
 
 import lotto.support.Message
 import lotto.Lotto
+import lotto.domain.PrizeResult
+import lotto.domain.Rank
 
 object OutputView {
 
@@ -12,8 +14,9 @@ object OutputView {
     fun printTickets(tickets: List<Lotto>){
         println()
         println(Message.PURCHASED_TICKETS_MESSAGE.format(tickets.size))
-        for (ticket in tickets)
+        for (ticket in tickets) {
             println(ticket)
+        }
     }
 
     fun printWinningTicketPrompt(){
@@ -28,5 +31,32 @@ object OutputView {
 
     fun printErrorMessage(message: String?) {
         println(message ?: Message.UNKNOWN_ERROR)
+    }
+
+    fun printResultTitle(){
+        println(Message.RESULT_TITLE)
+        println(Message.DRAW_LINE)
+    }
+
+    private fun printRankLine(rank: Rank, prizeResult: PrizeResult) {
+        if (rank.needsBonus) {
+            println(Message.BONUS_MATCH_RESULT_FORMAT.format(rank.matchCount, String.format("%,d", rank.prize),
+                prizeResult.getRankCount(rank)))
+            return
+        }
+        println(Message.NORMAL_MATCH_RESULT_FORMAT.format(rank.matchCount, String.format("%,d", rank.prize),
+            prizeResult.getRankCount(rank)))
+    }
+
+    fun printPrizeResult(prizeResult: PrizeResult) {
+        printResultTitle()
+        for (rank in Rank.entries.sortedBy { it.ordinal }) {
+            if (rank == Rank.NONE) continue
+            printRankLine(rank, prizeResult)
+        }
+    }
+
+    fun printProfit(profit: String){
+        println(Message.RETURN_RATE_MESSAGE.format(profit))
     }
 }
