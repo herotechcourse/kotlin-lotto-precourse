1ddafcbfec15f7e731871e36cf59d5d686a11c01
haeun <jhe0903@naver.com>
2025-04-28 18:23:52 +0900

    feat(input) : add validation to user inputs

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -5,19 +5,43 @@ import camp.nextstep.edu.missionutils.Console
 class InputView {
     fun readAmount(): Int {
         println("Please enter the purchase amount.")
-        val amount = Console.readLine().toInt()
+        val amount = try {
+            Console.readLine().toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("You must enter a valid number.")
+        }
         return amount
     }
 
     fun readWinningNumbers() : List<Int> {
         println("Please enter last week's winning numbers.")
-        val winningNumbers = Console.readLine().split(",").map { it.toInt() }
+        val winningNumbers = try {
+            Console.readLine().split(",").map { it.toInt() }
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("You must enter a valid number.")
+        }
+        if (winningNumbers.size != 6) {
+            throw IllegalArgumentException("You must enter the 6 numbers.")
+        }
+        if (winningNumbers.distinct().size != winningNumbers.size) {
+            throw IllegalArgumentException("You must not enter duplicate numbers.")
+        }
+        if (winningNumbers.any { it !in 1..45 }) {
+            throw IllegalArgumentException("You must enter numbers between 1 and 45.")
+        }
         return winningNumbers
     }
 
     fun readBonusNumber() : Int{
         println("Please enter the bonus number.")
-        val bonusNumber = Console.readLine().toInt()
+        val bonusNumber = try {
+            Console.readLine().toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("You must enter a valid number.")
+        }
+        if (bonusNumber !in 1..45) {
+            throw IllegalArgumentException("You must enter number between 1 and 45.")
+        }
         return bonusNumber
     }
 }
