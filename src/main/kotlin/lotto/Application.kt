2c7f2c3a05e4a813c1d2c19c6b9ece5863d095cd
kdozlo <eodud5730@naver.com>
2025-04-29 04:03:42 +0900

    feat(output-view): add display of ticket issuance result and winning statistics

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,17 @@
 package lotto
 
+import lotto.domain.*
+import lotto.presentation.InputView
+import lotto.presentation.OutputView
+
 fun main() {
-    // TODO: Implement the program
+    val purchase = PurchaseAmount(InputView.readPurchaseAmount())
+    val lottos = LottoMachine().buy(purchase)
+    OutputView.printTickets(lottos)
+    val calc = LottoResultCalculator(
+        WinningNumbers(InputView.readWinningNumbers()),
+        BonusNumber(InputView.readBonusNumber())
+    )
+    val stats = calc.calculateStats(lottos)
+    OutputView.printStatistics(stats, calc.calculateRate(stats, purchase))
 }
