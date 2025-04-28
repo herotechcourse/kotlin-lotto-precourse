fb7ab5f2b4014152b9f1c7ebf062c4edbfbaa757
soeunnPark <carol0724@naver.com>
2025-04-29 01:21:01 +0900

    refactor(purchaseAmountDto): purchase amount should be over 0 KRW

--- src/main/kotlin/lotto/view/dto/PurchaseAmountDto.kt
+++ src/main/kotlin/lotto/view/dto/PurchaseAmountDto.kt
@@ -4,6 +4,6 @@ import lotto.error.ExceptionMessage
 
 data class PurchaseAmountDto(val amount: Int) {
     init {
-        require(this.amount % 1_000 == 0) { ExceptionMessage.INVALID_PURCHASE_AMOUNT.errorMessage }
+        require(this.amount > 0 && this.amount % 1_000 == 0) { ExceptionMessage.INVALID_PURCHASE_AMOUNT.errorMessage }
     }
 }
