9e572d3a20e1bc1060eec20c64733355d72862a1
inaemin <inaemin@gmail.com>
2025-04-29 04:36:01 +0900

    update: add method to Lotto class

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,22 @@
 package lotto
 
+import lotto.util.Constants.LOTTERY_NUMBER_COUNT
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.toSet().size == LOTTERY_NUMBER_COUNT) {
+            "[ERROR] Lotto numbers cannot contain duplicate numbers."
+        }
     }
 
-    // TODO: Implement additional functions
+    fun match(winningNumbers: List<Int>, bonusNumber: Int): Pair<Int, Boolean> {
+        val matchCount = numbers.intersect(winningNumbers.toSet()).size
+        val hasBonus = numbers.contains(bonusNumber)
+        return Pair(matchCount, hasBonus)
+    }
+
+    override fun toString(): String {
+        return numbers.sorted().joinToString(", ", "[", "]")
+    }
 }
