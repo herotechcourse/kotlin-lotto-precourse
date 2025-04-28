6296af68dae3858ce647e97e93471494e32fd6c7
highballplz <kmkim405@gmail.com>
2025-04-28 18:06:37 +0900

    refactor(main): integrate InputHandler and LottoMachine for lotto generation

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,11 @@
 package lotto
 
 fun main() {
-    val purchaseAmount = InputHandler.getPurchaseAmount()
+    val inputHandler = InputHandler()
+    val lottoMachine = LottoMachine()
+
+    val purchaseAmount = inputHandler.getPurchaseAmount()
+
+    val lottoCount = purchaseAmount / 1000
+    val lottos = lottoMachine.generateLottos(lottoCount)
 }
