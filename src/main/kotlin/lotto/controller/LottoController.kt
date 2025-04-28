c7c171d9df01f116950536e8a1ced24b45f1814f
rootPark <rmsdnjs518@gmail.com>
2025-04-28 20:03:06 +0900

    feat: LottoController addition with other fun

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,16 +1,29 @@
 package lotto.controller
 
 import lotto.model.Lotto
+import lotto.model.LottoResult
+import lotto.model.Prize
+import lotto.model.WinningLotto
+import lotto.view.InputView
+import lotto.view.OutputView
+import lotto.controller.CreateController
 
-class LottoController(private val inputView: InputView) {
+class LottoController(private val inputView: InputView, private val outputView: OutputView, private val createController: CreateController) {
   fun run() {
     try {
       val purchaseAmount = inputView.readPurchaseAmount()
+      val tickets = purchaseLottoTickets(purchaseAmount)
+      outputView.printPurchasedTickets(tickets)
 
       val winningNumbers = inputView.readWinningNumbers()
       val bonusNumber = inputView.readBonusNumber()
+      val winningLotto = WinningLotto(winningNumbers, bonusNumber)
+
+      val result = createController.matchWinningLotto(tickets, winningLotto)
+      outputView.printLottoResult(result)
     } catch (e: IllegalArgumentException) {
       println(e.message)
+      run()
     }
   }
   private fun getPurchaseAmount(): Int {
