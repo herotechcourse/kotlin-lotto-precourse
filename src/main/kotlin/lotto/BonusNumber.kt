24075495a55b9fdfffc7c3316536502a1877607e
Hkunnath <honeyk.mec@gmail.com>
2025-04-28 20:46:49 +0200

    feat: implement BonusNumber class with validation and retrieval functionality

---
+++ src/main/kotlin/lotto/BonusNumber.kt
@@ -0,0 +1,12 @@
+package lotto
+
+class BonusNumber(private val bonusNumber: Int,private val lotto: Lotto) {
+    init {
+        require(bonusNumber in 1..45) { "[ERROR] Bonus Number should be a between 1 and 45" }
+        require(bonusNumber !in lotto.getLottoNumber()) { "[ERROR] Bonus Number should not be in Lotto numbers" }
+    }
+
+    fun getBonusNumber():Int{
+        return bonusNumber;
+    }
+}
