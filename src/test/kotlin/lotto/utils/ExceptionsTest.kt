499ec1d711e15dd844b5272a80deade9ca665d26
siyeonSon <siyeon0209@naver.com>
2025-04-28 22:57:59 +0900

    feat: throw an exception if winning numbers are out of valid range

--- src/test/kotlin/lotto/utils/ExceptionsTest.kt
+++ src/test/kotlin/lotto/utils/ExceptionsTest.kt
@@ -1,6 +1,7 @@
 import lotto.utils.Constant.AMOUNT_NOT_DIVISIBLE_ERROR_MESSAGE
 import lotto.utils.Constant.INVALID_WINNING_NUMERIC_ERROR_MESSAGE
 import lotto.utils.Constant.PURCHASE_AMOUNT_INPUT_ERROR_MESSAGE
+import lotto.utils.Constant.WINNING_NUMBER_RANGE_ERROR_MESSAGE
 import lotto.utils.Exceptions.validatePurchaseAmount
 import lotto.utils.Exceptions.validateWinningNumbers
 import org.assertj.core.api.Assertions.assertThatThrownBy
@@ -34,4 +35,18 @@ class ExceptionsTest {
             .hasMessageContaining(INVALID_WINNING_NUMERIC_ERROR_MESSAGE)
     }
 
+    @Test
+    fun `throw an exception if winning numbers are out of valid range`() {
+        assertThatThrownBy {
+            validateWinningNumbers(listOf("0", "2", "3", "4", "5", "6")).getOrThrow()
+        }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining(WINNING_NUMBER_RANGE_ERROR_MESSAGE)
+
+        assertThatThrownBy {
+            validateWinningNumbers(listOf("1", "2", "3", "4", "5", "46")).getOrThrow()
+        }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining(WINNING_NUMBER_RANGE_ERROR_MESSAGE)
+    }
 }
