1fefcddab3c4bca58e0f5b519a2cc82647815d73
minsang <minsang007@gmail.com>
2025-04-28 12:01:44 +0900

    refactor(main): update main flow to use Lottos for managing multiple tickets
    Refactor Application.kt to generate and handle multiple Lotto tickets using the Lottos class.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -8,7 +8,16 @@ fun main() {
     val inputViewFactory = InputViewFactory()
     val inputView = inputViewFactory.getInputView()
     val purchaseAmount = PurchaseAmount(inputView.readInput())
-    println("PurchaseAmount: ${purchaseAmount.amountValue}")
-    println("TicketCounts: ${purchaseAmount.getTicketCount()}")
+    println("PurchaseAmount: ${purchaseAmount}")
+    val ticketCounts = purchaseAmount.getTicketCount()
+    println("TicketCounts: ${ticketCounts}")
+
+    var tickets= mutableListOf<Lotto>()
+    val lottos = Lottos(tickets,ticketCounts)
+
+    for (lotto in lottos.getTickets()) {
+        println("Lotto: ${lotto.getNumbers()}")
+    }
+
 
 }
