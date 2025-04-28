8e5fd8aaf69d23ad7fa71d7198c0fa90b8f75c9d
kkonii <119216472+kkonii@users.noreply.github.com>
2025-04-28 17:40:35 +0900

    feat: validate lotto numbers are within valid range

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,11 +6,19 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == VALID_SIZE) { "[ERROR] Lotto must contain exactly $VALID_SIZE numbers." }
         require(numbers.size == numbers.distinct().size) { "[ERROR] Lotto must not contain distinct numbers." }
+        require(isValidRange(numbers)) { "[ERROR] Winning numbers must be between $START_INCLUSIVE and $END_INCLUSIVE." }
     }
+
+    private fun isValidRange(numbers: List<Int>): Boolean {
+        return numbers.all { it in START_INCLUSIVE..END_INCLUSIVE }
+    }
+
 
     fun toNumbersDto(): NumbersDto = NumbersDto(numbers.toList())
 
     companion object {
         private const val VALID_SIZE = 6
+        private const val START_INCLUSIVE = 1
+        private const val END_INCLUSIVE = 45
     }
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,7 +1,10 @@
 package lotto
 
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 class LottoTest {
     @Test
@@ -16,5 +19,22 @@ class LottoTest {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 5))
         }
+    }
+
+    @ParameterizedTest
+    @MethodSource("makeInvalidNumbers")
+    @DisplayName("[Exception] throws when lotto numbers are not between 1 and 45")
+    fun `throws when lotto numbers are not in valid range`(numbers: List<Int>) {
+        assertThrows<IllegalArgumentException> {
+            Lotto(numbers)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        private fun makeInvalidNumbers(): List<List<Int>> =
+            listOf(
+                listOf(1, 12, 13, 4, 15, 46), listOf(0, 20, 32, 40, 41, 42), listOf(-5, 15, 25, 35, 45, 1)
+            )
     }
 }
