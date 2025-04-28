dfdc5def9d7502275d5bb2c1a6c7e5d584bab79d
김소운 <sozzykim@naver.com>
2025-04-29 02:45:21 +0900

    refactor(app): Refactor code for better readability

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,28 +3,26 @@ package lotto
 import lotto.TicketChecker.addStatistics
 
 fun main() {
+    // 1. purchase lotto
     val purchaseAmount = InputView.purchaseAmount()
-    val winningLotto = Lotto(InputView.winningNum())
-    val bonusNumber = InputView.bonusNum()
-    val ticketCount = purchaseAmount/1000
-
+    val ticketCount = purchaseAmount / 1000
     val myLotto = mutableListOf<Lotto>()
-    for ( i in 0 until ticketCount) {
+    for (i in 0 until ticketCount) {
         myLotto.add(LottoGenerator.generateLotto())
     }
 
-    myLotto.forEach{ lotto ->
-        addStatistics( lotto, winningLotto, bonusNumber)
+    // 2. winning lotto input
+    val winningLotto = Lotto(InputView.winningNum())
+    val bonusNumber = InputView.bonusNum()
+
+    // 3. lotto statistics
+    myLotto.forEach { lotto ->
+        addStatistics(lotto, winningLotto, bonusNumber)
     }
 
     OutputView.myLotto(myLotto)
     OutputView.resultStatistics()
     OutputView.profitRate(ProfitRateCalaulator.calculateProfitRate(purchaseAmount))
-
-
-
-
-
 
 
 }
