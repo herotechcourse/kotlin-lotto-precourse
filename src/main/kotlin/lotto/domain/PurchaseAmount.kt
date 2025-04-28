d214d573de1efe9a0d8849d292cd7e7111c7d060
kdozlo <eodud5730@naver.com>
2025-04-28 16:46:43 +0900

    feat(purchase-amount): add purchase amount unit validation
    Require purchase amount to be a multiple of 1,000 and throw `IllegalArgumentException(INVALID_PURCHASE_UNIT)` otherwise.

---
+++ src/main/kotlin/lotto/domain/PurchaseAmount.kt
@@ -0,0 +1,13 @@
+package lotto.domain
+
+import lotto.presentation.ErrorMessages.INVALID_PURCHASE_UNIT
+
+data class PurchaseAmount(val amount: Int) {
+    init {
+        require(amount % MONETARY_UNIT == 0) { INVALID_PURCHASE_UNIT }
+    }
+
+    companion object {
+        private const val MONETARY_UNIT = 1_000
+    }
+}
