794c1f016e9fdfd61a2c89b5ddc627e444a8b183
siyeonSon <siyeon0209@naver.com>
2025-04-28 22:57:02 +0900

    feat: throw an exception if winning numbers contain non-numeric values

--- src/main/kotlin/lotto/utils/Exceptions.kt
+++ src/main/kotlin/lotto/utils/Exceptions.kt
@@ -1,8 +1,13 @@
 package lotto.utils
 
+import lotto.Lotto
 import lotto.utils.Constant.AMOUNT_NOT_DIVISIBLE_ERROR_MESSAGE
+import lotto.utils.Constant.INVALID_WINNING_NUMERIC_ERROR_MESSAGE
 import lotto.utils.Constant.PURCHASE_AMOUNT_INPUT_ERROR_MESSAGE
+import lotto.utils.Constant.WINNING_NUMBER_RANGE_ERROR_MESSAGE
 import lotto.utils.Constant.LOTTO_COST
+import lotto.utils.Constant.MAX_LOTTO_NUMBER
+import lotto.utils.Constant.MIN_LOTTO_NUMBER
 
 object Exceptions {
     fun validatePurchaseAmount(purchaseAmount: String): Result<Int> {
@@ -13,6 +18,21 @@ object Exceptions {
         }.recoverCatching { e ->
             when (e) {
                 is NumberFormatException -> throw NumberFormatException(PURCHASE_AMOUNT_INPUT_ERROR_MESSAGE)
+                else -> throw e
+            }
+        }
+    }
+
+    fun validateWinningNumbers(winningNumber: List<String>): Result<Lotto> {
+        return runCatching {
+            Lotto(winningNumber.map {
+                val intValue = it.toInt()
+                require(intValue in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) { WINNING_NUMBER_RANGE_ERROR_MESSAGE }
+                intValue
+            })
+        }.recoverCatching { e ->
+            when (e) {
+                is NumberFormatException -> throw NumberFormatException(INVALID_WINNING_NUMERIC_ERROR_MESSAGE)
                 else -> throw e
             }
         }
--- src/test/kotlin/lotto/utils/ExceptionsTest.kt
+++ src/test/kotlin/lotto/utils/ExceptionsTest.kt
@@ -1,6 +1,8 @@
 import lotto.utils.Constant.AMOUNT_NOT_DIVISIBLE_ERROR_MESSAGE
+import lotto.utils.Constant.INVALID_WINNING_NUMERIC_ERROR_MESSAGE
 import lotto.utils.Constant.PURCHASE_AMOUNT_INPUT_ERROR_MESSAGE
 import lotto.utils.Exceptions.validatePurchaseAmount
+import lotto.utils.Exceptions.validateWinningNumbers
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
@@ -15,12 +17,21 @@ class ExceptionsTest {
     }
 
     @Test
-    fun `throw an exception if purchase amount is not divisible by 1,000`() {
+    fun `throw an exception if purchase amount is not divisible by LOTTO_COST`() {
         assertThatThrownBy {
             validatePurchaseAmount("1501").getOrThrow()
         }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining(AMOUNT_NOT_DIVISIBLE_ERROR_MESSAGE)
+    }
+
+    @Test
+    fun `throw an exception if winning numbers contain non-numeric values`() {
+        assertThatThrownBy {
+            validateWinningNumbers(listOf("1", "2", "A", "4", "5", "6")).getOrThrow()
+        }
+            .isInstanceOf(NumberFormatException::class.java)
+            .hasMessageContaining(INVALID_WINNING_NUMERIC_ERROR_MESSAGE)
     }
 
 }
