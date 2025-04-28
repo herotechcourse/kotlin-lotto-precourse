f43cecdf911ecd1fdbd2ad21b5e4fe7c86309b39
minsang <minsang007@gmail.com>
2025-04-29 05:02:47 +0900

    fix(utils): prevent calculateMatches from returning null
    change calculateMatches logic not to output null

--- src/main/kotlin/lotto/utils/ResultCalculator.kt
+++ src/main/kotlin/lotto/utils/ResultCalculator.kt
@@ -5,29 +5,31 @@ import lotto.Lottos
 import lotto.WinningNumbers
 
 object ResultCalculator {
-    fun calculateMatches(lottos: Lottos, winningNumbers: WinningNumbers) : Map<String, Int> {
+    fun calculateMatches(lottos: Lottos, winningNumbers: WinningNumbers): Map<String, Int> {
         val tickets = lottos.getTickets()
-        val winningSet = winningNumbers.getWinningNumbers().toSet()
-        val bonus = winningNumbers.getBonusNumber()
-        val result = mutableMapOf<String, Int>()
+        val winningNumbersList = winningNumbers.getWinningNumbers()
+        val bonusNumber = winningNumbers.getBonusNumber()
+        val result = mutableMapOf<String, Int>("3" to 0, "4" to 0, "5" to 0, "5+bonus" to 0, "6" to 0)
 
         for (ticket in tickets) {
-            val matchCount = ticket.getNumbers().count { it in winningSet }
-            val key = if (matchCount == 5 && ticket.getNumbers().contains(bonus)) {
-                "5+bonus"
-            } else {
-                matchCount.toString()
-            }
-            result[key] = result.getOrDefault(key, 0) + 1
+            val matchCount = matchCount(ticket, winningNumbersList)
+            if (matchCount == "5" && ticket.getNumbers().contains(bonusNumber)) {
+                result["5+bonus"] = result["5+bonus"]!! + 1
+            } else if (matchCount.toInt() in 3..6) {
+                result[matchCount] = result[matchCount]!! + 1
+            } else continue
         }
-
         return result
-
     }
 
-    fun matchCount(lotto: Lotto, winningNumbers: WinningNumbers): String {
-        val winningNumbersList = winningNumbers.getWinningNumbers()
-        val total = lotto.getNumbers().count { it in winningNumbersList }
+    fun matchCount(lotto: Lotto, winningNumbersList: List<Int>): String {
+        val lottoNumbers = lotto.getNumbers()
+        var total = 0
+        for (number in lottoNumbers) {
+            if (winningNumbersList.contains(number)) {
+                total++
+            }
+        }
         return total.toString()
     }
 }
