3d8a8dd45076323cc38ed51419e542e23f7b2862
gokcesen <sengkce@gmail.com>
2025-04-28 14:09:41 +0200

    test(lotto): add additional unit tests to Lotto class methods

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -11,7 +13,15 @@ class LottoTest {
         }
     }
 
-    // TODO: Implement production code to pass the test
+    @Test
+    fun `throw exception when lotto numbers less than six`() {
+        val numbers = listOf(1, 2, 3, 4, 5)
+        assertThrows<IllegalArgumentException> {
+            Lotto(numbers)
+        }
+    }
+
+
     @Test
     fun `throws an exception when lotto numbers contain duplicates`() {
         assertThrows<IllegalArgumentException> {
@@ -19,5 +29,25 @@ class LottoTest {
         }
     }
 
-    // TODO: Implement tests based on the added features
+    @Test
+    fun `throw exception when creating with out of range numbers`() {
+        val numbers = listOf(1, 2, 3, 4, 5, 46)
+        assertThrows<IllegalArgumentException> {
+            Lotto(numbers)
+        }
+    }
+
+    @Test
+    fun `check bonus number inclusion`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        assertTrue(lotto.hasBonusNumber(6))
+    }
+
+    @Test
+    fun `check matching numbers count`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningNumbers = listOf(1, 2, 3, 7, 8, 9)
+        assertEquals(3, lotto.getMatchingNumbers(winningNumbers))
+    }
 }
+
