ee9ac19e6322f14c01b013c959f323fb98de327e
jongsoo <24ssafy@gmail.com>
2025-04-28 23:28:51 +0900

    refactor(app): generate and display tickets in Application.kt

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,7 +1,12 @@
 package lotto
 
+import lotto.domain.LottoMachine
 import lotto.view.InputView;
+import lotto.view.OutputView
 
 fun main() {
   val purchaseAmount = InputView.readPurchaseAmount();
+  val count = purchaseAmount/1000
+  val tickets = LottoMachine().issu(count)
+  OutputView.printTickets(tickets)
 }
