a1e2d4450f1229cb58cead5771bd856ce20717d7
rootPark <rmsdnjs518@gmail.com>
2025-04-28 19:43:39 +0900

    feat: CreateController - mathWinningLotto fun create

--- src/main/kotlin/lotto/controller/CreateController.kt
+++ src/main/kotlin/lotto/controller/CreateController.kt
@@ -2,6 +2,7 @@ package lotto.controller
 
 import camp.nextstep.edu.missionutils.Randoms
 import lotto.Lotto
+import lotto.model.WinningLotto
 
 private const val LOTTO_PRICE = 1000
 private const val LOTTO_NUMBER_COUNT = 6
@@ -14,5 +15,17 @@ class CreateController {
     return List(ticektCount) { 
       Lotto(Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBER_COUNT)) 
     }
+  }
+
+  fun matchWinningLotto(tickets: List<Lotto>, winningLotto: WinningLotto): LottoResult {
+    val results = Prize.values().associateWith { 0 }.toMutableMap()
+
+    tickets.forEach { ticket ->
+      val prize = winningLotto.match(ticket)
+      if(prize != Prize.NONE) {
+        results[prize] = results[prize]!! + 1
+      }
+    }
+    return LottoResult(results, tickets.size)
   }
 }
