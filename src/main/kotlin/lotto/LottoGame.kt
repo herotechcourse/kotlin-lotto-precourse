12f89e0f44a7ddefb506f79f94de53660cdd543a
Burcu <burcu@MacBook-Air-von-Burcu.local>
2025-04-28 12:21:01 +0200

    feat: implement ticket generation function

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -2,7 +2,7 @@ package lotto
 
 import lotto.view.InputView
 import lotto.view.OutputView
-
+import camp.nextstep.edu.missionutils.Randoms
 class LottoGame(private val inputView: InputView = InputView()) {
 
   fun play() {
@@ -10,7 +10,7 @@ class LottoGame(private val inputView: InputView = InputView()) {
     // get purchase amount
     val purchaseAmount = inputView.readPurchaseAmount()
     // generate and display tickets 
-  
+    val tickets = generateTickets(purchaseAmount)
     // get winning numbers
     val winningNumbers = inputView.readWinningNumbers()
     // get bonus
@@ -18,5 +18,14 @@ class LottoGame(private val inputView: InputView = InputView()) {
 	  // calculate winnings
     
     // display result
+  }
+
+  private fun generateTickets(purchaseAmount: Long): List<Lotto> {
+    val ticketCount = purchaseAmount / TICKET_PRICE
+    val tickets = mutableListOf<Lotto>()
+    for (i in 1..ticketCount) {
+      tickets.add(Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)))
+    }
+    return tickets
   }
 }
