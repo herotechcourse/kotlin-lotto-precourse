eb03768167ff1759f8647057d7414f66d1608a61
서창덕 <44630705+Garodden@users.noreply.github.com>
2025-04-29 04:49:08 +0900

    test(lotto) add test for Lotto number validation
    - verify that Lotto throws exceptions for invalid number inputs(too many, too few, not unique, not in range)
    - verify successful creation with six unique numbers

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.assertj.core.api.Assertions.assertThat
 
 class LottoTest {
     @Test
@@ -11,7 +12,6 @@ class LottoTest {
         }
     }
 
-    // TODO: Implement production code to pass the test
     @Test
     fun `throws an exception when lotto numbers contain duplicates`() {
         assertThrows<IllegalArgumentException> {
@@ -19,5 +19,28 @@ class LottoTest {
         }
     }
 
-    // TODO: Implement tests based on the added features
+    @Test
+    fun `throws an exception when lotto numbers are less than six`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5))
+        }
+    }
+
+    @Test
+    fun `throws an exception when lotto numbers are not in range of 1-45`(){
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(67, 2, 3, 4, 5))
+        }
+    }
+
+    @Test
+    fun `creates a valid lotto with six unique numbers`() {
+        val numbers = listOf(1, 2, 3, 4, 5, 6)
+
+        val lotto = Lotto(numbers)
+
+        assertThat(lotto.numbers).containsExactly(1, 2, 3, 4, 5, 6)
+    }
+
+
 }
