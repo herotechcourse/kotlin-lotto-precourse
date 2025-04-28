05534c01888e653a224fbb0f5a3feebb86a9676d
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-28 22:15:10 +0900

    feat(prize) set prizes directly to PrizeManager

--- src/main/kotlin/prize/PrizeManager.kt
+++ src/main/kotlin/prize/PrizeManager.kt
@@ -1,3 +1,10 @@
 package prize
 
-var prizeByRank = Array<Prize>
+var prizeByRank: Array<Prize> = arrayOf(
+    Prize(0, false, 0),
+    Prize(6, false, 2_000_000_000),
+    Prize(5, true, 30_000_000),
+    Prize(5, false, 1_500_000),
+    Prize(4, false, 50_000),
+    Prize(3, false, 5_000)
+)
