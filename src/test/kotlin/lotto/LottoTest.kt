fd306ac719d012e0f8fa8ac548013303ce88ecd7
Jiihyun <cwlgusc@gmail.com>
2025-04-28 14:36:40 +0900

    test: add Lotto test

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,23 +1,95 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class LottoTest {
+
     @Test
     fun `throws an exception when lotto numbers exceed six`() {
         assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
+            Lotto(
+                listOf(
+                    LottoNumber(1),
+                    LottoNumber(2),
+                    LottoNumber(3),
+                    LottoNumber(4),
+                    LottoNumber(5),
+                    LottoNumber(6),
+                    LottoNumber(7)
+                )
+            )
         }
     }
 
-    // TODO: Implement production code to pass the test
     @Test
     fun `throws an exception when lotto numbers contain duplicates`() {
         assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 5))
+            Lotto(
+                listOf(
+                    LottoNumber(1),
+                    LottoNumber(2),
+                    LottoNumber(3),
+                    LottoNumber(4),
+                    LottoNumber(5),
+                    LottoNumber(5)
+                )
+            )
         }
     }
 
-    // TODO: Implement tests based on the added features
+    @Test
+    fun `can count matching numbers between two lotto`() {
+        // given
+        val numbers = listOf(1, 2, 3, 4, 5, 6)
+        val comparisonNumbers = listOf(4, 5, 6, 7, 8, 9)
+        val lotto = createLotto(numbers)
+        val comparisonLotto = createLotto(comparisonNumbers)
+        // when
+        val matchingCount = lotto.countSameNumber(comparisonLotto)
+        // then
+        assertThat(matchingCount).isEqualTo(3)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [11, 20, 40])
+    fun `should return true when lotto does not contain the given number`(lottoNumber: Int) {
+        // given
+        val numbers = listOf(1, 2, 3, 4, 5, 6)
+        val lotto = createLotto(numbers)
+        // when
+        val isNotContained = lotto.doesNotContain(LottoNumber(lottoNumber))
+        // then
+        assertThat(isNotContained).isTrue()
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 3, 4, 5, 6])
+    fun `should return true when lotto contains a same lotto number`(lottoNumber: Int) {
+        // given
+        val numbers = listOf(1, 2, 3, 4, 5, 6)
+        val lotto = createLotto(numbers)
+        // when
+        val hasSameNumber = lotto.hasSameNumber(LottoNumber(lottoNumber))
+        // then
+        assertThat(hasSameNumber).isTrue()
+    }
+
+    private fun createLotto(numbers: List<Int>): Lotto {
+        return Lotto(
+            listOf(
+                LottoNumber(numbers[0]),
+                LottoNumber(numbers[1]),
+                LottoNumber(numbers[2]),
+                LottoNumber(numbers[3]),
+                LottoNumber(numbers[4]),
+                LottoNumber(numbers[5])
+            )
+        )
+    }
+
+
 }
