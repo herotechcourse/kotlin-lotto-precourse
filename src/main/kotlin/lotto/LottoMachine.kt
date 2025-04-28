08e2b75a600ef3425a9d7e95c01667750341b57a
LeeJaeHyeok97 <hazardous10@naver.com>
2025-04-29 01:38:18 +0900

    refactor: clean up unused companion object

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -1,6 +1,5 @@
 package lotto
 
-import camp.nextstep.edu.missionutils.Randoms
 
 class LottoMachine(private val lottoNumberGenerator: LottoNumberGenerator) {
     fun generate(ticketCount: Int) : Lottos {
@@ -10,11 +9,5 @@ class LottoMachine(private val lottoNumberGenerator: LottoNumberGenerator) {
             generatedLottos.add(Lotto(generatedNumbers))
         }
         return Lottos(generatedLottos)
-    }
-
-    companion object {
-        private const val START_NUMBER = 1
-        private const val END_NUMBER = 45
-        private const val PICK_NUMBER = 6
     }
 }
