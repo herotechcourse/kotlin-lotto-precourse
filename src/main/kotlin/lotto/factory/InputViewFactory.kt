2c821621d05dc47fd24e3681e5463b7b2ac6a09b
minsang <minsang007@gmail.com>
2025-04-29 05:32:32 +0900

    refactor(factory): simplify InputViewFactory by importing input views
    Add imports for WinningNumbersInputView and BonusNumberInputView to remove fully qualified names in when branches, improving readability.

--- src/main/kotlin/lotto/factory/InputViewFactory.kt
+++ src/main/kotlin/lotto/factory/InputViewFactory.kt
@@ -1,15 +1,17 @@
 package lotto.factory
 
 import lotto.view.input.InputView
-import lotto.view.input.PurchaseAmountInputView
 import lotto.enums.InputType
+import lotto.view.input.PurchaseAmountInputView
+import lotto.view.input.WinningNumbersInputView
+import lotto.view.input.BonusNumberInputView
 
 object InputViewFactory {
     fun createInputView(inputType: InputType): InputView {
         return when (inputType) {
             InputType.PURCHASE_AMOUNT -> PurchaseAmountInputView
-            InputType.WINNING_NUMBERS -> lotto.view.input.WinningNumbersInputView
-            InputType.BONUS_NUMBER -> lotto.view.input.BonusNumberInputView
+            InputType.WINNING_NUMBERS -> WinningNumbersInputView
+            InputType.BONUS_NUMBER -> BonusNumberInputView
         }
     }
 }
