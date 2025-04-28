8d244aa89de9cf948d310b622b1aa0d921c8c9bb
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-28 16:58:30 +0900

    feat(input) internalize constants and add print function for each constant

--- src/main/kotlin/input/config/InputDialogMsg.kt
+++ src/main/kotlin/input/config/InputDialogMsg.kt
@@ -1,15 +1,29 @@
 package input.config
 
-const val TICKET_AMOUNT_MSG = "Please enter the purchase amount."
+internal const val TICKET_AMOUNT_MSG = "Please enter the purchase amount."
 
-const val LAST_WEEK_WINNING_NUMBER_MSG = "Please enter last week's winning numbers."
+internal const val LAST_WEEK_WINNING_NUMBER_MSG = "Please enter last week's winning numbers."
 
-const val BONUS_NUMBER_MSG = "Please enter the bonus number."
+internal const val BONUS_NUMBER_MSG = "Please enter the bonus number."
 
-const val TICKET_RESULT_PREFIX = "You have purchased "
+internal const val TICKET_RESULT_PREFIX = "You have purchased "
 
-const val TICKET_RESULT_POSTFIX = " tickets."
+internal const val TICKET_RESULT_POSTFIX = " tickets."
 
-fun getTicketResultMsg(amount: Int): String{
+internal fun getTicketResultMsg(amount: Int): String{
     return TICKET_RESULT_PREFIX + amount + TICKET_RESULT_POSTFIX
+}
+
+fun printTicketAmountMsg(){
+    println(TICKET_AMOUNT_MSG)
+}
+fun printLastWeekWinningNumberMsg(){
+    println(LAST_WEEK_WINNING_NUMBER_MSG)
+}
+fun printBonusNumberMsg(){
+    println(BONUS_NUMBER_MSG)
+}
+
+fun printTicketResultMsgWithAmount(amount: Int){
+    println(getTicketResultMsg(amount))
 }
