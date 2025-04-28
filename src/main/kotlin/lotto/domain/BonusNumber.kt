ab4efbbcb2536f7b36e291e4d3be86f184b65628
차정혁 <ckwjdgur1234@gmail.com>
2025-04-29 00:47:06 +0900

    feat(bonus): validate bonus number

--- src/main/kotlin/lotto/domain/BonusNumber.kt
+++ src/main/kotlin/lotto/domain/BonusNumber.kt
@@ -1,11 +1,21 @@
 package lotto.domain
 
-import lotto.view.InputView
-
 class BonusNumber(number: Int) {
+    init {
+        require(number > 0) { ErrorMessages.NOT_A_POSITIVE_NUMBER_ERROR }
+        require(number in LottoConstants.MIN_LOTTO_NUMBER..LottoConstants.MAX_LOTTO_NUMBER) {
+            ErrorMessages.LOTTO_NUMBER_RANGE_ERROR
+        }
+    }
+
     companion object {
         fun from(input: String): BonusNumber {
-            return BonusNumber(input.toInt())
+            require(input.isNotBlank()) { ErrorMessages.BLANK_ERROR }
+
+            val parsedInput =
+                input.toIntOrNull() ?: throw IllegalArgumentException(ErrorMessages.NOT_A_NUMBER_ERROR)
+
+            return BonusNumber(parsedInput)
         }
     }
 }
