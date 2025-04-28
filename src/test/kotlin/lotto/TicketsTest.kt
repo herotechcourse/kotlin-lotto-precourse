0833df10ebdc0879b6e99ecb5cdf80a524479712
서창덕 <44630705+Garodden@users.noreply.github.com>
2025-04-29 04:56:13 +0900

    test(lotto): verify if generated numbers are in lotto number range

--- src/test/kotlin/lotto/TicketsTest.kt
+++ src/test/kotlin/lotto/TicketsTest.kt
@@ -29,4 +29,17 @@ class TicketsTest {
         assertThat(tickets.lottos).hasSize(8)
     }
 
+    @Test
+    fun `creates tickets with valid lotto numbers`() {
+        val money = 1000
+
+        val tickets = Tickets(money)
+
+        assertThat(tickets.lottos).hasSize(1)
+        val lotto = tickets.lottos[0]
+        assertThat(lotto.numbers).hasSize(6)
+        assertThat(lotto.numbers.distinct()).hasSize(6) // Ensure numbers are unique
+        assertThat(lotto.numbers.all { it in 1..45 }).isTrue() // Ensure numbers are in range
+    }
+
 }
