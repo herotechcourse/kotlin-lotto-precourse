600dd5d8a399cd87c88654d90fa4a8292dc950cd
Bengel Adoma <bengelansah@gmail.com>
2025-04-28 18:53:44 +0200

    feat: Implement Lotto class to represent a single ticket

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,13 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.toSet().size == 6) { "[ERROR] Numbers must be unique." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
     }
 
-    // TODO: Implement additional functions
+    val numbers: List<Int> = numbers.sorted()
 }
