a94c5adaf49fe935ce84c0c70f280f6aefab4b7a
puleugo <puleugo@gmail.com>
2025-04-28 16:18:13 +0900

    test(domain): write `TicketIssuer` unit tests

---
+++ src/test/kotlin/lotto/domain/TicketIssuerTest.kt
@@ -0,0 +1,32 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+
+class TicketIssuerTest {
+
+    @Test
+    fun `issue ticket list`() {
+        // given
+        val issuer = TicketIssuer()
+        val ticketCount = 5
+
+        // when
+        val tickets = issuer.issue(ticketCount)
+
+        // then
+        assert(tickets.size == ticketCount)
+    }
+
+    @Test
+    fun `throws an exception when purchase number is 0 or less`() {
+        // given
+        val issuer = TicketIssuer()
+        val invalidCount = -1
+
+        // when & then
+        assertThatThrownBy { issuer.issue(invalidCount) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("[ERR] Ticket count must be greater than 0.")
+    }
+}
