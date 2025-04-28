86c3bdfecf2656b2f329ca19ffa96cec0ffd6461
sunishae <sunishae44@gmail.com>
2025-04-28 15:05:27 +0900

    feat: add last week number input message in InputView

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,9 +3,22 @@ package lotto.view
 object InputView {
 
     private const val INPUT_PURCHASE_AMOUNT_MESSAGE = "Please enter the purchase amount."
+    private const val INPUT_LAST_WINNING_NUMBER_MESSAGE = "Please enter last week's winning numbers."
+    private const val INPUT_LAST_BONUS_NUMBER_MESSAGE = "Please enter the bonus number."
 
     fun readPurchaseAmount(): String {
         println(INPUT_PURCHASE_AMOUNT_MESSAGE)
         return readln()
     }
+
+    fun readLastWinningNumber(): String {
+        println(INPUT_LAST_WINNING_NUMBER_MESSAGE)
+        return readln()
+    }
+
+    fun readLastBonusNumber(): String {
+        println(INPUT_LAST_WINNING_NUMBER_MESSAGE)
+        return readln()
+    }
+
 }
