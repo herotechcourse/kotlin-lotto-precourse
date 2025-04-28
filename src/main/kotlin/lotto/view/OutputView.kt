c29f4f03e2dba8b1d75494ea7f1fb5c91c739a79
jongsoo <24ssafy@gmail.com>
2025-04-29 01:20:04 +0900

    feat(view): update outputview with result statics

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,6 +1,6 @@
 package lotto.view
-
 import lotto.Lotto
+import lotto.domain.Rank
 
 object OutputView {
   fun printTickets(tickets : List<Lotto>){
@@ -8,4 +8,22 @@ object OutputView {
     tickets.forEach{ println(it.getNumbers())}
   }
 
+  fun printResult(stats : Map<Rank,Int>, purchase: Int){
+    println("Winning Statistics")
+    println("---")
+    Rank.values().filter { it!=Rank.NONE }.forEach{
+      rank ->
+      val count = stats.getOrDefault(rank,0)
+      val label =
+        if(rank==Rank.SECOND) "5 Matches + Bonus Ball"
+        else  "${rank.matchCount} Matches"
+      println("$label (${"%,d".format(rank.prize)} KRW) - $count tickets")
+    }
+    var totalPrize = 0L;
+    for ((rank, count) in stats) {
+      totalPrize += rank.prize * count
+    }
+    val rate = totalPrize.toDouble()/purchase * 100
+    println("Total return rate is ${"%.1f".format(rate)}%.")
+  }
 }
