237a07574db4acd959053d6a39dbe4e8354f01f0
Arielleeeee <arielkatherine0421@gmail.com>
2025-04-28 19:18:51 +0200

    refactor(Lotto): update order class member

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,16 +1,10 @@
 package lotto
 
 class Lotto(private val numbers: List<Int>) {
-    companion object {
-        const val NUMBER_COUNT = 6
-        const val MIN_NUMBER = 1
-        const val MAX_NUMBER = 45
-    }
-
     init {
         require(numbers.size == NUMBER_COUNT) { "[ERROR] Lotto must contain exactly $NUMBER_COUNT numbers." }
         require(numbers.toSet().size == NUMBER_COUNT) { "[ERROR] Duplicate numbers are not allowed." }
-        require(numbers.all { it in MIN_NUMBER..Lotto.MAX_NUMBER }) { "[ERROR] Numbers must be between $MIN_NUMBER and $MAX_NUMBER." }
+        require(numbers.all { it in MIN_NUMBER..MAX_NUMBER }) { "[ERROR] Numbers must be between $MIN_NUMBER and $MAX_NUMBER." }
     }
 
     override fun toString(): String {
@@ -19,5 +13,11 @@ class Lotto(private val numbers: List<Int>) {
 
     fun getNumbers(): List<Int> {
         return numbers
+    }
+
+    companion object {
+        const val NUMBER_COUNT = 6
+        const val MIN_NUMBER = 1
+        const val MAX_NUMBER = 45
     }
 }
