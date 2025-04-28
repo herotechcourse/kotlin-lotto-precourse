51b867ef2350e8cb3dcc90aeb172eae482225aa3
Petra Bencze <petra.bncz@gmail.com>
2025-04-28 15:07:19 +0200

    refactor(LottoGame): split play into smaller functions
    - purchaseTickets
    - inputWinningMNumbers
    - evaluateResults
    - play

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -4,17 +4,32 @@ class LottoGame(
     private val inputView: InputView = InputView(),
     private val outputView: OutputView = OutputView()
 ) {
+
     fun play() {
+        val purchase = purchaseTickets()
+        val lotto = inputWinningNumbers()
+        evaluateResults(lotto, purchase)
+    }
+
+    private fun purchaseTickets(): Purchase {
         val amount = inputView.readPurchaseAmount()
         val purchase = Purchase(amount)
         outputView.displayTickets(purchase.tickets)
 
+        return purchase
+    }
+
+    private fun inputWinningNumbers(): Lotto {
         val winningNumbers = inputView.readWinningNumbers()
         val lotto = Lotto(winningNumbers)
 
         val bonusNumber = inputView.readBonusNumber()
         lotto.addBonusNumber(bonusNumber)
 
+        return lotto
+    }
+
+    private fun evaluateResults(lotto: Lotto, purchase: Purchase) {
         val result = lotto.compareNumbers(purchase.tickets)
         outputView.printStatistics(result)
 
