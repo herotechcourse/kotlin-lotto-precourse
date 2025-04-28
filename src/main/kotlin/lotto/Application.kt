8c25105d40ebc77759d52b5f9180d7437bed9984
jvnlee <lhjun1028@naver.com>
2025-04-28 20:41:47 +0900

    feat: complete main lotto application logic

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,23 @@
 package lotto
 
+import lotto.domain.LottoMachine
+import lotto.util.LottoValidator
+import lotto.view.InputView
+import lotto.view.OutputView
+
 fun main() {
-    // TODO: Implement the program
+    val amount = InputView.readPurchaseAmount()
+    LottoValidator.validateAmount(amount)
+
+    val tickets = LottoMachine.createTickets(amount)
+    OutputView.printTickets(tickets)
+
+    val winningNumbers = InputView.readWinningNumbers()
+    LottoValidator.validateWinningNumbers(winningNumbers)
+
+    val bonusNumber = InputView.readBonusNumber()
+    LottoValidator.validateBonusNumber(bonusNumber, winningNumbers)
+
+    val result = LottoMachine.calculateResult(tickets, winningNumbers, bonusNumber)
+    OutputView.printResult(result, amount)
 }
