0854058c404e780277bf27ac6cd18f23c0d7850f
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2025-04-28 11:06:09 +0200

    feat(domain): add LottoResult.totalPrizeMoney to get the rank prize of this Lotto ticket

--- src/main/kotlin/lotto/domain/LottoResult.kt
+++ src/main/kotlin/lotto/domain/LottoResult.kt
@@ -8,6 +8,8 @@ class LottoResult(
     val matchedNumbers: List<Int>,
     val bonusNumber: Boolean
 ) {
+    fun totalPrizeMoney() = prizeRank.prize
+
     override fun toString(): String {
         return "Ticket ${ticket.getNumbers()} -> Match: ${matchedNumbers.size} (${if (bonusNumber) "+ Bonus" else ""}), Prize: ${prizeRank.prize} KRW"
     }
