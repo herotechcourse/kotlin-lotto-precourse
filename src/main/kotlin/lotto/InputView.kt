8b6952f0e6489e67af2cd159681f4c765d73a963
yojin <roadlroal@naver.com>
2025-04-28 10:17:44 +0100

    fix(InputView): add while to every input function and re-prompt when user input is invalid

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -4,52 +4,64 @@ import camp.nextstep.edu.missionutils.Console
 
 object InputView {
     fun getPurchaseAmount(): Int {
-        println("Please enter the purchase amount.")
+        while (true) {
+            println("Please enter the purchase amount.")
 
-        try {
-            val input = Console.readLine()?.trim()?.toInt() ?: throw IllegalArgumentException("[ERROR] Input cannot be null")
+            try {
+                val input = Console.readLine()?.trim()?.toInt()
+                    ?: throw IllegalArgumentException("[ERROR] Input cannot be null")
 
-            println()
+                println()
 
-            return input
-        } catch (e: NumberFormatException) {
-            println("[ERROR] Please enter a valid integer")
-            throw IllegalArgumentException("[ERROR] Please enter a valid integer")
+                return input
+            } catch (e: NumberFormatException) {
+                println("[ERROR] Please enter a valid integer\n")
+            }
         }
     }
 
     fun getWinningNumbers(): List<Int> {
-        println("Please enter last week's winning numbers.")
+        while (true) {
+            println("Please enter last week's winning numbers.")
 
-        val input = Console.readLine()?.trim() ?: throw IllegalArgumentException("[ERROR] Input cannot be null")
+            try {
+                val input = Console.readLine()?.trim() ?: throw IllegalArgumentException("[ERROR] Input cannot be null")
 
-        val numbers = input.split(",")
-            .mapNotNull { it.trim().toIntOrNull() }
-            .toList()
+                val numbers = input.split(",")
+                    .mapNotNull { it.trim().toIntOrNull() }
+                    .toList()
 
-        if (numbers.size != 6 || numbers.size != numbers.distinct().size) {
-            throw IllegalArgumentException("You must enter exactly 6, non duplicate numbers.")
+                if (numbers.size != 6 || numbers.size != numbers.distinct().size) {
+                    throw IllegalArgumentException("[ERROR] You must enter exactly 6, non duplicate numbers.")
+                }
+
+                println()
+
+                return numbers
+            } catch (e: IllegalArgumentException) {
+                println("[ERROR] Please enter a valid 6 integer\n")
+            }
         }
-
-        println()
-
-        return numbers
     }
 
     fun getBonusNumber(winningNumbers: List<Int>): Int {
-        println("Please enter the bonus number.")
+        while (true) {
+            println("Please enter the bonus number.")
 
-        try {
-            val input = Console.readLine()?.trim()?.toInt() ?: throw IllegalArgumentException("[ERROR] Input cannot be null")
-            if (input < 1 || input > 45 || input in winningNumbers) {
-                throw IllegalArgumentException("[ERROR] bonus number must be between 1 and 45, not in winningNumbers.")
+            try {
+                val input = Console.readLine()?.trim()?.toInt()
+                    ?: throw IllegalArgumentException("[ERROR] Input cannot be null")
+
+                if (input < 1 || input > 45 || input in winningNumbers) {
+                    throw IllegalArgumentException("[ERROR] bonus number must be between 1 and 45, not in winningNumbers.")
+                }
+
+                println()
+
+                return input
+            } catch (e: IllegalArgumentException) {
+                println("[ERROR] Please enter valid number\n")
             }
-
-            println()
-
-            return input
-        } catch (e: NumberFormatException) {
-            throw IllegalArgumentException("[ERROR] Please enter a valid integer")
         }
     }
 }
