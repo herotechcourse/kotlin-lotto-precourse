b2857613e1914fbad9c333c6e27daed67b858380
sopa <sopa.sara@gmail.com>
2025-04-28 20:29:14 +0200

    refactor(generator): update LottoGenerator to generate list of Lotto objects

--- src/main/kotlin/lotto/LottoGenerator.kt
+++ src/main/kotlin/lotto/LottoGenerator.kt
@@ -4,8 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms
 import lotto.config.Config.TICKET_PRICE
 
 object LottoGenerator {
-    fun generateTicket(amount: Int): Int {
+    fun generateTicket(amount: Int): List<Lotto> {
         val count = amount / TICKET_PRICE
-        return count
+        return List(count) {
+            val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+            Lotto(numbers)
+        }
     }
 }
