8f209d158fc19c1c15b00f8ae2e3b59b92965abb
GSIII <kalseo901@gmail.com>
2025-04-28 23:15:08 +0900

    test: Add unit tests for prize calculation

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
-import lotto.view.InputView
+import lotto.services.LottoResult
+import lotto.services.Prize
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -20,6 +21,42 @@ class LottoTest {
         }
     }
 
-    // TODO: Implement tests based on the added features
+    @Test
+    fun `calculates result for first prize`() {
+        val tickets = listOf(Lotto(listOf(1, 2, 3, 4, 5, 6)))
+        val winningNumbers = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val bonusNumber = 7
+
+        val lottoResult = LottoResult(tickets, winningNumbers, bonusNumber)
+
+        val result = lottoResult.getResult()
+        assert(result[Prize.FIRST] == 1)
+    }
+
+    @Test
+    fun `calculates result for second prize`() {
+        val tickets = listOf(Lotto(listOf(1, 2, 3, 4, 5, 7)))
+        val winningNumbers = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val bonusNumber = 7
+
+        val lottoResult = LottoResult(tickets, winningNumbers, bonusNumber)
+
+        val result = lottoResult.getResult()
+        assert(result[Prize.SECOND] == 1)
+    }
+
+    @Test
+    fun `calculates result for third prize`() {
+        val tickets = listOf(Lotto(listOf(1, 2, 3, 4, 5, 8)))
+        val winningNumbers = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val bonusNumber = 7
+
+        val lottoResult = LottoResult(tickets, winningNumbers, bonusNumber)
+
+        val result = lottoResult.getResult()
+
+        assert(result[Prize.THIRD] == 1)
+    }
 
 }
+
