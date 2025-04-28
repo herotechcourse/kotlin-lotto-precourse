8a6f4bf8d8b19b6f29ffc0e3da1fcdfd15684c99
siyeonSon <siyeon0209@naver.com>
2025-04-28 22:50:35 +0900

    feat: issue the lotto tickets

--- src/main/kotlin/lotto/domain/LottoGame.kt
+++ src/main/kotlin/lotto/domain/LottoGame.kt
@@ -1,8 +1,22 @@
 package lotto.domain
 
+import camp.nextstep.edu.missionutils.Randoms
+import lotto.Lotto
 import lotto.utils.Constant.LOTTO_COST
+import lotto.utils.Constant.LOTTO_SIZE
+import lotto.utils.Constant.MAX_LOTTO_NUMBER
+import lotto.utils.Constant.MIN_LOTTO_NUMBER
 
 class LottoGame {
     fun getQuantity(amount: Int): Int = amount / LOTTO_COST
 
+    fun issueLottoTickets(quantity: Int): List<Lotto> = List(quantity) {
+        Lotto(
+            Randoms.pickUniqueNumbersInRange(
+                MIN_LOTTO_NUMBER,
+                MAX_LOTTO_NUMBER,
+                LOTTO_SIZE,
+            ).sorted()
+        )
+    }
 }
--- src/test/kotlin/lotto/domain/LottoGameTest.kt
+++ src/test/kotlin/lotto/domain/LottoGameTest.kt
@@ -13,4 +13,20 @@ class LottoGameTest {
 
         assertThat(quantity).isEqualTo(8)
     }
+
+    @Test
+    fun `issue the lotto tickets`() {
+        val quantity = 5
+        val lottos = lottoGame.issueLottoTickets(quantity)
+
+        assertThat(lottos).hasSize(quantity)
+        lottos.forEach { lotto ->
+            assertThat(lotto.getNumbers()).hasSize(6)
+            assertThat(lotto.getNumbers()).allSatisfy { number ->
+                assertThat(number).isBetween(1, 45)
+            }
+            assertThat(lotto.getNumbers().toSet().size).isEqualTo(6)
+        }
+    }
+
 }
