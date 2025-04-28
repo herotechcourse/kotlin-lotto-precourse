91dd8023dc1e4da28fc7269dddd8e6929aabcd5c
박채린 <bewithbomi@gmail.com>
2025-04-29 06:53:57 +0900

    feat(view): build OutputView to display outputs

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -1,20 +1,16 @@
 package lotto
 
 object OutputView {
-    fun printTickets(tickets: List<Lotto>) {
+    fun printTickets(tickets: List<LottoTicket>) {
         println("You have purchased ${tickets.size} tickets.")
-        tickets.forEach { println(it.getNumbers()) }
+        tickets.forEach { println(it.lotto.getNumbers()) }
     }
 
-    fun printResult(result: Result) {
-        println("Winning Statistics")
-        println("---")
-        Rank.values()
-            .filter { it != Rank.NONE }
-            .sortedByDescending { it }
-            .forEach { rank ->
-                println("${rank.matchCount} Matches${if (rank.hasBonus) " + Bonus Ball" else ""} (${rank.prize} KRW) – ${result.getCount(rank)} tickets")
-            }
-        println("Total return rate is ${"%.1f".format(result.getRate())}%.")
+    fun printResult(result: LottoResult) {
+        println("Winning Statistics\n---")
+        Rank.values().sortedBy { it.prize }.reversed().forEach { rank ->
+            println("${rank.description} – ${result.rankCount(rank)} tickets")
+        }
+        println("Total return rate is ${result.profitRate()}%.")
     }
 }
