d2df72c3993922b40be41abc9d9109acf95b9c27
minsang <minsang007@gmail.com>
2025-04-29 06:43:55 +0900

    feat(main): implement main lottery game flow
    Integrated purchase amount input, lotto generation, winning numbers and bonus number processing, and output display. Added try-catch for handling input validation errors.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,27 +1,36 @@
 package lotto
 
 import lotto.factory.InputViewFactory
-import lotto.view.input.BonusNumberInputView
-import lotto.view.input.WinningNumbersInputView
+import lotto.factory.OutputViewFactory
 import lotto.enums.InputType
-import lotto.view.output.DisplayLottosOutputView
+import lotto.enums.OutputType
+import lotto.validator.WinningNumbersValidator
 
 fun main() {
     // TODO: Implement the program
-    val purchaseView   = InputViewFactory.createInputView(InputType.PURCHASE_AMOUNT)
-    val purchaseAmount = PurchaseAmount(purchaseView.readInput())
-    println("PurchaseAmount: $purchaseAmount")
+    try {
+        val purchaseView   = InputViewFactory.createInputView(InputType.PURCHASE_AMOUNT)
+        val purchaseAmount = PurchaseAmount(purchaseView.readInput())
 
-    val ticketCounts = purchaseAmount.getTicketCount()
-    println("TicketCounts: $ticketCounts")
-    val lottos = Lottos(mutableListOf(), ticketCounts)
+        val ticketCounts = purchaseAmount.getTicketCount()
+        val lottos = Lottos(mutableListOf(), ticketCounts)
 
-    DisplayLottosOutputView.display(lottos)
+        val lottosView = OutputViewFactory.createOutputView<Lottos>(OutputType.LOTTOS)
+        lottosView.display(lottos)
 
-    val winningNumbers = WinningNumbersInputView.readInput().split(",")
-    val bonusNumber = BonusNumberInputView.readInput()
-    val allNumbers = WinningNumbers(winningNumbers, bonusNumber)
-    println("WinningNumbers: ${allNumbers.getWinningNumbers()}")
-    println("BonusNumber: ${allNumbers.getBonusNumber()}")
+        val winningNumbersView = InputViewFactory.createInputView(InputType.WINNING_NUMBERS)
+        val winningNumbers = winningNumbersView.readInput().split(",")
+
+        val bonusNumberView = InputViewFactory.createInputView(InputType.BONUS_NUMBER)
+        val bonusNumber = bonusNumberView.readInput()
+
+        val allNumbers = WinningNumbers(winningNumbers, bonusNumber)
+
+        val lottosResult = LottosResult(lottos, allNumbers)
+        val winningStatisticsView = OutputViewFactory.createOutputView<LottosResult>(OutputType.WINNING_STATISTICS)
+        winningStatisticsView.display(lottosResult)
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
 }
 
