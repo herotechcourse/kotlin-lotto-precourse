9b7d61a47d1b1207453f006c931c89fa5e24eeeb
김동영 <dongyi@gimdong-yeong-ui-MacBookAir.local>
2025-04-29 00:06:30 +0900

    feat(controller): add LottoController to manage application flow and connect it in main

---
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -0,0 +1,33 @@
+package lotto.controller
+
+import lotto.Lotto
+import lotto.LottoManager
+import lotto.input.InputReader
+import lotto.input.end
+import lotto.input.start
+import lotto.input.ticketCost
+import lotto.util.RandomNumbers
+import lotto.view.OutputView
+
+object LottoController {
+
+    fun run() {
+
+        val purchaseAmount = InputReader.purchaseAmount()
+        val numberOfTickets = purchaseAmount / ticketCost
+
+        repeat(numberOfTickets) {
+            LottoManager.addTicket(Lotto(RandomNumbers.generate(start, end, 6)))
+        }
+
+        OutputView.printNumberOfTickets(numberOfTickets)
+        OutputView.printTickets(LottoManager.getTickets())
+
+        val winningTicket = Lotto(InputReader.winningNumbers())
+        val bonusNumber = InputReader.bonusNumber(winningTicket.getNumbers())
+
+        LottoManager.makeRankTable(winningTicket, bonusNumber)
+
+        OutputView.printWinningStatistics(LottoManager.getRankTable(), LottoManager.returnRate(purchaseAmount))
+    }
+}
