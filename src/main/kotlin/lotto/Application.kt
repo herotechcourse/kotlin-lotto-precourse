dcaf51987aefbd7daec1eeed2cc34a7fa80c5245
haeun <jhe0903@naver.com>
2025-04-28 20:29:50 +0900

    fix(main) : handle IllegalArgumentException and print error message

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,19 +1,23 @@
 package lotto
 
 fun main() {
-    val inputView = InputView().readAmount()
+    try {
+        val inputView = InputView().readAmount()
 
-    val count = inputView / 1000
-    val outputView = OutputView()
-    val lottos = LottoMachine().createTickets(count)
+        val count = inputView / 1000
+        val outputView = OutputView()
+        val lottos = LottoMachine().createTickets(count)
 
-    outputView.printResult(lottos)
+        outputView.printResult(lottos)
 
-    val winningNum = InputView().readWinningNumbers()
-    val bonusNum = InputView().readBonusNumber()
+        val winningNum = InputView().readWinningNumbers()
+        val bonusNum = InputView().readBonusNumber()
 
-    val result = LottoResult().calculateStatics(winningNum, bonusNum, lottos)
-    val total = LottoResult().calculateTotal(result, inputView)
+        val result = LottoResult().calculateStatics(winningNum, bonusNum, lottos)
+        val total = LottoResult().calculateTotal(result, inputView)
 
-    outputView.printStatics(result, total)
+        outputView.printStatics(result, total)
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
 }
