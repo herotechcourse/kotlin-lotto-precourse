53d64ca14667ca40153ee28273ef0c6fbcf75617
ppour-ba <pouriya.pourbahrami@gmail.com>
2025-04-28 09:45:08 +0200

    feat(lotto): add rank field with setter for ticket results

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,16 +1,35 @@
 package lotto
-import lotto.support.Message
+import lotto.support.*
 
 class Lotto(private val numbers: List<Int>) {
+    var matchCount: Int = 0
+        private set
+    var bonusMatched: Boolean = false
+        private set
+    var rank: Rank? = null
+        private set
+
     init {
-        require(numbers.size == REQUIRED_NUMBER_COUNT) { Message.ERROR_LOTTO_NUMBERS_COUNT }
+        require(numbers.size == TICKET_NUMBERS_COUNT) { Message.ERROR_LOTTO_NUMBERS_COUNT }
     }
 
     override fun toString(): String {
         return numbers.sorted().toString()
     }
 
-    companion object {
-        private const val REQUIRED_NUMBER_COUNT = 6
+    fun settMatchCount(points: Int) {
+        matchCount = points
+    }
+
+    fun markBonusMatched() {
+        bonusMatched = true
+    }
+
+    fun getNumbers():List<Int> {
+        return (numbers)
+    }
+
+    fun setRank(rank: Rank) {
+        this.rank = rank
     }
 }
---
+++ src/main/kotlin/lotto/domain/LottoResult.kt
@@ -0,0 +1,6 @@
+package lotto.domain
+import lotto.Lotto
+
+class LottoResault(tickets: List<Lotto>, winnerLotto: WinningLotto){
+
+}
