77bf7dd73579d29a4b9efc3581e23d680578b63a
minsang <minsang007@gmail.com>
2025-04-28 17:11:01 +0900

    feat(factory): add InputType enum and refactor InputViewFactory to use it
    - Introduce `InputType` enum with `PURCHASE_AMOUNT`, `WINNING_NUMBERS`, and `BONUS_NUMBER`
    - Update `InputViewFactory.createInputView` to select the correct `InputView` based on `InputType`
    - Improves type safety and makes it easier to extend with new input views

--- src/main/kotlin/lotto/factory/InputViewFactory.kt
+++ src/main/kotlin/lotto/factory/InputViewFactory.kt
@@ -2,9 +2,14 @@ package lotto.factory
 
 import lotto.view.input.InputView
 import lotto.view.input.PurchaseAmountInputView
+import lotto.enums.InputType
 
 object InputViewFactory {
-    fun getInputView(): InputView {
-        return PurchaseAmountInputView
+    fun createInputView(inputType: InputType): InputView {
+        return when (inputType) {
+            InputType.PURCHASE_AMOUNT -> PurchaseAmountInputView
+            InputType.WINNING_NUMBERS -> lotto.view.input.WinningNumbersInputView
+            InputType.BONUS_NUMBER -> lotto.view.input.BonusNumberInputView
+        }
     }
 }
