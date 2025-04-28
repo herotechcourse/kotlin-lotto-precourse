7e8a9678f08d5b2950b9834d62f771f0f1adf865
차정혁 <ckwjdgur1234@gmail.com>
2025-04-29 03:22:33 +0900

    test(result): add tests for profit rate is correct

--- README.md
+++ README.md
@@ -37,8 +37,8 @@ A lotto simulator implemented in Kotlin as part of the Hero Tech Course pre-cour
 ### ✅ Result Calculation
 - [x] **Compare user's tickets to the winning numbers and bonus number**
 - [x] **Calculate and show lottery result statistics**
-- [ ] **Calculate and show profit rate**
-  - [ ] Validate that the profit rate is rounded to the nearest tenth
+- [x] **Calculate and show profit rate**
+  - [x] Validate that the profit rate is rounded to the nearest tenth
 
 ### ⚠️ Error Handling
 - [ ] **Error messages start with `[ERROR]`**
--- src/test/kotlin/lotto/domain/ResultCalculatorTest.kt
+++ src/test/kotlin/lotto/domain/ResultCalculatorTest.kt
@@ -2,6 +2,7 @@ package lotto.domain
 
 import lotto.Lotto
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 
@@ -16,6 +17,27 @@ class ResultCalculatorTest {
         assertThat(result[rank]).isEqualTo(expectedCount)
     }
 
+    @ParameterizedTest
+    @MethodSource("provideResultsAndExpectedProfitRate")
+    fun `calculate profit rate correctly`(input: Pair<Map<LottoRank, Int>, Double>) {
+        val (result, expectedProfitRate) = input
+
+        val purchaseAmount = 8000
+        val profitRate = ResultCalculator.calculateProfitRate(result, purchaseAmount)
+
+        assertThat(profitRate).isEqualTo(expectedProfitRate)
+    }
+
+    @Test
+    fun `calculate profit rate is rounded to the nearest tenth`() {
+        val result = mapOf(LottoRank.FIFTH to 1)
+        val purchaseAmount = 6000
+
+        val profitRate = ResultCalculator.calculateProfitRate(result, purchaseAmount)
+
+        assertThat(profitRate).isEqualTo(83.3)
+    }
+
     companion object {
         @JvmStatic
         fun provideRanksAndExpectedCounts() = listOf(
@@ -24,6 +46,17 @@ class ResultCalculatorTest {
             LottoRank.THIRD to 1,
             LottoRank.FOURTH to 1,
             LottoRank.FIFTH to 1
+        )
+
+        @JvmStatic
+        fun provideResultsAndExpectedProfitRate() = listOf(
+            mapOf(
+                LottoRank.FIFTH to 1,
+            ) to 62.5,
+            mapOf(
+                LottoRank.FIFTH to 1,
+                LottoRank.FOURTH to 1,
+            ) to 687.5
         )
 
         private val winningNumbers = WinningNumbers(listOf(1, 2, 3, 4, 5, 6))
