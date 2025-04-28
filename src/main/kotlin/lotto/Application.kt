d5090571ef15ef7f0de1e1bc6013d80e5c13ffb0
atash1986 <msc.ashourzadeh@gmail.com>
2025-04-28 22:34:40 +0200

    feat(Application): add reprompt ability using a helper function
    - a helper function was developed to add re-prompt ability
    - all the inputView functions are wrapped by the reprompt() before calling
    - the try/catch codes were removed from the input view functions

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,18 +2,19 @@ package lotto
 
 import views.OutputView
 import views.InputView
+import helpers.withReprompt
 
 fun main() {
    var inputView = InputView()
    val outputView = OutputView()
    val lottoMachine = LottoMachine()
 
-   var purchaseAmount=inputView.getPurchaseAmount()
+   var purchaseAmount=withReprompt{inputView.getPurchaseAmount()}
    val ticketsCount=lottoMachine.calcTicketNumber(purchaseAmount)
    val tickets=lottoMachine.issueTickets(ticketsCount)
    outputView.printTickets(tickets)
-   val winningNumbers=inputView.getWinningNumbers()
-   val bonusNumber=inputView.getBonusNumber()
+   val winningNumbers=withReprompt{inputView.getWinningNumbers()}
+   val bonusNumber=withReprompt{inputView.getBonusNumber()}
    lottoMachine.checkMatches(tickets, winningNumbers, bonusNumber)
    outputView.printMatches(lottoMachine.matchMap)
    val returnRate=lottoMachine.calcReturnRate(purchaseAmount)
--- src/main/kotlin/views/InputView.kt
+++ src/main/kotlin/views/InputView.kt
@@ -7,61 +7,42 @@ open class InputView {
         return Console.readLine()
     }
 
-    fun printError(e: Exception) {
-        println("[ERROR] ${e.message}.")
-    }
-
     fun getPurchaseAmount():Int {
-        try {
-            println()
-            println("Please Enter Purchase amount for lottery tickets:")
-            val input=readLineFromConsole()
-            val purchaseAmount = input?.toIntOrNull() ?: throw IllegalArgumentException("Invalid Purchase Amount Number")
-            if (purchaseAmount >= 0 && purchaseAmount % 1000 == 0) {
-                return purchaseAmount
-            }
-            throw IllegalArgumentException("Purchase Amount must be positive and dividable per 1000")
-        } catch (e: Exception) {
-            printError(e)
-            return 0
+        println()
+        println("Please Enter Purchase amount for lottery tickets:")
+        val input=readLineFromConsole()
+        val purchaseAmount = input?.toIntOrNull() ?: throw IllegalArgumentException("Invalid Purchase Amount Number")
+        if (purchaseAmount >= 0 && purchaseAmount % 1000 == 0) {
+            return purchaseAmount
         }
+        throw IllegalArgumentException("Purchase Amount must be positive and dividable per 1000")
     }
 
     fun getWinningNumbers(): List<Int>
     {
-        try {
-            println()
-            println("Please enter last week's winning numbers:")
-            val input=readLineFromConsole() ?: ""
-            val winningNumbers=input.split(',').map({ it.trim().toInt() })
-            if(winningNumbers.size < 6)
-                throw IllegalArgumentException("There must be exactly 6 numbers")
-            if(winningNumbers.distinct().size < 6)
-                throw IllegalArgumentException("There must not be repeated numbers")
-            if(!winningNumbers.all { it in 1..45 })
-                throw IllegalArgumentException("Each number must be between 1 and 45")
-            return winningNumbers
-        } catch (e: Exception) {
-            printError(e)
-            return emptyList()
-        }
+        println()
+        println("Please enter last week's winning numbers:")
+        val input=readLineFromConsole() ?: ""
+        val winningNumbers=input.split(',').map({ it.trim().toInt() })
+        if(winningNumbers.size < 6)
+            throw IllegalArgumentException("There must be exactly 6 numbers")
+        if(winningNumbers.distinct().size < 6)
+            throw IllegalArgumentException("There must not be repeated numbers")
+        if(!winningNumbers.all { it in 1..45 })
+            throw IllegalArgumentException("Each number must be between 1 and 45")
+        return winningNumbers
     }
     
     fun getBonusNumber(): Int
     {
-        try {
-            println()
-            println("Please enter the bonus number.")
-            val input=readLineFromConsole()
-            val bonusNumber = input?.toIntOrNull() ?: throw IllegalArgumentException("Invalid Bonus Number")
-            if (bonusNumber < 1 || bonusNumber > 45) {
-                throw IllegalArgumentException("Bonus number must be between 1 and 45")
-            }
-            return bonusNumber
-        } catch (e: Exception) {
-            printError(e)
-            return 0
+        println()
+        println("Please enter the bonus number.")
+        val input=readLineFromConsole()
+        val bonusNumber = input?.toIntOrNull() ?: throw IllegalArgumentException("Invalid Bonus Number")
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw IllegalArgumentException("Bonus number must be between 1 and 45")
         }
+        return bonusNumber
     }
 }
 
