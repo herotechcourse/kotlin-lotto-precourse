b56ce39d6f5bca6f92d9bf00e30b40d94cd67325
minsang <minsang007@gmail.com>
2025-04-28 23:21:15 +0900

    test(utils): add tests for calculateMatches in ResultCaculator
    I add two tests to check calculateMatches:
    - calculates the correct number of matches
    - calculates the correct number of matches with bonus

---
+++ src/test/kotlin/lotto/utils/ResultCaculatorTest.kt
@@ -0,0 +1,32 @@
+package lotto.utils
+
+import lotto.Lotto
+import lotto.Lottos
+import lotto.WinningNumbers
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class ResultCaculatorTest {
+    @Test
+    fun `calculates the correct number of matches`() {
+        val winningNumbers = WinningNumbers(listOf("1", "2", "3", "4", "5", "6"), "11")
+        val lotto = Lotto(listOf(1, 2, 3, 8, 9, 10))
+        val lottos = Lottos(mutableListOf(lotto),0)
+        val expectedMatches = "3"
+
+        val result = ResultCaculator.calculateMatches(lottos, winningNumbers)
+
+        assertEquals(1, result["3"])
+    }
+    @Test
+    fun `calculates the correct number of matches with bonus`() {
+        val winningNumbers = WinningNumbers(listOf("1", "2", "3", "4", "5", "6"), "7")
+        val lotto = Lotto(listOf(1,2,3,4,5,7))
+        val lottos = Lottos(mutableListOf(lotto),0)
+
+        val result = ResultCaculator.calculateMatches(lottos, winningNumbers)
+
+        assertEquals(1, result["5+bonus"])
+    }
+
+}
