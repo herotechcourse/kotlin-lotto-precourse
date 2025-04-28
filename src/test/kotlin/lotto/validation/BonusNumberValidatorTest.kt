5afa4515a81faa363a3f68a377ae2b082267ddca
jinny <ggab.challenge@gmail.com>
2025-04-28 19:48:57 +0900

    test: refactor to use ParameterizedTest

--- src/test/kotlin/lotto/validation/BonusNumberValidatorTest.kt
+++ src/test/kotlin/lotto/validation/BonusNumberValidatorTest.kt
@@ -2,6 +2,8 @@ package lotto.validation
 
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class BonusNumberValidatorTest {
     @Test
@@ -13,19 +15,17 @@ class BonusNumberValidatorTest {
             .hasMessageStartingWith("[ERROR]")
     }
 
-    @Test
-    fun `throw exception when the input is not number`() {
-        val input = "six"
-
+    @ParameterizedTest
+    @ValueSource(strings = ["six", "seven", "a", "eight"])
+    fun `throw exception when the input is not number`(input: String) {
         assertThatThrownBy { BonusNumberValidator.validate(input) }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageStartingWith("[ERROR]")
     }
 
-    @Test
-    fun `throw exception when the input is not between 1 and 45`() {
-        val input = "47"
-
+    @ParameterizedTest
+    @ValueSource(strings = ["46", "47", "50", "100", "101"])
+    fun `throw exception when the input is not between 1 and 45`(input: String) {
         assertThatThrownBy { BonusNumberValidator.validate(input) }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageStartingWith("[ERROR]")
--- src/test/kotlin/lotto/validation/PurchaseAmountValidatorTest.kt
+++ src/test/kotlin/lotto/validation/PurchaseAmountValidatorTest.kt
@@ -1,48 +1,41 @@
 package lotto.validation
 
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class PurchaseAmountValidatorTest {
     @Test
     fun `throw exception when the input is empty`() {
-        // given
         val input = ""
 
-        // when & then
+        assertThatThrownBy { PurchaseAmountValidator.validate(input) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageStartingWith("[ERROR]")
+        }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = ["46", "47", "50", "100", "300", "350"])
+    fun `throw exception when the amount is less than 1,000`(input: String) {
         assertThrows<IllegalArgumentException> {
             PurchaseAmountValidator.validate(input)
         }
     }
 
-    @Test
-    fun `throw exception when the amount is less than 1,000`() {
-        // given
-        val input = "900"
-
-        // when & then
+    @ParameterizedTest
+    @ValueSource(strings = ["1300", "1001", "1023", "1000345", "11233", "44444"])
+    fun `throw exception when the amount is not divisible by 1,000`(input: String) {
         assertThrows<IllegalArgumentException> {
             PurchaseAmountValidator.validate(input)
         }
     }
 
-    @Test
-    fun `throw exception when the amount is not divisible by 1,000`() {
-        // given
-        val input = "1300"
-
-        // when & then
-        assertThrows<IllegalArgumentException> {
-            PurchaseAmountValidator.validate(input)
-        }
-    }
-
-    @Test
-    fun `throw exception when the input is not a valid number`() {
-        // given
-        val input = "threeThousandDollars"
-
-        // when & then
+    @ParameterizedTest
+    @ValueSource(strings = ["threeThousandDollars", "dollars", "a", "b"])
+    fun `throw exception when the input is not a valid number`(input: String) {
         assertThrows<IllegalArgumentException> {
             PurchaseAmountValidator.validate(input)
         }
