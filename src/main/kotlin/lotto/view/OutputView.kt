c6e0c7f559fff06502f8bcc9292c57a31593332d
yeonupark <idepix@naver.com>
2025-04-28 16:16:34 +0900

    refactor(view): decouple OutputView from domain logic and simplify output methods

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,38 +1,32 @@
 package lotto.view
 
-import lotto.domain.LottoMachine
+import lotto.Lotto
 import lotto.view.messages.PromptMessages
 import lotto.domain.LottoRank
 
-class OutputView(private val lottoMachine: LottoMachine) {
+class OutputView() {
 
-    fun printTicketCount() {
-        val ticketCount = lottoMachine.tickets.size
+    fun printTicketCount(ticketCount: Int) {
         println(PromptMessages.TICKET_COUNT.with(ticketCount))
     }
 
-    fun printLottoNumbers() {
-        lottoMachine.tickets.forEach { lotto ->
-            println(lotto.getNumbers().sorted())
+    fun printLottoNumbers(tickets: List<Lotto>) {
+        tickets.forEach { lotto ->
+            println(lotto.numberList())
         }
     }
 
-    fun printWinningStatistics(guessNumbers: Set<Int>, bonusNumber: Int) {
+    fun printWinningStatistics(ranks: List<Int>) {
         println(PromptMessages.WINNING_STATISTICS.message)
         println("---")
-        printRank(guessNumbers, bonusNumber)
-    }
-
-    private fun printRank(guessNumbers: Set<Int>, bonusNumber: Int) {
-        val ranks = lottoMachine.setAllRank(guessNumbers, bonusNumber)
         LottoRank.entries
             .filter { it != LottoRank.NONE }
             .reversed()
             .forEach { println(it.getResultMessage(ranks[it.ordinal])) }
     }
 
-    fun printReturnRate() {
-        println(PromptMessages.TOTAL_RETURN_RATE.with(lottoMachine.calculateReturnRate()))
+    fun printReturnRate(returnRate: String) {
+        println(PromptMessages.TOTAL_RETURN_RATE.with(returnRate))
     }
 
 }
