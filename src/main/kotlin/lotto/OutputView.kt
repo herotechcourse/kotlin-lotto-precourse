038457b7a335e809a411718220e641ed39f8fc9e
haeun <jhe0903@naver.com>
2025-04-28 18:03:26 +0900

    feat(output) : implement OutputView to display tickets, results, and profit rate

---
+++ src/main/kotlin/lotto/OutputView.kt
@@ -0,0 +1,25 @@
+package lotto
+
+class OutputView {
+    fun printResult (lottos: List<Lotto>) {
+        println()
+        println("You have purchased ${lottos.size} tickets.")
+
+        lottos.forEach { lotto ->
+            println(lotto.getNumbers().sorted())
+        }
+        println()
+    }
+
+    fun printStatics (result: Array<Int>, total: Double) {
+        println()
+        println("Winning Statistics")
+        println("---")
+        println("3 Matches (5,000 KRW) - ${result[0]} tickets")
+        println("4 Matches (50,000 KRW) – ${result[1]} tickets")
+        println("5 Matches (1,500,000 KRW) – ${result[2]} tickets")
+        println("5 Matches + Bonus Ball (30,000,000 KRW) – ${result[3]} tickets")
+        println("6 Matches (2,000,000,000 KRW) – ${result[4]} tickets")
+        println("Total return rate is ${total}%.")
+    }
+}
