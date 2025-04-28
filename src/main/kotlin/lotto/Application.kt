b8c08b8d5420528e0fa1c31d991b4704a088c569
sopa <sopa.sara@gmail.com>
2025-04-28 19:31:08 +0200

    refactor(app): clean up Application.kt to use InputView and OutputView

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,15 +1,10 @@
 package lotto
-import camp.nextstep.edu.missionutils.Console
-//import camp.nextstep.edu.missionutils.Randoms
 
+import lotto.views.InputView
+import lotto.views.OutputView
 
 fun main() {
-    print("Please enter the purchase amount. ")
-    println()
-    val amount = Console.readLine().toIntOrNull()
-
-    if (amount == null || amount % 1000 != 0)
-        throw IllegalArgumentException("[ERROR] Purchase amount must be divisible by 1000.")
-
-    print("amount is $amount")
+    val amount = InputView.readPurchaseAmount()
+    val tickets = LottoGenerator.generateTicket(amount)
+    OutputView.printTickets(tickets)
 }
