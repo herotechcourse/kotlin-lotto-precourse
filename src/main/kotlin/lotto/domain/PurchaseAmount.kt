d0848ca2a2facbd7bd51dd7e4e29a5f1a53b734d
차정혁 <ckwjdgur1234@gmail.com>
2025-04-28 18:34:34 +0900

    feat(purchase): validate purchase number is divisible by 1000

--- src/main/kotlin/lotto/domain/PurchaseAmount.kt
+++ src/main/kotlin/lotto/domain/PurchaseAmount.kt
@@ -8,12 +8,15 @@ class PurchaseAmount(val amount: Int) {
             val parsedInput = input.toIntOrNull() ?: throw IllegalArgumentException(NOT_A_NUMBER_ERROR)
 
             require(parsedInput > 0) { NOT_A_POSITIVE_NUMBER_ERROR }
+            require(parsedInput % LOTTO_PRICE == 0) { NOT_DIVISIBLE_BY_1000_ERROR }
 
             return PurchaseAmount(parsedInput)
         }
 
+        private const val LOTTO_PRICE: Int = 1_000;
         private const val BLANK_ERROR: String = "[ERROR] Purchase Amount must not be blank"
         private const val NOT_A_NUMBER_ERROR: String = "[ERROR] Purchase Amount must be a number"
         private const val NOT_A_POSITIVE_NUMBER_ERROR: String = "[ERROR] Purchase Amount must be a positive number"
+        private const val NOT_DIVISIBLE_BY_1000_ERROR: String = "[ERROR] Purchase Amount must be divisible by 1_000"
     }
 }
