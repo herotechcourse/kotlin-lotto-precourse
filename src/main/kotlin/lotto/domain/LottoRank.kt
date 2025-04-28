4550d7fe6ae1bb8dad1f4dc42de52263bd5517f7
차정혁 <ckwjdgur1234@gmail.com>
2025-04-29 01:59:45 +0900

    feat(rank): add NONE rank and complete matching logic

--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -6,4 +6,18 @@ enum class LottoRank(val matchCount: Int, val matchBonus: Boolean, val prize: In
     THIRD(5, false, 1_500_000),
     FOURTH(4, false, 50_000),
     FIFTH(3, false, 5_000),
+    NONE(0, false, 0);
+
+    companion object {
+        fun from(matchCount: Int, matchBonus: Boolean): LottoRank {
+            return when {
+                matchCount == FIRST.matchCount -> FIRST
+                matchCount == SECOND.matchCount && matchBonus == SECOND.matchBonus -> SECOND
+                matchCount == THIRD.matchCount && matchBonus == THIRD.matchBonus -> THIRD
+                matchCount == FOURTH.matchCount -> FOURTH
+                matchCount == FIFTH.matchCount -> FIFTH
+                else -> NONE
+            }
+        }
+    }
 }
