1b94f87d1af58959a9e55b3781fc3c8e7bf32480
Taekeundo <pleasepose@gmail.com>
2025-04-28 03:46:09 +0200

    refactor(view): fix InputView for responsibility

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -4,24 +4,42 @@ import camp.nextstep.edu.missionutils.Console
 
 object InputView {
     fun readPurchaseAmount(): Int {
-        println("Please enter the purchase amount.")
-        val input = Console.readLine()
-        InputValidator.validatePurchaseAmount(input)
-        return input.toInt()
+        while (true) {
+            try {
+                println("Please enter the purchase amount.")
+                val input = Console.readLine()
+                InputValidator.validatePurchaseAmount(input)
+                return input.toInt()
+            } catch (e: IllegalArgumentException) {
+                OutputView.printError("${e.message}")
+            }
+        }
     }
 
     fun readWinningNumbers(): List<Int> {
-        println("Please enter last week's winning numbers.")
-        val input = Console.readLine()
-        InputValidator.validateWinningNumbers(input)
-        return input.split(",")
-            .map { it.trim().toInt() }
+        while (true) {
+            try {
+                println("Please enter last week's winning numbers.")
+                val input = Console.readLine()
+                InputValidator.validateWinningNumbers(input)
+                return input.split(",")
+                    .map { it.trim().toInt() }
+            } catch (e: IllegalArgumentException) {
+                OutputView.printError("${e.message}")
+            }
+        }
     }
 
     fun readBonusNumber(): Int {
-        println("Please enter the bonus number.")
-        val input = Console.readLine()
-        InputValidator.validateBonusNumber(input)
-        return input.toInt()
+        while (true) {
+            try {
+                println("Please enter the bonus number.")
+                val input = Console.readLine()
+                InputValidator.validateBonusNumber(input)
+                return input.toInt()
+            } catch (e: IllegalArgumentException) {
+                OutputView.printError("${e.message}")
+            }
+        }
     }
 }
