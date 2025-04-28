1c077f04804141b1b6c9ba05f288cae9d1ff4034
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-28 19:49:48 +0900

    feat(lotto): add prize determination logic based on kotlin map

---
+++ src/main/kotlin/lotto/PrizeManager.kt
@@ -0,0 +1,19 @@
+package lotto
+
+import lotto.config.bonusMap
+import lotto.config.prizeMap
+
+
+fun determinePrize(matches: Int, bonus: Boolean): Int {
+    var prize: Int? = null
+
+    if (bonus) {
+        prize = bonusMap[matches]
+    }
+
+    if (prize == null) {
+        prize = prizeMap[matches]
+    }
+
+    return prize ?: 0
+}
