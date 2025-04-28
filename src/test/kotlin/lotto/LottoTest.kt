d14f51e4a31ee4510302accedafc3af99b7a64f9
minsang <minsang007@gmail.com>
2025-04-28 11:21:31 +0900

    test(lotto): add unit test for getNumbers function
    Add a test to verify that the getNumbers function returns the correct lotto numbers.

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -20,4 +20,10 @@ class LottoTest {
     }
 
     // TODO: Implement tests based on the added features
+    @Test
+    fun `returns the correct lotto numbers`() {
+        val numbers = listOf(1, 2, 3, 4, 5, 6)
+        val lotto = Lotto(numbers)
+        assert(numbers == lotto.getNumbers())
+    }
 }
