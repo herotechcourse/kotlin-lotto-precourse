febf6df3f0b54d0b71c937d703873fb6701eabd2
Sanghyunn <shawnpark98@naver.com>
2025-04-28 22:57:41 +0900

    feat : Lotto model implemented

---
+++ src/main/kotlin/lotto/Model/Lotto.kt
@@ -0,0 +1,17 @@
+package lotto
+
+class Lotto(private val numbers: List<Int>) {
+    init {
+        require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.distinct().size == 6) { "[ERROR] Every numbers must be unique." }
+    }
+
+    // TODO: Implement additional functions
+    fun match(winningNumbers: List<Int>): Int {
+        return numbers.count { number in winningNumbers }
+    }
+
+    fun isBonusContain(bonusNumber: Int): Bool {
+        return number in numbers
+    }
+}
