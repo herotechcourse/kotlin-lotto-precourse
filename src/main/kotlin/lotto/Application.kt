d67188d63bf5810ee29eaa633295ac6c9a9cfae0
GSIII <kalseo901@gmail.com>
2025-04-28 22:39:19 +0900

    feat(service, view): implement LottoMachine to issue lottos and OutputView to print purchased tickets

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,16 @@
 package lotto
 
+import lotto.services.LottoMachine
+import lotto.view.InputView
+import lotto.view.OutputView
+
 fun main() {
-    // TODO: Implement the program
+    val purchaseAmount = InputView.readPurchaseAmount()
+    val tickets = LottoMachine.issueLottos(purchaseAmount)
+
+    OutputView.printTickets(tickets)
+
+    val winningNumbers = InputView.readWinningNumbers()
+
 }
+
---
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -0,0 +1,12 @@
+package lotto.view
+
+import lotto.Lotto
+
+object OutputView {
+    fun printTickets(tickets: List<Lotto>) {
+        println("\nYou have purchased ${tickets.size} tickets.")
+        for (ticket in tickets) {
+            println(ticket.getNumbers())
+        }
+    }
+}
