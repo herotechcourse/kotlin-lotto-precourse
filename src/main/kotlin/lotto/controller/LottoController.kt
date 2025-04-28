79113daf0bbdffda76e3c5155b18d3f202f4fe97
puleugo <puleugo@gmail.com>
2025-04-28 16:22:07 +0900

    feat(controller): implement print purchased lottery tickets with issued number
    - print purchased lottery tickets
    - print sorted lottery numbers
    - write unit test

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,15 +1,25 @@
 package lotto.controller
 
 import lotto.Lotto
+import lotto.domain.TicketIssuer
 import lotto.view.InputReader
 import lotto.view.OutputPrinter
 
-class LottoController(private val reader: InputReader, private val printer: OutputPrinter) {
+class LottoController(
+    private val reader: InputReader,
+    private val printer: OutputPrinter,
+    private val issuer: TicketIssuer = TicketIssuer()
+) {
 
     fun run(): Unit {
         // 1. Input purchase amount for buy the lottery tickets.
-        val purchaseAmount = reader.readPurchaseAmount()
+        val amount = reader.readPurchaseAmount()
+
         // 2. Print purchased lottery tickets with issued numbers (sorted)
+        val ticketCount = amount.toInt() / Lotto.PRICE
+        val tickets = issuer.issue(ticketCount)
+        printer.printPurchasedTickets(tickets)
+
         // 3. Input last week's numbers
         // 4. Input a bonus number.
         // 5. Print lotto result statistics.
