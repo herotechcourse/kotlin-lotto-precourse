27942e1ef70a955b80db53b0e70377e8ba33d552
inaemin <inaemin@gmail.com>
2025-04-29 05:05:29 +0900

    update: add public field for lotto numbers

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,13 +1,23 @@
 package lotto
 
 import lotto.util.Constants.LOTTERY_NUMBER_COUNT
+import lotto.util.Constants.MAX_LOTTERY_NUM
+import lotto.util.Constants.MIN_LOTTERY_NUM
 
-class Lotto(private val numbers: List<Int>) {
+class Lotto(inputNumbers: List<Int>) {
+    val numbers: List<Int>
+
     init {
-        require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
-        require(numbers.toSet().size == LOTTERY_NUMBER_COUNT) {
+        require(inputNumbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(inputNumbers.toSet().size == LOTTERY_NUMBER_COUNT) {
             "[ERROR] Lotto numbers cannot contain duplicate numbers."
         }
+        inputNumbers.forEach { number ->
+            require(number in MIN_LOTTERY_NUM..MAX_LOTTERY_NUM) {
+                "[ERROR] Lotto numbers must be between $MIN_LOTTERY_NUM and $MAX_LOTTERY_NUM"
+            }
+        }
+        this.numbers = inputNumbers.sorted()
     }
 
     fun match(winningNumbers: List<Int>, bonusNumber: Int): Pair<Int, Boolean> {
