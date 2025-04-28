5963877703eb37026d46a47c3b1e882816fae9cc
차정혁 <ckwjdgur1234@gmail.com>
2025-04-28 21:24:24 +0900

    feat(lotto): handle exceptionwhen lotto generation fails

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,6 +1,7 @@
 package lotto.controller
 
 import lotto.domain.LottoGenerator
+import lotto.domain.Lottos
 import lotto.domain.PurchaseAmount
 import lotto.view.InputView
 import lotto.view.OutputView
@@ -8,7 +9,7 @@ import lotto.view.OutputView
 class LottoController {
     fun run() {
         val purchaseAmount = getPurchaseAmount()
-        val lottos = LottoGenerator.generate(purchaseAmount.possibleLottoTicketCount)
+        val lottos = generateLottos(purchaseAmount.possibleLottoTicketCount)
     }
 
     fun getPurchaseAmount(): PurchaseAmount {
@@ -18,6 +19,15 @@ class LottoController {
             } catch (e: IllegalArgumentException) {
                 e.message?.let { OutputView.printError(it) }
             }
+        }
+    }
+
+    fun generateLottos(lottoTicketCount: Int): Lottos {
+        try {
+            return LottoGenerator.generate(lottoTicketCount)
+        } catch (e: IllegalArgumentException) {
+            e.message?.let { OutputView.printError(it) }
+            throw e
         }
     }
 }
