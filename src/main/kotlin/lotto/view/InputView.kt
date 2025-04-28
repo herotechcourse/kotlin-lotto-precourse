b7af2f02ef932afbfd4a0e09620fdfe7b2ff9cf2
차정혁 <ckwjdgur1234@gmail.com>
2025-04-28 22:31:04 +0900

    feat(view): read winning numbers from input

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,9 +1,12 @@
 package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 object InputView {
+    private const val DELIMITER: String = ","
     private const val PURCHASE_AMOUNT_MESSAGE: String = "Please enter the purchase amount."
+    private const val WINNING_NUMBER_MESSAGE: String = "Please enter last week's winning numbers."
 
     fun readPurchaseAmount(): String {
         println(PURCHASE_AMOUNT_MESSAGE)
@@ -11,5 +14,14 @@ object InputView {
         val purchaseAmount = Console.readLine().trim()
 
         return purchaseAmount
+    }
+
+    fun readWinningNumbers(): List<String> {
+        println(WINNING_NUMBER_MESSAGE)
+
+        val input = Console.readLine()
+        val winningNumbers = input.split(DELIMITER).map { it.trim() }
+
+        return winningNumbers
     }
 }
