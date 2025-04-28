84685ad09301d9dda1f155f595b606ee8583840c
ppour-ba <pouriya.pourbahrami@gmail.com>
2025-04-28 20:41:31 +0200

    style(Lotto): clean up imports, formatting, and simplify functions

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,18 +1,16 @@
 package lotto
+
 import lotto.support.*
-import lotto.domain.Rank
 
 class Lotto(private val numbers: List<Int>) {
-init {
-    require(numbers.toSet().size == TICKET_NUMBERS_COUNT) { Message.ERROR_WINNING_NUMBERS_DUPLICATE }
-    require(numbers.size == TICKET_NUMBERS_COUNT) { Message.ERROR_LOTTO_NUMBERS_COUNT }
-    }
+    init {
+        require(numbers.toSet().size == TICKET_NUMBERS_COUNT) { Message.ERROR_WINNING_NUMBERS_DUPLICATE }
+        require(numbers.size == TICKET_NUMBERS_COUNT) { Message.ERROR_LOTTO_NUMBERS_COUNT }
+        }
 
-    override fun toString(): String {
-        return numbers.sorted().toString()
-    }
+    override fun toString(): String = numbers.sorted().toString()
 
     fun getNumbers():List<Int> {
-        return (numbers)
-    }
+            return numbers
+        }
 }
