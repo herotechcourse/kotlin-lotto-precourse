91fdfdf79bae1cc53a933b1d3c6e69f96cdda41e
puleugo <puleugo@gmail.com>
2025-04-28 15:37:54 +0900

    feat(domain): implement `TicketIssuer`

--- src/main/kotlin/lotto/domain/TicketIssuer.kt
+++ src/main/kotlin/lotto/domain/TicketIssuer.kt
@@ -1,5 +1,21 @@
 package lotto.domain
 
+import camp.nextstep.edu.missionutils.Randoms
+import lotto.Lotto
+
 class TicketIssuer {
-    fun issue(amount: PurchaseAmount):
+    private fun createRandomNumbers(count: Int = Lotto.NUMBER_COUNT): List<Int> {
+        return Randoms.pickUniqueNumbersInRange(LottoNumber.MIN_NUMBER, LottoNumber.MAX_NUMBER, count)
+    }
+
+    fun issue(count: Int): List<Lotto> {
+        val tickets: MutableList<Lotto> = mutableListOf()
+
+        repeat(count) {
+            val lottoNumbers = createRandomNumbers()
+            tickets.add(Lotto.from(lottoNumbers))
+        }
+
+        return tickets
+    }
 }
