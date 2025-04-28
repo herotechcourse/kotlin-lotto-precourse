b28357078f07683c9de6ea9fe25f1aef92b602fe
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-28 22:21:43 +0900

    feat(prize): find prize info by array or map

--- src/main/kotlin/prize/PrizeManager.kt
+++ src/main/kotlin/prize/PrizeManager.kt
@@ -1,6 +1,6 @@
 package prize
 
-var prizeByRank: Array<Prize> = arrayOf(
+val prizeByRank: Array<Prize> = arrayOf(
     Prize(0, false, 0),
     Prize(6, false, 2_000_000_000),
     Prize(5, true, 30_000_000),
@@ -8,3 +8,15 @@ var prizeByRank: Array<Prize> = arrayOf(
     Prize(4, false, 50_000),
     Prize(3, false, 5_000)
 )
+
+val prizeByKey: Map<Int, Prize> = mapOf(
+    6 to prizeByRank[1],
+    -5 to prizeByRank[2],
+    5 to prizeByRank[3],
+    4 to prizeByRank[4],
+    3 to prizeByRank[5]
+)
+
+fun getPrizeKeyByMatchesAndBonus(matches: Int, bonus: Boolean): Int{
+    return if (bonus) -matches else matches
+}
