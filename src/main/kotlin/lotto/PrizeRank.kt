27bfc4346fd4c74953f31cbcd19431c9d358fde9
Marc Rodenbusch <87020460+MacMosby@users.noreply.github.com>
2025-04-28 13:12:59 +0200

    feat(PrizeRank): Add enum class to store prize ranks
    Add enum class PrizeRank to:
    - Store prize ranks with conditions and payouts
    - To simplify result output

---
+++ src/main/kotlin/lotto/PrizeRank.kt
@@ -0,0 +1,10 @@
+package lotto
+
+enum class PrizeRank(val matches: Int, val bonusNumber: Int, val prize: String, val amount: Int) {
+
+    FIRST(6, 0, "2,000,000,000 KRW", 2_000_000_000),
+    SECOND(5, 1, "30,000,000 KRW", 30_000_000),
+    THIRD(5, 0, "1,500,000 KRW", 1_500_000),
+    FOURTH(4, 0, "50,000 KRW", 50_000),
+    FIFTH(3, 0, "5,000 KRW", 5_000)
+}
