1a2be2b7eb487d1048c3535dfba84c251093c09f
minsang <minsang007@gmail.com>
2025-04-28 14:27:14 +0900

    feat(dto): add bonus number validation into WinningNumbers
    Combine bonusNumber into the WinningNumbers data class and invoke
    BonusNumberValidator in init. Removed separate BonusNumber DTO,
    as bonus validation requires the context of the winning numbers list.

--- src/main/kotlin/lotto/dto/WinningNumbers.kt
+++ src/main/kotlin/lotto/dto/WinningNumbers.kt
@@ -1,13 +1,19 @@
 package lotto.dto
 
+import lotto.validator.BonusNumberValidator
 import lotto.validator.WinningNumbersValidator
 
-data class WinningNumbers(val numbers: List<String>){
+data class WinningNumbers(val numbers: List<String>, val bonusNumber: String) {
     init{
         WinningNumbersValidator.validate(numbers)
+        BonusNumberValidator.validate(bonusNumber, numbers)
     }
 
     fun getWinningNumbers(): List<Int> {
         return numbers.map { it.toInt() }
+    }
+
+    fun getBonusNumber(): Int {
+        return bonusNumber.toInt()
     }
 }
