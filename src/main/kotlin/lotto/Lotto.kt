1f8d84d37591fcc134fbacfa9de57a5f8db1bb2c
Daae-Kim <wlsemrdl1212@naver.com>
2025-04-29 05:14:48 +0900

    fix(domain) : Initialize and modify Lotto class

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,24 +1,28 @@
-package lotto.model
+package lotto
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 
-class Lotto(val numbers: List<Int>) {
+class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
-        require(numbers.toSet().size == 6){"[ERROR] Lotto must be composed with unique numbers"}
+        require(numbers.toSet().size == 6) { "[ERROR] Lotto must be composed with unique numbers" }
     }
-    override fun toString(): String{
+
+    override fun toString(): String {
         return numbers.toString()
     }
-    companion object{
+
+    companion object {
         fun generate(): Lotto {
             val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
             return Lotto(numbers.sorted())
         }
     }
 
-
-
+    fun getNumbers(): List<Int> {
+        return numbers.toList()
+    }
 
 
 }
