b088375de60465550242dba24e2c5187f23e47d3
Petra Bencze <petra.bncz@gmail.com>
2025-04-28 19:30:02 +0200

    fix(Purchase): initialize List after require statements
    passes all tests -> also with -1000 purchase amount

--- src/main/kotlin/lotto/Purchase.kt
+++ src/main/kotlin/lotto/Purchase.kt
@@ -1,7 +1,7 @@
 package lotto
 
 class Purchase(private val amount: Int) {
-    val tickets: List<Ticket> = List(ticketCount()) { Ticket() }
+    val tickets: List<Ticket>
 
     init {
         require(amount % TICKET_PRICE == 0) {
@@ -11,6 +11,7 @@ class Purchase(private val amount: Int) {
         require(amount in TICKET_PRICE..MAX_AMOUNT) {
             "[ERROR] Amount cannot be lower than $TICKET_PRICE or higher than $MAX_AMOUNT."
         }
+        tickets = List(ticketCount()) { lotto.Ticket() }
     }
 
     fun calculateReturnRate(totalPrize: Int): Double {
