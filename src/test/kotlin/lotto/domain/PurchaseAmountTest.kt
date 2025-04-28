acd4e849cfb4e3a3fbf05321a0d9769bd6fee434
차정혁 <ckwjdgur1234@gmail.com>
2025-04-28 18:03:05 +0900

    test(purchase): ã add tests for empty and blank purchase amount input

---
+++ src/test/kotlin/lotto/domain/PurchaseAmountTest.kt
@@ -0,0 +1,21 @@
+package lotto.domain
+
+import lotto.Lotto
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class PurchaseAmountTest {
+    @Test
+    fun `throws an exception when purchase amount is empty string`() {
+        assertThrows<IllegalArgumentException> {
+            PurchaseAmount.from("")
+        }
+    }
+
+    @Test
+    fun `throws an exception when purchase amount is blank`() {
+        assertThrows<IllegalArgumentException> {
+            PurchaseAmount.from("      ")
+        }
+    }
+}
