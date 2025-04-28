bf4ea408b526c62b6847f1153ccf35ccb617809f
Petra Bencze <petra.bncz@gmail.com>
2025-04-28 19:06:47 +0200

    feat(PurchaseTest): add more tests after refactoring, previously inside InputView

--- src/test/kotlin/lotto/PurchaseTest.kt
+++ src/test/kotlin/lotto/PurchaseTest.kt
@@ -1,7 +1,6 @@
 package lotto
 
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -9,24 +8,34 @@ import org.junit.jupiter.params.provider.ValueSource
 class PurchaseTest {
     @ParameterizedTest
     @ValueSource(strings = ["1200", "999", "1001"])
-    fun `throws exception when budget is not divisible by 1000`() {
-        assertThrows<IllegalArgumentException> {
-            Purchase(1200)
-        }
+    fun `throws exception when budget is not divisible by 1000`(input: String) {
+        assertThatIllegalArgumentException()
+            .isThrownBy{ Purchase(input.toInt()) }
+            .withMessageStartingWith("[ERROR]")
     }
 
-    @Test
-    fun `throws exception when budget exceeds max amount`() {
-        assertThrows<IllegalArgumentException> {
-            Purchase(16000)
-        }
+    @ParameterizedTest
+    @ValueSource(strings = ["16000", "30000"])
+    fun `throws exception when budget exceeds max amount`(input: String) {
+        assertThatIllegalArgumentException()
+            .isThrownBy{ Purchase(input.toInt()) }
+            .withMessageStartingWith("[ERROR]")
     }
 
-    @Test
-    fun `throws exception when budget does not reach min amount`() {
-        assertThrows<IllegalArgumentException> {
-            Purchase(0)
-        }
+    @ParameterizedTest
+    @ValueSource(strings = ["0", "-1000"])
+    fun `throws exception when budget does not reach min amount`(input: String) {
+        assertThatIllegalArgumentException()
+            .isThrownBy{ Purchase(input.toInt()) }
+            .withMessageStartingWith("[ERROR]")
+    }
+
+    @ParameterizedTest(name = "{index}. valid purchase amount of \"{0}\"")
+    @ValueSource(strings = ["1000", "12000", "15000"])
+    fun `test valid purchase amount`(input: String) {
+        assertThatNoException().isThrownBy{ Purchase(input.toInt()) }
+        val value = Purchase(input.toInt())
+        assertThat(value).isEqualTo(input.toInt())
     }
 
     @Test
