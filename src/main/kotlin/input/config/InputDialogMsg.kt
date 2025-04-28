49f3a8910e9fcca5c0d859091e1b78fcf249061a
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-28 16:54:04 +0900

    feat(input): add ticket result message function

--- src/main/kotlin/input/config/InputDialogMsg.kt
+++ src/main/kotlin/input/config/InputDialogMsg.kt
@@ -1,8 +1,15 @@
 package input.config
 
-const val AMOUNT_MSG = "Please enter the purchase amount."
+const val TICKET_AMOUNT_MSG = "Please enter the purchase amount."
 
 const val LAST_WEEK_WINNING_NUMBER_MSG = "Please enter last week's winning numbers."
 
 const val BONUS_NUMBER_MSG = "Please enter the bonus number."
 
+const val TICKET_RESULT_PREFIX = "You have purchased "
+
+const val TICKET_RESULT_POSTFIX = " tickets."
+
+fun getTicketResultMsg(amount: Int): String{
+    return TICKET_RESULT_PREFIX + amount + TICKET_RESULT_POSTFIX
+}
