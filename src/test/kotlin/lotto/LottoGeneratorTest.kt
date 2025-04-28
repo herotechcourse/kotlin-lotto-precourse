cdc7bd48fa217cc618c5ef18c9fe8f93c746e3e0
Mariia Kulikova <111587633+mariyakulikova@users.noreply.github.com>
2025-04-28 15:09:18 +0200

    test(lotto): add constant for purchase amount value, replace assertTrue with assertEquals where values are equaled

--- src/test/kotlin/lotto/LottoGeneratorTest.kt
+++ src/test/kotlin/lotto/LottoGeneratorTest.kt
@@ -1,23 +1,25 @@
 package lotto
 
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class LottoGeneratorTest {
     @Test
     fun `issue 10 lotto tickets`() {
-        val lottoTickets = LottoGenerator.issue(10_000)
-        assertTrue(lottoTickets.size == 10_000 / TICKET_PRICE)
+        val lottoTickets = LottoGenerator.issue(PURCHASE_AMOUNT)
+        assertEquals(lottoTickets.size, PURCHASE_AMOUNT / TICKET_PRICE)
     }
 
     @Test
     fun `each lotto ticket contains 6 unique numbers`() {
-        val lottoTickets = LottoGenerator.issue(10_000)
+        val lottoTickets = LottoGenerator.issue(PURCHASE_AMOUNT)
         assertTrue(lottoTickets.all { it.getNumbers().toSet().size == TICKET_SIZE })
     }
 
     companion object {
         private const val TICKET_PRICE = 1000
+        private const val PURCHASE_AMOUNT = 10_000
         private const val TICKET_SIZE = 6
     }
 }
