cd9a95132bf100beed314e968d7df91bf99afc52
wonie <wonies@github.com>
2025-04-29 00:43:05 +0900

    feat: print outview including statistics

---
+++ src/main/kotlin/lotto/view/OutView.kt
@@ -0,0 +1,38 @@
+package lotto.view
+
+import lotto.domain.LottoTicket
+import lotto.domain.MatchLotto
+
+object OutView {
+    fun printTicket(tickets: List<LottoTicket>) {
+        println("You have purchased ${tickets.size} tickets.")
+        tickets.forEach { println(it.getNumbers()) }
+    }
+
+    fun printStatistics(resultMap: Map<MatchLotto, Int>, purchaseAmount: Int) {
+        println()
+        println("Winning Statistics")
+        println("---")
+
+        val order = listOf(
+            MatchLotto.FIFTH,
+            MatchLotto.FOURTH,
+            MatchLotto.THIRD,
+            MatchLotto.SECOND,
+            MatchLotto.FIRST
+        )
+
+        var totalReward = 0L
+
+        for (rank in order) {
+            val count = resultMap[rank] ?: 0
+            val reward = rank.reward
+            totalReward += reward * count
+            println("${rank.description} (${String.format("%,d", reward)} KRW) – $count tickets")
+        }
+
+        val returnRate = totalReward * 100.0 / purchaseAmount
+        val returnRateFormatted = String.format("%,.1f", returnRate)
+        println("Total return rate is $returnRateFormatted%")
+    }
+}
