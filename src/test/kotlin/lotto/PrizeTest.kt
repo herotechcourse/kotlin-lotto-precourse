06adb6e792a8c65f220a5f4676812882c724c165
hiyoons <arin9825@naver.com>
2025-04-29 05:32:18 +0900

    test(Prize): add test for prize amounts

--- src/test/kotlin/lotto/PrizeTest.kt
+++ src/test/kotlin/lotto/PrizeTest.kt
@@ -1,5 +1,16 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class PrizeTest
+class PrizeTest{
+
+
+    @Test
+    fun `match winner type and prize amounts`(){
+        assertThat(Prize.getMoney("6")).isEqualTo(2000000000)
+    }
+
+
+}
