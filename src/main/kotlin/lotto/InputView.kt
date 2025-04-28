a22eb1a1ccbbad1f72d7f1d53b4e107db396c4c0
박채린 <bewithbomi@gmail.com>
2025-04-29 06:53:38 +0900

    feat(view): build InputView to handle user input

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -5,32 +5,29 @@ import camp.nextstep.edu.missionutils.Console
 object InputView {
     fun readPurchaseAmount(): Int {
         println("Please enter the purchase amount.")
-        return readInt()
+        val input = Console.readLine()
+        return input.toIntOrNull()
+            ?.takeIf { it % 1000 == 0 }
+            ?: throw IllegalArgumentException("[ERROR] Amount must be a multiple of 1000.")
     }
 
     fun readWinningNumbers(): List<Int> {
         println("Please enter last week's winning numbers.")
-        return readNumbers()
+        val input = Console.readLine()
+        return input.split(",")
+            .map { it.trim().toInt() }
+            .also {
+                require(it.size == 6) { "[ERROR] You must enter exactly 6 numbers." }
+                require(it.distinct().size == 6) { "[ERROR] Numbers must be unique." }
+                require(it.all { num -> num in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
+            }
     }
 
     fun readBonusNumber(): Int {
         println("Please enter the bonus number.")
-        return readInt()
-    }
-
-    private fun readInt(): Int {
         val input = Console.readLine()
-        return input.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] Input must be a number.")
-    }
-
-    private fun readNumbers(): List<Int> {
-        val input = Console.readLine()
-        return input.split(",")
-            .map { it.trim().toIntOrNull() ?: throw IllegalArgumentException("[ERROR] Input must be a number.") }
-            .also {
-                if (it.size != 6) {
-                    throw IllegalArgumentException("[ERROR] Must enter exactly 6 numbers.")
-                }
-            }
+        val bonus = input.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] Invalid number.")
+        require(bonus in 1..45) { "[ERROR] Lotto numbers must be between 1 and 45." }
+        return bonus
     }
 }
