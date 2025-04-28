4056ab9fe228fdbac4f88c7476fccbca59887d11
Alex <alexis.mar.schmidt@gmail.com>
2025-04-28 07:54:16 +0200

    refactor(lottoConstants): add constants for prizes

--- src/main/kotlin/lotto/LottoConstants.kt
+++ src/main/kotlin/lotto/LottoConstants.kt
@@ -5,4 +5,10 @@ object LottoConstants {
     const val MIN_NUMBER = 1
     const val MAX_NUMBER = 45
     const val NUMBERS_PER_TICKET = 6
+    const val FIRST_PRIZE = 2_000_000_000
+    const val SECOND_PRIZE = 30_000_000
+    const val THIRD_PRIZE = 1_500_000
+    const val FOURTH_PRIZE = 50_000
+    const val FIFTH_PRIZE = 5_000
+    const val NONE_PRIZE = 0
 }
