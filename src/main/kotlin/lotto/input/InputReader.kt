f13493f0a0c558f5001dcb36a0527762e1422a68
김동영 <dongyi@gimdong-yeong-ui-MacBookAir.local>
2025-04-29 00:37:02 +0900

    feat(input): repeat input until valid for purchase amount, winning numbers, and bonus number

--- src/main/kotlin/lotto/input/InputReader.kt
+++ src/main/kotlin/lotto/input/InputReader.kt
@@ -12,35 +12,55 @@ const val end = 45
 object InputReader {
 
     fun purchaseAmount(): Int {
-        InputView.purchaseAmount()
-        val rawInput = Console.readLine()
-        Input.isEmpty(rawInput)
-        Input.isInteger(rawInput)
-        Input.isPositiveInteger(rawInput.toInt())
-        Input.isDivisible(rawInput.toInt(), ticketCost)
-        return rawInput.toInt()
+        while (true) {
+            InputView.purchaseAmount()
+            val rawInput = Console.readLine()
+            try {
+                Input.isEmpty(rawInput)
+                Input.isInteger(rawInput)
+                Input.isPositiveInteger(rawInput.toInt())
+                Input.isDivisible(rawInput.toInt(), ticketCost)
+                return rawInput.toInt()
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
     }
 
     fun winningNumbers(): List<Int> {
-        InputView.winningNumbers()
-        val rawInput = Console.readLine()
-        Input.isEmpty(rawInput)
-        val rawNumbers = Split.byComma(rawInput).map { it.trim() }
-        Input.isEachInteger(rawNumbers)
-        val numbers = rawNumbers.map { it.toInt() }
-        Input.isEachInRange(numbers, start, end)
-        Input.winningNumbersDuplication(numbers)
-        return numbers
+        while (true) {
+            InputView.winningNumbers()
+            val rawInput = Console.readLine()
+            try {
+                Input.isEmpty(rawInput)
+                val rawNumbers = Split.byComma(rawInput).map { it.trim() }
+                Input.isEachInteger(rawNumbers)
+                val numbers = rawNumbers.map { it.toInt() }
+                Input.isEachInRange(numbers, start, end)
+                Input.winningNumbersDuplication(numbers)
+                return numbers
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
     }
 
     fun bonusNumber(numbers: List<Int>): Int {
-        InputView.bonusNumber()
-        val rawInput = Console.readLine()
-        Input.isEmpty(rawInput)
-        Input.isInteger(rawInput)
-        Input.isInRange(rawInput.toInt(), start, end)
-        Input.bonusNumberDuplication(rawInput.toInt(), numbers)
-        return rawInput.toInt()
+        while (true) {
+            InputView.bonusNumber()
+            val rawInput = Console.readLine()
+            try {
+                Input.isEmpty(rawInput)
+                Input.isInteger(rawInput)
+                Input.isInRange(rawInput.toInt(), start, end)
+                Input.bonusNumberDuplication(rawInput.toInt(), numbers)
+                return rawInput.toInt()
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+
+
     }
 
 }
