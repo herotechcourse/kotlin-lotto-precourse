68f241f685bdea3314df9ba1f7b1a964b355d19c
hiyoons <arin9825@naver.com>
2025-04-28 21:23:16 +0900

    refactor(InputView): update readPurchase
    update readPurchase logic

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -5,21 +5,25 @@ import camp.nextstep.edu.missionutils.Console
 class InputView {
     val outputView = OutputView()
     var userPurchase = ""
+    var errorType = ""
+    var generateTickets = ArrayList<List<Int>>()
     fun run(): ArrayList<List<Int>> {
         return readPurchase()
     }
 
     fun readPurchase(): ArrayList<List<Int>> {
-        println(QUEST_PURCHASE)
-        userPurchase = Console.readLine()
-        while (checkPurchase(userPurchase)) {
-            try {
-                break
-            } catch (e: IllegalArgumentException) {
-                readPurchase()
+        while(true){
+            println(QUEST_PURCHASE)
+            userPurchase = Console.readLine()
+            try{
+                if (checkPurchase(userPurchase).isNotBlank()) { //if userPurchase is invalid data -> readPurchase ->
+                    throw IllegalArgumentException(errorMessage(errorType).toString())
+                }
+                return outputView.generateTickets(userPurchase)
+            } catch (e:IllegalArgumentException){
+                continue
             }
         }
-        return outputView.generateTickets(userPurchase)
     }
 
     fun getPurchase(): String {
@@ -29,11 +33,12 @@ class InputView {
     fun bonusNumber(): Int {
         println(QUEST_BONUS_NUM)
         val bonusNum = Console.readLine()
-        while (checkInRange(bonusNum) && checkBonusNumeric(bonusNum)) {
+        while (!checkInRange(bonusNum)) {
+            bonusNumber()
             try {
                 break
             } catch (e: IllegalArgumentException) {
-                bonusNumber()
+                continue
             }
         }
         return bonusNum.toInt()
@@ -42,11 +47,12 @@ class InputView {
     fun readWinningNumbers(): List<Int> {
         println(QUEST_WINNING_NUM)
         val winNumber = Console.readLine()
-        while (checkWinType(winNumber) && checkWinNumeric(winNumber) && checkListInRange(winNumber)) {
+        while (!checkWinType(winNumber) && !checkWinNumeric(winNumber) && !checkListInRange(winNumber)) {
+            readWinningNumbers()
             try {
                 break
             } catch (e: IllegalArgumentException) {
-                readWinningNumbers()
+                continue
             }
         }
         return generateWinNum(winNumber)
@@ -62,27 +68,33 @@ class InputView {
         return try {
             purchase.toInt()
             true
-        } catch (e: NumberFormatException) {
+        } catch (e: NotImplementedError) {
             false
         }
     }
 
-    fun checkPurchase(purchase: String): Boolean {
-        if (!isNumeric(purchase)) {
-            throw IllegalArgumentException(errorMessage(ERROR_MESSAGE))
+    fun checkPurchase(purchase: String): String {
+        if (purchase.toDoubleOrNull() == null) {
+            errorType = ERROR_MESSAGE
+            return ERROR_MESSAGE
         }
         if (purchase.toInt() % 1000 != 0) {
-            throw IllegalArgumentException(errorMessage(ERROR_MESSAGE_DIV))
+            errorType = ERROR_MESSAGE_DIV
+            return ERROR_MESSAGE_DIV
         }
-        return true
+        return ""
     }
 
     fun checkWinType(input: String): Boolean {
         if (!input.contains(",")) {
-            throw IllegalArgumentException(errorMessage(ERROR_MESSAGE_COMMA))
+//            throw IllegalArgumentException(errorMessage(ERROR_MESSAGE_COMMA))
+            errorMessage(ERROR_MESSAGE_COMMA)
+            return false
         }
         if (hasDuplicate(listOf(*input.split(',').toTypedArray()))) {
-            throw IllegalArgumentException(errorMessage(ERROR_MESSAGE_DUPL))
+            errorMessage(ERROR_MESSAGE_DUPL)
+            return false
+//            throw IllegalArgumentException(errorMessage(ERROR_MESSAGE_DUPL))
         }
         return true
     }
@@ -96,16 +108,23 @@ class InputView {
         val data = listOf(*input.split(',').toTypedArray())
         data.forEach {
             if (!isNumeric(it)) {
-                throw IllegalArgumentException(errorMessage(ERROR_MESSAGE_WINNUM))
+                errorMessage(ERROR_MESSAGE_WINNUM)
+                return false
+//                throw IllegalArgumentException(errorMessage(ERROR_MESSAGE_WINNUM))
             }
         }
         return true
     }
 
     fun checkInRange(num: String): Boolean {
-        val convert = num.toInt()
-        if (convert < 1 || convert > 45) {
-            throw IllegalArgumentException(errorMessage(ERROR_MESSAGE_RANGE))
+        if (num.toDoubleOrNull() == null) {
+            errorMessage(ERROR_MESSAGE)
+            return false
+        }
+        if (num.toInt() < 1 || num.toInt() > 45) {
+            errorMessage(ERROR_MESSAGE_RANGE)
+            return false
+//            throw IllegalArgumentException(errorMessage(ERROR_MESSAGE_RANGE))
         }
         return true
     }
@@ -114,22 +133,26 @@ class InputView {
         val num = listOf(*num.split(',').map { it.toInt() }.toTypedArray())
         num.forEach {
             if (it < 1 || it > 45) {
-                throw IllegalArgumentException(errorMessage(ERROR_MESSAGE_RANGE))
+//                throw IllegalArgumentException(errorMessage(ERROR_MESSAGE_RANGE))
+                errorMessage(ERROR_MESSAGE_RANGE)
+                return false
             }
         }
         return true
     }
 
     fun checkBonusNumeric(num: String): Boolean {
-        if (!isNumeric(num)) {
-            throw IllegalArgumentException(errorMessage(ERROR_MESSAGE))
+        if (num.toDoubleOrNull() == null) {
+            errorMessage(ERROR_MESSAGE)
+            return false
+//            throw IllegalArgumentException(errorMessage(ERROR_MESSAGE))
         }
         return true
     }
 
-    fun errorMessage(error: String): String {
-        val message = "[ERROR] $error"
-        return message
+    fun errorMessage(error: String) {
+        val message = "[ERROR] " + error
+        println(message)
     }
 
     companion object {
