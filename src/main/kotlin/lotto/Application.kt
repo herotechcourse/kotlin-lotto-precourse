9fb9e9b3f62a9dde70295d6c0afc6a0d3ba04fe0
coreyol <coreycharm@gmail.com>
2025-04-29 06:46:45 +0900

    feat: main

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,16 @@
 package lotto
 
 fun main() {
-    // TODO: Implement the program
+    val amount = InputView.getUserPurchaseAmount()
+//    val amount = InputView.parsePurchaseAmount("1000")
+
+    val numberOfTickets = amount / const.TICKET_PRICE
+    val tickets = LottoTickets.generate(numberOfTickets)
+    OutputView.printTickets(tickets)
+
+    val winningNumbers = InputView.getUserWinningNumbers()
+    val bonusNumbers = InputView.getUserBonusNumbers(winningNumbers)
+//    val winningNumbers = InputView.parseWinningNumbers("1,20,13,4,5,7")
+//    val bonusNumbers = InputView.parseBonusNumbers("8", winningNumbers)
+    OutputView.printStatistics(tickets, winningNumbers, bonusNumbers)
 }
