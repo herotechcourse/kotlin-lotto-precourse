a221dae2dff103e0e7e9b2cc4641d274f3bd4525
samkons <konzisamuel@gmail.com>
2025-04-28 22:31:16 +0200

    feat(implemet the reading and parsing of bonus and winning numbers): $

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -10,3 +10,9 @@ class Lotto(private val numbers: List<Int>) {
     fun getNumbers(): List<Int> = numbers
 }
 
+object LottoConstants {
+    const val TICKET_PRICE = 1000
+    const val LOTTO_NUMBER_MIN = 1
+    const val LOTTO_NUMBER_MAX = 45
+    const val LOTTO_NUMBER_COUNT = 6
+}
