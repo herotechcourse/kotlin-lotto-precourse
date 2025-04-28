b481928d9e7edfbc8a38bf939353f74f8ef7ffc3
YongLeeCode <yonghwan311@gmail.com>
2025-04-29 02:12:06 +0900

    refactor(winning lotto): apply constants for magic numbers

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -1,21 +1,23 @@
 package lotto.domain
 
+import lotto.common.Constants
+
 class WinningLotto(
     private val winningNumbers: List<Int>,
     private val bonusNumber: Int
 ) {
     init {
-        require(bonusNumber in 1..45) { "[ERROR] Bonus number must be between 1 and 45." }
+        require(bonusNumber in Constants.MIN_LOTTO_NUMBER..Constants.MAX_LOTTO_NUMBER) { "[ERROR] Bonus number must be between 1 and 45." }
         require(!winningNumbers.contains(bonusNumber)) { "[ERROR] Winning numbers must not contain bonus number." }
-
-        require(winningNumbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
-        require(winningNumbers.distinct().size == 6) { "[ERROR] Lotto numbers must be unique." }
-        require(winningNumbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
+        require(winningNumbers.size == Constants.LOTTO_SIZE) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(winningNumbers.distinct().size == Constants.LOTTO_SIZE) { "[ERROR] Lotto numbers must be unique." }
+        require(winningNumbers.all { it in Constants.MIN_LOTTO_NUMBER..Constants.MAX_LOTTO_NUMBER }) { "[ERROR] Lotto numbers must be between 1 and 45." }
     }
 
     fun getWinningNumbers(): List<Int> {
         return winningNumbers
     }
+
     fun getBonusNumber(): Int {
         return bonusNumber
     }
