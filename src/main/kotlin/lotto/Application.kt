fa27d78b28cf78853388078f83c14f731d2aa465
김소운 <sozzykim@naver.com>
2025-04-29 01:01:06 +0900

    feat(app): implement main application to run lotto game

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,30 @@
 package lotto
 
+import lotto.TicketChecker.addStatistics
+
 fun main() {
-    // TODO: Implement the program
+    val purchaseAmount = InputView.purchaseAmount()
+    val winningLotto = Lotto(InputView.winningNum())
+    val bonusNumber = InputView.bonusNum()
+    val ticketCount = purchaseAmount/1000
+
+    val myLotto = mutableListOf<Lotto>()
+    for ( i in 0 until ticketCount) {
+        myLotto.add(LottoGenerator.generateLotto())
+    }
+
+    myLotto.forEach{ lotto ->
+        addStatistics( lotto, winningLotto, bonusNumber)
+    }
+
+    OutputView.myLotto(myLotto)
+    OutputView.resultStatistics()
+    OutputView.profitRate(ProfitRateCalaulator.calculateProfitRate(purchaseAmount))
+
+
+
+
+
+
+
 }
