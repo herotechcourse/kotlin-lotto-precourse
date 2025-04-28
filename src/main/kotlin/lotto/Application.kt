90851eef14fbb0d15af9ea57aaccc506d124dc6a
rootPark <rmsdnjs518@gmail.com>
2025-04-28 20:05:55 +0900

    fix : import view, controller to Application

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,9 @@
 package lotto
 
 import lotto.controller.LottoController
+import lotto.view.InputView
+import lotto.view.OutputView
+import lotto.controller.CreateController
 
 fun main() {
     LottoController(InputView(), OutputView(), CreateController()).run()    
