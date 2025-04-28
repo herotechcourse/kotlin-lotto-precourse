d683d7e706d1ebef2c2dafe0c024ec0703cacefe
wonie <wonies@github.com>
2025-04-29 00:50:57 +0900

    feat: organize order in controller

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -3,16 +3,28 @@ package lotto.controller
 import lotto.domain.LottoMachine
 import lotto.domain.LottoTicket
 import lotto.view.InputView
+import lotto.view.OutView
+import lotto.domain.MatchLotto
 
 class LottoController {
     init {
-        var values = InputView.inputPurchase().toLong()
+        var values = InputView.inputPurchase().toInt()
         val ticketCount = (values / 1000).toInt()
-
         val lottoMachine = LottoMachine()
         val tickets = lottoMachine.issueLotto(ticketCount)
-        tickets.forEach {
-            ticket -> println(ticket.getNumbers())
+        OutView.printTicket(tickets)
+
+        val winNumber = InputView.inputWinNumbers()
+        val winBonusNumber = InputView.inputBonusNumbers(winNumber)
+
+        val matchResults = tickets.map { ticket ->
+            val matchCount = ticket.countMatching(winNumber)
+            val bonusMatch = ticket.containsBonus(winBonusNumber)
+            MatchLotto.from(matchCount, bonusMatch)
         }
+
+        val resultMap = matchResults.groupingBy { it }.eachCount()
+        OutView.printStatistics(resultMap, values)
+
     }
 }
