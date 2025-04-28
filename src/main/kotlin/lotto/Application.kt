62689a3e7ad8b61ef4456436164b9117e3b7388e
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2025-04-28 11:03:58 +0200

    feat(application): add return rate flow
    - Added StatisticsService.calculateReturnRate() to get the return rate.
    - Added Output.view.printReturnRate to print return rate message in a clean format.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -7,6 +7,7 @@ import lotto.exception.LottoInputException
 import lotto.domain.WinningNumbers
 import lotto.domain.LottoResult
 import lotto.service.ResultService
+import lotto.service.StatisticsService
 
 fun <T> retryInputUntilSuccess(block: () -> T): T {
     while (true) {
@@ -14,6 +15,7 @@ fun <T> retryInputUntilSuccess(block: () -> T): T {
             return block()
         } catch (e: LottoInputException) {
             println(e.message);
+            println()
         }
     }
 }
@@ -21,19 +23,27 @@ fun <T> retryInputUntilSuccess(block: () -> T): T {
 fun main() {
     val amount: Int =
         retryInputUntilSuccess { InputView.getPurchaseAmount().also { LottoService.validatePurchaseAmount(it) } }
-    val tickets: List<Lotto> = LottoService.generateTickets(amount)
+    val ticketsQuantity = amount / 1000
+    val tickets: List<Lotto> = LottoService.generateTickets(ticketsQuantity)
     println()
+
     OutputView.printTickets(tickets)
     println()
+
     val winningNumbersInput: List<Int> =
         retryInputUntilSuccess { InputView.getWinningNumbers().also { LottoService.validateWinningNumbers(it) } }
     println()
+
     val bonusNumber: Int =
         retryInputUntilSuccess {
             InputView.getBonusNumber().also { LottoService.validateBonusNumber(it, winningNumbersInput) }
         }
+    println()
+
     val winningNumbers = WinningNumbers(winningNumbersInput, bonusNumber)
     val lottoResults: List<LottoResult> = tickets.map { ResultService.evaluateTicket(it, winningNumbers)}
+    val returnRate = StatisticsService.calculateReturnRate(lottoResults, amount)
 
     OutputView.printWinningStatistics(lottoResults)
+    OutputView.printReturnRate(returnRate)
 }
