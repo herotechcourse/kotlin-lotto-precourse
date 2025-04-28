56ad4d6b5612305783d65c947fa6ec35a6d2bd50
Petra Bencze <petra.bncz@gmail.com>
2025-04-28 14:59:46 +0200

    chore(Application): extract game logic to game manager LottoGame

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,22 +2,8 @@ package lotto
 
 fun main() {
     try {
-        val inputView = InputView()
-        val outputView = OutputView()
-
-        val amount = inputView.readPurchaseAmount()
-        val purchase = Purchase(amount)
-        outputView.displayTickets(purchase.tickets)
-
-        val winningNumbers = inputView.readWinningNumbers()
-        val lotto = Lotto(winningNumbers)
-        val bonusNumber = inputView.readBonusNumber()
-        lotto.addBonusNumber(bonusNumber)
-        val result = lotto.compareNumbers(purchase.tickets)
-        outputView.printStatistics(result)
-        val totalPrize = lotto.calculateTotalPrize(result)
-        val returnRate = purchase.calculateReturnRate(totalPrize)
-        outputView.printReturnRate(returnRate)
+        val lottoGame = LottoGame()
+        lottoGame.play()
     } catch (e: InputView.MaxRetryException) {
         println(e.message + " Exiting...")
         return
