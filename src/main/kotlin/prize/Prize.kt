aef9fe53669e0852b14831d2427d496d652e64dc
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-28 22:15:50 +0900

    feat(prize): add award method to prize in order to sum up prizes

--- src/main/kotlin/prize/Prize.kt
+++ src/main/kotlin/prize/Prize.kt
@@ -1,25 +1,22 @@
 package prize
 
 import lotto.config.CURRENCY
-import lotto.config.bonusMap
-import lotto.config.prizeMap
 
 data class Prize(
     private val matches: Int,
-    private val bonus: Boolean
+    private val bonus: Boolean,
+    private val reward: Int
                  )
  {
-     private val reward: Int = if (bonus) {
-         bonusMap[matches] ?: 0
-     } else {
-         prizeMap[matches] ?: 0
-     }
-
      private var winCount: Int = 0
 
      fun increaseWinCount() {
         winCount++
-    }
+     }
+
+     fun award(): Int {
+         return winCount * reward
+     }
 
     override fun toString(): String {
         return "$matches Matches " +
