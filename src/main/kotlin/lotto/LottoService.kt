f63691b734da30eac27a4839b68c68ce665ec63d
Jisun Park <jisunpark.t@gmail.com>
2025-04-29 00:16:00 +0900

    feat(lottoservice): implement lotto generation based on purchase amount

---
+++ src/main/kotlin/lotto/LottoService.kt
@@ -0,0 +1,15 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class LottoService {
+    fun generateLottos(purchaseAmount: Int): List<Lotto> {
+        val numberOfTickets = purchaseAmount / 1000
+        return List(numberOfTickets) { createLotto() }
+    }
+
+    private fun createLotto(): Lotto {
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+        return Lotto(numbers)
+    }
+}
