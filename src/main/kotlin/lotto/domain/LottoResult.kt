4f4d333917e53ffd5f600cbe550c22a14b7e145e
puleugo <puleugo@gmail.com>
2025-05-04 13:15:39 +0900

    style: remove un-using codes

--- src/main/kotlin/lotto/domain/LottoResult.kt
+++ src/main/kotlin/lotto/domain/LottoResult.kt
@@ -1,3 +1,3 @@
 package lotto.domain
 
-class LottoResult(val lottoRanks: Map<Rank, Int>, val profitRate: Double) {}
+class LottoResult(val lottoRanks: Map<Rank, Int>, val profitRate: Double)
--- src/main/kotlin/lotto/view/OutputPrinter.kt
+++ src/main/kotlin/lotto/view/OutputPrinter.kt
@@ -1,9 +1,7 @@
 package lotto.view
 
 import lotto.Lotto
-import lotto.domain.LottoCalculator
 import lotto.domain.LottoResult
-import lotto.domain.Rank
 
 interface OutputPrinter {
     fun printPurchasedTickets(tickets: List<Lotto>): Unit
